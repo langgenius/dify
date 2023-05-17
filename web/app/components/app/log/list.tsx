fa183b525da7b36489023d4ec495b2ee961e5f28
llx_changed <xi.liu@goodwe.com>
2023-05-17 23:21:37 +0800

    feat(dialog: style)add scrolling to a container

--- web/app/components/app/log/list.tsx
+++ web/app/components/app/log/list.tsx
@@ -166,7 +166,7 @@ function DetailPanel<T extends ChatConversationFullDetailResponse | CompletionCo
     return res
   })?.name ?? 'custom'
 
-  return (<div className='rounded-xl border-[0.5px] border-gray-200 h-full flex flex-col'>
+  return (<div className='rounded-xl border-[0.5px] border-gray-200 h-full flex flex-col overflow-auto'>
     {/* Panel Header */}
     <div className='border-b border-gray-100 py-4 px-6 flex items-center justify-between'>
       <div className='flex-1'>
