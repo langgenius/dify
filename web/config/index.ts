3565023109a498588ac31d2e31103be181da00bf
yyzhang <yuanyuan.zhang@haochezhu.club>
2023-05-18 18:23:44 +0800

    fix the problem of bad character range \w-. at position 2

--- web/config/index.ts
+++ web/config/index.ts
@@ -80,7 +80,7 @@ export const DEFAULT_VALUE_MAX_LEN = 48
 
 export const zhRegex = /^[\u4e00-\u9fa5]$/gm
 export const emojiRegex = /^[\uD800-\uDBFF][\uDC00-\uDFFF]$/gm
-export const emailRegex = /^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/gm
+export const emailRegex = /^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/gm
 const MAX_ZN_VAR_NAME_LENGHT = 8
 const MAX_EN_VAR_VALUE_LENGHT = 16
 export const getMaxVarNameLength = (value: string) => {
