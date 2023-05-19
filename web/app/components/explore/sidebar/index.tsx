072a90222fefa1f90f46eca6de30d28be732a195
金伟强 <iamjoel007@gmail.com>
2023-05-19 18:12:09 +0800

    chore: sidebar overflowy

--- web/app/components/explore/sidebar/index.tsx
+++ web/app/components/explore/sidebar/index.tsx
@@ -79,7 +79,11 @@ const SideBar: FC<{
       {installedApps.length > 0 && (
         <div className='mt-10'>
           <div className='pl-2 text-xs text-gray-500 font-medium uppercase'>{t('explore.sidebar.workspace')}</div>
-          <div className='mt-3 space-y-1'>
+          <div className='mt-3 space-y-1 overflow-y-auto'
+            style={{
+              maxHeight: 'calc(100vh - 250px)'
+            }}
+          >
             {installedApps.map(({id, is_pinned, uninstallable, app : { name }}) => {
               return (
                 <Item 
