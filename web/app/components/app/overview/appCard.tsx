4ac637477baa3e9e44ebe281b262ab9dd67f34bf
crazywoola <li.zheng@dentsplysirona.com>
2023-05-19 15:00:24 +0800

    cleanup code

--- web/app/components/app/overview/appCard.tsx
+++ web/app/components/app/overview/appCard.tsx
@@ -29,9 +29,6 @@ export type IAppCardProps = {
   onGenerateCode?: () => Promise<any>
 }
 
-// todo: get image url from appInfo
-const defaultUrl = 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-
 function AppCard({
   appInfo,
   cardType = 'app',
