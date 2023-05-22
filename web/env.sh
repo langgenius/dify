5d3e0f2a98852544561e1bb4ac2a6f090fc0966a
yuhao1118 <liyuhao20001118@gmail.com>
2023-05-22 20:34:50 +0800

    feat: remove env.sh

--- web/env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-[ ! -f .env ] || export $(grep -v '^#' .env | xargs)
-
-export NEXT_PUBLIC_DEPLOY_ENV=${DEPLOY_ENV}
-export NEXT_PUBLIC_EDITION=${EDITION}
-export NEXT_PUBLIC_API_PREFIX=${CONSOLE_URL}/console/api
-export NEXT_PUBLIC_PUBLIC_API_PREFIX=${APP_URL}/api
