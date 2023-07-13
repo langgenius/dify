e80ee32f0102966238ece96eccc05a5eb0197169
John Wang <takatost@gmail.com>
2023-07-13 12:04:38 +0800

    feat: modify default url env

--- web/docker/entrypoint.sh
+++ web/docker/entrypoint.sh
@@ -5,16 +5,16 @@ set -e
 export NEXT_PUBLIC_DEPLOY_ENV=${DEPLOY_ENV}
 export NEXT_PUBLIC_EDITION=${EDITION}
 
-if [[ -z "$CONSOLE_API_URL" ]]; then
-  export NEXT_PUBLIC_API_PREFIX=${CONSOLE_URL}/console/api
-else
+if [[ -z "$CONSOLE_URL" ]]; then
   export NEXT_PUBLIC_API_PREFIX=${CONSOLE_API_URL}/console/api
+else
+  export NEXT_PUBLIC_API_PREFIX=${CONSOLE_URL}/console/api
 fi
 
-if [[ -z "$APP_API_URL" ]]; then
-  export NEXT_PUBLIC_PUBLIC_API_PREFIX=${APP_URL}/api
-else
+if [[ -z "$APP_URL" ]]; then
   export NEXT_PUBLIC_PUBLIC_API_PREFIX=${APP_API_URL}/api
+else
+  export NEXT_PUBLIC_PUBLIC_API_PREFIX=${APP_URL}/api
 fi
 
 export NEXT_PUBLIC_SENTRY_DSN=${SENTRY_DSN}
