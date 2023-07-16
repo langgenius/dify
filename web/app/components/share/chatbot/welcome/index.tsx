dd93f9c6073852d36d5fdfebd323660760e83b29
Joel <iamjoel007@gmail.com>
2023-07-16 16:21:08 +0800

    fix: change chatbot avart to dify icon

--- web/app/components/share/chatbot/welcome/index.tsx
+++ web/app/components/share/chatbot/welcome/index.tsx
@@ -307,7 +307,7 @@ const Welcome: FC<IWelcomeProps> = ({
   }
 
   return (
-    <div className='relative mobile:min-h-[48px] tablet:min-h-[64px]'>
+    <div className='relative tablet:min-h-[64px]'>
       {/* {hasSetInputs && renderHeader()} */}
       <div className='mx-auto pc:w-[794px] max-w-full mobile:w-full px-3.5'>
         {/*  Has't set inputs  */}
