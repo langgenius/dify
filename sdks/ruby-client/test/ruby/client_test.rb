257d9a80cda533af69a2e9a485d34a0c914ead6e
crazywoola <427733928@qq.com>
2023-07-18 09:10:48 +0800

    feat: add ruby client

---
+++ sdks/ruby-client/test/ruby/client_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Dify::ClientTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Dify::Client::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
