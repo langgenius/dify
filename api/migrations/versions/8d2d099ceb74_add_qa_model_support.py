65f032de9a465b80bcab367297b0a76283566699
jyong <718720800@qq.com>
2023-07-21 13:16:14 +0800

    migration merge

--- api/migrations/versions/8d2d099ceb74_add_qa_model_support.py
+++ api/migrations/versions/8d2d099ceb74_add_qa_model_support.py
@@ -1,7 +1,7 @@
 """add_qa_model_support
 
 Revision ID: 8d2d099ceb74
-Revises: a5b56fb053ef
+Revises: 2beac44e5f5f
 Create Date: 2023-07-18 15:25:15.293438
 
 """
@@ -11,7 +11,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = '8d2d099ceb74'
-down_revision = 'a5b56fb053ef'
+down_revision = '2beac44e5f5f'
 branch_labels = None
 depends_on = None
 
