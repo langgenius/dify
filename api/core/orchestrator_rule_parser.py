b221f72c830ac903817082b7893d7a94335ef88c
John Wang <takatost@gmail.com>
2023-07-26 21:50:45 +0800

    fix: use 16k model to summary context

--- api/core/orchestrator_rule_parser.py
+++ api/core/orchestrator_rule_parser.py
@@ -31,6 +31,7 @@ class OrchestratorRuleParser:
     def __init__(self, tenant_id: str, app_model_config: AppModelConfig):
         self.tenant_id = tenant_id
         self.app_model_config = app_model_config
+        self.agent_summary_model_name = "gpt-3.5-turbo-16k"
 
     def to_agent_executor(self, conversation_message_task: ConversationMessageTask, memory: Optional[BaseChatMemory],
                        rest_tokens: int, chain_callback: MainChainGatherCallbackHandler) \
@@ -71,7 +72,7 @@ class OrchestratorRuleParser:
 
             summary_llm = LLMBuilder.to_llm(
                 tenant_id=self.tenant_id,
-                model_name=agent_model_name,
+                model_name=self.agent_summary_model_name,
                 temperature=0,
                 max_tokens=500,
                 callbacks=[DifyStdOutCallbackHandler()]
@@ -80,7 +81,7 @@ class OrchestratorRuleParser:
             tools = self.to_tools(
                 tool_configs=tool_configs,
                 conversation_message_task=conversation_message_task,
-                model_name=agent_model_name,
+                model_name=self.agent_summary_model_name,
                 rest_tokens=rest_tokens,
                 callbacks=[agent_callback, DifyStdOutCallbackHandler()]
             )
