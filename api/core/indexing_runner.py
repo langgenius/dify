7cf1c1241ccc55e74515191771932abcc29510d1
jyong <718720800@qq.com>
2023-07-29 16:57:13 +0800

    add thread control

--- api/core/indexing_runner.py
+++ api/core/indexing_runner.py
@@ -524,7 +524,6 @@ class IndexingRunner:
         return all_documents
 
     def format_document(self, llm: StreamableOpenAI, document_node, split_documents, document_form: str):
-        print(document_node.page_content)
         format_documents = []
         if document_node.page_content is None or not document_node.page_content.strip():
             return format_documents
