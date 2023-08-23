117a209ad42200f83b7c5bafc3ee65fa585fa1b5
yezhwi <yzhw.2008@163.com>
2023-08-23 19:57:27 +0800

    Fix:condition for dataset availability check (#973)

--- api/services/hit_testing_service.py
+++ api/services/hit_testing_service.py
@@ -19,7 +19,7 @@ from models.dataset import Dataset, DocumentSegment, DatasetQuery
 class HitTestingService:
     @classmethod
     def retrieve(cls, dataset: Dataset, query: str, account: Account, limit: int = 10) -> dict:
-        if dataset.available_document_count == 0 or dataset.available_document_count == 0:
+        if dataset.available_document_count == 0 or dataset.available_segment_count == 0:
             return {
                 "query": {
                     "content": query,
