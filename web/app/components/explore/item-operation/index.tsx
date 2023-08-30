e5c64e5048605e5eaff21ae8470f38f61ac94285
Joel <iamjoel007@gmail.com>
2023-08-30 18:19:17 +0800

    feat: portal

--- web/app/components/explore/item-operation/index.tsx
+++ web/app/components/explore/item-operation/index.tsx
@@ -1,12 +1,12 @@
 'use client'
 import type { FC } from 'react'
-import React from 'react'
+import React, { useState } from 'react'
 import cn from 'classnames'
 import { useTranslation } from 'react-i18next'
 import { Edit03, Pin02, Trash03 } from '../../base/icons/src/vender/line/general'
 
 import s from './style.module.css'
-import Popover from '@/app/components/base/popover'
+import { PortalToFollowElem, PortalToFollowElemContent, PortalToFollowElemTrigger } from '@/app/components/base/portal-to-follow-elem'
 
 export type IItemOperationProps = {
   className?: string
@@ -28,11 +28,17 @@ const ItemOperation: FC<IItemOperationProps> = ({
   onDelete,
 }) => {
   const { t } = useTranslation()
+  const [open, setOpen] = useState(false)
 
   return (
-    <Popover
-      htmlContent={
-        <div className='w-full py-1' onClick={(e) => {
+    <PortalToFollowElem
+      open={open} onOpenChange={setOpen}
+    >
+      <PortalToFollowElemTrigger onClick={() => setOpen(v => !v)}>
+        <div className={cn(className, s.btn, 'h-6 w-6 rounded-md border-none py-1', open && '!bg-gray-100 !shadow-none')}></div>
+      </PortalToFollowElemTrigger>
+      <PortalToFollowElemContent className="PortalToFollowElem">
+        <div className={`${s.popupPanel} w-full py-1 bg-white`} onClick={(e) => {
           e.stopPropagation()
         }}>
           <div className={cn(s.actionItem, 'hover:bg-gray-50 group')} onClick={togglePin}>
@@ -53,13 +59,8 @@ const ItemOperation: FC<IItemOperationProps> = ({
           )}
 
         </div>
-      }
-      trigger='click'
-      position='br'
-      btnElement={<div />}
-      btnClassName={open => cn(className, s.btn, 'h-6 w-6 rounded-md border-none py-1', open && '!bg-gray-100 !shadow-none')}
-      className={'!w-[120px] !px-0 h-fit !z-20'}
-    />
+      </PortalToFollowElemContent>
+    </PortalToFollowElem>
   )
 }
 export default React.memo(ItemOperation)
