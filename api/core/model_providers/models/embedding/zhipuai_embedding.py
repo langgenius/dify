31540811dea9ae19e93f720792ceef87e2877a88
John Wang <takatost@gmail.com>
2023-09-14 23:44:28 +0800

    fix: zhipuai embedding exception handle

--- api/core/model_providers/models/embedding/zhipuai_embedding.py
+++ api/core/model_providers/models/embedding/zhipuai_embedding.py
@@ -1,6 +1,3 @@
-from core.third_party.langchain.embeddings.xinference_embedding import XinferenceEmbedding as XinferenceEmbeddings
-from replicate.exceptions import ModelError, ReplicateError
-
 from core.model_providers.error import LLMBadRequestError
 from core.model_providers.providers.base import BaseModelProvider
 from core.model_providers.models.embedding.base import BaseEmbedding
@@ -22,7 +19,4 @@ class ZhipuAIEmbedding(BaseEmbedding):
         super().__init__(model_provider, client, name)
 
     def handle_exceptions(self, ex: Exception) -> Exception:
-        if isinstance(ex, (ModelError, ReplicateError)):
-            return LLMBadRequestError(f"ZhipuAI embedding: {str(ex)}")
-        else:
-            return ex
+        return LLMBadRequestError(f"ZhipuAI embedding: {str(ex)}")
