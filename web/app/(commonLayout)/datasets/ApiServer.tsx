34d8227a78e0a332d9d5e8bf2934f0ad1d37eccc
StyleZhang <jasonapring2015@outlook.com>
2023-09-25 12:50:01 +0800

    add doc

--- web/app/(commonLayout)/datasets/ApiServer.tsx
+++ web/app/(commonLayout)/datasets/ApiServer.tsx
@@ -1,12 +1,28 @@
+import CopyFeedback from '@/app/components/base/copy-feedback'
+import SecretKeyButton from '@/app/components/develop/secret-key/secret-key-button'
+import { randomString } from '@/utils'
+
 const ApiServer = () => {
   return (
     <div className='flex items-center'>
-      <div className='flex items-center pl-1.5 pr-1 h-8 bg-white/80 border-[0.5px] border-white rounded'>
+      <div className='flex items-center mr-2 pl-1.5 pr-1 h-8 bg-white/80 border-[0.5px] border-white rounded'>
         <div className='mr-0.5 px-1.5 h-5 border border-gray-200 text-[11px] text-gray-500 rounded-md'>API Server</div>
-        <div className='px-1 w-[169px] text-[13px] font-medium text-gray-800'>https://api.langgenius.ai/v1</div>
+        <div className='px-1 w-[248px] text-[13px] font-medium text-gray-800'>https://api.langgenius.ai/v1</div>
         <div className='mx-1 w-[1px] h-[14px] bg-gray-200'></div>
-        <div className='flex justify-center items-center w-6 h-6'></div>
+        <CopyFeedback
+          content=''
+          selectorId={randomString(8)}
+          className={'!w-6 !h-6 hover:bg-gray-200'}
+        />
       </div>
+      <div className='flex items-center mr-2 px-3 h-8 bg-[#ECFDF3] text-xs font-semibold text-[#039855] rounded-lg border-[0.5px] border-[#D1FADF]'>
+        In Service
+      </div>
+      <SecretKeyButton
+        className='flex-shrink-0 !h-8 bg-white mr-2'
+        textCls='!text-gray-700 font-medium'
+        iconCls='stroke-[1.2px]'
+      />
     </div>
   )
 }
--- web/app/(commonLayout)/datasets/Doc.tsx
+++ web/app/(commonLayout)/datasets/Doc.tsx
@@ -1,6 +1,30 @@
-const Doc = () => {
+'use client'
+
+import { useContext } from 'use-context-selector'
+import TemplateEn from './template/template.en.mdx'
+import TemplateZh from './template/template.zh.mdx'
+import I18n from '@/context/i18n'
+
+type IDocProps = {
+  appDetail: any
+}
+
+const Doc = ({ appDetail }: IDocProps) => {
+  const { locale } = useContext(I18n)
+  const variables = appDetail?.model_config?.configs?.prompt_variables || []
+  const inputs = variables.reduce((res: any, variable: any) => {
+    res[variable.key] = variable.name || ''
+    return res
+  }, {})
+
   return (
-    <div>doc</div>
+    <article className="prose prose-xl bg-white" >
+      {
+        locale === 'en'
+          ? <TemplateEn appDetail={appDetail} variables={variables} inputs={inputs} />
+          : <TemplateZh appDetail={appDetail} variables={variables} inputs={inputs} />
+      }
+    </article>
   )
 }
 
