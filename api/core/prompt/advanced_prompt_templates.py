94a0e0bacb93f785aa86aa7b150f2c0a20b645a8
Garfield Dai <dai.hai@foxmail.com>
2023-10-10 20:37:39 +0800

    update.

--- api/core/prompt/advanced_prompt_templates.py
+++ api/core/prompt/advanced_prompt_templates.py
@@ -4,8 +4,8 @@ CHAT_APP_COMPLETION_PROMPT_CONFIG = {
             "text": "Use the following context as your learned knowledge, inside <context></context> XML tags.\n\n<context>{{#context#}}</context>\n\nWhen answer to user:\n- If you don't know, just say that you don't know.\n- If you don't know when you are not sure, ask for clarification.\nAvoid mentioning that you obtained the information from the context.\nAnd answer according to the language of the user's question.\n{{#pre_prompt#}}\nHere is the chat histories between human and assistant, inside <histories></histories> XML tags.\n\n<histories>{{#histories#}}</histories>\n\n\nHuman: {{#query#}}\n\nAssistant: "
         },
         "conversation_histories_role": {
-            "user_prefix": "user",
-            "assistant_prefix": "assistant"
+            "user_prefix": "Human: ",
+            "assistant_prefix": "Assistant: "
         }
     }
 }
@@ -45,8 +45,8 @@ BAICHUAN_CHAT_APP_COMPLETION_PROMPT_CONFIG = {
             "text": "用户在与一个客观的助手对话。助手会尊重找到的材料，给出全面专业的解释，但不会过度演绎。同时回答中不会暴露引用的材料：\n\n```\n{{#context#}}\n```\n\n{{#pre_prompt#}}\n\n用户和助手的历史对话内容如下：\n```\n{{#histories#}}\n```\n\n\n\n用户：{{#query#}}"
         },
         "conversation_histories_role": {
-            "user_prefix": "user",
-            "assistant_prefix": "assistant"
+            "user_prefix": "用户：",
+            "assistant_prefix": "助手："
         }
     }
 }
