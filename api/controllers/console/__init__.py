45553f3d965a6bc6f2f973ed6c22e02c4ba43443
Garfield Dai <dai.hai@foxmail.com>
2023-10-10 14:14:01 +0800

    feat: add advanced prompt templates.

--- api/controllers/console/__init__.py
+++ api/controllers/console/__init__.py
@@ -9,7 +9,7 @@ api = ExternalApi(bp)
 from . import setup, version, apikey, admin
 
 # Import app controllers
-from .app import app, site, completion, model_config, statistic, conversation, message, generator, audio
+from .app import advanced_prompt_template, app, site, completion, model_config, statistic, conversation, message, generator, audio
 
 # Import auth controllers
 from .auth import login, oauth, data_source_oauth, activate
---
+++ api/services/advanced_prompt_template_service.py
@@ -0,0 +1,42 @@
+
+from api.core.prompt.advanced_prompt_templates import CHAT_APP_COMPLETION_PROMPT_CONFIG, CHAT_APP_CHAT_PROMPT_CONFIG, COMPLETION_APP_CHAT_PROMPT_CONFIG, COMPLETION_APP_COMPLETION_PROMPT_CONFIG, \
+    BAICHUAN_CHAT_APP_COMPLETION_PROMPT_CONFIG, BAICHUAN_CHAT_APP_CHAT_PROMPT_CONFIG, BAICHUAN_COMPLETION_APP_COMPLETION_PROMPT_CONFIG, BAICHUAN_COMPLETION_APP_CHAT_PROMPT_CONFIG
+
+class AdvancedPromptTemplateService:
+
+    @staticmethod
+    def get_prompt(args: dict):
+        app_mode = args['app_mode']
+        model_mode = args['model_mode']
+        model_name = args['model_name']
+
+        if model_name == 'baichuan':
+            return AdvancedPromptTemplateService.get_baichuan_prompt(app_mode, model_mode)
+        else:
+            return AdvancedPromptTemplateService.get_common_prompt(app_mode, model_mode)
+
+    @staticmethod
+    def get_common_prompt(app_mode: str, model_mode:str):
+        if app_mode == 'chat':
+            if model_mode == 'completion':
+                return CHAT_APP_COMPLETION_PROMPT_CONFIG
+            elif model_mode == 'chat':
+                return CHAT_APP_CHAT_PROMPT_CONFIG
+        elif app_mode == 'completion':
+            if model_mode == 'completion':
+                return COMPLETION_APP_COMPLETION_PROMPT_CONFIG
+            elif model_mode == 'chat':
+                return COMPLETION_APP_CHAT_PROMPT_CONFIG
+
+    @staticmethod
+    def get_baichuan_prompt(app_mode: str, model_mode:str):
+        if app_mode == 'chat':
+            if model_mode == 'completion':
+                return BAICHUAN_CHAT_APP_COMPLETION_PROMPT_CONFIG
+            elif model_mode == 'chat':
+                return BAICHUAN_CHAT_APP_CHAT_PROMPT_CONFIG
+        elif app_mode == 'completion':
+            if model_mode == 'completion':
+                return BAICHUAN_COMPLETION_APP_COMPLETION_PROMPT_CONFIG
+            elif model_mode == 'chat':
+                return BAICHUAN_COMPLETION_APP_CHAT_PROMPT_CONFIG
