cd65b6b920e6f94bf6b0e392aaf96fc8dbe17972
Joel <iamjoel007@gmail.com>
2023-10-11 10:07:11 +0800

    fix: first check

--- web/app/components/base/prompt-editor/constants.tsx
+++ web/app/components/base/prompt-editor/constants.tsx
@@ -4,13 +4,19 @@ export const QUERY_PLACEHOLDER_TEXT = '{{#query#}}'
 export const PRE_PROMPT_PLACEHOLDER_TEXT = '{{#pre_prompt#}}'
 
 export const checkHasContextBlock = (text: string) => {
+  if (!text)
+    return false
   return text.includes(CONTEXT_PLACEHOLDER_TEXT)
 }
 
 export const checkHasHistoryBlock = (text: string) => {
+  if (!text)
+    return false
   return text.includes(HISTORY_PLACEHOLDER_TEXT)
 }
 
 export const checkHasQueryBlock = (text: string) => {
+  if (!text)
+    return false
   return text.includes(QUERY_PLACEHOLDER_TEXT)
 }
