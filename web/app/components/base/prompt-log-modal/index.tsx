28678fd009fc95394cc784e188d9c64c2a60e418
StyleZhang <jasonapring2015@outlook.com>
2023-10-12 14:44:32 +0800

    fix: prompt-log-modal style

--- web/app/components/base/prompt-log-modal/index.tsx
+++ web/app/components/base/prompt-log-modal/index.tsx
@@ -18,9 +18,9 @@ const PromptLogModal: FC<PromptLogModalProps> = ({
 }) => {
   return (
     <div
-      className='fixed top-16 left-2 bottom-2 bg-white border-[0.5px] border-gray-200 rounded-xl shadow-xl z-10'
+      className='fixed top-16 left-2 bottom-2 flex flex-col bg-white border-[0.5px] border-gray-200 rounded-xl shadow-xl z-10'
       style={{ width }}>
-      <div className='flex justify-between items-center pl-6 pr-5 h-14 border-b border-b-gray-100'>
+      <div className='shrink-0 flex justify-between items-center pl-6 pr-5 h-14 border-b border-b-gray-100'>
         <div className='text-base font-semibold text-gray-900'>PROMPT LOG</div>
         <div className='flex items-center'>
           {
@@ -39,7 +39,7 @@ const PromptLogModal: FC<PromptLogModalProps> = ({
           </div>
         </div>
       </div>
-      <div className='p-2'>
+      <div className='grow p-2 overflow-y-auto'>
         <Card log={log} />
       </div>
     </div>
