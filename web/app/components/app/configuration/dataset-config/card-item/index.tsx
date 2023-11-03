0d7a9daa049a116f2aacdd0c0df2909a9a37c368
Joel <iamjoel007@gmail.com>
2023-11-03 14:32:58 +0800

    feat: new dataset setting ui

--- web/app/components/app/configuration/dataset-config/card-item/index.tsx
+++ web/app/components/app/configuration/dataset-config/card-item/index.tsx
@@ -9,6 +9,7 @@ import s from './style.module.css'
 import type { DataSet } from '@/models/datasets'
 import { formatNumber } from '@/utils/format'
 import Tooltip from '@/app/components/base/tooltip'
+import { Settings01 } from '@/app/components/base/icons/src/vender/line/general'
 
 export type ICardItemProps = {
   className?: string
@@ -29,14 +30,14 @@ const CardItem: FC<ICardItemProps> = ({
     <div
       className={
         cn(className, s.card,
-          'relative flex items-center rounded-xl  px-3 py-2.5 bg-white border border-gray-200  cursor-pointer')
+          'relative flex items-center justify-between rounded-xl px-2.5 py-2 bg-white border border-gray-200  cursor-pointer')
       }>
       <div className='flex items-center space-x-2'>
         <div className={cn(!config.embedding_available && 'opacity-50')}>
-          <TypeIcon type="upload_file" />
+          <TypeIcon type="upload_file" size='md' />
         </div>
         <div>
-          <div className='flex items-center w-[160px] mr-1'>
+          <div className='flex items-center w-[290px]'>
             <div className={cn('text-[13px] leading-[18px] font-medium text-gray-800 overflow-hidden text-ellipsis whitespace-nowrap', !config.embedding_available && 'opacity-50')}>{config.name}</div>
             {!config.embedding_available && (
               <Tooltip
@@ -47,13 +48,22 @@ const CardItem: FC<ICardItemProps> = ({
               </Tooltip>
             )}
           </div>
-          <div className={cn('max-w-[150px] flex text-xs text-gray-500', !config.embedding_available && 'opacity-50')}>
-            {formatNumber(config.word_count)} {t('appDebug.feature.dataSet.words')} · {formatNumber(config.document_count)} {t('appDebug.feature.dataSet.textBlocks')}
-          </div>
         </div>
       </div>
+      <div className={cn('max-w-[150px] flex text-xs text-gray-500', !config.embedding_available && 'opacity-50')}>
+        {formatNumber(config.word_count)} {t('appDebug.feature.dataSet.words')} · {formatNumber(config.document_count)} {t('appDebug.feature.dataSet.textBlocks')}
+      </div>
 
-      {!readonly && <RemoveIcon className={`${s.deleteBtn} absolute right-1 top-1/2 translate-y-[-50%]`} onClick={() => onRemove(config.id)} />}
+      {!readonly
+      && (
+        <div className={`${s.btnWrap} absolute right-2 top-1/2 translate-y-[-50%] flex items-center space-x-1`}>
+          <div className={cn(s.settingBtn, 'rounded-md text-gray-500 p-1 cursor-pointer')} >
+            <Settings01 className='w-4 h-4' />
+          </div>
+          <RemoveIcon className={`${s.deleteBtn}`} onClick={() => onRemove(config.id)} />
+        </div>
+      )
+      }
     </div>
   )
 }
