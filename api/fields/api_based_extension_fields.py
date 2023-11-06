c7c62dd4313aae8fcf5ff0311c8de4e3e506319a
Garfield Dai <dai.hai@foxmail.com>
2023-11-06 22:15:24 +0800

    feat: hidden api key enhancement.

--- api/fields/api_based_extension_fields.py
+++ api/fields/api_based_extension_fields.py
@@ -5,7 +5,19 @@ from libs.helper import TimestampField
 
 class HiddenAPIKey(fields.Raw):
     def output(self, key, obj):
-        return obj.api_key[:3] + '***' + obj.api_key[-3:]
+
+        api_key = obj.api_key
+        min_length = 5
+
+        if len(api_key) < min_length:
+            raise ValueError(f"api_key must be at least {min_length} characters long.")
+
+        # If the length of the api_key is less than 8 characters, show the first and last characters
+        if len(api_key) <= 8:
+            return api_key[0] + '******' + api_key[-1]
+        # If the api_key is greater than 8 characters, show the first three and the last three characters
+        else:
+            return api_key[:3] + '******' + api_key[-3:]
 
 
 api_based_extension_fields = {
