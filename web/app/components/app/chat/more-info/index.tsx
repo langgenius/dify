ae4246886b28e1950736911060e665e2cc8cf317
StyleZhang <jasonapring2015@outlook.com>
2023-11-06 20:07:20 +0800

    fix: chat style

--- web/app/components/app/chat/more-info/index.tsx
+++ web/app/components/app/chat/more-info/index.tsx
@@ -9,7 +9,7 @@ export type IMoreInfoProps = { more: MessageMore; isQuestion: boolean }
 
 const MoreInfo: FC<IMoreInfoProps> = ({ more, isQuestion }) => {
   const { t } = useTranslation()
-  return (<div className={`mt-1 space-x-2 text-xs text-gray-400 ${isQuestion ? 'mr-2 text-right ' : 'ml-2 text-left float-right'}`}>
+  return (<div className={`mt-1 w-full text-xs text-gray-400 !text-right ${isQuestion ? 'mr-2 text-right ' : 'ml-2 text-left float-right'}`}>
     <span>{`${t('appLog.detail.timeConsuming')} ${more.latency}${t('appLog.detail.second')}`}</span>
     <span>{`${t('appLog.detail.tokenCost')} ${formatNumber(more.tokens)}`}</span>
     <span>· </span>
