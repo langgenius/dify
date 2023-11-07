e530083d293a84337f7818420184d4d780bd0a52
crazywoola <427733928@qq.com>
2023-11-08 07:42:56 +0800

    chore: ui

--- web/app/components/app/overview/embedded/index.tsx
+++ web/app/components/app/overview/embedded/index.tsx
@@ -9,8 +9,6 @@ import Tooltip from '@/app/components/base/tooltip'
 import { useAppContext } from '@/context/app-context'
 import { IS_CE_EDITION } from '@/config'
 
-// const isDevelopment = process.env.NODE_ENV === 'development'
-
 type Props = {
   isShow: boolean
   onClose: () => void
@@ -126,7 +124,17 @@ const Embedded = ({ isShow, onClose, appBaseUrl, accessToken }: Props) => {
           )
         })}
       </div>
-      <div className="mt-6 w-full bg-gray-100 rounded-lg flex-col justify-start items-start inline-flex">
+      {option === 'chromePlugin' && (
+        <div className="mt-6 w-full">
+          <div className={cn('gap-2 py-3 justify-center items-center inline-flex w-full rounded-lg',
+            'bg-primary-600 hover:bg-primary-600/75 hover:shadow-md cursor-pointer text-white hover:shadow-sm flex-shrink-0')}>
+            <div className={`w-4 h-4 relative ${style.pluginInstallIcon}`}></div>
+            <div className="text-white text-sm font-medium font-['Inter'] leading-tight" onClick={navigateToChromeUrl}>{t(`${prefixEmbedded}.chromePlugin`)}</div>
+          </div>
+        </div>
+      )}
+      <div className={cn('w-full bg-gray-100 rounded-lg flex-col justify-start items-start inline-flex',
+        'mt-6')}>
         <div className="self-stretch pl-3 pr-1 py-1 bg-gray-50 rounded-tl-lg rounded-tr-lg border border-black border-opacity-5 justify-start items-center gap-2 inline-flex">
           <div className="grow shrink basis-0 text-slate-700 text-[13px] font-medium leading-none">
             {t(`${prefixEmbedded}.${option}`)}
@@ -148,14 +156,6 @@ const Embedded = ({ isShow, onClose, appBaseUrl, accessToken }: Props) => {
           </div>
         </div>
       </div>
-      {option === 'chromePlugin' && (
-        <div className="py-3 w-full">
-          <div className="py-3 bg-blue-600 justify-center items-center gap-2 inline-flex w-full rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-            <div className={`w-4 h-4 relative ${style.pluginInstallIcon}`}></div>
-            <div className="text-white text-sm font-medium font-['Inter'] leading-tight" onClick={navigateToChromeUrl}>{t(`${prefixEmbedded}.chromePlugin`)}</div>
-          </div>
-        </div>
-      )}
     </Modal>
   )
 }
