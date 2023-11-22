68ad052ba3ee01f1b8d736dfb70ad5be824813c7
Joel <iamjoel007@gmail.com>
2023-11-22 11:34:11 +0800

    feat: billing en i18n

--- web/i18n/lang/billing.en.ts
+++ web/i18n/lang/billing.en.ts
@@ -4,6 +4,59 @@ const translation = {
     plain: 'Upgrade Plan',
     encourage: 'Upgrade Now',
   },
+  plansCommon: {
+    mostPopular: 'Most Popular',
+    month: 'month',
+    free: 'Free',
+    startForFree: 'Start for free',
+    getStartedWith: 'Get started with ',
+    supportModels: 'OpenAI/Anthropic/Azure OpenAI/Llama2/Hugging Face/Replicate',
+    teamMembers: 'Team Members',
+    buildApps: 'Build Apps',
+    vectorSpace: 'Vector Space',
+    documentProcessingPriority: 'Document Processing Priority',
+    priority: {
+      standard: 'Standard',
+      priority: 'Priority',
+      topPriority: 'Top Priority',
+    },
+    logsHistory: 'Logs history',
+    unlimited: 'Unlimited',
+    support: 'Support',
+    supportItems: {
+      communityForums: 'Community forums',
+      emailSupport: 'Email support',
+      priorityEmail: 'Priority email & chat support',
+      logoChange: 'Logo change',
+      SSOAuthentication: 'SSO authentication',
+      personalizedSupport: 'Personalized support',
+      dedicatedAPISupport: 'Dedicated API support',
+      customIntegration: 'Custom integration and support',
+    },
+    comingSoon: 'Coming soon',
+  },
+  plans: {
+    sandbox: {
+      name: 'Sandbox',
+      description: '200 times GPT free trial',
+      includesTitle: 'Includes:',
+    },
+    professional: {
+      name: 'Professional',
+      description: 'For individuals and small teams to unlock more power affordably.',
+      includesTitle: 'Everything in free plan, plus:',
+    },
+    team: {
+      name: 'Team',
+      description: 'Collaborate without limits and enjoy top-tier performance.',
+      includesTitle: 'Everything in Professional plan, plus:',
+    },
+    enterprise: {
+      name: 'Enterprise',
+      description: 'Get full capabilities and support for large-scale mission-critical systems.',
+      includesTitle: 'Everything in Team plan, plus:',
+    },
+  },
 }
 
 export default translation
