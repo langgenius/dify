396db22183b6d685ddc95003bec2876360e7d4e3
Garfield Dai <dai.hai@foxmail.com>
2023-12-02 21:02:49 +0800

    refactor.

--- api/controllers/console/billing/billing.py
+++ api/controllers/console/billing/billing.py
@@ -1,5 +1,9 @@
+import stripe
+import os
+
 from flask_restful import Resource, reqparse
 from flask_login import current_user
+from flask import current_app, request
 
 from controllers.console import api
 from controllers.console.setup import setup_required
@@ -14,30 +18,13 @@ class BillingInfo(Resource):
     @setup_required
     @login_required
     @account_initialization_required
-    @only_edition_cloud
     def get(self):
-        return {
-            "enabled": True,
-            "subscription": {
-                "plan": "professional"
-            },
-            "members": {
-                "size": 2,
-                "limit": 3
-            },
-            "apps": {
-                "size": 20,
-                "limit": 50
-            },
-            "vector_space": {
-                "size": 100,
-                "limit": 200
-            },
-            "docs_processing": "top_priority",
-            "can_replace_logo": True
-        }
 
-        # return BillingService.get_info(current_user.current_tenant_id)
+        edition = current_app.config['EDITION']
+        if edition != 'CLOUD':
+            return {"enabled": False}
+
+        return BillingService.get_info(current_user.current_tenant_id)
 
 
 class Subscription(Resource):
@@ -47,17 +34,13 @@ class Subscription(Resource):
     @account_initialization_required
     @only_edition_cloud
     def get(self):
-        prefilled_email = current_user.email
-        return {
-            "url": f'https://buy.stripe.com/test_fZeaGB7Te2NPgBGeUX?prefilled_email={prefilled_email}'
-        }
 
-        # parser = reqparse.RequestParser()
-        # parser.add_argument('plan', type=str, required=True, location='args', choices=['professional', 'team'])
-        # parser.add_argument('interval', type=str, required=True, location='args', choices=['month', 'year'])
-        # args = parser.parse_args()
-        #
-        # return BillingService.get_subscription(args['plan'], args['interval'], current_user.email, current_user.name, current_user.current_tenant_id)
+        parser = reqparse.RequestParser()
+        parser.add_argument('plan', type=str, required=True, location='args', choices=['professional', 'team'])
+        parser.add_argument('interval', type=str, required=True, location='args', choices=['month', 'year'])
+        args = parser.parse_args()
+
+        return BillingService.get_subscription(args['plan'], args['interval'], current_user.email, current_user.name, current_user.current_tenant_id)
 
 
 class Invoices(Resource):
@@ -67,13 +50,36 @@ class Invoices(Resource):
     @account_initialization_required
     @only_edition_cloud
     def get(self):
-        return {
-            "url": f'https://billing.stripe.com/p/login/test_eVa7vw9EvgJf78Q7ss?prefilled_email={current_user.email}'
-        }
 
-        # return BillingService.get_invoices(current_user.email)
+        return BillingService.get_invoices(current_user.email)
+
+
+class StripeBillingWebhook(Resource):
+
+    @setup_required
+    @only_edition_cloud
+    def post(self):
+        payload = request.data
+        sig_header = request.headers.get('STRIPE_SIGNATURE')
+        webhook_secret = os.environ.get('STRIPE_WEBHOOK_BILLING_SECRET', 'STRIPE_WEBHOOK_BILLING_SECRET')
+
+        try:
+            event = stripe.Webhook.construct_event(
+                payload, sig_header, webhook_secret
+            )
+        except ValueError as e:
+            # Invalid payload
+            return 'Invalid payload', 400
+        except stripe.error.SignatureVerificationError as e:
+            # Invalid signature
+            return 'Invalid signature', 400
+
+        BillingService.process_event(event)
+
+        return 'success', 200
 
 
 api.add_resource(BillingInfo, '/billing/info')
 api.add_resource(Subscription, '/billing/subscription')
 api.add_resource(Invoices, '/billing/invoices')
+api.add_resource(StripeBillingWebhook, '/billing/webhook/stripe')
