76d3c1e3b64ed620a4eb7de346751c2955829f4e
takatost <takatost@gmail.com>
2023-12-27 21:28:39 +0800

    fix message not save

--- api/core/app_runner/generate_task_pipeline.py
+++ api/core/app_runner/generate_task_pipeline.py
@@ -332,6 +332,8 @@ class GenerateTaskPipeline:
         """
         usage = llm_result.usage
 
+        self._message = db.session.query(Message).filter(Message.id == self._message.id).first()
+
         self._message.message = self._prompt_messages_to_prompt_for_saving(self._task_state.llm_result.prompt_messages)
         self._message.message_tokens = usage.prompt_tokens
         self._message.message_unit_price = usage.prompt_unit_price
