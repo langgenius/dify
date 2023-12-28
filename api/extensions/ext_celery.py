59a2c8c70afff4c127fb673bb91233fbdb210397
jyong <jyong@dify.ai>
2023-12-28 18:53:01 +0800

    add clean unused datasets and embedding cache

--- api/extensions/ext_celery.py
+++ api/extensions/ext_celery.py
@@ -46,7 +46,7 @@ def init_app(app: Flask) -> Celery:
     beat_schedule = {
         'clean_embedding_cache_task': {
             'task': 'schedule.clean_embedding_cache_task.clean_embedding_cache_task',
-            'schedule': timedelta(minutes=10),
+            'schedule': timedelta(minutes=1),
         },
         'clean_unused_datasets_task': {
             'task': 'schedule.clean_unused_datasets_task.clean_unused_datasets_task',
