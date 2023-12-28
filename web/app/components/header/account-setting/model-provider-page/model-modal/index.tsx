58b8495aa871280c549686cd2d329aed9da55e4c
StyleZhang <jasonapring2015@outlook.com>
2023-12-28 18:27:36 +0800

    fix: model modal style

--- web/app/components/header/account-setting/model-provider-page/model-modal/index.tsx
+++ web/app/components/header/account-setting/model-provider-page/model-modal/index.tsx
@@ -240,7 +240,7 @@ const ModelModal: FC<ModelModalProps> = ({
                 validatedSuccess={validatedStatusState.status === ValidatedStatus.Success}
                 showOnVariableMap={showOnVariableMap}
               />
-              <div className='flex justify-between items-center py-6 flex-wrap gap-y-2'>
+              <div className='sticky bottom-0 flex justify-between items-center py-6 flex-wrap gap-y-2 bg-white'>
                 {
                   (provider.help && (provider.help.title || provider.help.url))
                     ? (
