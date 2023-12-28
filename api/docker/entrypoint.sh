39fb92196718d013d8152c03e1004e081c4ec593
jyong <jyong@dify.ai>
2023-12-28 12:42:18 +0800

    add clean unused datasets and embedding cache

--- api/docker/entrypoint.sh
+++ api/docker/entrypoint.sh
@@ -10,6 +10,8 @@ fi
 if [[ "${MODE}" == "worker" ]]; then
   celery -A app.celery worker -P ${CELERY_WORKER_CLASS:-gevent} -c ${CELERY_WORKER_AMOUNT:-1} --loglevel INFO \
     -Q ${CELERY_QUEUES:-dataset,generation,mail}
+if [[ "${MODE}" == "beat" ]]; then
+  celery -A app.celery beat -P ${CELERY_WORKER_CLASS:-gevent} --loglevel INFO
 else
   if [[ "${DEBUG}" == "true" ]]; then
     flask run --host=${DIFY_BIND_ADDRESS:-0.0.0.0} --port=${DIFY_PORT:-5001} --debug
