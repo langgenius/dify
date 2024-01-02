de3f328ccacf2aa92439d5e9b085465f5de0fff2
takatost <takatost@gmail.com>
2024-01-02 22:10:56 +0800

    bump version to 0.4.0

--- api/config.py
+++ api/config.py
@@ -87,7 +87,7 @@ class Config:
         # ------------------------
         # General Configurations.
         # ------------------------
-        self.CURRENT_VERSION = "0.3.34"
+        self.CURRENT_VERSION = "0.4.0"
         self.COMMIT_SHA = get_env('COMMIT_SHA')
         self.EDITION = "SELF_HOSTED"
         self.DEPLOY_ENV = get_env('DEPLOY_ENV')
