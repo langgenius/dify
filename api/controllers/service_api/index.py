510593f8e42bc9257bdb1dd2ea4f3376979afe6f
takatost <takatost@gmail.com>
2024-01-16 15:13:20 +0800

    modify welcome text

--- api/controllers/service_api/index.py
+++ api/controllers/service_api/index.py
@@ -7,7 +7,7 @@ from controllers.service_api import api
 class IndexApi(Resource):
     def get(self):
         return {
-            "welcome": "Dify Service API",
+            "welcome": "Dify OpenAPI",
             "api_version": "v1",
             "server_version": current_app.config['CURRENT_VERSION']
         }
