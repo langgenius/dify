605cd5280215261d6ca16c8dc12585a799eac818
Yeuoly <admin@srmxy.cn>
2024-01-17 16:25:30 +0800

    add plot

--- api/core/tools/provider/builtin/chart/chart.py
+++ api/core/tools/provider/builtin/chart/chart.py
@@ -3,6 +3,10 @@ from core.tools.errors import ToolProviderCredentialValidationError
 
 from core.tools.provider.builtin.chart.tools.line import LinearChartTool
 
+import matplotlib.pyplot as plt
+# use a business theme
+plt.style.use('seaborn-v0_8-darkgrid')
+
 class ChartProvider(BuiltinToolProviderController):
     def _validate_credentials(self, credentials: dict) -> None:
         try:
--- api/core/tools/provider/builtin/chart/tools/line.py
+++ api/core/tools/provider/builtin/chart/tools/line.py
@@ -1,5 +1,7 @@
 from core.tools.tool.builtin_tool import BuiltinTool
 from core.tools.entities.tool_entities import ToolInvokeMessage
+import matplotlib.pyplot as plt
+import io
 
 from typing import Any, Dict, List, Union
 
@@ -8,6 +10,21 @@ class LinearChartTool(BuiltinTool):
                 user_id: str, 
                tool_paramters: Dict[str, Any], 
         ) -> Union[ToolInvokeMessage, List[ToolInvokeMessage]]:
+        data = tool_paramters.get('data', '')
+        if not data:
+            return self.create_text_message('Please input data')
+        data = data.split(',')
+        data = [float(i) for i in data]
 
-        return self.create_text_message("success")
+        flg, ax = plt.subplots()
+        ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+
+        buf = io.BytesIO()
+        flg.savefig(buf, format='png')
+        buf.seek(0)
+
+        return self.create_blob_message(blob=buf.read(),
+                                        meta={
+                                            'type': 'image/png'
+                                        })
     
