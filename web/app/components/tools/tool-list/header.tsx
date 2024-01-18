01d49952dd6d0daada385d4114d2432fd0a0e29f
Joel <iamjoel007@gmail.com>
2024-01-18 10:40:48 +0800

    chore: tool des show

--- web/app/components/tools/tool-list/header.tsx
+++ web/app/components/tools/tool-list/header.tsx
@@ -34,16 +34,16 @@ const Header: FC<Props> = ({
   const isAuthed = collection.is_team_authorization
   return (
     <div className={cn(isInToolsPage ? 'py-4 px-6' : 'py-[11px] pl-4 pr-3', 'flex justify-between  items-center border-b border-gray-200')}>
-      <div className='flex items-center'>
+      <div className='flex items-center w-full'>
         {icon}
-        <div className='ml-3'>
+        <div className='ml-3 grow w-0'>
           <div className='flex items-center h-6 space-x-1'>
-            <div className={cn(isInDebugPage && 'max-w-[160px] truncate', 'text-base font-semibold text-gray-900')}>{collection.label[locale === 'en' ? 'en_US' : 'zh_Hans']}</div>
+            <div className={cn(isInDebugPage && 'truncate', 'text-base font-semibold text-gray-900')}>{collection.label[locale === 'en' ? 'en_US' : 'zh_Hans']}</div>
             <div className='text-xs font-normal text-gray-500'>·</div>
             <div className='text-xs font-normal text-gray-500'>{t('tools.author')}&nbsp;{collection.author}</div>
           </div>
           {collection.description && (
-            <div className={cn(isInDebugPage && 'max-w-[260px] truncate', 'leading-[18px] text-[13px] font-normal text-gray-500')}>
+            <div className={cn(isInDebugPage && 'truncate', 'leading-[18px] text-[13px] font-normal text-gray-500')}>
               {collection.description[locale === 'en' ? 'en_US' : 'zh_Hans']}
             </div>
           )}
@@ -51,7 +51,7 @@ const Header: FC<Props> = ({
       </div>
       {collection.type === CollectionType.builtIn && needAuth && (
         <div
-          className={cn('cursor-pointer', 'flex items-center h-8 border border-gray-200 rounded-lg px-3 space-x-2 shadow-xs')}
+          className={cn('cursor-pointer', 'ml-1 shrink-0 flex items-center h-8 border border-gray-200 rounded-lg px-3 space-x-2 shadow-xs')}
           onClick={() => onShowAuth()}
         >
           <div className={cn(isAuthed ? 'border-[#12B76A] bg-[#32D583]' : 'border-gray-400 bg-gray-300', 'rounded h-2 w-2 border')}></div>
