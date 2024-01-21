2f3d2f8c6bfc4d7cad991f1dd74f7826613d5f09
Yeuoly <admin@srmxy.cn>
2024-01-21 12:26:34 +0800

    support integration test

--- api/tests/integration_tests/tools/test_all_provider.py
+++ api/tests/integration_tests/tools/test_all_provider.py
@@ -1,6 +1,9 @@
 from core.tools.tool_manager import ToolManager
 
-def test_assistant_providers():
-    providers = ToolManager.list_providers()
+def test_tool_providers():
+    """
+    Test that all tool providers can be loaded
+    """
+    providers = ToolManager.list_builtin_providers()
     for provider in providers:
         provider.get_tools()
