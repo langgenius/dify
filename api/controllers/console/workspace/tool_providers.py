92c876ab23d201178eaaf6b68a58e1bea788ce9f
Yeuoly <admin@srmxy.cn>
2024-01-23 11:08:20 +0800

    fix: nullable privacy_policy

--- api/controllers/console/workspace/tool_providers.py
+++ api/controllers/console/workspace/tool_providers.py
@@ -102,7 +102,7 @@ class ToolApiProviderAddApi(Resource):
         parser.add_argument('schema', type=str, required=True, nullable=False, location='json')
         parser.add_argument('provider', type=str, required=True, nullable=False, location='json')
         parser.add_argument('icon', type=dict, required=True, nullable=False, location='json')
-        parser.add_argument('privacy_policy', type=str, required=False, nullable=False, location='json')
+        parser.add_argument('privacy_policy', type=str, required=False, nullable=True, location='json')
 
         args = parser.parse_args()
 
