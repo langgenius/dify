6db852df4cbf1911f2d5b59b38e3007bbb4e8a56
takatost <takatost@gmail.com>
2024-01-24 01:52:13 +0800

    version to 0.5.0.

--- api/config.py
+++ api/config.py
@@ -93,7 +93,7 @@ class Config:
         # ------------------------
         # General Configurations.
         # ------------------------
-        self.CURRENT_VERSION = "0.4.9"
+        self.CURRENT_VERSION = "0.5.0"
         self.COMMIT_SHA = get_env('COMMIT_SHA')
         self.EDITION = "SELF_HOSTED"
         self.DEPLOY_ENV = get_env('DEPLOY_ENV')
