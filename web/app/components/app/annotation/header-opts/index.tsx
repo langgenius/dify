a3e849ed10501f6ad23873dc25e1cfa4f87a4cca
JzoNg <jzongcode@gmail.com>
2024-01-25 11:57:27 +0800

    use JSON.stringify

--- web/app/components/app/annotation/header-opts/index.tsx
+++ web/app/components/app/annotation/header-opts/index.tsx
@@ -46,7 +46,7 @@ const HeaderOptions: FC<Props> = ({
 
   const listTransformer = (list: AnnotationItemBasic[]) => list.map(
     (item: AnnotationItemBasic) => {
-      const dataString = `{"messages": [{"role": "system", "content": ""}, {"role": "user", "content": "${item.question.replace(/\n/g, '\\n')}"}, {"role": "assistant", "content": "${item.answer.replace(/\n/g, '\\n')}"}]}`
+      const dataString = `{"messages": [{"role": "system", "content": ""}, {"role": "user", "content": "${JSON.stringify(item.question)}"}, {"role": "assistant", "content": "${JSON.stringify(item.answer)}"}]}`
       return dataString
     },
   )
