a213a3834116512cb5685abc9e0e24b043328d9b
leslie2046 <253605712@qq.com>
2024-01-31 15:56:46 +0800

    fix

--- api/core/model_runtime/model_providers/spark/llm/_client.py
+++ api/core/model_runtime/model_providers/spark/llm/_client.py
@@ -35,7 +35,7 @@ class SparkLLMClient:
             'spark-3': {
                 'version': 'v3.1',
                 'chat_domain': 'generalv3'
-            }
+            },
             'spark-3.5': {
                 'version': 'v3.5',
                 'chat_domain': 'generalv3.5'
