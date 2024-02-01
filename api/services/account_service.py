cf9c245a84dea23eca350629344974698ccb312a
takatost <takatost@users.noreply.github.com>
2024-02-01 12:02:49 +0800

    Revert "remove unused session store codes (#2313)"
    This reverts commit 44397e3062467497e34028c0c0e36c5ea5ae383e.

--- api/services/account_service.py
+++ api/services/account_service.py
@@ -66,6 +66,7 @@ class AccountService:
                         account.current_tenant_id = tenant_account_join.tenant_id
                     else:
                         _create_tenant_for_account(account)
+                    session['workspace_id'] = account.current_tenant_id
                 else:
                     account.current_tenant_id = workspace_id
             else:
@@ -75,6 +76,7 @@ class AccountService:
                     account.current_tenant_id = tenant_account_join.tenant_id
                 else:
                     _create_tenant_for_account(account)
+                session['workspace_id'] = account.current_tenant_id
 
             current_time = datetime.utcnow()
 
@@ -286,6 +288,7 @@ class TenantService:
 
         # Set the current tenant for the account
         account.current_tenant_id = tenant_account_join.tenant_id
+        session['workspace_id'] = account.current_tenant.id
 
     @staticmethod
     def get_tenant_members(tenant: Tenant) -> List[Account]:
