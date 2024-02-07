65a02f7d32ccfb096b1a2f1bd3d3964ca9a0f00c
Bowen Liang <liangbowen@gf.com.cn>
2024-02-07 16:28:45 +0800

    chore: apply F811 linter rule to eliminate redefined imports and methods (#2412)

--- api/controllers/service_api/dataset/document.py
+++ api/controllers/service_api/dataset/document.py
@@ -1,7 +1,6 @@
 import json
 
 from flask import request
-from flask_login import current_user
 from flask_restful import marshal, reqparse
 from sqlalchemy import desc
 from werkzeug.exceptions import NotFound
