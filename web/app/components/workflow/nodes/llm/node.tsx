da0d9aab390e9d2bc58a6b544984480b69f0b08d
Joel <iamjoel007@gmail.com>
2024-02-18 14:08:08 +0800

    chore: remove node code to panel

--- web/app/components/workflow/nodes/llm/node.tsx
+++ web/app/components/workflow/nodes/llm/node.tsx
@@ -1,31 +1,15 @@
 import type { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import BaseNode from '../_base/node'
-import Field from '@/app/components/workflow/nodes/_base/components/field'
-import AddButton from '@/app/components/base/button/add-button'
 const i18nPrefix = 'workflow.nodes.llm'
 
 const Node: FC = () => {
   const { t } = useTranslation()
-  const handleAddVariable = () => {
-    console.log('add variable')
-  }
+
   return (
     <BaseNode>
       <div>
-        <Field
-          title={t(`${i18nPrefix}.model`)}
-        >
-          Model Selector
-        </Field>
-        <Field
-          title={t(`${i18nPrefix}.variables`)}
-          operations={
-            <AddButton onClick={handleAddVariable} />
-          }
-        >
-          Var Selector
-        </Field>
+        llm
       </div>
     </BaseNode>
   )
--- web/app/components/workflow/nodes/llm/panel.tsx
+++ web/app/components/workflow/nodes/llm/panel.tsx
@@ -1,10 +1,32 @@
 import type { FC } from 'react'
+import { useTranslation } from 'react-i18next'
 import BasePanel from '../_base/panel'
+import Field from '@/app/components/workflow/nodes/_base/components/field'
+import AddButton from '@/app/components/base/button/add-button'
+const i18nPrefix = 'workflow.nodes.llm'
 
 const Panel: FC = () => {
+  const { t } = useTranslation()
+  const handleAddVariable = () => {
+    console.log('add variable')
+  }
   return (
     <BasePanel
-      inputsElement={<div>start panel inputs</div>}
+      inputsElement={<div>
+        <Field
+          title={t(`${i18nPrefix}.model`)}
+        >
+          Model Selector
+        </Field>
+        <Field
+          title={t(`${i18nPrefix}.variables`)}
+          operations={
+            <AddButton onClick={handleAddVariable} />
+          }
+        >
+          Var Selector
+        </Field>
+      </div>}
       outputsElement={<div>start panel outputs</div>}
     />
   )
