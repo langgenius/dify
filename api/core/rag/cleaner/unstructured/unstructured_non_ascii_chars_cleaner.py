d541adc6fffadbdb51bc99b8f13c2cb01fd209ce
jyong <jyong@dify.ai>
2024-02-22 15:22:00 +0800

    rag

--- api/core/rag/cleaner/unstructured/unstructured_non_ascii_chars_cleaner.py
+++ api/core/rag/cleaner/unstructured/unstructured_non_ascii_chars_cleaner.py
@@ -1,11 +1,5 @@
 """Abstract interface for document clean implementations."""
-from typing import List, Iterator, Optional, Dict
-from app.models.document import Document
-from app.services.cleaner.cleaner_base import BaseCleaner
-from app.services.extractor.extractor_base import BaseExtractor
-from openpyxl.reader.excel import load_workbook
-
-from app.services.extractor.helpers import detect_file_encodings
+from core.rag.cleaner.cleaner_base import BaseCleaner
 
 
 class UnstructuredNonAsciiCharsCleaner(BaseCleaner):
