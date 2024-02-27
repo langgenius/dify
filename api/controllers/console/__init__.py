4d7b5dddacfd22ebddf465972995a094e72ad0e2
Bowen Liang <liangbowen@gf.com.cn>
2024-02-27 17:07:09 +0800

    apply ruff to __init__.py

--- api/controllers/console/__init__.py
+++ api/controllers/console/__init__.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+
 from libs.external_api import ExternalApi
 
 bp = Blueprint('console', __name__, url_prefix='/console/api')
@@ -6,16 +7,33 @@ api = ExternalApi(bp)
 
 # Import other controllers
 from . import admin, apikey, extension, feature, setup, version
+
 # Import app controllers
-from .app import (advanced_prompt_template, annotation, app, audio, completion, conversation, generator, message,
-                  model_config, site, statistic)
+from .app import (
+    advanced_prompt_template,
+    annotation,
+    app,
+    audio,
+    completion,
+    conversation,
+    generator,
+    message,
+    model_config,
+    site,
+    statistic,
+)
+
 # Import auth controllers
 from .auth import activate, data_source_oauth, login, oauth
+
 # Import billing controllers
 from .billing import billing
+
 # Import datasets controllers
 from .datasets import data_source, datasets, datasets_document, datasets_segments, file, hit_testing
+
 # Import explore controllers
 from .explore import audio, completion, conversation, installed_app, message, parameter, recommended_app, saved_message
+
 # Import workspace controllers
 from .workspace import account, members, model_providers, models, tool_providers, workspace
--- api/services/errors/__init__.py
+++ api/services/errors/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding:utf-8 -*-
 __all__ = [
     'base', 'conversation', 'message', 'index', 'app_model_config', 'account', 'document', 'dataset',
     'app', 'completion', 'audio', 'file'
