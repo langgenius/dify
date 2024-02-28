dd70aeff247be188c834e8af06efab3c0c0e61be
takatost <takatost@gmail.com>
2024-02-28 18:27:16 +0800

    lint fix

--- api/services/app_service.py
+++ api/services/app_service.py
@@ -7,7 +7,7 @@ import yaml
 from flask_sqlalchemy.pagination import Pagination
 
 from constants.model_template import default_app_templates
-from core.errors.error import ProviderTokenNotInitError, LLMBadRequestError
+from core.errors.error import LLMBadRequestError, ProviderTokenNotInitError
 from core.model_manager import ModelManager
 from core.model_runtime.entities.model_entities import ModelPropertyKey, ModelType
 from core.model_runtime.model_providers.__base.large_language_model import LargeLanguageModel
