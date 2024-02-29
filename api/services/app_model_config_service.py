be1500bf7d479cddd64bf0cc886db3f1e0eda990
takatost <takatost@gmail.com>
2024-02-29 22:20:31 +0800

    lint fix

--- api/services/app_model_config_service.py
+++ api/services/app_model_config_service.py
@@ -1,8 +1,6 @@
-from core.app.advanced_chat.config_validator import AdvancedChatAppConfigValidator
 from core.app.agent_chat.config_validator import AgentChatAppConfigValidator
 from core.app.chat.config_validator import ChatAppConfigValidator
 from core.app.completion.config_validator import CompletionAppConfigValidator
-from core.app.workflow.config_validator import WorkflowAppConfigValidator
 from models.model import AppMode
 
 
