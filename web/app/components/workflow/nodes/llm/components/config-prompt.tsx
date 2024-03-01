c2eaa320365804dfb59356f023369a8de30248e4
Joel <iamjoel007@gmail.com>
2024-03-01 17:09:53 +0800

    temp

---
+++ web/app/components/workflow/nodes/llm/components/config-prompt.tsx
@@ -0,0 +1,17 @@
+'use client'
+import type { FC } from 'react'
+import React from 'react'
+
+type Props = {
+  prompt: string
+}
+
+const ConfigPrompt: FC<Props> = ({
+  prompt,
+}) => {
+  return (
+    <div>
+    </div>
+  )
+}
+export default React.memo(ConfigPrompt)
