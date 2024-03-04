a806a0b448dc9fb3ac012d63e03c500ae63db47f
Bowen Liang <liangbowen@gf.com.cn>
2024-03-04 21:17:48 +0800

    update

--- api/extensions/ext_compress.py
+++ api/extensions/ext_compress.py
@@ -4,6 +4,6 @@ from flask import Flask
 def init_app(app: Flask):
     from flask_compress import Compress
 
-    if app.config['API_COMPRESSION_ENABLED']:
+    if app.config('API_COMPRESSION_ENABLED', False):
         compress = Compress()
         compress.init_app(app)
