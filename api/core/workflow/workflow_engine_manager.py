46296d777c4b8e7851ce291fda1b7b9ac653ed7f
takatost <takatost@gmail.com>
2024-03-07 10:09:23 +0800

    move funcs

--- api/core/workflow/workflow_engine_manager.py
+++ api/core/workflow/workflow_engine_manager.py
@@ -51,30 +51,6 @@ node_classes = {
 
 
 class WorkflowEngineManager:
-    def get_draft_workflow(self, app_model: App) -> Optional[Workflow]:
-        """
-        Get draft workflow
-        """
-        # fetch draft workflow by app_model
-        workflow = db.session.query(Workflow).filter(
-            Workflow.tenant_id == app_model.tenant_id,
-            Workflow.app_id == app_model.id,
-            Workflow.version == 'draft'
-        ).first()
-
-        # return draft workflow
-        return workflow
-
-    def get_published_workflow(self, app_model: App) -> Optional[Workflow]:
-        """
-        Get published workflow
-        """
-        if not app_model.workflow_id:
-            return None
-
-        # fetch published workflow by workflow_id
-        return self.get_workflow(app_model, app_model.workflow_id)
-
     def get_workflow(self, app_model: App, workflow_id: str) -> Optional[Workflow]:
         """
         Get workflow
@@ -404,7 +380,6 @@ class WorkflowEngineManager:
         :param max_execution_time: max execution time
         :return:
         """
-        # TODO check queue is stopped
         return time.perf_counter() - start_at > max_execution_time
 
     def _run_workflow_node(self, workflow_run_state: WorkflowRunState,
