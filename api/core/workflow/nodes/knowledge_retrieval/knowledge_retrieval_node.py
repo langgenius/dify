7372776992ac2fd2e5976be1a5396ad6503e06ea
jyong <jyong@dify.ai>
2024-03-11 20:06:38 +0800

    knowledge node

--- api/core/workflow/nodes/knowledge_retrieval/knowledge_retrieval_node.py
+++ api/core/workflow/nodes/knowledge_retrieval/knowledge_retrieval_node.py
@@ -1,5 +1,13 @@
+from core.workflow.entities.base_node_data_entities import BaseNodeData
+from core.workflow.entities.node_entities import NodeRunResult
+from core.workflow.entities.variable_pool import VariablePool
 from core.workflow.nodes.base_node import BaseNode
 
 
 class KnowledgeRetrievalNode(BaseNode):
-    pass
+    def _run(self, variable_pool: VariablePool) -> NodeRunResult:
+        pass
+
+    @classmethod
+    def _extract_variable_selector_to_variable_mapping(cls, node_data: BaseNodeData) -> dict[str, list[str]]:
+        pass
