1e47a06b784c1dc2a0ebce1cc600e071c499d14a
Laurent Magnien <laurent.magnien@adsn.fr>
2024-03-12 11:20:26 +0100

    init fr-FR

---
+++ web/i18n/fr-FR/app.ts
@@ -0,0 +1,54 @@
+const translation = {
+  createApp: 'Create new App',
+  types: {
+    all: 'All',
+    assistant: 'Assistant',
+    completion: 'Completion',
+  },
+  modes: {
+    completion: 'Text Generator',
+    chat: 'Basic Assistant',
+  },
+  createFromConfigFile: 'Create app from config file',
+  deleteAppConfirmTitle: 'Delete this app?',
+  deleteAppConfirmContent:
+    'Deleting the app is irreversible. Users will no longer be able to access your app, and all prompt configurations and logs will be permanently deleted.',
+  appDeleted: 'App deleted',
+  appDeleteFailed: 'Failed to delete app',
+  join: 'Join the community',
+  communityIntro:
+    'Discuss with team members, contributors and developers on different channels.',
+  roadmap: 'See our roadmap',
+  appNamePlaceholder: 'Please enter the name of the app',
+  newApp: {
+    startToCreate: 'Let\'s start with your new app',
+    captionName: 'App icon & name',
+    captionAppType: 'What type of app do you want to create?',
+    previewDemo: 'Preview demo',
+    chatApp: 'Assistant',
+    chatAppIntro:
+      'I want to build a chat-based application. This app uses a question-and-answer format, allowing for multiple rounds of continuous conversation.',
+    agentAssistant: 'New Agent Assistant',
+    completeApp: 'Text Generator',
+    completeAppIntro:
+      'I want to create an application that generates high-quality text based on prompts, such as generating articles, summaries, translations, and more.',
+    showTemplates: 'I want to choose from a template',
+    hideTemplates: 'Go back to mode selection',
+    Create: 'Create',
+    Cancel: 'Cancel',
+    nameNotEmpty: 'Name cannot be empty',
+    appTemplateNotSelected: 'Please select a template',
+    appTypeRequired: 'Please select an app type',
+    appCreated: 'App created',
+    appCreateFailed: 'Failed to create app',
+  },
+  editApp: {
+    startToEdit: 'Edit App',
+  },
+  emoji: {
+    ok: 'OK',
+    cancel: 'Cancel',
+  },
+}
+
+export default translation
