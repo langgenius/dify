660b5c39f7879494c221f383e7673dfbfeede5d7
crazywoola <427733928@qq.com>
2024-03-12 14:43:36 +0800

    fix: missing self

--- api/core/prompt/output_parser/suggested_questions_after_answer.py
+++ api/core/prompt/output_parser/suggested_questions_after_answer.py
@@ -12,7 +12,7 @@ class SuggestedQuestionsAfterAnswerOutputParser(BaseOutputParser):
     def get_format_instructions(self) -> str:
         return SUGGESTED_QUESTIONS_AFTER_ANSWER_INSTRUCTION_PROMPT
 
-    def parse(text: str) -> Any:
+    def parse(self, text: str) -> Any:
         action_match = re.search(r"\[.*?\]", text.strip(), re.DOTALL)
         if action_match is not None:
             json_obj = json.loads(action_match.group(0).strip())
