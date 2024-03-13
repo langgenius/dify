fc875cf4cc66e3a027ac12b90fc9d1d322a125ca
leslie2046 <253605712@qq.com>
2024-03-13 23:22:47 +0800

    fix

--- api/core/model_runtime/model_providers/azure_openai/_common.py
+++ api/core/model_runtime/model_providers/azure_openai/_common.py
@@ -9,15 +9,17 @@ from core.model_runtime.errors.invoke import (
     InvokeRateLimitError,
     InvokeServerUnavailableError,
 )
+from core.model_runtime.model_providers.azure_openai._constant import AZURE_OPENAI_API_VERSION
 
 
 class _CommonAzureOpenAI:
     @staticmethod
     def _to_credential_kwargs(credentials: dict) -> dict:
+        api_version = credentials.get('openai_api_version', AZURE_OPENAI_API_VERSION)
         credentials_kwargs = {
             "api_key": credentials['openai_api_key'],
             "azure_endpoint": credentials['openai_api_base'],
-            "api_version": credentials['openai_api_version'],
+            "api_version": api_version,
             "timeout": Timeout(315.0, read=300.0, write=10.0, connect=5.0),
             "max_retries": 1,
         }
