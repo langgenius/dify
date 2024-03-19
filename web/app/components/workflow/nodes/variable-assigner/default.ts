18159b1a4bd905e3199737cc08d6deaf31a77fe4
Joel <iamjoel007@gmail.com>
2024-03-19 18:03:37 +0800

    feat: valid assignes

--- web/app/components/workflow/nodes/variable-assigner/default.ts
+++ web/app/components/workflow/nodes/variable-assigner/default.ts
@@ -3,6 +3,8 @@ import { BlockEnum } from '../../types'
 import type { VariableAssignerNodeType } from './types'
 import { ALL_CHAT_AVAILABLE_BLOCKS, ALL_COMPLETION_AVAILABLE_BLOCKS } from '@/app/components/workflow/constants'
 
+const i18nPrefix = 'workflow'
+
 const nodeDefault: NodeDefault<VariableAssignerNodeType> = {
   defaultValue: {
     output_type: VarType.string,
@@ -18,15 +20,20 @@ const nodeDefault: NodeDefault<VariableAssignerNodeType> = {
     const nodes = isChatMode ? ALL_CHAT_AVAILABLE_BLOCKS : ALL_COMPLETION_AVAILABLE_BLOCKS
     return nodes
   },
-  checkValid(payload: VariableAssignerNodeType) {
-    let isValid = true
+  checkValid(payload: VariableAssignerNodeType, t: any) {
     let errorMessages = ''
-    if (payload.type) {
-      isValid = true
-      errorMessages = ''
+    const { variables } = payload
+    if (!variables || variables.length === 0)
+      errorMessages = t(`${i18nPrefix}.errorMsg.fieldRequired`, { field: t(`${i18nPrefix}.nodes.variableAssigner.title`) })
+    if (!errorMessages) {
+      variables.forEach((variable) => {
+        if (!variable || variable.length === 0)
+          errorMessages = t(`${i18nPrefix}.errorMsg.fieldRequired`, { field: t(`${i18nPrefix}.errorMsg.fields.variableValue`) })
+      })
     }
+
     return {
-      isValid,
+      isValid: !errorMessages,
       errorMessage: errorMessages,
     }
   },
