61f5de966236ebb239135c6c1e82021030ecaece
zxhlyh <jasonapring2015@outlook.com>
2024-03-26 16:19:41 +0800

    fix: chat scroll (#2981)

--- web/app/components/base/chat/chat/index.tsx
+++ web/app/components/base/chat/chat/index.tsx
@@ -9,7 +9,6 @@ import {
   useRef,
 } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useThrottleEffect } from 'ahooks'
 import { debounce } from 'lodash-es'
 import type {
   ChatConfig,
@@ -92,10 +91,19 @@ const Chat: FC<ChatProps> = ({
       chatFooterInnerRef.current.style.width = `${chatContainerInnerRef.current.clientWidth}px`
   }, [])
 
-  useThrottleEffect(() => {
+  useEffect(() => {
     handleScrolltoBottom()
     handleWindowResize()
-  }, [chatList], { wait: 500 })
+  }, [handleScrolltoBottom, handleWindowResize])
+
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      requestAnimationFrame(() => {
+        handleScrolltoBottom()
+        handleWindowResize()
+      })
+    }
+  })
 
   useEffect(() => {
     window.addEventListener('resize', debounce(handleWindowResize))
