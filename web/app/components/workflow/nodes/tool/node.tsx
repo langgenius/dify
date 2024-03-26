46f4e61edc6ec0541116a7f6a4b09ce48e65b307
Joel <iamjoel007@gmail.com>
2024-03-26 13:52:35 +0800

    fix: tools value too long  ui

--- web/app/components/workflow/nodes/tool/node.tsx
+++ web/app/components/workflow/nodes/tool/node.tsx
@@ -17,10 +17,10 @@ const Node: FC<NodeProps<ToolNodeType>> = ({
       <div className='space-y-0.5'>
         {toolConfigs.map((key, index) => (
           <div key={index} className='flex items-center h-6 justify-between bg-gray-100 rounded-md  px-1 space-x-1 text-xs font-normal text-gray-700'>
-            <div className='text-xs font-medium text-gray-500 uppercase'>
+            <div title={key} className='max-w-[100px] shrink-0 truncate text-xs font-medium text-gray-500 uppercase'>
               {key}
             </div>
-            <div className='text-xs font-normal text-gray-700'>
+            <div title={tool_configurations[key]} className='grow w-0 shrink-0 truncate text-right text-xs font-normal text-gray-700'>
               {tool_configurations[key]}
             </div>
           </div>
