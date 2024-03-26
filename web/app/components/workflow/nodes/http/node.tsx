1ac96564a0dc1be11a39365bd59c8504e4d694a0
Joel <iamjoel007@gmail.com>
2024-03-26 16:35:53 +0800

    feat: http node hightlight node

--- web/app/components/workflow/nodes/http/node.tsx
+++ web/app/components/workflow/nodes/http/node.tsx
@@ -2,6 +2,7 @@ import type { FC } from 'react'
 import React from 'react'
 import type { HttpNodeType } from './types'
 import type { NodeProps } from '@/app/components/workflow/types'
+import SupportVarInput from '@/app/components/workflow/nodes/_base/components/support-var-input'
 
 const Node: FC<NodeProps<HttpNodeType>> = ({
   data,
@@ -11,7 +12,12 @@ const Node: FC<NodeProps<HttpNodeType>> = ({
     <div className='mb-1 px-3 py-1'>
       <div className='flex items-center p-1 rounded-md bg-gray-100'>
         <div className='shrink-0 px-1 h-7 leading-7 rounded bg-gray-25 text-xs font-semibold text-gray-700 uppercase'>{method}</div>
-        <div className='ml-1 w-0 grow truncate text-xs font-normal text-gray-700'>{url}</div>
+        <SupportVarInput
+          wrapClassName='w-0 grow truncate flex items-center'
+          textClassName='ml-1 text-xs font-normal text-gray-700'
+          value={url}
+          readonly
+        />
       </div>
     </div>
   )
