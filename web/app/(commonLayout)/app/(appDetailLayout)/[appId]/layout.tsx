5d9eb04ec5da00ec2fb3908e774930f39774346c
weili20 <vccler@163.com>
2024-03-28 07:29:45 +0000

    change some texts

--- web/app/(commonLayout)/app/(appDetailLayout)/[appId]/layout.tsx
+++ web/app/(commonLayout)/app/(appDetailLayout)/[appId]/layout.tsx
@@ -58,7 +58,7 @@ const AppDetailLayout: FC<IAppDetailLayoutProps> = (props) => {
   })()
   useEffect(() => {
     if (response?.name)
-      document.title = `${(response.name || 'App')} - Dify`
+      document.title = `${(response.name || 'App')} - FinRegHub`
   }, [response])
   if (!response)
     return null
