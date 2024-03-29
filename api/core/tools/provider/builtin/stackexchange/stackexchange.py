240b6656186fa728a1e93a92aa0215089bb692ff
Richards Tu <142148415+richards199999@users.noreply.github.com>
2024-03-29 20:11:02 +0800

    Update Tool logic

--- api/core/tools/provider/builtin/stackexchange/stackexchange.py
+++ api/core/tools/provider/builtin/stackexchange/stackexchange.py
@@ -22,5 +22,21 @@ class StackExchangeProvider(BuiltinToolProviderController):
                     "pagesize": 1
                 },
             )
+            FetchAnsByStackExQuesIDTool().fork_tool_runtime(
+                meta={
+                    "credentials": credentials,
+                }
+            ).invoke(
+                user_id='',
+                tool_parameters={
+                    "id": "30375",
+                    "site": "tex",
+                    "filter": "!nNPvSNdWme",
+                    "order": "desc",
+                    "sort": "votes",
+                    "pagesize": 1,
+                    "page": 1
+                },
+            )
         except Exception as e:
             raise ToolProviderCredentialValidationError(str(e))
