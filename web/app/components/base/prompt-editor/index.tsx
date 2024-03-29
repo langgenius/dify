218f591a5df84bebf244aac9650e777097447079
zxhlyh <jasonapring2015@outlook.com>
2024-03-29 21:01:04 +0800

    fix: prompt editor linebreak (#3036)

--- web/app/components/base/prompt-editor/index.tsx
+++ web/app/components/base/prompt-editor/index.tsx
@@ -9,6 +9,7 @@ import {
   $getRoot,
   TextNode,
 } from 'lexical'
+import { CodeNode } from '@lexical/code'
 import { LexicalComposer } from '@lexical/react/LexicalComposer'
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin'
 import { ContentEditable } from '@lexical/react/LexicalContentEditable'
@@ -118,6 +119,7 @@ const PromptEditor: FC<PromptEditorProps> = ({
   const initialConfig = {
     namespace: 'prompt-editor',
     nodes: [
+      CodeNode,
       CustomTextNode,
       {
         replace: TextNode,
