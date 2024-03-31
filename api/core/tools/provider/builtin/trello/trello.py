6de1313d3a2de5735603e18c7eb7fafcd4e0d7f1
Yash_1124 <yashp3020@gmail.com>
2024-03-31 20:48:43 +0530

    ADD: add api validation logic by calling one tool and update help url of trello for api and token

--- api/core/tools/provider/builtin/trello/trello.py
+++ api/core/tools/provider/builtin/trello/trello.py
@@ -1,12 +1,22 @@
 from typing import Any
 
 from core.tools.errors import ToolProviderCredentialValidationError
+from core.tools.provider.builtin.trello.tools.fetch_all_boards import FetchAllBoardsTool
 from core.tools.provider.builtin_tool_provider import BuiltinToolProviderController
 
 
 class TrelloProvider(BuiltinToolProviderController):
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
         try:
-            pass
+            FetchAllBoardsTool().fork_tool_runtime(
+                meta={
+                    "credentials": credentials,
+                }
+            ).invoke(
+                user_id='',
+                tool_parameters={
+                    "boards": "open",
+                },
+            )
         except Exception as e:
             raise ToolProviderCredentialValidationError(str(e))
