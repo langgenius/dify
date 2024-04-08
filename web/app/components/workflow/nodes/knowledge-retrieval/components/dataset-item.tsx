ea1fdf72c784aa3d3fe8ec2b963bc784c60a5529
Joel <iamjoel007@gmail.com>
2024-04-08 15:28:10 +0800

    chore: handle some dataset type may not show file icon

--- web/app/components/workflow/nodes/knowledge-retrieval/components/dataset-item.tsx
+++ web/app/components/workflow/nodes/knowledge-retrieval/components/dataset-item.tsx
@@ -41,18 +41,15 @@ const DatasetItem: FC<Props> = ({
     <div className='flex items-center h-10 justify-between rounded-xl px-2 bg-white border border-gray-200  cursor-pointer group/dataset-item'>
       <div className='w-0 grow flex items-center space-x-1.5'>
         {
-          payload.data_source_type === DataSourceType.FILE && (
-            <div className='shrink-0 flex items-center justify-center w-6 h-6 bg-[#F5F8FF] rounded-md border-[0.5px] border-[#E0EAFF]'>
+          payload.data_source_type === DataSourceType.NOTION
+            ? (
+              <div className='shrink-0 flex items-center justify-center w-6 h-6 rounded-md border-[0.5px] border-[#EAECF5]'>
+                <FileIcon type='notion' className='w-4 h-4' />
+              </div>
+            )
+            : <div className='shrink-0 flex items-center justify-center w-6 h-6 bg-[#F5F8FF] rounded-md border-[0.5px] border-[#E0EAFF]'>
               <Folder className='w-4 h-4 text-[#444CE7]' />
             </div>
-          )
-        }
-        {
-          payload.data_source_type === DataSourceType.NOTION && (
-            <div className='shrink-0 flex items-center justify-center w-6 h-6 rounded-md border-[0.5px] border-[#EAECF5]'>
-              <FileIcon type='notion' className='w-4 h-4' />
-            </div>
-          )
         }
         <div className='w-0 grow text-[13px] font-normal text-gray-800 truncate'>{payload.name}</div>
       </div>
