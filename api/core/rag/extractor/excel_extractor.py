03194c61ea3f95ba8267a0c018d858a66301be78
jyong <jyong@dify.ai>
2024-04-08 16:01:51 +0800

    csv and excel extract optimize

--- api/core/rag/extractor/excel_extractor.py
+++ api/core/rag/extractor/excel_extractor.py
@@ -2,7 +2,6 @@
 from typing import Optional
 
 import pandas as pd
-from openpyxl.reader.excel import load_workbook
 
 from core.rag.extractor.extractor_base import BaseExtractor
 from core.rag.models.document import Document
