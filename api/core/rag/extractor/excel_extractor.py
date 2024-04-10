103d510fd52f604cbc628e333c2695cc82578412
陈旭 <chenxu9741@cvte.com>
2024-04-10 18:12:37 +0800

    feat:Add parsing support for xls files

--- api/core/rag/extractor/excel_extractor.py
+++ api/core/rag/extractor/excel_extractor.py
@@ -2,7 +2,7 @@
 from typing import Optional
 
 import pandas as pd
-
+import xlrd
 from core.rag.extractor.extractor_base import BaseExtractor
 from core.rag.models.document import Document
 
@@ -27,9 +27,36 @@ class ExcelExtractor(BaseExtractor):
         self._autodetect_encoding = autodetect_encoding
 
     def extract(self) -> list[Document]:
+        """ parse excel file"""
+        if self._file_path.endswith('.xls'):
+            return self._extract4xls()
+        elif self._file_path.endswith('.xlsx'):
+            return self._extract4xlsx()
+
+    def _extract4xls(self) -> list[Document]:
+        wb = xlrd.open_workbook(filename=self._file_path)
+        row_header = None
+        documents = []
+        # loop over all sheets
+        for sheet in wb.sheets():
+            for row_index, row in enumerate(sheet.get_rows(), start=1):
+                if self.is_blank_row(row):
+                    continue
+                if row_header is None:
+                    row_header = row
+                    continue
+                item_arr = []
+                for index, cell in enumerate(row):
+                    txt_value = str(cell.value)
+                    item_arr.append(f'{row_header[index].value}:{txt_value}')
+                item_str = "\n".join(item_arr)
+                document = Document(page_content=item_str, metadata={'source': self._file_path})
+                documents.append(document)
+        return documents
+
+    def _extract4xlsx(self) -> list[Document]:
         """Load from file path using Pandas."""
         data = []
-
         # 使用 Pandas 读取 Excel 文件的每个工作表
         xls = pd.ExcelFile(self._file_path)
         for sheet_name in xls.sheet_names:
@@ -43,5 +70,18 @@ class ExcelExtractor(BaseExtractor):
                 item = ';'.join(f'{k}:{v}' for k, v in row.items() if pd.notna(v))
                 document = Document(page_content=item, metadata={'source': self._file_path})
                 data.append(document)
-
         return data
+
+    @staticmethod
+    def is_blank_row(row):
+        """
+        判断指定行是否为空白行。
+        :param row: 行对象。
+        :return: 如果行是空白的，则返回True，否则返回False。
+        """
+        # 遍历单元格，如果发现非空单元格，则返回False
+        for cell in row:
+            if cell.value is not None and cell.value != '':
+                return False
+        # 如果所有单元格都是空的，则返回True
+        return True
