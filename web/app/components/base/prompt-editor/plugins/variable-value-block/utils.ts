00c55231b148f4a10ca35a4b79873a6a11f03d86
nite-knite <nkCoding@gmail.com>
2024-04-10 15:51:51 +0800

    chore: fix security alerts on braces escape

--- web/app/components/base/prompt-editor/plugins/variable-value-block/utils.ts
+++ web/app/components/base/prompt-editor/plugins/variable-value-block/utils.ts
@@ -1,5 +1,5 @@
 export function getHashtagRegexString(): string {
-  const hashtag = '(\{)(\{)([a-zA-Z_][a-zA-Z0-9_]{0,29})(\})(\})'
+  const hashtag = '\\{\\{[a-zA-Z_][a-zA-Z0-9_]{0,29}\\}\\}'
 
   return hashtag
 }
