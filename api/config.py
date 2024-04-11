6fa0e4072dab27a456f27de4e3ecc5adf0832e16
takatost <takatost@users.noreply.github.com>
2024-04-12 00:04:09 +0800

    fix: yarn install extract package err when using GitHub Cache in amd6… (#3383)

--- api/config.py
+++ api/config.py
@@ -99,7 +99,7 @@ class Config:
         # ------------------------
         # General Configurations.
         # ------------------------
-        self.CURRENT_VERSION = "0.6.2-fix1"
+        self.CURRENT_VERSION = "0.6.2"
         self.COMMIT_SHA = get_env('COMMIT_SHA')
         self.EDITION = "SELF_HOSTED"
         self.DEPLOY_ENV = get_env('DEPLOY_ENV')
