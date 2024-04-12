c227f3d98566041f0c0a9820d87b0a7a445a2e7f
LIU HONGWEI <1327374483@qq.com>
2024-04-12 16:22:24 +0800

    feat: Deprecate datetime.utcnow() in favor of datetime.now(timezone.utc).replace(tzinfo=None) for better timezone handling (#3408) (#3416)

--- api/tasks/document_indexing_task.py
+++ api/tasks/document_indexing_task.py
@@ -47,7 +47,7 @@ def document_indexing_task(dataset_id: str, document_ids: list):
             if document:
                 document.indexing_status = 'error'
                 document.error = str(e)
-                document.stopped_at = datetime.datetime.utcnow()
+                document.stopped_at = datetime.datetime.now(datetime.timezone.utc).replace(tzinfo=None)
                 db.session.add(document)
         db.session.commit()
         return
@@ -62,7 +62,7 @@ def document_indexing_task(dataset_id: str, document_ids: list):
 
         if document:
             document.indexing_status = 'parsing'
-            document.processing_started_at = datetime.datetime.utcnow()
+            document.processing_started_at = datetime.datetime.now(datetime.timezone.utc).replace(tzinfo=None)
             documents.append(document)
             db.session.add(document)
     db.session.commit()
