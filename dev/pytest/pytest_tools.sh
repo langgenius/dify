8c437556e831b8e8238b65cb32fe14326d8ec6f4
Bowen Liang <liangbowen@gf.com.cn>
2024-04-15 21:34:56 +0800

    update

--- dev/pytest/pytest_tools.sh
+++ dev/pytest/pytest_tools.sh
@@ -2,8 +2,4 @@
 
 set -x
 
-pytest api/tests/integration_tests/model_runtime/anthropic \
-  api/tests/integration_tests/model_runtime/azure_openai \
-  api/tests/integration_tests/model_runtime/openai api/tests/integration_tests/model_runtime/chatglm \
-  api/tests/integration_tests/model_runtime/google api/tests/integration_tests/model_runtime/xinference \
-  api/tests/integration_tests/model_runtime/huggingface_hub/test_llm.py
+pytest api/tests/integration_tests/tools/test_all_provider.py
