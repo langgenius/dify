e619f8aaf2db45ed2b689143cd944ec0e528d9ad
Bowen Liang <liangbowen@gf.com.cn>
2024-04-17 14:34:43 +0800

    skip sh

--- dev/pytest/pytest_all_tests.sh
+++ dev/pytest/pytest_all_tests.sh
@@ -2,10 +2,10 @@
 set -x
 
 # ModelRuntime
-sh dev/pytest/pytest_model_runtime.sh
+dev/pytest/pytest_model_runtime.sh
 
 # Tools
-sh dev/pytest/pytest_tools.sh
+dev/pytest/pytest_tools.sh
 
 # Workflow
-sh dev/pytest/pytest_workflow.sh
+dev/pytest/pytest_workflow.sh
