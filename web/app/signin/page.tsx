4481906be20319ff9415773f2d5b2b97a77f37b0
Garfield Dai <dai.hai@foxmail.com>
2024-04-18 17:33:32 +0800

    Feat/enterprise sso (#3602)

--- web/app/signin/page.tsx
+++ web/app/signin/page.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import cn from 'classnames'
 import Script from 'next/script'
+import Loading from '../components/base/loading'
 import Forms from './forms'
 import Header from './_header'
 import style from './page.module.css'
+import EnterpriseSSOForm from './enterpriseSSOForm'
 import { IS_CE_EDITION } from '@/config'
+import { getEnterpriseFeatures } from '@/service/enterprise'
+import type { EnterpriseFeatures } from '@/types/enterprise'
+import { defaultEnterpriseFeatures } from '@/types/enterprise'
 
 const SignIn = () => {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [enterpriseFeatures, setEnterpriseFeatures] = useState<EnterpriseFeatures>(defaultEnterpriseFeatures)
+
+  useEffect(() => {
+    getEnterpriseFeatures().then((res) => {
+      setEnterpriseFeatures(res)
+    }).finally(() => {
+      setLoading(false)
+    })
+  }, [])
+
   return (
     <>
       {!IS_CE_EDITION && (
@@ -40,10 +57,31 @@ gtag('config', 'AW-11217955271"');
           )
         }>
           <Header />
-          <Forms />
-          <div className='px-8 py-6 text-sm font-normal text-gray-500'>
-            © {new Date().getFullYear()} LangGenius, Inc. All rights reserved.
-          </div>
+
+          {loading && (
+            <div className={
+              cn(
+                'flex flex-col items-center w-full grow items-center justify-center',
+                'px-6',
+                'md:px-[108px]',
+              )
+            }>
+              <Loading type='area' />
+            </div>
+          )}
+
+          {!loading && !enterpriseFeatures.sso_enforced_for_signin && (
+            <>
+              <Forms />
+              <div className='px-8 py-6 text-sm font-normal text-gray-500'>
+                © {new Date().getFullYear()} LangGenius, Inc. All rights reserved.
+              </div>
+            </>
+          )}
+
+          {!loading && enterpriseFeatures.sso_enforced_for_signin && (
+            <EnterpriseSSOForm protocol={enterpriseFeatures.sso_enforced_for_signin_protocol} />
+          )}
         </div>
 
       </div>
