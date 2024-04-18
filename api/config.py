f43072dadda9f6eff25b68a5a9abc190a3ce7bee
liuzhenghua-jk <liuzhenghua-jk@360shuke.com>
2024-04-18 20:06:37 +0800

    feat: add file log

--- api/config.py
+++ api/config.py
@@ -39,7 +39,7 @@ DEFAULTS = {
     'CELERY_BACKEND': 'database',
     'LOG_LEVEL': 'INFO',
     'LOG_FILE': 'logs/app.log',
-    'LOG_FORMAT': '%(asctime)s %(levelname)s [%(threadName)s] [] [%(filename)s:%(lineno)d] - %(message)s',
+    'LOG_FORMAT': '%(asctime)s %(levelname)s [%(threadName)s] [%(filename)s:%(lineno)d] - %(message)s',
     'HOSTED_OPENAI_QUOTA_LIMIT': 200,
     'HOSTED_OPENAI_TRIAL_ENABLED': 'False',
     'HOSTED_OPENAI_TRIAL_MODELS': 'gpt-3.5-turbo,gpt-3.5-turbo-1106,gpt-3.5-turbo-instruct,gpt-3.5-turbo-16k,gpt-3.5-turbo-16k-0613,gpt-3.5-turbo-0613,gpt-3.5-turbo-0125,text-davinci-003',
