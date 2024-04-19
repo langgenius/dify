caec3aa804ee4a207487b47cc66e54c94b172c72
Joshua <jeroen20211101@gmail.com>
2024-04-19 13:35:43 +0800

    temp

--- api/core/model_runtime/model_providers/leptonai/llm/llm.py
+++ api/core/model_runtime/model_providers/leptonai/llm/llm.py
@@ -15,15 +15,15 @@ class LeptonAILargeLanguageModel(OAIAPICompatLargeLanguageModel):
                 tools: Optional[list[PromptMessageTool]] = None, stop: Optional[list[str]] = None,
                 stream: bool = True, user: Optional[str] = None) \
             -> Union[LLMResult, Generator]:
-        self._add_custom_parameters(credentials)
+        self._add_custom_parameters(credentials, model)
         return super()._invoke(model, credentials, prompt_messages, model_parameters, tools, stop, stream)
 
     def validate_credentials(self, model: str, credentials: dict) -> None:
-        self._add_custom_parameters(credentials)
+        self._add_custom_parameters(credentials, model)
         super().validate_credentials(model, credentials)
 
-@classmethod
-def _add_custom_parameters(cls, credentials: dict, model: str) -> None:
-    credentials['mode'] = 'chat'
-    credentials['endpoint_url'] = f'https://{cls.MODEL_SUFFIX_MAP[model]}.lepton.run/api/v1'
-    
+    @classmethod
+    def _add_custom_parameters(cls, credentials: dict, model: str) -> None:
+        credentials['mode'] = 'chat'
+        credentials['endpoint_url'] = f'https://{cls.MODEL_SUFFIX_MAP[model]}.lepton.run/api/v1'
+        
