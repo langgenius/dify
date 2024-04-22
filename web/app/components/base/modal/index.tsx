36bccffdf0eb5f8c986745dcb97b7891a975377e
crazywoola <427733928@qq.com>
2024-04-22 19:31:54 +0800

    fix: lint

--- web/app/components/base/modal/index.tsx
+++ web/app/components/base/modal/index.tsx
@@ -41,11 +41,11 @@ export default function Modal({
           <div className="fixed inset-0 bg-black bg-opacity-25" />
         </Transition.Child>
 
-        <div 
+        <div
           className="fixed inset-0 overflow-y-auto"
           onClick={(e) => {
             e.preventDefault()
-            e.stopPropagation();
+            e.stopPropagation()
           }}
         >
           <div className="flex min-h-full items-center justify-center p-4 text-center">
