9fda3fab4d09a4ecad8f20945263aac22bb3345a
Bowen Liang <liangbowen@gf.com.cn>
2024-04-22 23:37:51 +0800

    vdb tests

--- api/tests/unit_tests/core/rag/datasource/vdb/milvus/test_milvus.py
+++ api/tests/unit_tests/core/rag/datasource/vdb/milvus/test_milvus.py
@@ -1,7 +1,15 @@
+import uuid
+
 import pytest
 from pydantic.error_wrappers import ValidationError
 
-from core.rag.datasource.vdb.milvus.milvus_vector import MilvusConfig
+from core.rag.datasource.vdb.milvus.milvus_vector import MilvusConfig, MilvusVector
+from models.dataset import Dataset
+from tests.unit_tests.core.rag.datasource.vdb.test_vector_store import (
+    get_sample_document,
+    get_sample_embedding,
+    setup_mock_redis,
+)
 
 
 def test_default_value():
@@ -18,7 +26,35 @@ def test_default_value():
         with pytest.raises(ValidationError) as e:
             MilvusConfig(**config)
         assert e.value.errors()[1]['msg'] == f'config MILVUS_{key.upper()} is required'
-    
+
     config = MilvusConfig(**valid_config)
     assert config.secure is False
     assert config.database == 'default'
+
+
+def test_milvus_vector(setup_mock_redis) -> None:
+    dataset_id = str(uuid.uuid4())
+    vector = MilvusVector(
+        collection_name=Dataset.gen_collection_name_by_id(dataset_id),
+        config=MilvusConfig(
+            host='localhost',
+            port=19530,
+            user='root',
+            password='Milvus',
+        )
+    )
+
+    # create vector
+    vector.create(
+        texts=[get_sample_document(dataset_id)],
+        embeddings=[get_sample_embedding()],
+    )
+
+    # search by vector
+    # hits_by_vector = vector.search_by_vector(query_vector=_get_sample_query_vector())
+    # assert len(hits_by_vector) >= 1
+
+    # milvus dos not support full text searching yet in < 2.3.x
+
+    # delete vector
+    vector.delete()
---
+++ dev/pytest/pytest_vdb.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -x
+
+pytest api/tests/unit_tests/core/rag/datasource/vdb/
