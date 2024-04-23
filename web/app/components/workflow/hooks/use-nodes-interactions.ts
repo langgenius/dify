39ae56e136c2bab219a64753260172330e3085f2
zxhlyh <jasonapring2015@outlook.com>
2024-04-23 18:02:15 +0800

    fix: workflow connection (#3713)

--- web/app/components/workflow/hooks/use-nodes-interactions.ts
+++ web/app/components/workflow/hooks/use-nodes-interactions.ts
@@ -324,18 +324,12 @@ export const useNodesInteractions = () => {
         return
     }
     const needDeleteEdges = edges.filter((edge) => {
-      if (edge.source === source) {
-        if (edge.sourceHandle)
-          return edge.sourceHandle === sourceHandle
-        else
-          return true
-      }
-      if (edge.target === target) {
-        if (edge.targetHandle)
-          return edge.targetHandle === targetHandle
-        else
-          return true
-      }
+      if (
+        (edge.source === source && edge.sourceHandle === sourceHandle)
+        || (edge.target === target && edge.targetHandle === targetHandle)
+      )
+        return true
+
       return false
     })
     const needDeleteEdgesIds = needDeleteEdges.map(edge => edge.id)
