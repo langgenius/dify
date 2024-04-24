71516fef6de811e9bde43246a5159eb239890fb6
Bowen Liang <liangbowen@gf.com.cn>
2024-04-24 18:07:39 +0800

    update

--- api/tests/unit_tests/models/test_account.py
+++ api/tests/unit_tests/models/test_account.py
@@ -2,6 +2,11 @@ from models.account import AccountRole
 
 
 def test_account_is_privileged_role() -> None:
-    assert AccountRole.is_privileged_role(AccountRole.ADMIN.value)
-    assert AccountRole.is_privileged_role(AccountRole.OWNER.value)
-    assert not AccountRole.is_privileged_role(AccountRole.NORMAL.value)
+    assert AccountRole.ADMIN == 'admin'
+    assert AccountRole.OWNER == 'owner'
+    assert AccountRole.NORMAL == 'normal'
+
+    assert AccountRole.is_privileged_role(AccountRole.ADMIN)
+    assert AccountRole.is_privileged_role(AccountRole.OWNER)
+    assert not AccountRole.is_privileged_role(AccountRole.NORMAL)
+    assert not AccountRole.is_privileged_role('')
