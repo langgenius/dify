3338fe335578b51bbda8751f28df8b54b46819b6
jyong <718720800@qq.com>
2024-04-24 14:16:14 +0800

    dev/reformat

--- api/services/dataset_service.py
+++ api/services/dataset_service.py
@@ -616,7 +616,7 @@ class DocumentService:
                         "upload_file_id": file_id,
                     }
                     # check duplicate
-                    if document_data.get('duplicate', True):
+                    if document_data.get('duplicate', False):
                         document = Document.query.filter_by(
                             dataset_id=dataset.id,
                             tenant_id=current_user.current_tenant_id,
