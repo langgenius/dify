99ac3549ef18e5575258385ee1ac4bea16320e24
Richards Tu <142148415+richards199999@users.noreply.github.com>
2024-04-25 20:28:31 +0800

    Update tool configuration

--- api/core/tools/provider/builtin/firecrawl/tools/crawl.py
+++ api/core/tools/provider/builtin/firecrawl/tools/crawl.py
@@ -14,15 +14,23 @@ class CrawlTool(BuiltinTool):
         app = FirecrawlApp(api_key=self.runtime.credentials['firecrawl_api_key'])
 
         # crawl the url
-        crawl_result = app.crawl_url(url, params=tool_parameters, wait_until_done=True, timeout=5)
+        crawl_result = app.crawl_url(url=tool_parameters['url'], params=tool_parameters['crawlOptions'+'pageOptions'], wait_until_done=True, timeout=60)
         
         # reformat crawl result
         crawl_output = "**Crawl Result**\n\n"
-        for result in crawl_result:
-            crawl_output += f"**Title:** {result['metadata']['title']}\n"
-            crawl_output += f"**Description:** {result['metadata']['description']}\n"
-            crawl_output += f"**URL:** {result['metadata']['ogUrl']}\n\n"
-            crawl_output += f"**Web Content:**\n{result['markdown']}\n\n"
+        try:
+            for result in crawl_result:
+                crawl_output += f"**- Title:** {result['metadata']['title']}\n"
+                crawl_output += f"**- Description:** {result['metadata']['description']}\n"
+                crawl_output += f"**- URL:** {result['metadata']['ogUrl']}\n\n"
+                crawl_output += f"**- Web Content:**\n{result['markdown']}\n\n"
+                crawl_output += "---\n\n"
+        except Exception as e:
+            crawl_output += f"An error occurred: {str(e)}\n"
+            crawl_output += f"**- Title:** {result['metadata']['title']}\n"
+            crawl_output += f"**- Description:** {result['metadata']['description']}\n"
+            crawl_output += f"**- URL:** {result['metadata']['ogUrl']}\n\n"
+            crawl_output += f"**- Web Content:**\n{result['markdown']}\n\n"
             crawl_output += "---\n\n"
 
 
