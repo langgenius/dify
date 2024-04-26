5a5720537cb260f92e1a4112da79e7c24ca97b76
jyong <718720800@qq.com>
2024-04-26 19:29:14 +0800

    add pgvecto-rs testing

--- api/tests/integration_tests/vdb/pgvecto_rs/test_pgvecto_rs.py
+++ api/tests/integration_tests/vdb/pgvecto_rs/test_pgvecto_rs.py
@@ -1,11 +1,9 @@
-from core.rag.datasource.vdb.milvus.milvus_vector import MilvusConfig, MilvusVector
 from core.rag.datasource.vdb.pgvecto_rs.pgvecto_rs import PgvectoRSConfig, PGVectoRS
 from tests.integration_tests.vdb.test_vector_store import (
     AbstractTestVector,
-    get_sample_text,
+    get_example_text,
     setup_mock_redis,
 )
-
 
 class TestPgvectoRSVector(AbstractTestVector):
     def __init__(self):
@@ -24,9 +22,9 @@ class TestPgvectoRSVector(AbstractTestVector):
 
     def search_by_full_text(self):
         # pgvecto rs only support english text search, So it’s not open for now
-        hits_by_full_text = self.vector.search_by_full_text(query=get_sample_text())
+        hits_by_full_text = self.vector.search_by_full_text(query=get_example_text())
         assert len(hits_by_full_text) == 0
 
 
-def test_milvus_vector(setup_mock_redis):
-    TestPgvectoRSVector().run_all_test()
+def test_pgvecot_rs(setup_mock_redis):
+    TestPgvectoRSVector().run_all_tests()
