5f6b4a649a3e5cf208156c014f8af66528f7fbc6
Bowen Liang <liangbowen@gf.com.cn>
2024-04-28 21:11:47 +0800

    Revert "append"
    This reverts commit 6ae8880b123db126fda8bf446b9b2da5691c3177.

--- dev/pytest/pytest_unit_tests.sh
+++ dev/pytest/pytest_unit_tests.sh
@@ -2,4 +2,4 @@
 set -x
 
 # libs
-coverage run -m pytest -a api/tests/unit_tests
+coverage run -m pytest api/tests/unit_tests
--- dev/pytest/pytest_workflow.sh
+++ dev/pytest/pytest_workflow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 
-coverage run -m pytest -a api/tests/integration_tests/workflow
+coverage run -m pytest api/tests/integration_tests/workflow
