88ec613c41242144e24d87877598fadf1c2f7739
Yeuoly <admin@srmxy.cn>
2024-04-30 21:47:38 +0800

    feat: parameter extractor

--- api/core/workflow/nodes/parameter_extractor/parameter_extractor_node.py
+++ api/core/workflow/nodes/parameter_extractor/parameter_extractor_node.py
@@ -1,3 +1,4 @@
+from core.model_runtime.entities.message_entities import PromptMessage, PromptMessageTool
 from core.workflow.entities.node_entities import NodeRunResult, NodeType
 from core.workflow.entities.variable_pool import VariablePool
 from core.workflow.nodes.base_node import BaseNode
@@ -14,4 +15,42 @@ class ParameterExtractorNode(BaseNode):
     def _run(self, variable_pool: VariablePool) -> NodeRunResult:
         """
         Run the node.
+        """
+
+    def _generate_function_call_prompt(self, 
+        data: ParameterExtractorNodeData
+    ) -> tuple[list[PromptMessage], list[PromptMessageTool]]:
+        """
+        Generate function call prompt.
+        """
+
+    def _generate_prompt_engineering_prompt(self, 
+        data: ParameterExtractorNodeData
+    ) -> list[PromptMessage]:
+        """
+        Generate prompt engineering prompt.
+        """
+
+    def _generate_prompt_engineering_completion_prompt(self,
+        data: ParameterExtractorNodeData
+    ) -> list[PromptMessage]:
+        """
+        Generate completion prompt.
+        """
+
+    def _generate_prompt_engineering_chat_prompt(self,
+        data: ParameterExtractorNodeData
+    ) -> list[PromptMessage]:
+        """
+        Generate chat prompt.
+        """
+
+    def _validate_result(self, data: ParameterExtractorNodeData, result: dict) -> dict:
+        """
+        Validate result.
+        """
+
+    def _extract_complete_json_response(self, result: str) -> dict:
+        """
+        Extract complete json response.
         """
