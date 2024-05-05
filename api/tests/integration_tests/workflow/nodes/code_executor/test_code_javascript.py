6729425d941edfc8749749f6f9c0414b2f02eca3
Bowen Liang <liangbowen@gf.com.cn>
2024-05-05 23:41:55 +0800

    update

--- api/tests/integration_tests/workflow/nodes/code_executor/test_code_javascript.py
+++ api/tests/integration_tests/workflow/nodes/code_executor/test_code_javascript.py
@@ -1,6 +1,7 @@
-from core.helper.code_executor.code_executor import CodeExecutor
+from core.helper.code_executor.code_executor import CodeExecutor, CodeLanguage
+from core.workflow.nodes.code.code_node import JAVASCRIPT_DEFAULT_CODE
 
-CODE_LANGUAGE = 'javascript'
+CODE_LANGUAGE = CodeLanguage.JAVASCRIPT
 
 
 def test_javascript_plain():
@@ -16,3 +17,9 @@ console.log(JSON.stringify(obj))
     """
     result = CodeExecutor.execute_code(language=CODE_LANGUAGE, preload='', code=code)
     assert result == '{"Hello":"World"}\n'
+
+
+def test_javascript_with_code_template():
+    result = CodeExecutor.execute_workflow_code_template(
+        language=CODE_LANGUAGE, code=JAVASCRIPT_DEFAULT_CODE, inputs={'arg1': 'Hello', 'arg2': 'World'})
+    assert result == {'result': 'HelloWorld'}
