b3ffb092c71b55af3165dc88140a7ea7d6003685
TinsFox <fox@tinsfox.com>
2024-05-08 19:46:23 +0800

    chore: use prettier to format code

---
+++ web/.prettierrc.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+const config = {
+  singleQuote: true,
+  jsxSingleQuote: false,
+  semi: false,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  quoteProps: 'consistent',
+  bracketSpacing: true,
+  printWidth: 120,
+};
+
+module.exports = config;
