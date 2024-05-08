cad36403e76ec0c0e123362a074174848c2c73bf
miendinh <miendinh@users.noreply.github.com>
2024-05-08 11:48:56 +0700

    rollback ci

--- dev/pytest/pytest_vdb.sh
+++ dev/pytest/pytest_vdb.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -x
 
-docker ps
-
-curl localhost:9200/_cat/health
-
-curl 127.0.0.1:9200/_cat/health
-
-pytest api/tests/integration_tests/vdb/elasticsearch
+pytest api/tests/integration_tests/vdb/
