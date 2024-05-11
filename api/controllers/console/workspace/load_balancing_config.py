19cd0174cbcef860e3ef413ce872f616bb6d1269
takatost <takatost@gmail.com>
2024-05-11 19:42:48 +0800

    add lb config list

--- api/controllers/console/workspace/load_balancing_config.py
+++ api/controllers/console/workspace/load_balancing_config.py
@@ -1,10 +1,11 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource, marshal_with, reqparse
 from werkzeug.exceptions import Forbidden
 
 from controllers.console import api
 from controllers.console.setup import setup_required
 from controllers.console.wraps import account_initialization_required
 from core.model_runtime.entities.model_entities import ModelType
+from fields.load_balancing_config import load_balancing_config_list_fields
 from libs.login import current_user, login_required
 from models.account import TenantAccountRole
 from services.model_load_balancing_service import ModelLoadBalancingService
@@ -65,6 +66,7 @@ class LoadBalancingConfigListApi(Resource):
     @setup_required
     @login_required
     @account_initialization_required
+    @marshal_with(load_balancing_config_list_fields)
     def get(self, provider: str):
         tenant_id = current_user.current_tenant_id
 
@@ -74,9 +76,17 @@ class LoadBalancingConfigListApi(Resource):
                             choices=[mt.value for mt in ModelType], location='args')
         args = parser.parse_args()
 
-        # TODO
+        model_load_balancing_service = ModelLoadBalancingService()
+        load_balancing_configs = model_load_balancing_service.get_load_balancing_configs(
+            tenant_id=tenant_id,
+            provider=provider,
+            model=args['model'],
+            model_type=args['model_type']
+        )
 
-        return {}
+        return {
+            'data': load_balancing_configs
+        }
 
     @setup_required
     @login_required
--- api/services/model_load_balancing_service.py
+++ api/services/model_load_balancing_service.py
@@ -2,6 +2,8 @@ import logging
 
 from core.model_runtime.entities.model_entities import ModelType
 from core.provider_manager import ProviderManager
+from extensions.ext_database import db
+from models.provider import LoadBalancingModelConfig
 
 logger = logging.getLogger(__name__)
 
@@ -59,3 +61,71 @@ class ModelLoadBalancingService:
             model_type=ModelType.value_of(model_type)
         )
 
+    def get_load_balancing_configs(self, tenant_id: str, provider: str, model: str, model_type: str) \
+            -> list[LoadBalancingModelConfig]:
+        """
+        Get load balancing configurations.
+        :param tenant_id: workspace id
+        :param provider: provider name
+        :param model: model name
+        :param model_type: model type
+        :return:
+        """
+        # Get all provider configurations of the current workspace
+        provider_configurations = self.provider_manager.get_configurations(tenant_id)
+
+        # Get provider configuration
+        provider_configuration = provider_configurations.get(provider)
+        if not provider_configuration:
+            raise ValueError(f"Provider {provider} does not exist.")
+
+        # Convert model type to ModelType
+        model_type = ModelType.value_of(model_type)
+
+        # Get load balancing configurations
+        load_balancing_configs = db.session.query(LoadBalancingModelConfig) \
+            .filter(
+            LoadBalancingModelConfig.tenant_id == tenant_id,
+            LoadBalancingModelConfig.provider_name == provider_configuration.provider.provider,
+            LoadBalancingModelConfig.model_type == model_type.to_origin_model_type(),
+            LoadBalancingModelConfig.model_name == model
+        ).order_by(LoadBalancingModelConfig.created_at).all()
+
+        # check if the inherit configuration exists, inherit is represented for the provider or model custom credentials
+        inherit_config_exists = False
+        for load_balancing_config in load_balancing_configs:
+            if load_balancing_config.name == '__inherit__':
+                inherit_config_exists = True
+                break
+
+        if not inherit_config_exists:
+            # Initialize the inherit configuration
+            inherit_config = self._init_inherit_config(tenant_id, provider, model, model_type)
+
+            # prepend the inherit configuration
+            load_balancing_configs.insert(0, inherit_config)
+
+        return load_balancing_configs
+
+    def _init_inherit_config(self, tenant_id: str, provider: str, model: str, model_type: ModelType) \
+            -> LoadBalancingModelConfig:
+        """
+        Initialize the inherit configuration.
+        :param tenant_id: workspace id
+        :param provider: provider name
+        :param model: model name
+        :param model_type: model type
+        :return:
+        """
+        # Initialize the inherit configuration
+        inherit_config = LoadBalancingModelConfig(
+            tenant_id=tenant_id,
+            provider_name=provider,
+            model_type=model_type.to_origin_model_type(),
+            model_name=model,
+            name='__inherit__'
+        )
+        db.session.add(inherit_config)
+        db.session.commit()
+
+        return inherit_config
