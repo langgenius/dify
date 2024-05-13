c2dfc948c513ce32782f0418ead5d8fb565f5e9d
Anoyi <545544032@qq.com>
2024-05-13 18:31:40 +0800

    fix: use smtp to send email

--- api/libs/smtp.py
+++ api/libs/smtp.py
@@ -1,27 +1,28 @@
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import asyncio
+from email.message import EmailMessage
+
+import aiosmtplib
 
 
 class SMTPClient:
-    def __init__(self, server: str, port: int, username: str, password: str, _from: str, use_tls=False):
+    def __init__(self, server: str, port: str, username: str, password: str, _from: str, use_tls=False):
         self.server = server
-        self.port = port
+        self.port = int(port)
         self._from = _from
         self.username = username
         self.password = password
         self._use_tls = use_tls
 
     def send(self, mail: dict):
-        smtp = smtplib.SMTP(self.server, self.port)
-        if self._use_tls:
-            smtp.starttls()
-        if self.username and self.password:
-            smtp.login(self.username, self.password)
-        msg = MIMEMultipart()
-        msg['Subject'] = mail['subject']
-        msg['From'] = self._from
-        msg['To'] = mail['to']
-        msg.attach(MIMEText(mail['html'], 'html'))
-        smtp.sendmail(self.username, mail['to'], msg.as_string())
-        smtp.quit()
+        message = EmailMessage()
+        message["From"] = self._from
+        message["To"] = mail['to']
+        message["Subject"] = mail['subject']
+        message.set_content(mail['html'], subtype='html')
+        asyncio.run(aiosmtplib.send(message,
+                                    hostname=self.server,
+                                    port=self.port,
+                                    use_tls=self._use_tls,
+                                    username=self.username,
+                                    password=self.password))
+
