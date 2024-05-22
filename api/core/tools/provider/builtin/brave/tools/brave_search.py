601584f27e182dde10594dc1f06f68b47530ae90
Bowen Liang <liangbowen@gf.com.cn>
2024-05-23 00:29:20 +0800

    missing type in BraveSearch

--- api/core/tools/provider/builtin/brave/tools/brave_search.py
+++ api/core/tools/provider/builtin/brave/tools/brave_search.py
@@ -58,8 +58,8 @@ class BraveSearchWrapper(BaseModel):
 class BraveSearch(BaseModel):
     """Tool that queries the BraveSearch."""
 
-    name = "brave_search"
-    description = (
+    name: str = "brave_search"
+    description: str = (
         "a search engine. "
         "useful for when you need to answer questions about current events."
         " input should be a search query."
