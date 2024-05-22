8c9f9e0ed18ce95dc751c62aa1d965949644c394
Bowen Liang <liangbowen@gf.com.cn>
2024-05-23 00:00:47 +0800

    missing type in PubMedAPIWrapper

--- api/core/tools/provider/builtin/pubmed/tools/pubmed_search.py
+++ api/core/tools/provider/builtin/pubmed/tools/pubmed_search.py
@@ -30,7 +30,7 @@ class PubMedAPIWrapper(BaseModel):
 
     base_url_esearch: str = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?"
     base_url_efetch: str = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?"
-    max_retry: str = 5
+    max_retry: int = 5
     sleep_time: float = 0.2
 
     # Default values for the parameters
