7c751a82fa79ed3836745058af47c68007e75377
kronus <kronus@istarshine.com>
2024-05-24 19:01:01 +0800

    修复返回值自动添加\n的问题

--- api/core/workflow/nodes/tool/tool_node.py
+++ api/core/workflow/nodes/tool/tool_node.py
@@ -174,9 +174,9 @@ class ToolNode(BaseNode):
         """
         Extract tool response text
         """
-        return ''.join([
-            f'{message.message}\n' if message.type == ToolInvokeMessage.MessageType.TEXT else
-            f'Link: {message.message}\n' if message.type == ToolInvokeMessage.MessageType.LINK else ''
+        return '\n'.join([
+            f'{message.message}' if message.type == ToolInvokeMessage.MessageType.TEXT else
+            f'Link: {message.message}' if message.type == ToolInvokeMessage.MessageType.LINK else ''
             for message in tool_response
         ])
 
