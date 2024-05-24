6d70bb54f34697284062902aa1079a42c80cde30
miendinh <miendinh@users.noreply.github.com>
2024-05-24 15:51:01 +0700

    change docker es port

--- api/tests/integration_tests/vdb/elasticsearch/test_elasticsearch.py
+++ api/tests/integration_tests/vdb/elasticsearch/test_elasticsearch.py
@@ -13,7 +13,7 @@ class ElasticSearchVectorTest(AbstractVectorTest):
             index_name=self.collection_name.lower(),
             config=ElasticSearchConfig(
                 host='http://localhost',
-                port='9200',
+                port='9222',
                 api_key_id='difyai',
                 api_key='difyai123456'
             ),
