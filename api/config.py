e60350d95dbc6302fe19d78c28f63768afbcd20e
takatost <takatost@users.noreply.github.com>
2024-05-27 22:48:34 +0800

    version to 0.6.9 (#4692)

--- api/config.py
+++ api/config.py
@@ -113,7 +113,7 @@ class Config:
         # ------------------------
         # General Configurations.
         # ------------------------
-        self.CURRENT_VERSION = "0.6.8"
+        self.CURRENT_VERSION = "0.6.9"
         self.COMMIT_SHA = get_env('COMMIT_SHA')
         self.EDITION = get_env('EDITION')
         self.DEPLOY_ENV = get_env('DEPLOY_ENV')
