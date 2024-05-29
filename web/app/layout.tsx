ab169981ba255c76c545ff5ee1ad3781cea388c3
lesenelir <hi@lesenelir.me>
2024-05-29 23:59:36 +0800

    fix: remove redundant props

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -47,7 +47,7 @@ const LocaleLayout = ({
         <BrowerInitor>
           <SentryInitor>
             {/* @ts-expect-error Async Server Component */}
-            <I18nServer locale={locale}>{children}</I18nServer>
+            <I18nServer>{children}</I18nServer>
           </SentryInitor>
         </BrowerInitor>
       </body>
