a325a294bd4a3aca827fe5f693f2aa433a2b51fc
Charles Zhou <zhou@linux.com>
2024-05-30 05:56:46 -0500

    feat: opportunistic tls flag for smtp (#4794)

--- api/config.py
+++ api/config.py
@@ -295,6 +295,7 @@ class Config:
         self.SMTP_USERNAME = get_env('SMTP_USERNAME')
         self.SMTP_PASSWORD = get_env('SMTP_PASSWORD')
         self.SMTP_USE_TLS = get_bool_env('SMTP_USE_TLS')
+        self.SMTP_OPPORTUNISTIC_TLS = get_bool_env('SMTP_OPPORTUNISTIC_TLS')
 
         # ------------------------
         # Workspace Configurations.
