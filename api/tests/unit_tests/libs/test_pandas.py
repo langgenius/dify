0ebb372e698ebee01fa1a10ba42105f75db452f8
Bowen Liang <liangbowen@gf.com.cn>
2024-05-31 22:26:11 +0800

    nit

--- api/tests/unit_tests/libs/test_pandas.py
+++ api/tests/unit_tests/libs/test_pandas.py
@@ -8,13 +8,11 @@ def test_pandas_csv(tmp_path, monkeypatch):
     df1 = pd.DataFrame(data)
 
     # write to csv file
-    csv_file_name = 'example.csv'
-    file_path = tmp_path.joinpath(csv_file_name)
-    df1.to_csv(file_path, index=False)
+    csv_file_path = tmp_path.joinpath('example.csv')
+    df1.to_csv(csv_file_path, index=False)
 
     # read from csv file
-    df2 = pd.read_csv(csv_file_name)
-    assert len(df2.index) == len(data['col1'])
+    df2 = pd.read_csv(csv_file_path)
     assert df2[df2.columns[0]].to_list() == data['col1']
     assert df2[df2.columns[1]].to_list() == data['col2']
 
@@ -26,13 +24,11 @@ def test_pandas_xlsx(tmp_path, monkeypatch):
     df1 = pd.DataFrame(data)
 
     # write to xlsx file
-    xlsx_file_name = 'example.xlsx'
-    file_path = tmp_path.joinpath(xlsx_file_name)
-    df1.to_excel(file_path, index=False)
+    xlsx_file_path = tmp_path.joinpath('example.xlsx')
+    df1.to_excel(xlsx_file_path, index=False)
 
     # read from xlsx file
-    df2 = pd.read_excel(xlsx_file_name)
-    assert len(df2.index) == len(data['col1'])
+    df2 = pd.read_excel(xlsx_file_path)
     assert df2[df2.columns[0]].to_list() == data['col1']
     assert df2[df2.columns[1]].to_list() == data['col2']
 
@@ -48,22 +44,19 @@ def test_pandas_xlsx_with_sheets(tmp_path, monkeypatch):
     df2 = pd.DataFrame(data2)
 
     # write to xlsx file with sheets
-    xlsx_file_name = 'example_with_sheets.xlsx'
-    file_path = tmp_path.joinpath(xlsx_file_name)
+    xlsx_file_path = tmp_path.joinpath('example_with_sheets.xlsx')
     sheet1 = 'Sheet1'
     sheet2 = 'Sheet2'
-    with pd.ExcelWriter(file_path) as excel_writer:
+    with pd.ExcelWriter(xlsx_file_path) as excel_writer:
         df1.to_excel(excel_writer, sheet_name=sheet1, index=False)
         df2.to_excel(excel_writer, sheet_name=sheet2, index=False)
 
     # read from xlsx file with sheets
-    with pd.ExcelFile(file_path) as excel_file:
+    with pd.ExcelFile(xlsx_file_path) as excel_file:
         df1 = pd.read_excel(excel_file, sheet_name=sheet1)
-        assert len(df1.index) == len(data1['col1'])
         assert df1[df1.columns[0]].to_list() == data1['col1']
         assert df1[df1.columns[1]].to_list() == data1['col2']
 
         df2 = pd.read_excel(excel_file, sheet_name=sheet2)
-        assert len(df2.index) == len(data2['col1'])
         assert df2[df2.columns[0]].to_list() == data2['col1']
         assert df2[df2.columns[1]].to_list() == data2['col2']
