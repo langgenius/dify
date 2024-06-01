a2a19926303ddc5f55e590066a070f70c9f50942
Pan, Wen-Ming <smalljelly2018@gmail.com>
2024-06-01 15:16:25 +0800

    Bugfix: Vertex AI vision model not support image

--- api/core/model_runtime/model_providers/vertex_ai/llm/llm.py
+++ api/core/model_runtime/model_providers/vertex_ai/llm/llm.py
@@ -367,9 +367,7 @@ class VertexAiLargeLanguageModel(LargeLanguageModel):
                     else:
                         metadata, data = c.data.split(',', 1)
                         mime_type = metadata.split(';', 1)[0].split(':')[1]
-                        blob = {"inline_data":{"mime_type":mime_type,"data":data}}
-                        parts.append(blob)
-                
+                        parts.append(glm.Part.from_data(mime_type=mime_type, data=data))
                 glm_content = glm.Content(role="user", parts=[parts])
             return glm_content
         elif isinstance(message, AssistantPromptMessage):
