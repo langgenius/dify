2d64535b36d80ce562d62ca7bcd66a5aeea6f384
dar0xt <62972912+dar0xt@users.noreply.github.com>
2024-06-02 17:22:40 +0900

    revert formatter

--- sdks/nodejs-client/index.d.ts
+++ sdks/nodejs-client/index.d.ts
@@ -1,22 +1,23 @@
 // Types.d.ts
-export const BASE_URL: string
+export const BASE_URL: string;
 
-export type RequestMethods = "GET" | "POST" | "PATCH" | "DELETE"
+export type RequestMethods = 'GET' | 'POST' | 'PATCH' | 'DELETE';
 
 interface Params {
-  [key: string]: any
+  [key: string]: any;
 }
 
 interface HeaderParams {
-  [key: string]: string
+  [key: string]: string;
 }
 
-interface User {}
+interface User {
+}
 
 export declare class DifyClient {
-  constructor(apiKey: string, baseUrl?: string)
+  constructor(apiKey: string, baseUrl?: string);
 
-  updateApiKey(apiKey: string): void
+  updateApiKey(apiKey: string): void;
 
   sendRequest(
     method: RequestMethods,
@@ -25,13 +26,13 @@ export declare class DifyClient {
     params?: Params,
     stream?: boolean,
     headerParams?: HeaderParams
-  ): Promise<any>
+  ): Promise<any>;  
 
-  messageFeedback(message_id: string, rating: number, user: User): Promise<any>
+  messageFeedback(message_id: string, rating: number, user: User): Promise<any>;
 
-  getApplicationParameters(user: User): Promise<any>
+  getApplicationParameters(user: User): Promise<any>;
 
-  fileUpload(data: FormData): Promise<any>
+  fileUpload(data: FormData): Promise<any>;
 }
 
 export declare class CompletionClient extends DifyClient {
@@ -40,7 +41,7 @@ export declare class CompletionClient extends DifyClient {
     user: User,
     stream?: boolean,
     files?: File[] | null
-  ): Promise<any>
+  ): Promise<any>;
 }
 
 export declare class ChatClient extends DifyClient {
@@ -51,27 +52,18 @@ export declare class ChatClient extends DifyClient {
     stream?: boolean,
     conversation_id?: string | null,
     files?: File[] | null
-  ): Promise<any>
+  ): Promise<any>;
 
   getConversationMessages(
     user: User,
     conversation_id?: string,
     first_id?: string | null,
     limit?: number | null
-  ): Promise<any>
+  ): Promise<any>;
 
-  getConversations(
-    user: User,
-    first_id?: string | null,
-    limit?: number | null,
-    pinned?: boolean | null
-  ): Promise<any>
+  getConversations(user: User, first_id?: string | null, limit?: number | null, pinned?: boolean | null): Promise<any>;
 
-  renameConversation(
-    conversation_id: string,
-    name: string,
-    user: User
-  ): Promise<any>
+  renameConversation(conversation_id: string, name: string, user: User): Promise<any>;
 
-  deleteConversation(conversation_id: string, user: User): Promise<any>
+  deleteConversation(conversation_id: string, user: User): Promise<any>;
 }
