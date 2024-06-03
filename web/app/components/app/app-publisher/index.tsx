cf60d44917b54ff21396b5ae9053cb1703b30078
dromerolovo <dromerolovo@gmail.com>
2024-06-03 05:58:09 +0000

    polish

--- web/app/components/app/app-publisher/index.tsx
+++ web/app/components/app/app-publisher/index.tsx
@@ -226,6 +226,7 @@ const AppPublisher = ({
         </div>
       </PortalToFollowElemContent>
       <EmbeddedModal
+        siteInfo={appDetail?.site}
         isShow={embeddingModalOpen}
         onClose={() => setEmbeddingModalOpen(false)}
         appBaseUrl={appBaseURL}
