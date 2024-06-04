8eb597ea0c9d0e10aa84863d784a075ee5ac2be6
Bowen Liang <liangbowen@gf.com.cn>
2024-06-04 20:28:10 +0800

    migrate to field_validator for ToolNodeData

--- api/core/workflow/nodes/tool/entities.py
+++ api/core/workflow/nodes/tool/entities.py
@@ -1,6 +1,7 @@
 from typing import Any, Literal, Union
 
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, field_validator, validator
+from pydantic_core.core_schema import ValidationInfo
 
 from core.workflow.entities.base_node_data_entities import BaseNodeData
 
@@ -32,12 +33,11 @@ class ToolNodeData(BaseNodeData, ToolEntity):
         value: Union[Any, list[str]]
         type: Literal['mixed', 'variable', 'constant']
 
-        # TODO[pydantic]: We couldn't refactor the `validator`, please replace it by `field_validator` manually.
-        # Check https://docs.pydantic.dev/dev-v2/migration/#changes-to-validators for more information.
-        @validator('type', pre=True, always=True)
-        def check_type(cls, value, values):
+        @classmethod
+        @field_validator('type', mode='before')
+        def check_type(cls, value, validation_info: ValidationInfo):
             typ = value
-            value = values.get('value')
+            value = validation_info.data.get('value')
             if typ == 'mixed' and not isinstance(value, str):
                 raise ValueError('value must be a string')
             elif typ == 'variable':
@@ -49,7 +49,7 @@ class ToolNodeData(BaseNodeData, ToolEntity):
             elif typ == 'constant' and not isinstance(value, str | int | float | bool):
                 raise ValueError('value must be a string, int, float, or bool')
             return typ
-        
+
     """
     Tool Node Schema
     """
