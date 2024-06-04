05d155ef6860698a2d6ac8536400c7c0fe3b443a
Jason <ggbbddjm@gmail.com>
2024-06-04 19:27:26 +0800

    feat: add novita.ai as new model provider.

---
+++ api/tests/integration_tests/model_runtime/novita/test_provider.py
@@ -0,0 +1,21 @@
+import os
+
+import pytest
+
+from core.model_runtime.errors.validate import CredentialsValidateFailedError
+from core.model_runtime.model_providers.novita.novita import NovitaProvider
+
+
+def test_validate_provider_credentials():
+    provider = NovitaProvider()
+
+    with pytest.raises(CredentialsValidateFailedError):
+        provider.validate_provider_credentials(
+            credentials={}
+        )
+
+    provider.validate_provider_credentials(
+        credentials={
+            'api_key': os.environ.get('NOVITA_API_KEY'),
+        }
+    )
