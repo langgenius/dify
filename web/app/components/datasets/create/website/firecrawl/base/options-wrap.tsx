93113766a1ad0a416304f1ba966986c97574a3a2
Joel <iamjoel007@gmail.com>
2024-06-04 22:48:20 +0800

    just add

---
+++ web/app/components/datasets/create/website/firecrawl/base/options-wrap.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { useBoolean } from 'ahooks'
+import type { FC } from 'react'
+import React from 'react'
+import { Settings04 } from '@/app/components/base/icons/src/vender/line/general'
+
+type Props = {
+  children: React.ReactNode
+}
+
+const OptionsWrap: FC<Props> = ({
+  children,
+}) => {
+  const [fold, {
+    setTrue: foldTrue,
+    setFalse: foldFalse,
+    toggle: foldToggle,
+  }] = useBoolean(false)
+  return (
+    <div>
+      <div className='flex justify-between items-center h-[18px] py-1'>
+        <div className='flex items-center text-gray-700'>
+          <Settings04 className='mr-1 w-4 h-4' />
+          <div>Options</div>
+        </div>
+
+      </div>
+      {!fold && (
+        <div>
+          {children}
+        </div>
+      )}
+
+    </div>
+  )
+}
+export default React.memo(OptionsWrap)
