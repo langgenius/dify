48c730b5f41ca76b327c3b229dd4fc1f2f9a0218
doufa <doufa1113@gmail.com>
2024-06-13 08:33:48 +0000

    delete: unused code

--- web/hooks/use-tab-searchparams.ts
+++ web/hooks/use-tab-searchparams.ts
@@ -1,4 +1,4 @@
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { usePathname, useSearchParams } from 'next/navigation'
 import { useState } from 'react'
 
 type UseTabSearchParamsOptions = {
@@ -24,7 +24,6 @@ export const useTabSearchParams = ({
   searchParamName = 'category',
   disableSearchParams = false,
 }: UseTabSearchParamsOptions) => {
-  const router = useRouter()
   const pathName = usePathname()
   const searchParams = useSearchParams()
   const [activeTab, setTab] = useState<string>(
