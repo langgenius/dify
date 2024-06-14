281ade3f2624ac77be127d316855b9e419a3b3a7
jyong <718720800@qq.com>
2024-06-15 00:28:10 +0800

    add optional type

--- api/core/rag/extractor/entity/extract_setting.py
+++ api/core/rag/extractor/entity/extract_setting.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel, ConfigDict
 
 from models.dataset import Document
@@ -42,10 +44,10 @@ class ExtractSetting(BaseModel):
     Model class for provider response.
     """
     datasource_type: str
-    upload_file: UploadFile = None
-    notion_info: NotionInfo = None
-    website_info: WebsiteInfo = None
-    document_model: str = None
+    upload_file: Optional[UploadFile]
+    notion_info: Optional[NotionInfo]
+    website_info: Optional[WebsiteInfo]
+    document_model: Optional[str]
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     def __init__(self, **data) -> None:
