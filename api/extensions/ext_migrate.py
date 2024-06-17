8ba95ef63199104f977ecf84111b195e53abfeb1
Bowen Liang <liangbowen@gf.com.cn>
2024-06-17 23:11:13 +0800

    nit

--- api/extensions/ext_migrate.py
+++ api/extensions/ext_migrate.py
@@ -3,10 +3,3 @@ import flask_migrate
 
 def init(app, db):
     flask_migrate.Migrate(app, db)
-
-
-def db_upgrade():
-    """
-    Upgrade the database with Flask-Migrate
-    """
-    flask_migrate.upgrade()
