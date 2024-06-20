bdcfecbb5070775761946c5c28ee58501b20d12d
Xiao Ley <xiao.ley@outlook.com>
2024-06-20 22:59:56 +0800

    feat: Database Control tool supported markdown format

--- api/core/tools/provider/builtin/databaseoperation/tools/databasecontrol_sqlexec.py
+++ api/core/tools/provider/builtin/databaseoperation/tools/databasecontrol_sqlexec.py
@@ -3,6 +3,7 @@ from typing import Any, Union
 
 import psycopg2
 import pymysql
+from pymysql.constants import CLIENT
 
 from core.tools.entities.tool_entities import ToolInvokeMessage
 from core.tools.tool.builtin_tool import BuiltinTool
@@ -13,12 +14,8 @@ class DatabaseControlSqlExecTool(BuiltinTool):
                 user_id: str,
                 tool_parameters: dict[str, Any]
                 ) -> Union[ToolInvokeMessage, list[ToolInvokeMessage]]:
-        """
-            invoke tools
-            output format: [{"column1":"value1","column2":"value2"},{"column1":"value1","column2":"value2"}]
-        """
-
         sql = tool_parameters.get('sql')
+        output_format = tool_parameters.get('output_format', 'json')
         dbtype = tool_parameters.get('dbtype')
         host = tool_parameters.get('host')
         port = tool_parameters.get('port')
@@ -28,15 +25,16 @@ class DatabaseControlSqlExecTool(BuiltinTool):
 
         result_str = ''
         if dbtype == 'postgresql':
-            result_str = self._pg_exec(sql, host, port, username, password, dbname)
+            result_str = self._pg_exec(sql, output_format, host, port, username, password, dbname)
         elif dbtype == 'mysql':
-            result_str = self._mysql_exec(sql, host, port, username, password, dbname)
+            result_str = self._mysql_exec(sql, output_format, host, port, username, password, dbname)
         else:
             raise ValueError(f"Unsupported database type: {dbtype}")
 
         return self.create_text_message(result_str)
 
-    def _pg_exec(self, sql: str, host: str, port: str, username: str, password: str, dbname: str) -> str:
+    def _pg_exec(self, sql: str, output_format: str, host: str, port: str,
+                 username: str, password: str, dbname: str) -> str:
         result_str = ''
 
         conn = psycopg2.connect(
@@ -51,13 +49,19 @@ class DatabaseControlSqlExecTool(BuiltinTool):
         conn.commit()
 
         if cur.description is not None:
-            if cur.rowcount == 0:
-                result_str = '[]'
-
             fields_tuple = cur.description
-            result_data_list = cur.fetchall()
-            result_list = [dict(zip([i[0] for i in fields_tuple], data_tuple)) for data_tuple in result_data_list]
-            result_str = json.dumps(result_list)
+            fields_list = [i[0] for i in fields_tuple]
+
+            if cur.rowcount == 0:
+                result_str = self._convert_to_markdown_table(fields_list, []) \
+                    if output_format == 'markdown' else '[]'
+            else:
+                result_data_list = cur.fetchall()
+                result_list = [dict(zip(fields_list, data_tuple)) for data_tuple in result_data_list]
+                if output_format == 'markdown':
+                    result_str = self._convert_to_markdown_table(fields_list, result_list)
+                else:
+                    result_str = json.dumps(result_list)
         else:
             result_str = ''
 
@@ -66,7 +70,8 @@ class DatabaseControlSqlExecTool(BuiltinTool):
 
         return result_str
 
-    def _mysql_exec(self, sql: str, host: str, port: str, username: str, password: str, dbname: str) -> str:
+    def _mysql_exec(self, sql: str, output_format: str, host: str, port: str,
+                    username: str, password: str, dbname: str) -> str:
         result_str = ''
 
         conn = pymysql.connect(
@@ -74,24 +79,64 @@ class DatabaseControlSqlExecTool(BuiltinTool):
             port=int(port),
             user=username,
             passwd=password,
-            db=dbname
+            db=dbname,
+            client_flag=CLIENT.MULTI_STATEMENTS
         )
-        cur = conn.cursor()
-        cur.execute(sql)
-        conn.commit()
 
-        if cur.description is not None:
-            if cur.rowcount == 0:
-                result_str = '[]'
+        try:
+            with conn.cursor() as cur:
+                conn.begin()
 
-            fields_tuple = cur.description
-            result_data_list = cur.fetchall()
-            result_list = [dict(zip([i[0] for i in fields_tuple], data_tuple)) for data_tuple in result_data_list]
-            result_str = json.dumps(result_list)
-        else:
-            result_str = ''
+                try:
+                    cur.execute(sql)
 
-        cur.close()
-        conn.close()
+                    result_list = []
+                    fields_list = []
+
+                    while True:
+                        result_list = []
+                        fields_list = []
+                        if cur.description is not None:
+                            fields_tuple = cur.description
+                            fields_list = [i[0] for i in fields_tuple]
+
+                            if cur.rowcount > 0:
+                                result_data_list = cur.fetchall()
+                                result_list = [dict(zip(fields_list, data_tuple)) for data_tuple in result_data_list]
+
+                        if not cur.nextset():
+                            break
+
+                    if fields_list:
+                        if output_format == 'markdown':
+                            result_str = self._convert_to_markdown_table(fields_list, result_list)
+                        else:
+                            result_str = json.dumps(result_list)
+                    else:
+                        result_str = ''
+                except Exception as e:
+                    conn.rollback()
+                    raise e
+                else:
+                    conn.commit()
+        finally:
+            conn.close()
 
         return result_str
+
+    def _convert_to_markdown_table(self, fields: list, data: list[dict[str, Any]]) -> str:
+        if not fields:
+            return ""
+
+        markdown_table = ["| " + " | ".join(fields) + " |", "| " + " | ".join(["---"] * len(fields)) + " |"]
+
+        for item in data:
+            # Convert each entry to a Markdown table row
+            row = [str(item.get(header, "")) for header in fields]
+            markdown_row = "| " + " | ".join(row) + " |"
+            markdown_table.append(markdown_row)
+
+        # Combine all rows into a single string
+        markdown_result = "\n".join(markdown_table)
+
+        return markdown_result
