65d34ebb96bd1e77154022715953b4cb9125a366
Bowen Liang <liangbowen@gf.com.cn>
2024-06-20 16:24:10 +0800

    refactor: extract vdb configs into pydantic-setting based dify configs (#5426)

---
+++ api/configs/middleware/vdb/weaviate_configs.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field, PositiveInt
+
+
+class WeaviateConfigs(BaseModel):
+    """
+    Weaviate configs
+    """
+
+    WEAVIATE_ENDPOINT: Optional[str] = Field(
+        description='Weaviate endpoint URL',
+        default=None,
+    )
+
+    WEAVIATE_API_KEY: Optional[str] = Field(
+        description='Weaviate API key',
+        default=None,
+    )
+
+    WEAVIATE_GRPC_ENABLED: bool = Field(
+        description='whether to enable gRPC for Weaviate connection',
+        default=True,
+    )
+
+    WEAVIATE_BATCH_SIZE: PositiveInt = Field(
+        description='Weaviate batch size',
+        default=100,
+    )
