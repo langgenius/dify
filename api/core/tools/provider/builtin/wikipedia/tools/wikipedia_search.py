995bfbfdcbde8f0273eb6d64e809ccc970c5451b
Joe <1264204425@qq.com>
2024-06-21 15:07:44 +0800

    Revert "feat: allow non-english wikipedias to be searched (#5371)"
    This reverts commit 0e3113b7ce5502d25fd41efe88512b9c085b82c1.

--- api/core/tools/provider/builtin/wikipedia/tools/wikipedia_search.py
+++ api/core/tools/provider/builtin/wikipedia/tools/wikipedia_search.py
@@ -7,7 +7,6 @@ from core.tools.tool.builtin_tool import BuiltinTool
 
 WIKIPEDIA_MAX_QUERY_LENGTH = 300
 
-
 class WikipediaAPIWrapper:
     """Wrapper around WikipediaAPI.
 
@@ -26,10 +25,7 @@ class WikipediaAPIWrapper:
     def __init__(self, doc_content_chars_max: int = 4000):
         self.doc_content_chars_max = doc_content_chars_max
 
-    def run(self, query: str, lang: str = "") -> str:
-        if lang in wikipedia.languages().keys():
-            self.lang = lang
-
+    def run(self, query: str) -> str:
         wikipedia.set_lang(self.lang)
         wiki_client = wikipedia
 
@@ -57,7 +53,6 @@ class WikipediaAPIWrapper:
         ):
             return None
 
-
 class WikipediaQueryRun:
     """Tool that searches the Wikipedia API."""
 
@@ -76,31 +71,26 @@ class WikipediaQueryRun:
     def _run(
         self,
         query: str,
-        lang: str = "",
     ) -> str:
         """Use the Wikipedia tool."""
-        return self.api_wrapper.run(query, lang)
-
-
+        return self.api_wrapper.run(query)
 class WikiPediaSearchTool(BuiltinTool):
-
-    def _invoke(
-        self,
-        user_id: str,
-        tool_parameters: dict[str, Any],
-    ) -> Union[ToolInvokeMessage, list[ToolInvokeMessage]]:
+    def _invoke(self, 
+                user_id: str, 
+               tool_parameters: dict[str, Any], 
+        ) -> Union[ToolInvokeMessage, list[ToolInvokeMessage]]:
         """
-        invoke tools
+            invoke tools
         """
-        query = tool_parameters.get("query", "")
-        lang = tool_parameters.get("language", "")
+        query = tool_parameters.get('query', '')
         if not query:
-            return self.create_text_message("Please input query")
-
+            return self.create_text_message('Please input query')
+        
         tool = WikipediaQueryRun(
             api_wrapper=WikipediaAPIWrapper(doc_content_chars_max=4000),
         )
 
-        result = tool._run(query, lang)
+        result = tool._run(query)
 
-        return self.create_text_message(self.summary(user_id=user_id, content=result))
+        return self.create_text_message(self.summary(user_id=user_id,content=result))
+    
