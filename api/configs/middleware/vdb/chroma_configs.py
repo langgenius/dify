9b24d68bb2f08ca3d5b6219efd0efdcf3aa47037
Bowen Liang <liangbowen@gf.com.cn>
2024-06-22 17:00:02 +0800

    single config 2

--- api/configs/middleware/vdb/chroma_configs.py
+++ api/configs/middleware/vdb/chroma_configs.py
@@ -3,7 +3,7 @@ from typing import Optional
 from pydantic import BaseModel, Field, PositiveInt
 
 
-class ChromaConfigs(BaseModel):
+class ChromaConfig(BaseModel):
     """
     Chroma configs
     """
