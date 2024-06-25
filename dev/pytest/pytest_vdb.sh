27196dc420a5d90f39f4c323b37023cdc46dd157
takatost <takatost@gmail.com>
2024-06-25 15:44:03 +0800

    fix test

--- dev/pytest/pytest_vdb.sh
+++ dev/pytest/pytest_vdb.sh
@@ -3,7 +3,7 @@ set -x
 
 pytest api/tests/integration_tests/vdb/chroma \
   api/tests/integration_tests/vdb/milvus \
-  api/tests/integration_tests/vdb/pgvector_rs \
+  api/tests/integration_tests/vdb/pgvecto_rs \
   api/tests/integration_tests/vdb/pgvector \
   api/tests/integration_tests/vdb/qdrant \
   api/tests/integration_tests/vdb/weaviate \
