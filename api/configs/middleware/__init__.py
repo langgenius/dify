37b2d7b08df85d05fb703eedc07d88ac26c3dae6
han yong <hanyongmvp@163.com>
2024-06-27 22:35:47 +0800

    modify name

--- api/configs/middleware/__init__.py
+++ api/configs/middleware/__init__.py
@@ -8,7 +8,7 @@ from configs.middleware.storage.amazon_s3_storage_config import S3StorageConfig
 from configs.middleware.storage.azure_blob_storage_config import AzureBlobStorageConfig
 from configs.middleware.storage.google_cloud_storage_config import GoogleCloudStorageConfig
 from configs.middleware.storage.tencent_cos_storage_config import TencentCloudCOSStorageConfig
-from configs.middleware.storage.oci_cloud_storage_config import OCIStorageConfig
+from configs.middleware.storage.oci_storage_config import OCIStorageConfig
 from configs.middleware.vdb.chroma_config import ChromaConfig
 from configs.middleware.vdb.milvus_config import MilvusConfig
 from configs.middleware.vdb.opensearch_config import OpenSearchConfig
