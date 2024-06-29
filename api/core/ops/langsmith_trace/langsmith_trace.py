6392d3110f24510211e9a8550653b16056695600
Joe <79627742+ZhouhaoJiang@users.noreply.github.com>
2024-06-29 23:32:52 +0800

    fix: workflow trace none type error (#5758)

--- api/core/ops/langsmith_trace/langsmith_trace.py
+++ api/core/ops/langsmith_trace/langsmith_trace.py
@@ -114,7 +114,9 @@ class LangSmithDataTrace(BaseTraceInstance):
             node_type = node_execution.node_type
             status = node_execution.status
             if node_type == "llm":
-                inputs = json.loads(node_execution.process_data).get("prompts", {})
+                inputs = json.loads(node_execution.process_data).get(
+                    "prompts", {}
+                    ) if node_execution.process_data else {}
             else:
                 inputs = json.loads(node_execution.inputs) if node_execution.inputs else {}
             outputs = (
