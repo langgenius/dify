6b5824660e37dd2c8776a67fe92dcfdf208e7116
AIxGEEK <lujx1994@gmail.com>
2024-07-02 14:15:13 +0800

    fix: unable to enter variable name when there is a duplicate name

--- web/app/components/workflow/nodes/_base/components/variable/output-var-list.tsx
+++ web/app/components/workflow/nodes/_base/components/variable/output-var-list.tsx
@@ -1,6 +1,6 @@
 'use client'
 import type { FC } from 'react'
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import produce from 'immer'
 import { useTranslation } from 'react-i18next'
 import type { OutputVar } from '../../../code/types'
@@ -14,6 +14,7 @@ type Props = {
   readonly: boolean
   outputs: OutputVar
   outputKeyOrders: string[]
+  onOutputKeyOrdersChange?: (newOutputKeyOrders: string[]) => void
   onChange: (payload: OutputVar, changedIndex?: number, newKey?: string) => void
   onRemove: (index: number) => void
 }
@@ -26,17 +27,35 @@ const OutputVarList: FC<Props> = ({
   onRemove,
 }) => {
   const { t } = useTranslation()
+  const [list, setList] = useState<{ variable: string; variable_type: VarType }[]>([])
 
-  const list = outputKeyOrders.map((key) => {
-    return {
-      variable: key,
-      variable_type: outputs[key]?.type,
-    }
-  })
+  useEffect(() => {
+    const l = outputKeyOrders.map((key) => {
+      return {
+        variable: key,
+        variable_type: outputs[key]?.type,
+      }
+    })
+    setList(l)
+  }, [outputKeyOrders, outputs])
+
   const handleVarNameChange = useCallback((index: number) => {
     return (e: React.ChangeEvent<HTMLInputElement>) => {
-      const oldKey = list[index].variable
       const newKey = e.target.value
+      const newList = list.map((o, i) => {
+        return i === index ? { ...o, variable: newKey } : o
+      })
+      setList(newList)
+    }
+  }, [list])
+
+  const handleVarNameBlur = useCallback((index: number) => {
+    return (e: React.ChangeEvent<HTMLInputElement>) => {
+      const oldKey = outputKeyOrders[index]
+      const newKey = e.target.value
+
+      if (oldKey === newKey)
+        return
 
       const { isValid, errorKey, errorMessageKey } = checkKeys([newKey], true)
       if (!isValid) {
@@ -47,11 +66,18 @@ const OutputVarList: FC<Props> = ({
         return
       }
 
-      if (list.map(item => item.variable?.trim()).includes(newKey.trim())) {
+      if (list.filter((_, i) => i !== index).map(item => item.variable?.trim()).includes(newKey.trim())) {
         Toast.notify({
           type: 'error',
           message: t('appDebug.varKeyError.keyAlreadyExists', { key: newKey }),
         })
+        // reset list, when keyAlreadyExists
+        const l = list.map((o, i) => {
+          return i === index
+            ? { ...o, variable: oldKey }
+            : o
+        })
+        setList(l)
         return
       }
 
@@ -61,19 +87,17 @@ const OutputVarList: FC<Props> = ({
       })
       onChange(newOutputs, index, newKey)
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [list, onChange, outputs, outputKeyOrders])
+  }, [onChange, outputs, outputKeyOrders, list, t])
 
   const handleVarTypeChange = useCallback((index: number) => {
     return (value: string) => {
-      const key = list[index].variable
+      const key = outputKeyOrders[index]
       const newOutputs = produce(outputs, (draft) => {
         draft[key].type = value as VarType
       })
       onChange(newOutputs)
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [list, onChange, outputs, outputKeyOrders])
+  }, [onChange, outputs, outputKeyOrders])
 
   const handleVarRemove = useCallback((index: number) => {
     return () => {
@@ -89,6 +113,7 @@ const OutputVarList: FC<Props> = ({
             readOnly={readonly}
             value={item.variable}
             onChange={handleVarNameChange(index)}
+            onBlur={handleVarNameBlur(index)}
             className='w-0 grow h-8 leading-8 px-2.5 rounded-lg border-0 bg-gray-100  text-gray-900 text-[13px] placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-inset focus:ring-gray-200'
             type='text' />
           <VarTypePicker
