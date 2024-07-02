b06abf9a00d80fad2197fce228f6b0a67fdfefa4
AIxGEEK <lujx1994@gmail.com>
2024-07-02 17:12:28 +0800

    fix: node-title-is-overflow-in-checklist

--- web/app/components/workflow/header/checklist.tsx
+++ web/app/components/workflow/header/checklist.tsx
@@ -122,7 +122,7 @@ const WorkflowChecklist = ({
                               className='mr-1.5'
                               toolIcon={node.toolIcon}
                             />
-                            {node.title}
+                            <span className='w-[340px] truncate'>{node.title}</span>
                           </div>
                           <div className='border-t-[0.5px] border-t-black/2'>
                             {
