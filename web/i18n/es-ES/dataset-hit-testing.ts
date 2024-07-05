e3dc51ec9e3a1f607b970819a1efb5bf007ab073
crazywoola <100913391+crazywoola@users.noreply.github.com>
2024-07-05 21:05:33 +0800

    6014 i18n add support for spanish (#6017)

---
+++ web/i18n/es-ES/dataset-hit-testing.ts
@@ -0,0 +1,28 @@
+const translation = {
+  title: 'Prueba de recuperación',
+  desc: 'Prueba del efecto de impacto del conocimiento basado en el texto de consulta proporcionado.',
+  dateTimeFormat: 'MM/DD/YYYY hh:mm A',
+  recents: 'Recientes',
+  table: {
+    header: {
+      source: 'Fuente',
+      text: 'Texto',
+      time: 'Tiempo',
+    },
+  },
+  input: {
+    title: 'Texto fuente',
+    placeholder: 'Por favor ingrese un texto, se recomienda una oración declarativa corta.',
+    countWarning: 'Hasta 200 caracteres.',
+    indexWarning: 'Solo conocimiento de alta calidad.',
+    testing: 'Prueba',
+  },
+  hit: {
+    title: 'PÁRRAFOS DE RECUPERACIÓN',
+    emptyTip: 'Los resultados de la prueba de recuperación se mostrarán aquí',
+  },
+  noRecentTip: 'No hay resultados de consulta recientes aquí',
+  viewChart: 'Ver GRÁFICO VECTORIAL',
+}
+
+export default translation
