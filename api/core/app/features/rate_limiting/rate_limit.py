60e9381fc3b11f7431ea38509f0d759bbf2f618a
liuzhenghua-jk <liuzhenghua-jk@360shuke.com>
2024-07-05 11:42:26 +0800

    feature: since hlen is performed in constant time, remove active_request_count key

--- api/core/app/features/rate_limiting/rate_limit.py
+++ api/core/app/features/rate_limiting/rate_limit.py
@@ -11,7 +11,6 @@ logger = logging.getLogger(__name__)
 
 
 class RateLimit:
-    _ACTIVE_REQUESTS_COUNT_KEY = "dify:rate_limit:{}:active_requests_count"
     _ACTIVE_REQUESTS = "dify:rate_limit:{}:active_requests"
     _UNLIMITED_REQUEST_ID = "unlimited_request_id"
     _REQUEST_MAX_ALIVE_TIME = 10 * 60  # 10 minutes
@@ -30,27 +29,19 @@ class RateLimit:
         self.initialized = True
         self.client_id = client_id
         self.max_active_requests = max_active_requests
-        self.active_requests_count_key = self._ACTIVE_REQUESTS_COUNT_KEY.format(client_id)
         self.active_requests_key = self._ACTIVE_REQUESTS.format(client_id)
-        self.active_requests_count = 0
         self.last_recalculate_time = float('-inf')
-        self.recalculate_active_requests_count()
+        self.flush_active_requests()
 
-    def recalculate_active_requests_count(self):
-        if self.max_active_requests <= 0:
-            return
+    def flush_active_requests(self):
         if not redis_client.exists(self.active_requests_key):
-            self.active_requests_count = 0
             return
         redis_client.expire(self.active_requests_key, timedelta(days=1))
-        redis_client.expire(self.active_requests_count_key, timedelta(days=1))
         request_details = redis_client.hgetall(self.active_requests_key)
         timeout_requests = [k for k, v in request_details.items() if
                             time.time() - float(v.decode('utf-8')) > RateLimit._REQUEST_MAX_ALIVE_TIME]
         if timeout_requests:
             redis_client.hdel(self.active_requests_key, *timeout_requests)
-        self.active_requests_count = redis_client.hlen(self.active_requests_key)
-        redis_client.set(self.active_requests_count_key, self.active_requests_count, ex=timedelta(days=1))
         self.last_recalculate_time = time.time()
 
     def enter(self, request_id: Optional[str] = None) -> str:
@@ -58,12 +49,11 @@ class RateLimit:
             return RateLimit._UNLIMITED_REQUEST_ID
         if not request_id:
             request_id = RateLimit.gen_request_key()
-        with redis_client.pipeline() as pipe:
-            pipe.hset(self.active_requests_key, request_id, str(time.time()))
-            self.active_requests_count = pipe.incr(self.active_requests_count_key).execute()[1]
         if time.time() - self.last_recalculate_time > RateLimit._ACTIVE_REQUESTS_COUNT_FLUSH_INTERVAL:
-            self.recalculate_active_requests_count()
-        if self.active_requests_count > self.max_active_requests:
+            self.flush_active_requests()
+        redis_client.hset(self.active_requests_key, request_id, str(time.time()))
+        active_requests_count = redis_client.hlen(self.active_requests_key)
+        if active_requests_count > self.max_active_requests:
             raise AppInvokeQuotaExceededError("Too many requests. Please try again later. The current maximum "
                                               "concurrent requests allowed is {}.".format(self.max_active_requests))
         return request_id
@@ -71,12 +61,7 @@ class RateLimit:
     def exit(self, request_id: str):
         if request_id == RateLimit._UNLIMITED_REQUEST_ID:
             return
-        with redis_client.pipeline() as pipe:
-            pipe.hdel(self.active_requests_key, request_id)
-            self.active_requests_count = pipe.decr(self.active_requests_count_key).execute()[1]
-        if self.active_requests_count < 0:
-            self.active_requests_count = 0
-            redis_client.set(self.active_requests_count_key, 0)
+        redis_client.hdel(self.active_requests_key, request_id)
 
     @staticmethod
     def gen_request_key() -> str:
