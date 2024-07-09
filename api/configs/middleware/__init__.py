f7823bba6c27bbf391bb7da0719d532037e57b28
xiaozeyu <xiaozeyu.xzy@alibaba-inc.com>
2024-07-09 09:28:30 +0800

    feat: support AnalyticDB vector store

--- api/configs/middleware/__init__.py
+++ api/configs/middleware/__init__.py
@@ -10,6 +10,7 @@ from configs.middleware.storage.azure_blob_storage_config import AzureBlobStorag
 from configs.middleware.storage.google_cloud_storage_config import GoogleCloudStorageConfig
 from configs.middleware.storage.oci_storage_config import OCIStorageConfig
 from configs.middleware.storage.tencent_cos_storage_config import TencentCloudCOSStorageConfig
+from configs.middleware.vdb.analyticdb_config import AnalyticdbConfig
 from configs.middleware.vdb.chroma_config import ChromaConfig
 from configs.middleware.vdb.milvus_config import MilvusConfig
 from configs.middleware.vdb.opensearch_config import OpenSearchConfig
@@ -183,6 +184,7 @@ class MiddlewareConfig(
 
     # configs of vdb and vdb providers
     VectorStoreConfig,
+    AnalyticdbConfig,
     ChromaConfig,
     MilvusConfig,
     OpenSearchConfig,
