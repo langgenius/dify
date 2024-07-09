7c70eb87bc4d281288b140ed6c6c2a8cb92b1ff5
8bitpd <51897400+lpdink@users.noreply.github.com>
2024-07-09 13:32:04 +0800

    feat: support AnalyticDB vector store (#5586)
    Co-authored-by: xiaozeyu <xiaozeyu.xzy@alibaba-inc.com>

--- api/core/rag/datasource/vdb/vector_factory.py
+++ api/core/rag/datasource/vdb/vector_factory.py
@@ -84,6 +84,9 @@ class Vector:
             case VectorType.OPENSEARCH:
                 from core.rag.datasource.vdb.opensearch.opensearch_vector import OpenSearchVectorFactory
                 return OpenSearchVectorFactory
+            case VectorType.ANALYTICDB:
+                from core.rag.datasource.vdb.analyticdb.analyticdb_vector import AnalyticdbVectorFactory
+                return AnalyticdbVectorFactory
             case _:
                 raise ValueError(f"Vector store {vector_type} is not supported.")
 
