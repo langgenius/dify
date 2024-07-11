5ed325c2a23ea037f5def97cfe2da8d263b77926
svcvit <svcvit@gmail.com>
2024-07-11 10:54:57 +0800

    Rewrite the translation function

--- api/core/tools/provider/builtin/google_translate/tools/translate.py
+++ api/core/tools/provider/builtin/google_translate/tools/translate.py
@@ -1,7 +1,5 @@
 from typing import Any, Union
-
-from py_trans import PyTranslator
-
+import requests
 from core.tools.entities.tool_entities import ToolInvokeMessage
 from core.tools.tool.builtin_tool import BuiltinTool
 
@@ -14,12 +12,10 @@ class GoogleTranslate(BuiltinTool):
         """
             invoke tools
         """
-        # get content
         content = tool_parameters.get('content', '')
         if not content:
             return self.create_text_message('Invalid parameter content')
         
-        # get json filter
         dest = tool_parameters.get('dest', '')
         if not dest:
             return self.create_text_message('Invalid parameter destination language')
@@ -30,13 +26,24 @@ class GoogleTranslate(BuiltinTool):
         except Exception:
             return self.create_text_message('Translation service error, please check the network')
 
-    # Extract data from JSON content
     def _translate(self, content: str, dest: str) -> str:
         try:
-            tr = PyTranslator()
-            response_json = tr.google(content, dest)
-            translation = response_json['translation']
-            return str(translation)
+            url = "https://translate.googleapis.com/translate_a/single"
+            params = {
+                "client": "gtx",
+                "sl": "auto",
+                "tl": dest,
+                "dt": "t",
+                "q": content
+            }
 
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+            }
+
+            response_json = requests.get(url, params=params, headers=headers).json()
+            result = response_json[0]
+            translated_text = ''.join([item[0] for item in result if item[0]])
+            return str(translated_text)
         except Exception as e:
             return str(e)
