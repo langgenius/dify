10c2b133d2824c37a2f3110bcabb276e0a05150d
StyleZhang <jasonapring2015@outlook.com>
2024-07-12 11:33:34 +0800

    add

--- web/app/components/app/create-from-dsl-modal/index.tsx
+++ web/app/components/app/create-from-dsl-modal/index.tsx
@@ -16,19 +16,22 @@ import { useProviderContext } from '@/context/provider-context'
 import AppsFull from '@/app/components/billing/apps-full-in-dialog'
 import { NEED_REFRESH_APP_LIST_KEY } from '@/config'
 import { getRedirection } from '@/utils/app-redirection'
+import cn from '@/utils/classnames'
 
 type CreateFromDSLModalProps = {
   show: boolean
   onSuccess?: () => void
   onClose: () => void
+  activeTab?: string
 }
 
-const CreateFromDSLModal = ({ show, onSuccess, onClose }: CreateFromDSLModalProps) => {
+const CreateFromDSLModal = ({ show, onSuccess, onClose, activeTab = 'from-file' }: CreateFromDSLModalProps) => {
   const { push } = useRouter()
   const { t } = useTranslation()
   const { notify } = useContext(ToastContext)
   const [currentFile, setDSLFile] = useState<File>()
   const [fileContent, setFileContent] = useState<string>()
+  const [currentTab, setCurrentTab] = useState(activeTab)
 
   const readFile = (file: File) => {
     const reader = new FileReader()
@@ -76,20 +79,66 @@ const CreateFromDSLModal = ({ show, onSuccess, onClose }: CreateFromDSLModalProp
     isCreatingRef.current = false
   }
 
+  const tabs = [
+    {
+      key: 'from-file',
+      label: 'from dsl file',
+    },
+    {
+      key: 'from-url',
+      label: 'from url',
+    },
+  ]
+
   return (
     <Modal
-      className='px-8 py-6 max-w-[520px] w-[520px] rounded-xl'
+      className='p-0 w-[520px] rounded-2xl border-[0.5px] border-components-panel-border bg-components-panel-bg shadow-xl'
       isShow={show}
       onClose={() => { }}
     >
-      <div className='relative pb-2 text-xl font-medium leading-[30px] text-gray-900'>{t('app.createFromConfigFile')}</div>
-      <div className='absolute right-4 top-4 p-2 cursor-pointer' onClick={onClose}>
-        <RiCloseLine className='w-4 h-4 text-gray-500' />
+      <div className='flex items-center justify-between pt-3 pl-6 pr-5 text-text-primary title-2xl-semi-bold'>
+        {t('app.createFromConfigFile')}
+        <div className='flex items-center w-8 h-8 cursor-pointer'>
+          <RiCloseLine className='w-5 h-5 text-text-tertiary' />
+        </div>
       </div>
-      <Uploader
-        file={currentFile}
-        updateFile={handleFile}
-      />
+      <div className='flex items-center px-6 h-9 space-x-6 system-md-semibold text-text-tertiary border-b border-divider-subtle'>
+        {
+          tabs.map(tab => (
+            <div
+              key={tab.key}
+              className={cn(
+                'border-[2px] cursor-pointer',
+                currentTab === tab.key && 'text-text-primary',
+              )}
+              onClick={() => setCurrentTab(tab.key)}
+            >
+              {tab.label}
+            </div>
+          ))
+        }
+      </div>
+      <div className='px-6 py-4'>
+        {
+          currentTab === 'from-file' && (
+            <Uploader
+              className='mt-0'
+              file={currentFile}
+              updateFile={handleFile}
+            />
+          )
+        }
+        {
+          currentTab === 'from-url' && (
+            <div>
+              <div className='mb-1 system-md-semibold leading6'>DSL URL</div>
+              <input
+                className='px-2 w-full h-8 border border-components-input-border-active bg-components-input-bg-active rounded-lg outline-none appearance-none placeholder:text-components-input-text-placeholder system-sm-regular'
+              />
+            </div>
+          )
+        }
+      </div>
       {isAppsFull && <AppsFull loc='app-create-dsl' />}
       <div className='pt-6 flex justify-end'>
         <Button className='mr-2' onClick={onClose}>{t('app.newApp.Cancel')}</Button>
