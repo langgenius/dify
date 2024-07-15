c3656a137908f31b9448db62224819837beda5c1
StyleZhang <jasonapring2015@outlook.com>
2024-07-15 14:22:19 +0800

    add

--- web/app/components/system-features-initor/license-info/index.tsx
+++ web/app/components/system-features-initor/license-info/index.tsx
@@ -6,7 +6,7 @@ import s from './styles.module.css'
 const LicenseInfo = () => {
   const { systemFeatures } = useSystemFeaturesStore()
 
-  if (!systemFeatures.expired_at) {
+  if (systemFeatures.expired_at) {
     return (
       <div className='fixed inset-0 flex flex-col pt-14 z-[99999]'>
         <div className={cn(s.bg, 'grow flex flex-col items-center justify-center bg-white')}>
