5789d9cb5bda3740963b69055d80d60707e721fa
coldstone <tainray@gmail.com>
2024-07-16 09:37:18 +0800

    fix: changed the name from `mingdao-hap` to `hap`

--- api/core/tools/provider/builtin/hap/hap.py
+++ api/core/tools/provider/builtin/hap/hap.py
@@ -3,6 +3,6 @@ from typing import Any
 from core.tools.provider.builtin_tool_provider import BuiltinToolProviderController
 
 
-class MingdaoHapProvider(BuiltinToolProviderController):
+class HapProvider(BuiltinToolProviderController):
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
         pass
