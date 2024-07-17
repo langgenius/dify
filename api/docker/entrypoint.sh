57fc93eb3d15414eea31a1f8a87750218aa3887d
ZuzooVn <zuzoovn@gmail.com>
2024-07-17 11:20:01 +0700

    feat: auto get the number of available CPU cores

--- api/docker/entrypoint.sh
+++ api/docker/entrypoint.sh
@@ -8,12 +8,18 @@ if [[ "${MIGRATION_ENABLED}" == "true" ]]; then
 fi
 
 if [[ "${MODE}" == "worker" ]]; then
+  # Get the number of available CPU cores
+  AVAILABLE_CORES=$(nproc)
+
   if [ "${CELERY_AUTO_SCALE,,}" = "true" ]; then
-    MAX_WORKERS=${CELERY_MAX_WORKERS:-4}
+    # Set MAX_WORKERS to the number of available cores if not specified
+    MAX_WORKERS=${CELERY_MAX_WORKERS:-$AVAILABLE_CORES}
     MIN_WORKERS=${CELERY_MIN_WORKERS:-1}
     CONCURRENCY_OPTION="--autoscale=${MAX_WORKERS},${MIN_WORKERS}"
   else
-    CONCURRENCY_OPTION="-c ${CELERY_WORKER_AMOUNT:-1}"
+    # If auto-scale is not enabled, use the number of available cores as the default
+    WORKER_AMOUNT=${CELERY_WORKER_AMOUNT:-$AVAILABLE_CORES}
+    CONCURRENCY_OPTION="-c ${WORKER_AMOUNT}"
   fi
 
   celery -A app.celery worker -P ${CELERY_WORKER_CLASS:-gevent} $CONCURRENCY_OPTION --loglevel INFO \
