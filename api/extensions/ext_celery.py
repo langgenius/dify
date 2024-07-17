0885c2ad640e9d11c834213910fe30e8dd2d95c3
jyong <718720800@qq.com>
2024-07-17 17:30:27 +0800

    optimize the unused dataset clean logic

--- api/extensions/ext_celery.py
+++ api/extensions/ext_celery.py
@@ -51,7 +51,7 @@ def init_app(app: Flask) -> Celery:
         },
         'clean_unused_datasets_task': {
             'task': 'schedule.clean_unused_datasets_task.clean_unused_datasets_task',
-            'schedule': timedelta(days=day),
+            'schedule': timedelta(minutes=1),
         }
     }
     celery_app.conf.update(
