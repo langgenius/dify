66b5cb58853585d9ce59a0f49b0bc0068f2b8ab1
Joe <1264204425@qq.com>
2024-07-18 17:32:27 +0800

    feat: update RULE_CONFIG_PROMPT_GENERATE_TEMPLATE

--- api/core/llm_generator/prompts.py
+++ api/core/llm_generator/prompts.py
@@ -86,12 +86,12 @@ Here is a task description for which I would like you to create a high-quality p
 {{TASK_DESCRIPTION}}
 </task_description>
 Based on task description, please create a well-structured prompt template that another AI could use to consistently complete the task. The prompt template should include:
-Descriptive variable names surrounded by {{ }} (two curly brackets) to indicate where the actual values will be substituted in. Choose variable names that clearly indicate the type of value expected. 
-Clear instructions for the AI that will be using this prompt, demarcated with <instructions> tags. The instructions should provide step-by-step directions on how to complete the task using the input variables. Also Specifies in the instructions that the output should not contain any xml tag. 
-Relevant examples if needed to clarify the task further, demarcated with <example> tags. Do not use curly brackets any other than in <instruction> section. 
-Any other relevant sections demarcated with appropriate XML tags like <input>, <output>, etc.
-Use the same language as task description. 
-Output in xml and start with <instruction>
+- Descriptive variable names surrounded by {{ }} (two curly brackets) to indicate where the actual values will be substituted in. Choose variable names that clearly indicate the type of value expected. Variable names have to be composed of number, english alphabets and underline and nothing else. 
+- Clear instructions for the AI that will be using this prompt, demarcated with <instructions> tags. The instructions should provide step-by-step directions on how to complete the task using the input variables. Also Specifies in the instructions that the output should not contain any xml tag. 
+- Relevant examples if needed to clarify the task further, demarcated with <example> tags. Do not use curly brackets any other than in <instruction> section. 
+- Any other relevant sections demarcated with appropriate XML tags like <input>, <output>, etc.
+- Use the same language as task description. 
+- Output in ``` xml ``` and start with <instruction>
 Please generate the full prompt template and output only the prompt template.
 """
 
