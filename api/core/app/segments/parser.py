d2ec591e815b18d25fe043e6e41ca97c9ce4913e
-LAN- <laipz8200@outlook.com>
2024-07-19 11:08:20 +0800

    refactor(api/core/app/segments/parser.py): Rename `convert_mixed_template` to `convert_template`

--- api/core/app/segments/parser.py
+++ api/core/app/segments/parser.py
@@ -6,7 +6,7 @@ from core.workflow.entities.variable_pool import VariablePool
 VARIABLE_PATTERN = re.compile(r'\{\{#([a-zA-Z0-9_]{1,50}(?:\.[a-zA-Z_][a-zA-Z0-9_]{0,29}){1,10})#\}\}')
 
 
-def convert_mixed_template(*, template: str, variable_pool: VariablePool):
+def convert_template(*, template: str, variable_pool: VariablePool):
     parts = re.split(VARIABLE_PATTERN, template)
     segments = []
     for part in parts:
--- api/tests/unit_tests/app/test_segment.py
+++ api/tests/unit_tests/app/test_segment.py
@@ -15,8 +15,39 @@ def test_segment_group_to_text():
         ],
     )
     variable_pool.add(('node_id', 'custom_query'), 'fake-user-query')
-    template = "Hello, {{#sys.user_id#}}! Your query is {{#node_id.custom_query#}}. And your key is {{#env.secret_key#}}."
-    segments_group = parser.convert_mixed_template(template=template, variable_pool=variable_pool)
+    template = (
+        'Hello, {{#sys.user_id#}}! Your query is {{#node_id.custom_query#}}. And your key is {{#env.secret_key#}}.'
+    )
+    segments_group = parser.convert_template(template=template, variable_pool=variable_pool)
 
-    assert segments_group.text == "Hello, fake-user-id! Your query is fake-user-query. And your key is fake-secret-key."
-    assert segments_group.log == f"Hello, fake-user-id! Your query is fake-user-query. And your key is {encrypter.obfuscated_token('fake-secret-key')}."
+    assert segments_group.text == 'Hello, fake-user-id! Your query is fake-user-query. And your key is fake-secret-key.'
+    assert (
+        segments_group.log
+        == f"Hello, fake-user-id! Your query is fake-user-query. And your key is {encrypter.obfuscated_token('fake-secret-key')}."
+    )
+
+
+def test_convert_constant_to_segment_group():
+    variable_pool = VariablePool(
+        system_variables={},
+        user_inputs={},
+        environment_variables=[],
+    )
+    template = 'Hello, world!'
+    segments_group = parser.convert_template(template=template, variable_pool=variable_pool)
+    assert segments_group.text == 'Hello, world!'
+    assert segments_group.log == 'Hello, world!'
+
+
+def test_convert_variable_to_segment_group():
+    variable_pool = VariablePool(
+        system_variables={
+            SystemVariable('user_id'): 'fake-user-id',
+        },
+        user_inputs={},
+        environment_variables=[],
+    )
+    template = '{{#sys.user_id#}}'
+    segments_group = parser.convert_template(template=template, variable_pool=variable_pool)
+    assert segments_group.text == 'fake-user-id'
+    assert segments_group.log == 'fake-user-id'
