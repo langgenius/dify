a5fcd91ba5e669806672de54a3be62f0bb2eeea8
Sangmin Ahn <gijigae@gmail.com>
2024-07-19 13:54:15 +0900

    chore: make text generation timeout duration configurable (#6450)

--- web/config/index.ts
+++ web/config/index.ts
@@ -245,3 +245,5 @@ Thought: {{agent_scratchpad}}
 }
 
 export const VAR_REGEX = /\{\{(#[a-zA-Z0-9_-]{1,50}(\.[a-zA-Z_][a-zA-Z0-9_]{0,29}){1,10}#)\}\}/gi
+
+export const TEXT_GENERATION_TIMEOUT_MS = 60000
