87d583f4549a2fe001d5d72f01a77a7652ab52b4
Jason Tan <cuckootan@users.noreply.github.com>
2024-07-22 15:01:25 +0800

    fix: privilege for editor role (#6521)

--- web/app/components/header/account-setting/model-provider-page/provider-card/index.tsx
+++ web/app/components/header/account-setting/model-provider-page/provider-card/index.tsx
@@ -19,6 +19,7 @@ import ProviderIcon from '../provider-icon'
 import s from './index.module.css'
 import { Settings01 } from '@/app/components/base/icons/src/vender/line/general'
 import Button from '@/app/components/base/button'
+import { useAppContext } from '@/context/app-context'
 
 type ProviderCardProps = {
   provider: ModelProvider
@@ -31,6 +32,7 @@ const ProviderCard: FC<ProviderCardProps> = ({
 }) => {
   const { t } = useTranslation()
   const language = useLanguage()
+  const { isCurrentWorkspaceManager } = useAppContext()
   const configurateMethods = provider.configurate_methods.filter(method => method !== ConfigurationMethodEnum.fetchFromRemote)
 
   return (
@@ -72,6 +74,7 @@ const ProviderCard: FC<ProviderCardProps> = ({
                     key={method}
                     className={'h-7 text-xs shrink-0'}
                     onClick={() => onOpenModal(method)}
+                    disabled={!isCurrentWorkspaceManager}
                   >
                     <Settings01 className={`mr-[5px] w-3.5 h-3.5 ${s.icon}`} />
                     <span className='text-xs inline-flex items-center justify-center overflow-ellipsis shrink-0'>{t('common.operation.setup')}</span>
@@ -83,6 +86,7 @@ const ProviderCard: FC<ProviderCardProps> = ({
                   key={method}
                   className='px-0 h-7 text-xs'
                   onClick={() => onOpenModal(method)}
+                  disabled={!isCurrentWorkspaceManager}
                 >
                   <RiAddLine className='mr-[5px] w-3.5 h-3.5' />
                   {t('common.modelProvider.addModel')}
