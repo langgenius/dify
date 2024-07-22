6e865d8f827a9dab4292c0aa6bd7e48fcc6c07cf
jyong <718720800@qq.com>
2024-07-22 13:18:15 +0800

    add reranking enable field

--- api/core/workflow/nodes/knowledge_retrieval/entities.py
+++ api/core/workflow/nodes/knowledge_retrieval/entities.py
@@ -45,6 +45,7 @@ class MultipleRetrievalConfig(BaseModel):
     top_k: int
     score_threshold: Optional[float] = None
     reranking_mode: str = 'reranking_model'
+    reranking_enable: bool = False
     reranking_model: RerankingModelConfig
     weights: WeightedScoreConfig
 
