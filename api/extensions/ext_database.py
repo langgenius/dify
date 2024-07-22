05c892cc9ec5629b12c1315b9f7ed13d1836bf53
-LAN- <laipz8200@outlook.com>
2024-07-22 13:44:04 +0800

    chore(db): Remove `expire_on_commit=False`

--- api/extensions/ext_database.py
+++ api/extensions/ext_database.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
-# TODO: Check if this is the right way to do it
-db = SQLAlchemy(session_options={"expire_on_commit": False})
+db = SQLAlchemy()
 
 
 def init_app(app):
