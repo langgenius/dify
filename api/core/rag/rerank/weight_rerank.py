bf6d729a20e202454339c8a601294633bbdb38a9
jyong <718720800@qq.com>
2024-07-23 17:35:17 +0800

    add reranking enable field

--- api/core/rag/rerank/weight_rerank.py
+++ api/core/rag/rerank/weight_rerank.py
@@ -111,10 +111,10 @@ class WeightRerankRunner:
 
         def cosine_similarity(vec1, vec2):
             intersection = set(vec1.keys()) & set(vec2.keys())
-            numerator = sum([vec1[x] * vec2[x] for x in intersection])
+            numerator = sum(vec1[x] * vec2[x] for x in intersection)
 
-            sum1 = sum([vec1[x] ** 2 for x in vec1.keys()])
-            sum2 = sum([vec2[x] ** 2 for x in vec2.keys()])
+            sum1 = sum(vec1[x] ** 2 for x in vec1.keys())
+            sum2 = sum(vec2[x] ** 2 for x in vec2.keys())
             denominator = math.sqrt(sum1) * math.sqrt(sum2)
 
             if not denominator:
