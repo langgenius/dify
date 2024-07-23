7303b53af1990d203558b13c92931498c330c763
takatost <takatost@gmail.com>
2024-07-23 16:18:52 +0800

    fix bug

--- api/core/workflow/nodes/code/code_node.py
+++ api/core/workflow/nodes/code/code_node.py
@@ -57,7 +57,7 @@ class CodeNode(BaseNode):
         variables = {}
         for variable_selector in node_data.variables:
             variable = variable_selector.variable
-            value = variable_pool.get_any(variable_selector.value_selector)
+            value = self.graph_runtime_state.variable_pool.get_any(variable_selector.value_selector)
 
             variables[variable] = value
         # Run code
