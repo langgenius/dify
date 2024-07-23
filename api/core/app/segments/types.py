f4b328552879e055c906b81f81c3fe1405894581
-LAN- <laipz8200@outlook.com>
2024-07-23 21:46:08 +0800

    fix(variables): NoneVariable should inherit from NoneSegment. (#6584)

--- api/core/app/segments/types.py
+++ api/core/app/segments/types.py
@@ -2,17 +2,10 @@ from enum import Enum
 
 
 class SegmentType(str, Enum):
-    STRING = 'string'
-    NUMBER = 'number'
-    FILE = 'file'
-
-    SECRET = 'secret'
-
-    OBJECT = 'object'
-
-    ARRAY = 'array'
-    ARRAY_STRING = 'array[string]'
-    ARRAY_NUMBER = 'array[number]'
-    ARRAY_OBJECT = 'array[object]'
-    ARRAY_FILE = 'array[file]'
     NONE = 'none'
+    NUMBER = 'number'
+    STRING = 'string'
+    SECRET = 'secret'
+    ARRAY = 'array'
+    OBJECT = 'object'
+    FILE = 'file'
