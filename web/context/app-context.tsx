e6444822861a6b69c1bf838cf3b4dd16393acf68
Joel <iamjoel007@gmail.com>
2024-07-23 13:35:52 +0800

    chore: adds

--- web/context/app-context.tsx
+++ web/context/app-context.tsx
@@ -133,7 +133,7 @@ export const AppContextProvider: FC<AppContextProviderProps> = ({ children }) =>
     }}>
       <div className='flex flex-col h-full overflow-y-auto'>
         {globalThis.document?.body?.getAttribute('data-public-maintenance-notice') && <MaintenanceNotice />}
-        <div ref={pageContainerRef} className='grow relative flex flex-col overflow-y-auto overflow-x-hidden bg-gray-100'>
+        <div ref={pageContainerRef} className='grow relative flex flex-col overflow-y-auto overflow-x-hidden bg-background-body'>
           {children}
         </div>
       </div>
