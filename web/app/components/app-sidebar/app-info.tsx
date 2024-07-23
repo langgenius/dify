6b920833f1c76c2f4e0a36ee10171189e1b7bb29
Joel <iamjoel007@gmail.com>
2024-07-23 17:11:02 +0800

    chore: layout UI upgrade (#6577)

--- web/app/components/app-sidebar/app-info.tsx
+++ web/app/components/app-sidebar/app-info.tsx
@@ -218,7 +218,7 @@ const AppInfo = ({ expand }: IAppInfoProps) => {
             </div>
             {expand && (
               <div className="grow w-0">
-                <div className='flex justify-between items-center text-sm leading-5 font-medium text-gray-900'>
+                <div className='flex justify-between items-center text-sm leading-5 font-medium text-text-secondary'>
                   <div className='truncate' title={appDetail.name}>{appDetail.name}</div>
                   {isCurrentWorkspaceEditor && <RiArrowDownSLine className='shrink-0 ml-[2px] w-3 h-3 text-gray-500' />}
                 </div>
