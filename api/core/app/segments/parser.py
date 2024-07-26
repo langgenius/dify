5d77dc4f582060f02dae0dd95aded60718062a20
-LAN- <laipz8200@outlook.com>
2024-07-26 18:19:33 +0800

    feat(api/core/app/segments/parser.py): Remove blank segment in `convert_template` (#6709)
    Signed-off-by: -LAN- <laipz8200@outlook.com>

--- api/core/app/segments/parser.py
+++ api/core/app/segments/parser.py
@@ -10,7 +10,7 @@ VARIABLE_PATTERN = re.compile(r'\{\{#([a-zA-Z0-9_]{1,50}(?:\.[a-zA-Z_][a-zA-Z0-9
 def convert_template(*, template: str, variable_pool: VariablePool):
     parts = re.split(VARIABLE_PATTERN, template)
     segments = []
-    for part in parts:
+    for part in filter(lambda x: x, parts):
         if '.' in part and (value := variable_pool.get(part.split('.'))):
             segments.append(value)
         else:
