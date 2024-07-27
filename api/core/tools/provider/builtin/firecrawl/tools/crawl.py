21f6caacd4d08817a5ce3243d36600665c30daba
非法操作 <hjlarry@163.com>
2024-07-27 15:00:06 +0800

    feat: enhance the firecrawl tool (#6705)

--- api/core/tools/provider/builtin/firecrawl/tools/crawl.py
+++ api/core/tools/provider/builtin/firecrawl/tools/crawl.py
@@ -1,36 +1,48 @@
-import json
-from typing import Any, Union
+from typing import Any
 
 from core.tools.entities.tool_entities import ToolInvokeMessage
-from core.tools.provider.builtin.firecrawl.firecrawl_appx import FirecrawlApp
+from core.tools.provider.builtin.firecrawl.firecrawl_appx import FirecrawlApp, get_array_params, get_json_params
 from core.tools.tool.builtin_tool import BuiltinTool
 
 
 class CrawlTool(BuiltinTool):
-    def _invoke(self, user_id: str, tool_parameters: dict[str, Any]) -> Union[ToolInvokeMessage, list[ToolInvokeMessage]]:
-        app = FirecrawlApp(api_key=self.runtime.credentials['firecrawl_api_key'], base_url=self.runtime.credentials['base_url'])
+    def _invoke(self, user_id: str, tool_parameters: dict[str, Any]) -> ToolInvokeMessage:
+        """
+        the crawlerOptions and pageOptions comes from doc here:
+        https://docs.firecrawl.dev/api-reference/endpoint/crawl
+        """
+        app = FirecrawlApp(api_key=self.runtime.credentials['firecrawl_api_key'],
+                           base_url=self.runtime.credentials['base_url'])
+        crawlerOptions = {}
+        pageOptions = {}
 
-        options = {
-            'crawlerOptions': {
-                'excludes': tool_parameters.get('excludes', '').split(',') if tool_parameters.get('excludes') else [],
-                'includes': tool_parameters.get('includes', '').split(',') if tool_parameters.get('includes') else [],
-                'limit': tool_parameters.get('limit', 5)
-            },
-            'pageOptions': {
-                'onlyMainContent': tool_parameters.get('onlyMainContent', False)
-            }
-        }
+        wait_for_results = tool_parameters.get('wait_for_results', True)
+
+        crawlerOptions['excludes'] = get_array_params(tool_parameters, 'excludes')
+        crawlerOptions['includes'] = get_array_params(tool_parameters, 'includes')
+        crawlerOptions['returnOnlyUrls'] = tool_parameters.get('returnOnlyUrls', False)
+        crawlerOptions['maxDepth'] = tool_parameters.get('maxDepth')
+        crawlerOptions['mode'] = tool_parameters.get('mode')
+        crawlerOptions['ignoreSitemap'] = tool_parameters.get('ignoreSitemap', False)
+        crawlerOptions['limit'] = tool_parameters.get('limit', 5)
+        crawlerOptions['allowBackwardCrawling'] = tool_parameters.get('allowBackwardCrawling', False)
+        crawlerOptions['allowExternalContentLinks'] = tool_parameters.get('allowExternalContentLinks', False)
+
+        pageOptions['headers'] = get_json_params(tool_parameters, 'headers')
+        pageOptions['includeHtml'] = tool_parameters.get('includeHtml', False)
+        pageOptions['includeRawHtml'] = tool_parameters.get('includeRawHtml', False)
+        pageOptions['onlyIncludeTags'] = get_array_params(tool_parameters, 'onlyIncludeTags')
+        pageOptions['removeTags'] = get_array_params(tool_parameters, 'removeTags')
+        pageOptions['onlyMainContent'] = tool_parameters.get('onlyMainContent', False)
+        pageOptions['replaceAllPathsWithAbsolutePaths'] = tool_parameters.get('replaceAllPathsWithAbsolutePaths', False)
+        pageOptions['screenshot'] = tool_parameters.get('screenshot', False)
+        pageOptions['waitFor'] = tool_parameters.get('waitFor', 0)
 
         crawl_result = app.crawl_url(
-            url=tool_parameters['url'], 
-            params=options,
-            wait=True
+            url=tool_parameters['url'],
+            wait=wait_for_results,
+            crawlerOptions=crawlerOptions,
+            pageOptions=pageOptions
         )
 
-        if not isinstance(crawl_result, str):
-            crawl_result = json.dumps(crawl_result, ensure_ascii=False, indent=4)
-
-        if not crawl_result:
-            return self.create_text_message("Crawl request failed.")
-
-        return self.create_text_message(crawl_result)
+        return self.create_json_message(crawl_result)
