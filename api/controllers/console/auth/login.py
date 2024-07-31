70ba2454fdc62311ea2f12c60056c0d0f0e85187
lixiangcheng1 <lixiangcheng1@wanda.cn>
2024-07-31 10:03:42 +0800

    [ADD]Add GitHubOAuth and GoogleOAuth third-party logout functionality
    Add GitHubOAuth and GoogleOAuth third-party logout functionality

--- api/controllers/console/auth/login.py
+++ api/controllers/console/auth/login.py
@@ -9,8 +9,9 @@ from controllers.console import api
 from controllers.console.setup import setup_required
 from libs.helper import email, get_remote_ip
 from libs.password import valid_password
-from models.account import Account
+from models.account import Account, AccountIntegrate
 from services.account_service import AccountService, TenantService
+from extensions.ext_database import db
 
 
 class LoginApi(Resource):
@@ -47,6 +48,16 @@ class LogoutApi(Resource):
     @setup_required
     def get(self):
         account = cast(Account, flask_login.current_user)
+        # OAuth backend exit
+        from controllers.console.auth.oauth import get_oauth_providers
+        OAUTH_PROVIDERS = get_oauth_providers()
+        account_integrates = AccountIntegrate.query.filter_by(account_id=account.id).all()
+        for account_integrate in account_integrates:
+            oauth_provider = OAUTH_PROVIDERS.get(account_integrate.provider)
+            if oauth_provider and account_integrate.encrypted_token:
+                oauth_provider.logout(account_integrate.encrypted_token)
+                account_integrate.encrypted_token = ""
+                db.session.commit()
         token = request.headers.get('Authorization', '').split(' ')[1]
         AccountService.logout(account=account, token=token)
         flask_login.logout_user()
