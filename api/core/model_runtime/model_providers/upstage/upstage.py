2cb2b65c877e98d3c6cf2963654acfed4b206f34
JuHyung-Son <sonju0427@gmail.com>
2024-08-01 17:04:21 +0900

    reformat

--- api/core/model_runtime/model_providers/upstage/upstage.py
+++ api/core/model_runtime/model_providers/upstage/upstage.py
@@ -1,5 +1,4 @@
 import logging
-from collections.abc import Mapping
 
 from core.model_runtime.entities.model_entities import ModelType
 from core.model_runtime.errors.validate import CredentialsValidateFailedError
