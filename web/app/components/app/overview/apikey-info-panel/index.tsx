85d974e95ed3ac3c9754d426e53ec8973b42bcb1
like <likenamehaojie@hotmail.com>
2024-08-02 17:21:20 +0800

    add sias env

--- web/app/components/app/overview/apikey-info-panel/index.tsx
+++ web/app/components/app/overview/apikey-info-panel/index.tsx
@@ -52,7 +52,7 @@ const APIKeyInfoPanel: FC = () => {
         <div className='text-sm font-medium'>{t('appOverview.apiKeyInfo.setAPIBtn')}</div>
         <LinkExternal02 className='w-4 h-4' />
       </Button>
-      {!isCloud && (
+      {/* {!isCloud && (
         <a
           className='mt-2 flex items-center h-[26px] text-xs  font-medium text-[#155EEF] p-1 space-x-1'
           href='https://cloud.dify.ai/apps'
@@ -61,7 +61,7 @@ const APIKeyInfoPanel: FC = () => {
           <div>{t('appOverview.apiKeyInfo.tryCloud')}</div>
           <LinkExternal02 className='w-3 h-3' />
         </a>
-      )}
+      )} */}
       <div
         onClick={() => setIsShow(false)}
         className='absolute right-4 top-4 flex items-center justify-center w-8 h-8 cursor-pointer '>
