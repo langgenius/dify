07b3ea47c13b87ee06224aad3a37038886a56e45
-LAN- <laipz8200@outlook.com>
2024-08-03 15:50:46 +0800

    refactor(api/core/workflow/workflow_engine_manager.py): Take variable_pool out of workflow engine manager.

--- api/core/workflow/workflow_engine_manager.py
+++ api/core/workflow/workflow_engine_manager.py
@@ -8,7 +8,7 @@ from core.app.apps.base_app_queue_manager import GenerateTaskStoppedException
 from core.app.entities.app_invoke_entities import InvokeFrom
 from core.file.file_obj import FileExtraConfig, FileTransferMethod, FileType, FileVar
 from core.workflow.callbacks.base_workflow_callback import WorkflowCallback
-from core.workflow.entities.node_entities import NodeRunMetadataKey, NodeRunResult, NodeType, SystemVariable
+from core.workflow.entities.node_entities import NodeRunMetadataKey, NodeRunResult, NodeType
 from core.workflow.entities.variable_pool import VariablePool, VariableValue
 from core.workflow.entities.workflow_entities import WorkflowNodeAndResult, WorkflowRunState
 from core.workflow.errors import WorkflowNodeRunFailedError
@@ -93,10 +93,9 @@ class WorkflowEngineManager:
         user_id: str,
         user_from: UserFrom,
         invoke_from: InvokeFrom,
-        user_inputs: Mapping[str, Any],
-        system_inputs: Mapping[SystemVariable, Any],
         callbacks: Sequence[WorkflowCallback],
-        call_depth: int = 0
+        call_depth: int = 0,
+        variable_pool: VariablePool,
     ) -> None:
         """
         :param workflow: Workflow instance
@@ -121,13 +120,6 @@ class WorkflowEngineManager:
         if not isinstance(graph.get('edges'), list):
             raise ValueError('edges in workflow graph must be a list')
 
-        # init variable pool
-        variable_pool = VariablePool(
-            system_variables=system_inputs,
-            user_inputs=user_inputs,
-            environment_variables=workflow.environment_variables,
-            conversation_variables=workflow.conversation_variables,
-        )
 
         workflow_call_max_depth = dify_config.WORKFLOW_CALL_MAX_DEPTH
         if call_depth > workflow_call_max_depth:
