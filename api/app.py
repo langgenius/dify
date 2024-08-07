8c798a575fbc42aa1c66fcde6432b86fa7ba6c57
jyong <718720800@qq.com>
2024-08-07 19:52:00 +0800

    add nltk punkt resource

--- api/app.py
+++ api/app.py
@@ -9,10 +9,6 @@ if os.environ.get("DEBUG", "false").lower() != 'true':
 
     grpc.experimental.gevent.init_gevent()
 
-import nltk
-
-nltk.download('punkt')
-
 import json
 import logging
 import sys
