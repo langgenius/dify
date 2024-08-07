f81031c467640f32fd7452a482489c99d7f61ced
Joe <1264204425@qq.com>
2024-08-07 17:55:58 +0800

    feat: add get_app_web_sso_enabled

--- api/services/enterprise/enterprise_service.py
+++ api/services/enterprise/enterprise_service.py
@@ -6,3 +6,7 @@ class EnterpriseService:
     @classmethod
     def get_info(cls):
         return EnterpriseRequest.send_request('GET', '/info')
+
+    @classmethod
+    def get_app_web_sso_enabled(cls, app_code):
+        return EnterpriseRequest.send_request('GET', f'/console/api/enterprise/app-setting/sso?appCode={app_code}')
