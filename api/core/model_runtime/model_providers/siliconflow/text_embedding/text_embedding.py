b4041a3375e3bc582f0a4fa8b2e12a475487a9de
ShAlfred <hi@caizhe.cz>
2024-08-08 16:51:43 +0800

    updates

--- api/core/model_runtime/model_providers/siliconflow/text_embedding/text_embedding.py
+++ api/core/model_runtime/model_providers/siliconflow/text_embedding/text_embedding.py
@@ -10,15 +10,19 @@ class SiliconflowTextEmbeddingModel(OAICompatEmbeddingModel):
     """
     Model class for Siliconflow text embedding model.
     """
+    def validate_credentials(self, model: str, credentials: dict) -> None:
+        self._add_custom_parameters(credentials)
+        super().validate_credentials(model, credentials)
+
     def _invoke(self, model: str, credentials: dict,
                 texts: list[str], user: Optional[str] = None) \
             -> TextEmbeddingResult:
         self._add_custom_parameters(credentials)
-        return super()._invoke(model,credentials,texts,user)
+        return super()._invoke(model, credentials, texts, user)
     
     def get_num_tokens(self, model: str, credentials: dict, texts: list[str]) -> int:
         self._add_custom_parameters(credentials)
-        return super().get_num_tokens(model,credentials,texts)
+        return super().get_num_tokens(model, credentials, texts)
     
     @classmethod
     def _add_custom_parameters(cls, credentials: dict) -> None:
