6a73312720821763d5da9183aa431d3f52fa7678
罗威 <luowei@cvte.com>
2024-08-09 01:04:20 +0800

    Video player Update

--- web/app/components/base/video-gallery/index.tsx
+++ web/app/components/base/video-gallery/index.tsx
@@ -1,53 +1,19 @@
-'use client'
-import React, { useEffect, useMemo, useRef } from 'react'
+import React from 'react'
+import VideoPlayer from './VideoPlayer'
+import styles from './VideoPlayer.module.css'
 
 type Props = {
   srcs: string[]
 }
 
-const getWidthStyle = (imgNum: number) => {
-  if (imgNum === 1) {
-    return {
-      maxWidth: '100%',
-    }
-  }
-
-  if (imgNum === 2 || imgNum === 4) {
-    return {
-      width: 'calc(50% - 4px)',
-    }
-  }
-
-  return {
-    width: 'calc(33.3333% - 5.3333px)',
-  }
-}
-
 const VideoGallery: React.FC<Props> = ({ srcs }) => {
-  const videoRefs = useRef<(HTMLVideoElement | null)[]>([])
-  const videoNum = srcs.length
-  const videoStyle = useMemo(() => getWidthStyle(videoNum), [videoNum])
-
-  useEffect(() => {
-    srcs.forEach((src, index) => {
-      const video = videoRefs.current[index]
-      if (video && src)
-        video.src = src
-    })
-  }, [srcs])
-
   return (
-    <>
+    <div className={styles.audioGallery}>
       {srcs.map((src, index) => (
-        <video
-          controls
-          key={`video_${src}`}
-          style={videoStyle}
-          ref={(ref) => { videoRefs.current[index] = ref }}
-          aria-label={`Video ${index + 1}`}
-        />
+        <VideoPlayer key={`audio_${index}`} src={src} />
       ))}
-    </>
+    </div>
   )
 }
+
 export default React.memo(VideoGallery)
