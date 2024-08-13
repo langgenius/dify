4417dd6fb8775e5e20fa392fad8a0a0696c9a12f
android <android@gmail.com>
2024-08-13 10:26:44 +0700

    title

--- web/app/(commonLayout)/app/(appDetailLayout)/[appId]/layout.tsx
+++ web/app/(commonLayout)/app/(appDetailLayout)/[appId]/layout.tsx
@@ -93,7 +93,7 @@ const AppDetailLayout: FC<IAppDetailLayoutProps> = (props) => {
 
   useEffect(() => {
     if (appDetail) {
-      document.title = `${(appDetail.name || 'App')} - Dify`
+      document.title = `${(appDetail.name || 'App')}`
       const localeMode = localStorage.getItem('app-detail-collapse-or-expand') || 'expand'
       const mode = isMobile ? 'collapse' : 'expand'
       setAppSiderbarExpand(isMobile ? mode : localeMode)
--- web/app/(commonLayout)/datasets/page.tsx
+++ web/app/(commonLayout)/datasets/page.tsx
@@ -7,7 +7,7 @@ const AppList = async () => {
 }
 
 export const metadata = {
-  title: 'Datasets - Dify',
+  title: 'Datasets',
 }
 
 export default AppList
