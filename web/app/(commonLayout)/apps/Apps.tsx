cf901297db6b25f2961dc0c03f66420ef94b4706
android <android@gmail.com>
2024-08-13 10:26:44 +0700

    title

--- web/app/(commonLayout)/apps/Apps.tsx
+++ web/app/(commonLayout)/apps/Apps.tsx
@@ -82,7 +82,7 @@ const Apps = () => {
   ]
 
   useEffect(() => {
-    document.title = `${t('common.menus.apps')} -  Dify`
+    document.title = `${t('common.menus.apps')}`
     if (localStorage.getItem(NEED_REFRESH_APP_LIST_KEY) === '1') {
       localStorage.removeItem(NEED_REFRESH_APP_LIST_KEY)
       mutate()
