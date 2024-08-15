b5a5614f200841b74db7c6513681b9a16247d43f
-LAN- <laipz8200@outlook.com>
2024-08-15 12:34:26 +0800

    chore(api): Introduce Ruff Formatter.

--- api/contexts/__init__.py
+++ api/contexts/__init__.py
@@ -2,6 +2,6 @@ from contextvars import ContextVar
 
 from core.workflow.entities.variable_pool import VariablePool
 
-tenant_id: ContextVar[str] = ContextVar('tenant_id')
+tenant_id: ContextVar[str] = ContextVar("tenant_id")
 
-workflow_variable_pool: ContextVar[VariablePool] = ContextVar('workflow_variable_pool')
+workflow_variable_pool: ContextVar[VariablePool] = ContextVar("workflow_variable_pool")
