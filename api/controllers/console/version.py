feac875c0369027e515ddce2ee8cde0fcba540fa
Inderjot Singh <inderjotsingh141@gmail.com>
2024-08-16 11:50:04 +0530

    version error

--- api/controllers/console/version.py
+++ api/controllers/console/version.py
@@ -14,9 +14,9 @@ class VersionApi(Resource):
 
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('current_version', type=str, required=True, location='args')
+        # parser.add_argument('current_version', type=str, required=True, location='args')
         args = parser.parse_args()
-        check_update_url = dify_config.CHECK_UPDATE_URL
+        # check_update_url = dify_config.CHECK_UPDATE_URL
 
         result = {
             'version': dify_config.CURRENT_VERSION,
@@ -29,24 +29,26 @@ class VersionApi(Resource):
             }
         }
 
-        if not check_update_url:
-            return result
-
-        try:
-            response = requests.get(check_update_url, {
-                'current_version': args.get('current_version')
-            })
-        except Exception as error:
-            logging.warning("Check update version error: {}.".format(str(error)))
-            result['version'] = args.get('current_version')
-            return result
-
-        content = json.loads(response.content)
-        result['version'] = content['version']
-        result['release_date'] = content['releaseDate']
-        result['release_notes'] = content['releaseNotes']
-        result['can_auto_update'] = content['canAutoUpdate']
         return result
+
+        # if not check_update_url:
+        #     return result
+
+        # try:
+        #     response = requests.get(check_update_url, {
+        #         'current_version': args.get('current_version')
+        #     })
+        # except Exception as error:
+        #     logging.warning("Check update version error: {}.".format(str(error)))
+        #     result['version'] = args.get('current_version')
+        #     return result
+
+        # content = json.loads(response.content)
+        # result['version'] = content['version']
+        # result['release_date'] = content['releaseDate']
+        # result['release_notes'] = content['releaseNotes']
+        # result['can_auto_update'] = content['canAutoUpdate']
+        # return result
 
 
 api.add_resource(VersionApi, '/version')
