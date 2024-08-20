6a0272aa2332d38fab8e34321c54f09734980258
android <android@gmail.com>
2024-08-20 18:32:33 +0700

    to_dict

--- api/controllers/stock_api/app/stock.py
+++ api/controllers/stock_api/app/stock.py
@@ -31,7 +31,9 @@ class StockPrice(Resource):
         
         price = get_stock_price(ticker)
         logger.info(f"get_stock_price {price}")
-        return jsonify(price)
+        return {
+            "data": price.to_dict()      
+        }
     
 class StockNews(Resource):
     def get(self):
@@ -45,7 +47,9 @@ class StockFinancial(Resource):
         
         finance = get_financial_data(ticker)
         logger.info(f"get_financial_data {finance}")
-        return jsonify(finance)
+        return {
+            "data": finance.to_dict()        
+        }
 
 api.add_resource(StockIndexApi, '/')
 api.add_resource(StockTicker, '/ticker')
