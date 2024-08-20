6209bfd2fecfaf727e4e588c4fa561485ef0a50d
android <android@gmail.com>
2024-08-20 16:11:42 +0700

    stock_api

--- api/controllers/stock_api/stock.py
+++ api/controllers/stock_api/stock.py
@@ -1,8 +1,8 @@
 from flask_restful import Resource
 
 from configs import dify_config
-from controllers.service_api import api
-from controllers.service_api.wraps import get_stock_price, get_recent_stock_news, get_financial_data
+from controllers.stock_api import api
+from controllers.stock_api.wraps import get_stock_price, get_recent_stock_news, get_financial_data
 
 
 class StockInfo(Resource):
