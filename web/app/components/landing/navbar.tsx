7ceeb39d4c47622fdbd84f25473ce513a171c943
Inderjot Singh <inderjotsingh141@gmail.com>
2024-08-20 13:16:34 +0530

    slight changes

--- web/app/components/landing/navbar.tsx
+++ web/app/components/landing/navbar.tsx
@@ -24,7 +24,7 @@ export default function navbar() {
           </Link>
         </div>
 
-        <div className="flex  items-center gap-4 mr-2 text-sm">
+        <div className="flex  items-center gap-4 text-sm">
           <Link href="/billing" className="cursor-pointer">
             Plans
           </Link>
