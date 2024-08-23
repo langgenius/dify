98e45b17be6a8fe84e5ab76f3a254efd8f54cbf4
douxc <douxc512@gmail.com>
2024-08-23 17:11:06 +0800

    fix: use constant variable replace magic number

--- web/app/signin/check-code/countdown.tsx
+++ web/app/signin/check-code/countdown.tsx
@@ -3,21 +3,24 @@ import { useCountDown } from 'ahooks'
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
+const COUNT_DOWN_TIME_MS = 59000
+const COUNT_DOWN_KEY = 'leftTime'
+
 export default function Countdown() {
   const { t } = useTranslation()
-  const [leftTime, setLeftTime] = useState(Number(localStorage.getItem('leftTime') || 59000))
+  const [leftTime, setLeftTime] = useState(Number(localStorage.getItem(COUNT_DOWN_KEY) || COUNT_DOWN_TIME_MS))
   const [time] = useCountDown({
     leftTime,
-    onEnd: () => { localStorage.removeItem('leftTime') },
+    onEnd: () => { localStorage.removeItem(COUNT_DOWN_KEY) },
   })
 
   const resend = async function () {
-    setLeftTime(59000)
-    localStorage.setItem('timeCountDown', '59000')
+    setLeftTime(COUNT_DOWN_TIME_MS)
+    localStorage.setItem(COUNT_DOWN_KEY, `${COUNT_DOWN_TIME_MS}`)
   }
 
   useEffect(() => {
-    localStorage.setItem('leftTime', `${time}`)
+    localStorage.setItem(COUNT_DOWN_KEY, `${time}`)
   }, [time])
 
   return <p className='text-text-tertiary text-xs'>
