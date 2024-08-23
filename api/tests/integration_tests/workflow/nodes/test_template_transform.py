b035c02f78924020e2329de36b473189851bb409
Bowen Liang <liangbowen@gf.com.cn>
2024-08-23 23:52:25 +0800

    chore(api/tests): apply ruff reformat #7590 (#7591)
    Co-authored-by: -LAN- <laipz8200@outlook.com>

--- api/tests/integration_tests/workflow/nodes/test_template_transform.py
+++ api/tests/integration_tests/workflow/nodes/test_template_transform.py
@@ -8,42 +8,39 @@ from models.workflow import WorkflowNodeExecutionStatus
 from tests.integration_tests.workflow.nodes.__mock.code_executor import setup_code_executor_mock
 
 
-@pytest.mark.parametrize('setup_code_executor_mock', [['none']], indirect=True)
+@pytest.mark.parametrize("setup_code_executor_mock", [["none"]], indirect=True)
 def test_execute_code(setup_code_executor_mock):
-    code = '''{{args2}}'''
+    code = """{{args2}}"""
     node = TemplateTransformNode(
-        tenant_id='1',
-        app_id='1',
-        workflow_id='1',
-        user_id='1',
+        tenant_id="1",
+        app_id="1",
+        workflow_id="1",
+        user_id="1",
         invoke_from=InvokeFrom.WEB_APP,
         user_from=UserFrom.END_USER,
         config={
-            'id': '1',
-            'data': {
-                'title': '123',
-                'variables': [
+            "id": "1",
+            "data": {
+                "title": "123",
+                "variables": [
                     {
-                        'variable': 'args1',
-                        'value_selector': ['1', '123', 'args1'],
+                        "variable": "args1",
+                        "value_selector": ["1", "123", "args1"],
                     },
-                    {
-                        'variable': 'args2',
-                        'value_selector': ['1', '123', 'args2']
-                    }
+                    {"variable": "args2", "value_selector": ["1", "123", "args2"]},
                 ],
-                'template': code,
-            }
-        }
+                "template": code,
+            },
+        },
     )
 
     # construct variable pool
     pool = VariablePool(system_variables={}, user_inputs={}, environment_variables=[])
-    pool.add(['1', '123', 'args1'], 1)
-    pool.add(['1', '123', 'args2'], 3)
-    
+    pool.add(["1", "123", "args1"], 1)
+    pool.add(["1", "123", "args2"], 3)
+
     # execute node
     result = node.run(pool)
-    
+
     assert result.status == WorkflowNodeExecutionStatus.SUCCEEDED
-    assert result.outputs['output'] == '3'
+    assert result.outputs["output"] == "3"
