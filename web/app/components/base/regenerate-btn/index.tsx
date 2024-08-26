f15004c6c8d0dcc5d6f7dd06c7b8782a55f324e4
xuzuodong <hi@xzd.me>
2024-08-26 18:10:24 +0800

    chore: adapt new Tooltip component

--- web/app/components/base/regenerate-btn/index.tsx
+++ web/app/components/base/regenerate-btn/index.tsx
@@ -1,9 +1,7 @@
 'use client'
-import { useRef } from 'react'
 import { t } from 'i18next'
 import { MaterialSymbolsRefresh } from '../icons/src/vender/line/general'
 import Tooltip from '@/app/components/base/tooltip'
-import { randomString } from '@/utils'
 
 type Props = {
   className?: string
@@ -11,14 +9,10 @@ type Props = {
 }
 
 const RegenerateBtn = ({ className, onClick }: Props) => {
-  const selector = useRef(`copy-tooltip-${randomString(4)}`)
-
   return (
     <div className={`${className}`}>
       <Tooltip
-        selector={selector.current}
-        content={t('appApi.regenerate') as string}
-        className='z-10'
+        popupContent={t('appApi.regenerate') as string}
       >
         <div
           className={'box-border p-0.5 flex items-center justify-center rounded-md bg-white cursor-pointer'}
