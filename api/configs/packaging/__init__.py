aa47745a0b975b0f1c7df7e6abdf3e8019bbc9f0
-LAN- <laipz8200@outlook.com>
2024-08-26 15:34:40 +0800

    chore: Update version to 0.7.2

--- api/configs/packaging/__init__.py
+++ api/configs/packaging/__init__.py
@@ -9,7 +9,7 @@ class PackagingInfo(BaseSettings):
 
     CURRENT_VERSION: str = Field(
         description="Dify version",
-        default="0.7.1",
+        default="0.7.2",
     )
 
     COMMIT_SHA: str = Field(
