0d116905097e2f6805698031e4e51739ad3715ca
kurokobo <kuro664@gmail.com>
2024-08-27 13:40:44 +0900

    feat: display account name on the logs page for the apps (#7668)
    Co-authored-by: crazywoola <100913391+crazywoola@users.noreply.github.com>

--- web/i18n/zh-Hans/app-log.ts
+++ web/i18n/zh-Hans/app-log.ts
@@ -6,7 +6,7 @@ const translation = {
     header: {
       updatedTime: '更新时间',
       time: '创建时间',
-      endUser: '用户',
+      endUser: '用户或账户',
       input: '输入',
       output: '输出',
       summary: '标题',
@@ -17,7 +17,7 @@ const translation = {
       status: '状态',
       runtime: '运行时间',
       tokens: 'TOKENS',
-      user: '用户',
+      user: '用户或账户',
       version: '版本',
     },
     pagination: {
