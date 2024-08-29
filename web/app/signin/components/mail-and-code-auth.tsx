2ef05d8a71a83dbc5cfb0188b3293fe652b8fada
douxc <douxc512@gmail.com>
2024-08-29 11:41:59 +0800

    wip: stash

--- web/app/signin/components/mail-and-code-auth.tsx
+++ web/app/signin/components/mail-and-code-auth.tsx
@@ -36,7 +36,7 @@ export default function MailAndCodeAuth({ isInvite }: MailAndCodeAuthProps) {
     <div className='mb-2'>
       <label htmlFor="email" className='my-2 block text-sm font-medium text-text-secondary'>{t('login.email')}</label>
       <div className='mt-1'>
-        <Input type="email" value={email} placeholder={t('login.emailPlaceholder') as string} onChange={setEmail} className="px-3 h-9" />
+        <Input id='email' type="email" disabled={isInvite} value={email} placeholder={t('login.emailPlaceholder') as string} onChange={e => setEmail(e.target.value)} className="px-3 h-9" />
       </div>
       <div className='mt-3'>
         <Button variant='primary' className='w-full' onClick={handleGetEMailVerificationCode}>{t('login.continueWithCode')}</Button>
