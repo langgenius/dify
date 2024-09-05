7f6c7b67729be77c5d9acbe0395badffba25f2f3
android <android@gmail.com>
2024-09-05 15:15:55 +0700

    setDatasetId

--- web/app/(commonLayout)/files/page.tsx
+++ web/app/(commonLayout)/files/page.tsx
@@ -2,16 +2,27 @@
 import type { FC } from 'react'
 import { useRouter } from 'next/navigation'
 import { useTranslation } from 'react-i18next'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAppContext } from '@/context/app-context'
 import Main from '@/app/components/datasets/documents'
-
-const datasetId = '28cc0f54-5762-464d-92c6-d85c9b413a38'
+import { fetchDatasets } from '@/service/datasets'
+import type { DataSet } from '@/models/datasets'
 
 const Layout: FC = () => {
   const { t } = useTranslation()
   const router = useRouter()
   const { isCurrentWorkspaceDatasetOperator } = useAppContext()
+  const [datasetId, setDatasetId] = useState<string>('28cc0f54-5762-464d-92c6-d85c9b413a38')
+
+  useEffect(() => {
+    (async () => {
+      const { data: dataSetsdataWithDetail } = await fetchDatasets({ url: '/datasets', params: { page: 1 } })
+      if (dataSetsdataWithDetail && dataSetsdataWithDetail.length) {
+        setDatasetId(dataSetsdataWithDetail?.[0]?.id)
+      }
+    })()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   useEffect(() => {
     if (typeof window !== 'undefined')
