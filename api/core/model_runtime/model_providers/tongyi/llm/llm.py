ae68300924447f604370a35a4b7bef2afaa8a2f1
hejl <hjlarry@163.com>
2024-09-11 10:54:26 +0800

    empty content to redo CI

--- api/core/model_runtime/model_providers/tongyi/llm/llm.py
+++ api/core/model_runtime/model_providers/tongyi/llm/llm.py
@@ -418,6 +418,7 @@ class TongyiLargeLanguageModel(LargeLanguageModel):
                                 file_url = self._save_base64_image_to_file(message_content.data)
 
                             is_video_url = re.search(r"\.(mp4|webm|ogg|mov|avi|mkv|flv|wmv)$", file_url, re.IGNORECASE)
+
                             if is_video_url:
                                 sub_message_dict = {"video": file_url}
                             else:
