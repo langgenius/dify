82c42b9ec5fdd78cf72c78f481d486d542964ff7
HowardChan <lonely829@gmail.com>
2024-09-11 10:25:45 +0800

    fix:error when adding the ollama embedding model (#8236)
    Co-authored-by: crazywoola <427733928@qq.com>

--- api/core/model_runtime/model_providers/ollama/text_embedding/text_embedding.py
+++ api/core/model_runtime/model_providers/ollama/text_embedding/text_embedding.py
@@ -77,15 +77,10 @@ class OllamaEmbeddingModel(TextEmbeddingModel):
                 inputs.append(text)
 
         # Prepare the payload for the request
-        payload = {
-            "input": inputs,
-            "model": model,
-        }
+        payload = {"input": inputs, "model": model, "options": {"use_mmap": "true"}}
 
-        # Make the request to the OpenAI API
-        response = requests.post(
-            endpoint_url, headers=headers, data=json.dumps(payload), timeout=(10, 300), options={"use_mmap": "true"}
-        )
+        # Make the request to the Ollama API
+        response = requests.post(endpoint_url, headers=headers, data=json.dumps(payload), timeout=(10, 300))
 
         response.raise_for_status()  # Raise an exception for HTTP errors
         response_data = response.json()
