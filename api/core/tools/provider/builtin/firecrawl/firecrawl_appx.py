2dfa7a7b9c4f38054727064ac3c4efdec5a5bbf3
hejl <hjlarry@163.com>
2024-09-13 17:18:20 +0800

    update firecrawl crawl to V1 api

--- api/core/tools/provider/builtin/firecrawl/firecrawl_appx.py
+++ api/core/tools/provider/builtin/firecrawl/firecrawl_appx.py
@@ -66,27 +66,29 @@ class FirecrawlApp:
     def crawl_url(
         self, url: str, wait: bool = True, poll_interval: int = 5, idempotency_key: str | None = None, **kwargs
     ):
-        endpoint = f"{self.base_url}/v0/crawl"
+        endpoint = f"{self.base_url}/v1/crawl"
         headers = self._prepare_headers(idempotency_key)
         data = {"url": url, **kwargs}
         logger.debug(f"Sent request to {endpoint=} body={data}")
         response = self._request("POST", endpoint, data, headers)
         if response is None:
             raise HTTPError("Failed to initiate crawl after multiple retries")
-        job_id: str = response["jobId"]
+        elif response.get("success") == False:
+            raise HTTPError(f'Failed to crawl: {response.get("error")}')
+        job_id: str = response["id"]
         if wait:
             return self._monitor_job_status(job_id=job_id, poll_interval=poll_interval)
         return response
 
     def check_crawl_status(self, job_id: str):
-        endpoint = f"{self.base_url}/v0/crawl/status/{job_id}"
+        endpoint = f"{self.base_url}/v1/crawl/{job_id}"
         response = self._request("GET", endpoint)
         if response is None:
             raise HTTPError(f"Failed to check status for job {job_id} after multiple retries")
         return response
 
     def cancel_crawl_job(self, job_id: str):
-        endpoint = f"{self.base_url}/v0/crawl/cancel/{job_id}"
+        endpoint = f"{self.base_url}/v1/crawl/{job_id}"
         response = self._request("DELETE", endpoint)
         if response is None:
             raise HTTPError(f"Failed to cancel job {job_id} after multiple retries")
