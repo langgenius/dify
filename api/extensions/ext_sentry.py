db8462a583165f0ab200f5554bb944605faa3ee6
Joe <79627742+ZhouhaoJiang@users.noreply.github.com>
2024-09-13 16:08:08 +0800

    fix: add before send to remove langfuse defaultErrorResponse (#8361)

--- api/extensions/ext_sentry.py
+++ api/extensions/ext_sentry.py
@@ -6,6 +6,15 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 from werkzeug.exceptions import HTTPException
 
 
+def before_send(event, hint):
+    if "exc_info" in hint:
+        exc_type, exc_value, tb = hint["exc_info"]
+        if parse_error.defaultErrorResponse in str(exc_value):
+            return None
+
+    return event
+
+
 def init_app(app):
     if app.config.get("SENTRY_DSN"):
         sentry_sdk.init(
@@ -16,4 +25,5 @@ def init_app(app):
             profiles_sample_rate=app.config.get("SENTRY_PROFILES_SAMPLE_RATE", 1.0),
             environment=app.config.get("DEPLOY_ENV"),
             release=f"dify-{app.config.get('CURRENT_VERSION')}-{app.config.get('COMMIT_SHA')}",
+            before_send=before_send,
         )
