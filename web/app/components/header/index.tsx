f320fc6f86f6a24af3c75c0aba7cc44da931bc1f
cpanel10x <me@vutn.net>
2024-09-14 09:44:59 +0700

    Update index.tsx

--- web/app/components/header/index.tsx
+++ web/app/components/header/index.tsx
@@ -11,7 +11,6 @@ import DatasetNav from './dataset-nav'
 import EnvNav from './env-nav'
 import ExploreNav from './explore-nav'
 import ToolsNav from './tools-nav'
-import GithubStar from './github-star'
 import { WorkspaceProvider } from '@/context/workspace-context'
 import { useAppContext } from '@/context/app-context'
 import LogoSite from '@/app/components/base/logo/logo-site'
@@ -59,7 +58,6 @@ const Header = () => {
           <Link href="/apps" className='flex items-center mr-4'>
             <LogoSite className='object-contain' />
           </Link>
-          <GithubStar />
         </>}
       </div>
       {isMobile && (
@@ -67,7 +65,6 @@ const Header = () => {
           <Link href="/apps" className='flex items-center mr-4'>
             <LogoSite />
           </Link>
-          <GithubStar />
         </div>
       )}
       {!isMobile && (
