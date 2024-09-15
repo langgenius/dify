e0bd1b1b834f1c19b976b9b4d5024cf7a9b9c56b
Wang Ying <wangying@xkool.org>
2024-09-16 03:15:46 +0800

    porting the feature of hit testing, from dataset console API to dataset service API. updating the documents for dataset service API as well.

--- api/controllers/service_api/dataset/dataset.py
+++ api/controllers/service_api/dataset/dataset.py
@@ -3,6 +3,7 @@ from flask_restful import marshal, reqparse
 from werkzeug.exceptions import NotFound
 
 import services.dataset_service
+from controllers.console.datasets.hit_testing import HitTestingMixin
 from controllers.service_api import api
 from controllers.service_api.dataset.error import DatasetInUseError, DatasetNameDuplicateError
 from controllers.service_api.wraps import DatasetApiResource
@@ -12,7 +13,6 @@ from fields.dataset_fields import dataset_detail_fields
 from libs.login import current_user
 from models.dataset import Dataset, DatasetPermissionEnum
 from services.dataset_service import DatasetService
-from controllers.console.datasets.hit_testing import HitTestingMixin
 
 
 def _validate_name(name):
