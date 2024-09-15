18b9a9b63808934d12c67a31b27952de0b99e0e8
yusuke-ten <net.escape.com@gmail.com>
2024-09-16 00:44:36 +0900

    fix styles

--- web/app/components/base/chat/chat/answer/index.tsx
+++ web/app/components/base/chat/chat/answer/index.tsx
@@ -86,89 +86,106 @@ const Answer: FC<AnswerProps> = ({
   }, [responding])
 
   return (
-    <div className={isMobile ? 'flex flex-col items-start mb-2 last:mb-0' : 'flex mb-2 last:mb-0'}>
-      <div className='shrink-0 relative w-10 h-10'>
+    <div
+      className={
+        isMobile
+          ? 'flex flex-col items-start mb-2 last:mb-0'
+          : 'flex mb-2 last:mb-0'
+      }
+    >
+      <div className="shrink-0 relative w-10 h-10">
         {answerIcon || <AnswerIcon />}
         {responding && (
-          <div className='absolute -top-[3px] -left-[3px] pl-[6px] flex items-center w-4 h-4 bg-white rounded-full shadow-xs border-[0.5px] border-gray-50'>
-            <LoadingAnim type='avatar' />
+          <div className="absolute -top-[3px] -left-[3px] pl-[6px] flex items-center w-4 h-4 bg-white rounded-full shadow-xs border-[0.5px] border-gray-50">
+            <LoadingAnim type="avatar" />
           </div>
         )}
       </div>
-      <div className='chat-answer-container group grow w-0 ml-4' ref={containerRef}>
-        <div className={cn('group relative pr-10', chatAnswerContainerInner)}>
-          <AnswerTriangle className='absolute -left-2 top-0 w-2 h-3 text-gray-100' />
+      <div
+        className={`chat-answer-container group grow ${
+          isMobile ? 'max-w-full mt-4' : 'w-0 ml-4'
+        }`}
+        ref={containerRef}
+      >
+        <div
+          className={cn(
+            `group relative ${!isMobile && 'pr-10'}`,
+            chatAnswerContainerInner,
+          )}
+        >
+          <AnswerTriangle
+            className={
+              isMobile
+                ? 'absolute left-0 -top-2 w-2 h-3 text-gray-100 rotate-180'
+                : 'absolute -left-2 top-0 w-2 h-3 text-gray-100'
+            }
+          />
           <div
             ref={contentRef}
-            className={cn('relative inline-block px-4 py-3 max-w-full bg-gray-100 rounded-b-2xl rounded-tr-2xl text-sm text-gray-900', workflowProcess && 'w-full')}
+            className={cn(
+              'relative inline-block px-4 py-3 max-w-full bg-gray-100 rounded-b-2xl rounded-tr-2xl text-sm text-gray-900',
+              workflowProcess && 'w-full',
+            )}
           >
-            {
-              !responding && (
-                <Operation
-                  hasWorkflowProcess={!!workflowProcess}
-                  maxSize={containerWidth - contentWidth - 4}
-                  contentWidth={contentWidth}
-                  item={item}
-                  question={question}
-                  index={index}
-                  showPromptLog={showPromptLog}
-                />
-              )
-            }
+            {!responding && (
+              <Operation
+                hasWorkflowProcess={!!workflowProcess}
+                maxSize={containerWidth - contentWidth - 4}
+                contentWidth={contentWidth}
+                item={item}
+                question={question}
+                index={index}
+                showPromptLog={showPromptLog}
+              />
+            )}
             {/** Render the normal steps */}
-            {
-              workflowProcess && !hideProcessDetail && (
-                <WorkflowProcess
-                  data={workflowProcess}
-                  item={item}
-                  hideInfo
-                  hideProcessDetail={hideProcessDetail}
-                />
-              )
-            }
+            {workflowProcess && !hideProcessDetail && (
+              <WorkflowProcess
+                data={workflowProcess}
+                item={item}
+                hideInfo
+                hideProcessDetail={hideProcessDetail}
+              />
+            )}
             {/** Hide workflow steps by it's settings in siteInfo */}
-            {
-              workflowProcess && hideProcessDetail && appData && appData.site.show_workflow_steps && (
-                <WorkflowProcess
-                  data={workflowProcess}
-                  item={item}
-                  hideInfo
-                  hideProcessDetail={hideProcessDetail}
-                />
-              )
-            }
-            {
-              responding && !content && !hasAgentThoughts && (
-                <div className='flex items-center justify-center w-6 h-5'>
-                  <LoadingAnim type='text' />
-                </div>
-              )
-            }
-            {
-              content && !hasAgentThoughts && (
-                <BasicContent item={item} />
-              )
-            }
-            {
-              hasAgentThoughts && (
-                <AgentContent
-                  item={item}
-                  responding={responding}
-                  allToolIcons={allToolIcons}
-                />
-              )
-            }
-            {
-              annotation?.id && annotation.authorName && (
-                <EditTitle
-                  className='mt-1'
-                  title={t('appAnnotation.editBy', { author: annotation.authorName })}
-                />
-              )
-            }
+            {workflowProcess
+              && hideProcessDetail
+              && appData
+              && appData.site.show_workflow_steps && (
+              <WorkflowProcess
+                data={workflowProcess}
+                item={item}
+                hideInfo
+                hideProcessDetail={hideProcessDetail}
+              />
+            )}
+            {responding && !content && !hasAgentThoughts && (
+              <div className="flex items-center justify-center w-6 h-5">
+                <LoadingAnim type="text" />
+              </div>
+            )}
+            {content && !hasAgentThoughts && <BasicContent item={item} />}
+            {hasAgentThoughts && (
+              <AgentContent
+                item={item}
+                responding={responding}
+                allToolIcons={allToolIcons}
+              />
+            )}
+            {annotation?.id && annotation.authorName && (
+              <EditTitle
+                className="mt-1"
+                title={t('appAnnotation.editBy', {
+                  author: annotation.authorName,
+                })}
+              />
+            )}
             <SuggestedQuestions item={item} />
             {!!citation?.length && !responding && (
-              <Citation data={citation} showHitInfo={config?.supportCitationHitInfo} />
+              <Citation
+                data={citation}
+                showHitInfo={config?.supportCitationHitInfo}
+              />
             )}
           </div>
         </div>
