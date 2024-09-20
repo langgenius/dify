8c3630ae72cd6ed7b3281e65a3190716e5ae50f6
xhb <466010723@qq.com>
2024-09-20 17:20:24 +0800

    Delete the imported ModelType CredentialsValidateFailedError

--- api/core/model_runtime/model_providers/perfxcloud/perfxcloud.py
+++ api/core/model_runtime/model_providers/perfxcloud/perfxcloud.py
@@ -1,7 +1,5 @@
 import logging
 
-from core.model_runtime.entities.model_entities import ModelType
-from core.model_runtime.errors.validate import CredentialsValidateFailedError
 from core.model_runtime.model_providers.__base.model_provider import ModelProvider
 
 logger = logging.getLogger(__name__)
