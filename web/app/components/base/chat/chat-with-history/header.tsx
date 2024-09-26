93de7f991d3652731164328ec9ddaf5b6a0ee9ed
Lz名约山炮 <1035807396@qq.com>
2024-09-26 14:55:40 +0800

    在前端页面添加问题示例

--- web/app/components/base/chat/chat-with-history/header.tsx
+++ web/app/components/base/chat/chat-with-history/header.tsx
@@ -12,12 +12,18 @@ const Header: FC<HeaderProps> = ({
   return (
     <div
       className={`
-      sticky top-0 flex items-center px-8 h-16 bg-white/80 text-base font-medium 
-      text-gray-900 border-b-[0.5px] border-b-gray-100 backdrop-blur-md z-10
+      sticky top-0 flex items-center px-8 h-18 bg-white-100 text-base font-bold 
+      text-black border-b-[0.5px] border-b-gray-100 backdrop-blur-md z-10
       ${isMobile && '!h-12'}
       `}
     >
-      {title}
+      <div>
+        当前会话：{title}。下面是文案生成的一些提问示例：<br />
+          1.请生成开学季，有关防晒的科普文案<br />
+          2.生成给宝宝洗澡时的种草文案，仔细描述洗澡的心得或感受<br />
+          3.生成给宝宝洗脸时的种草文案，仔细描述清洁的心得或感受<br />
+          4.生成宝宝夏天睡觉太热的种草文案，仔细描述宝宝睡不着或者惊醒时妈妈的心得或感受<br />
+      </div>
     </div>
   )
 }
