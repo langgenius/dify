b0071bc4e1d62467a43d78eded4acd686703fd1c
-LAN- <laipz8200@outlook.com>
2024-09-27 15:50:49 +0800

    chore(api/config): Extend default variable size limit to 200 KB

--- api/configs/feature/__init__.py
+++ api/configs/feature/__init__.py
@@ -368,8 +368,8 @@ class WorkflowConfig(BaseSettings):
     )
 
     MAX_VARIABLE_SIZE: PositiveInt = Field(
-        description="Maximum size in bytes for a single variable in workflows. Default to 5KB.",
-        default=5 * 1024,
+        description="Maximum size in bytes for a single variable in workflows. Default to 200 KB.",
+        default=200 * 1024,
     )
 
 
