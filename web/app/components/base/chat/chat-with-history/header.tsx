34fa3cdecb680a74b8f9ce9e11049d2590788c22
Lz名约山炮 <1035807396@qq.com>
2024-09-27 09:31:33 +0800

    在前端页面添加问题示例

--- web/app/components/base/chat/chat-with-history/header.tsx
+++ web/app/components/base/chat/chat-with-history/header.tsx
@@ -17,13 +17,22 @@ const Header: FC<HeaderProps> = ({
       ${isMobile && '!h-12'}
       `}
     >
-      <div>
-        当前会话：{title}。下面是文案生成的一些提问示例：<br />
-          1.请生成开学季，有关防晒的科普文案<br />
-          2.生成给宝宝洗澡时的种草文案，仔细描述洗澡的心得或感受<br />
-          3.生成给宝宝洗脸时的种草文案，仔细描述清洁的心得或感受<br />
-          4.生成宝宝夏天睡觉太热的种草文案，仔细描述宝宝睡不着或者惊醒时妈妈的心得或感受<br />
+      {/* 当前会话：{title}<br /> */}
+      
+      <p style={{ color: '#155eef' }}>示例：<br />
+      <div style={{ border: '1px solid gray', fontWeight: 'normal', display: 'block' , margin: '0', padding: '4px' }}>
+        请生成开学季，有关防晒的科普文案
       </div>
+      <div style={{ border: '1px solid gray', fontWeight: 'normal', display: 'block' , margin: '0', padding: '4px' }}>
+        生成给宝宝洗澡时的种草文案，仔细描述洗澡的心得或感受
+      </div>
+      <div style={{ border: '1px solid gray', fontWeight: 'normal', display: 'block' , margin: '0', padding: '4px' }}>
+        生成给宝宝洗脸时的种草文案，仔细描述清洁的心得或感受
+      </div>
+      <div style={{ border: '1px solid gray', fontWeight: 'normal', display: 'block' , margin: '0', padding: '4px' }}>
+        生成宝宝夏天睡觉太热的种草文案，仔细描述宝宝睡不着或者惊醒时妈妈的心得或感受
+      </div>
+      </p >
     </div>
   )
 }
