0eb7f0f739288c54a6f80799ac948a2bee8b19da
ice yao <yao3690093@gmail.com>
2024-10-01 09:15:18 +0800

    chore: remove unexecuted scripts to avoid ambiguity (#8946)

--- dev/pytest/pytest_vdb.sh
+++ dev/pytest/pytest_vdb.sh
@@ -7,5 +7,4 @@ pytest api/tests/integration_tests/vdb/chroma \
   api/tests/integration_tests/vdb/pgvector \
   api/tests/integration_tests/vdb/qdrant \
   api/tests/integration_tests/vdb/weaviate \
-  api/tests/integration_tests/vdb/elasticsearch \
-  api/tests/integration_tests/vdb/test_vector_store.py
+  api/tests/integration_tests/vdb/elasticsearch
