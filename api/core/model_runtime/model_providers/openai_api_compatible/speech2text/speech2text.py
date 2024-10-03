8914799dd835240d5f713f691ea8aa15ebf1ac33
zhaoyi233 <11971297+zhao85@users.noreply.github.com>
2024-10-03 18:37:50 +0800

    open_api_compitable speech2text model bug
    fixed speech2text model not appears after added here

--- api/core/model_runtime/model_providers/openai_api_compatible/speech2text/speech2text.py
+++ api/core/model_runtime/model_providers/openai_api_compatible/speech2text/speech2text.py
@@ -7,7 +7,12 @@ from core.model_runtime.errors.invoke import InvokeBadRequestError
 from core.model_runtime.errors.validate import CredentialsValidateFailedError
 from core.model_runtime.model_providers.__base.speech2text_model import Speech2TextModel
 from core.model_runtime.model_providers.openai_api_compatible._common import _CommonOaiApiCompat
-
+from core.model_runtime.entities.model_entities import (
+    AIModelEntity,
+    FetchFrom,
+    ModelType,
+)
+from core.model_runtime.entities.common_entities import I18nObject
 
 class OAICompatSpeech2TextModel(_CommonOaiApiCompat, Speech2TextModel):
     """
@@ -59,3 +64,19 @@ class OAICompatSpeech2TextModel(_CommonOaiApiCompat, Speech2TextModel):
                 self._invoke(model, credentials, audio_file)
         except Exception as ex:
             raise CredentialsValidateFailedError(str(ex))
+        
+    def get_customizable_model_schema(self, model: str, credentials: dict) -> AIModelEntity | None:
+        """
+        used to define customizable model schema
+        """
+        entity = AIModelEntity(
+            model=model,
+            label=I18nObject(en_US=model),
+            fetch_from=FetchFrom.CUSTOMIZABLE_MODEL,
+            model_type=ModelType.SPEECH2TEXT,
+            model_properties={},
+            parameter_rules=[],
+        )
+
+        return entity
+
