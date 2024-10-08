f98fe20969dac1c7a65fc4d2a8f640971aa89f4b
Bowen Liang <liangbowen@gf.com.cn>
2024-10-08 18:20:34 +0800

    update

--- api/core/tools/provider/builtin/typewriter/tools/markdown_to_docx.py
+++ api/core/tools/provider/builtin/typewriter/tools/markdown_to_docx.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from tempfile import NamedTemporaryFile
 from typing import Any, Union
 
-from markdowntodocx import markdownconverter
+from markdowntodocx.markdownconverter import markdownToWordFromString
 
 from core.tools.entities.tool_entities import ToolInvokeMessage
 from core.tools.provider.builtin.typewriter.tools.markdown_utils import MarkdownUtils
@@ -27,7 +27,7 @@ class MarkdownToDocxFile(BuiltinTool):
         try:
             markdown_text = MarkdownUtils.strip_markdown_wrapper(markdown_text)
             with NamedTemporaryFile(suffix=".docx", delete=True) as temp_docx_file:
-                markdownconverter.markdownToWordFromString(string=markdown_text, outfile=temp_docx_file)
+                markdownToWordFromString(string=markdown_text, outfile=temp_docx_file)
                 result_file_bytes = Path(temp_docx_file.name).read_bytes()
         except Exception as e:
             return self.create_text_message(f"Failed to convert markdown text to DOCX file, error: {str(e)}")
