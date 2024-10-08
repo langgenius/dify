4c352f8cde5c82d832c2ec5bc6b2e7c659cc49a3
Bowen Liang <liangbowen@gf.com.cn>
2024-10-08 18:19:34 +0800

    update

--- api/core/tools/provider/builtin/typewriter/tools/markdown_to_docx.py
+++ api/core/tools/provider/builtin/typewriter/tools/markdown_to_docx.py
@@ -28,8 +28,7 @@ class MarkdownToDocxFile(BuiltinTool):
             markdown_text = MarkdownUtils.strip_markdown_wrapper(markdown_text)
             with NamedTemporaryFile(suffix=".docx", delete=True) as temp_docx_file:
                 markdownconverter.markdownToWordFromString(string=markdown_text, outfile=temp_docx_file)
-                file_path = temp_docx_file.name
-                result_file_bytes = Path(file_path).read_bytes()
+                result_file_bytes = Path(temp_docx_file.name).read_bytes()
         except Exception as e:
             return self.create_text_message(f"Failed to convert markdown text to DOCX file, error: {str(e)}")
 
