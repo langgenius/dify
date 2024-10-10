99f563227f70747f5a4c8e26b5cf166762b3e34c
黎斌 <libin.23@bytedance.com>
2024-10-10 19:59:26 +0800

    update feishu base tools

--- api/core/tools/utils/feishu_api_utils.py
+++ api/core/tools/utils/feishu_api_utils.py
@@ -1,4 +1,4 @@
-from typing import Optional
+import json
 
 import httpx
 
@@ -17,8 +17,47 @@ def auth(credentials):
         raise ToolProviderCredentialValidationError(str(e))
 
 
+def convert_add_records(json_str):
+    try:
+        data = json.loads(json_str)
+        if not isinstance(data, list):
+            raise ValueError("Parsed data must be a list")
+        converted_data = [{"fields": json.dumps(item, ensure_ascii=False)} for item in data]
+        return converted_data
+    except json.JSONDecodeError:
+        raise ValueError("The input string is not valid JSON")
+    except Exception as e:
+        raise ValueError(f"An error occurred while processing the data: {e}")
+
+
+def convert_update_records(json_str):
+    try:
+        data = json.loads(json_str)
+        if not isinstance(data, list):
+            raise ValueError("Parsed data must be a list")
+
+        converted_data = [
+            {
+                "fields": json.dumps(record['fields'], ensure_ascii=False),
+                "record_id": record['record_id']
+            }
+            for record in data
+            if 'fields' in record and 'record_id' in record
+        ]
+
+        if len(converted_data) != len(data):
+            raise ValueError("Each record must contain 'fields' and 'record_id'")
+
+        return converted_data
+    except json.JSONDecodeError:
+        raise ValueError("The input string is not valid JSON")
+    except Exception as e:
+        raise ValueError(f"An error occurred while processing the data: {e}")
+
+
 class FeishuRequest:
-    API_BASE_URL = "https://lark-plugin-api.solutionsuite.cn/lark-plugin"
+    # API_BASE_URL = "https://lark-plugin-api.solutionsuite.cn/lark-plugin"
+    API_BASE_URL = "http://10.174.151.112:6789/lark-plugin"
 
     def __init__(self, app_id: str, app_secret: str):
         self.app_id = app_id
@@ -34,12 +73,7 @@ class FeishuRequest:
         return res.get("tenant_access_token")
 
     def _send_request(
-        self,
-        url: str,
-        method: str = "post",
-        require_token: bool = True,
-        payload: Optional[dict] = None,
-        params: Optional[dict] = None,
+            self, url: str, method: str = "post", require_token: bool = True, payload: dict = None, params: dict = None
     ):
         headers = {
             "Content-Type": "application/json",
@@ -121,7 +155,7 @@ class FeishuRequest:
         return res.get("data").get("content")
 
     def list_document_blocks(
-        self, document_id: str, page_token: str, user_id_type: str = "open_id", page_size: int = 500
+            self, document_id: str, page_token: str, user_id_type: str = "open_id", page_size: int = 500
     ) -> dict:
         """
         API url: https://open.larkoffice.com/document/server-docs/docs/docs/docx-v1/document/list
@@ -163,13 +197,13 @@ class FeishuRequest:
         return res
 
     def get_chat_messages(
-        self,
-        container_id: str,
-        start_time: str,
-        end_time: str,
-        page_token: str,
-        sort_type: str = "ByCreateTimeAsc",
-        page_size: int = 20,
+            self,
+            container_id: str,
+            start_time: str,
+            end_time: str,
+            page_token: str,
+            sort_type: str = "ByCreateTimeAsc",
+            page_size: int = 20,
     ) -> dict:
         """
         API url: https://open.larkoffice.com/document/server-docs/im-v1/message/list
@@ -187,7 +221,7 @@ class FeishuRequest:
         return res.get("data")
 
     def get_thread_messages(
-        self, container_id: str, page_token: str, sort_type: str = "ByCreateTimeAsc", page_size: int = 20
+            self, container_id: str, page_token: str, sort_type: str = "ByCreateTimeAsc", page_size: int = 20
     ) -> dict:
         """
         API url: https://open.larkoffice.com/document/server-docs/im-v1/message/list
@@ -216,7 +250,7 @@ class FeishuRequest:
         return res.get("data")
 
     def update_task(
-        self, task_guid: str, summary: str, start_time: str, end_time: str, completed_time: str, description: str
+            self, task_guid: str, summary: str, start_time: str, end_time: str, completed_time: str, description: str
     ) -> dict:
         # 更新任务
         url = f"{self.API_BASE_URL}/task/update_task"
@@ -272,14 +306,14 @@ class FeishuRequest:
         return res.get("data")
 
     def create_event(
-        self,
-        summary: str,
-        description: str,
-        start_time: str,
-        end_time: str,
-        attendee_ability: str,
-        need_notification: bool = True,
-        auto_record: bool = False,
+            self,
+            summary: str,
+            description: str,
+            start_time: str,
+            end_time: str,
+            attendee_ability: str,
+            need_notification: bool = True,
+            auto_record: bool = False,
     ) -> dict:
         url = f"{self.API_BASE_URL}/calendar/create_event"
         payload = {
@@ -295,14 +329,14 @@ class FeishuRequest:
         return res.get("data")
 
     def update_event(
-        self,
-        event_id: str,
-        summary: str,
-        description: str,
-        need_notification: bool,
-        start_time: str,
-        end_time: str,
-        auto_record: bool,
+            self,
+            event_id: str,
+            summary: str,
+            description: str,
+            need_notification: bool,
+            start_time: str,
+            end_time: str,
+            auto_record: bool,
     ) -> dict:
         url = f"{self.API_BASE_URL}/calendar/update_event/{event_id}"
         payload = {}
@@ -341,13 +375,13 @@ class FeishuRequest:
         return res.get("data")
 
     def search_events(
-        self,
-        query: str,
-        start_time: str,
-        end_time: str,
-        page_token: str,
-        user_id_type: str = "open_id",
-        page_size: int = 20,
+            self,
+            query: str,
+            start_time: str,
+            end_time: str,
+            page_token: str,
+            user_id_type: str = "open_id",
+            page_size: int = 20,
     ) -> dict:
         url = f"{self.API_BASE_URL}/calendar/search_events"
         payload = {
@@ -373,9 +407,9 @@ class FeishuRequest:
         return res.get("data")
 
     def create_spreadsheet(
-        self,
-        title: str,
-        folder_token: str,
+            self,
+            title: str,
+            folder_token: str,
     ) -> dict:
         # 创建电子表格
         url = f"{self.API_BASE_URL}/spreadsheet/create_spreadsheet"
@@ -387,9 +421,9 @@ class FeishuRequest:
         return res.get("data")
 
     def get_spreadsheet(
-        self,
-        spreadsheet_token: str,
-        user_id_type: str = "open_id",
+            self,
+            spreadsheet_token: str,
+            user_id_type: str = "open_id",
     ) -> dict:
         # 获取电子表格信息
         url = f"{self.API_BASE_URL}/spreadsheet/get_spreadsheet"
@@ -401,8 +435,8 @@ class FeishuRequest:
         return res.get("data")
 
     def list_spreadsheet_sheets(
-        self,
-        spreadsheet_token: str,
+            self,
+            spreadsheet_token: str,
     ) -> dict:
         # 列出电子表格的所有工作表
         url = f"{self.API_BASE_URL}/spreadsheet/list_spreadsheet_sheets"
@@ -413,12 +447,12 @@ class FeishuRequest:
         return res.get("data")
 
     def add_rows(
-        self,
-        spreadsheet_token: str,
-        sheet_id: str,
-        sheet_name: str,
-        length: int,
-        values: str,
+            self,
+            spreadsheet_token: str,
+            sheet_id: str,
+            sheet_name: str,
+            length: int,
+            values: str,
     ) -> dict:
         # 增加行,在工作表最后添加
         url = f"{self.API_BASE_URL}/spreadsheet/add_rows"
@@ -433,12 +467,12 @@ class FeishuRequest:
         return res.get("data")
 
     def add_cols(
-        self,
-        spreadsheet_token: str,
-        sheet_id: str,
-        sheet_name: str,
-        length: int,
-        values: str,
+            self,
+            spreadsheet_token: str,
+            sheet_id: str,
+            sheet_name: str,
+            length: int,
+            values: str,
     ) -> dict:
         #  增加列,在工作表最后添加
         url = f"{self.API_BASE_URL}/spreadsheet/add_cols"
@@ -453,13 +487,13 @@ class FeishuRequest:
         return res.get("data")
 
     def read_rows(
-        self,
-        spreadsheet_token: str,
-        sheet_id: str,
-        sheet_name: str,
-        start_row: int,
-        num_rows: int,
-        user_id_type: str = "open_id",
+            self,
+            spreadsheet_token: str,
+            sheet_id: str,
+            sheet_name: str,
+            start_row: int,
+            num_rows: int,
+            user_id_type: str = "open_id",
     ) -> dict:
         # 读取工作表行数据
         url = f"{self.API_BASE_URL}/spreadsheet/read_rows"
@@ -475,13 +509,13 @@ class FeishuRequest:
         return res.get("data")
 
     def read_cols(
-        self,
-        spreadsheet_token: str,
-        sheet_id: str,
-        sheet_name: str,
-        start_col: int,
-        num_cols: int,
-        user_id_type: str = "open_id",
+            self,
+            spreadsheet_token: str,
+            sheet_id: str,
+            sheet_name: str,
+            start_col: int,
+            num_cols: int,
+            user_id_type: str = "open_id",
     ) -> dict:
         # 读取工作表列数据
         url = f"{self.API_BASE_URL}/spreadsheet/read_cols"
@@ -497,13 +531,13 @@ class FeishuRequest:
         return res.get("data")
 
     def read_table(
-        self,
-        spreadsheet_token: str,
-        sheet_id: str,
-        sheet_name: str,
-        num_range: str,
-        query: str,
-        user_id_type: str = "open_id",
+            self,
+            spreadsheet_token: str,
+            sheet_id: str,
+            sheet_name: str,
+            num_range: str,
+            query: str,
+            user_id_type: str = "open_id",
     ) -> dict:
         # 自定义读取行列数据
         url = f"{self.API_BASE_URL}/spreadsheet/read_table"
@@ -516,4 +550,272 @@ class FeishuRequest:
             "user_id_type": user_id_type,
         }
         res = self._send_request(url, method="GET", params=params)
+        return res.get("data")
+
+    def create_base(
+            self,
+            name: str,
+            folder_token: str,
+    ) -> dict:
+        # 创建多维表格
+        url = f"{self.API_BASE_URL}/base/create_base"
+        payload = {
+            "name": name,
+            "folder_token": folder_token,
+        }
+        res = self._send_request(url, payload=payload)
+        return res.get("data")
+
+    def add_records(
+            self,
+            app_token: str,
+            table_id: str,
+            table_name: str,
+            records: str,
+            user_id_type: str = "open_id",
+    ) -> dict:
+        # 新增多条记录
+        url = f"{self.API_BASE_URL}/base/add_records"
+        params = {
+            "app_token": app_token,
+            "table_id": table_id,
+            "table_name": table_name,
+            "user_id_type": user_id_type,
+        }
+        payload = {
+            "records": convert_add_records(records),
+        }
+        res = self._send_request(url, params=params, payload=payload)
+        return res.get("data")
+
+    def update_records(
+            self,
+            app_token: str,
+            table_id: str,
+            table_name: str,
+            records: str,
+            user_id_type: str,
+    ) -> dict:
+        # 更新多条记录
+        url = f"{self.API_BASE_URL}/base/update_records"
+        params = {
+            "app_token": app_token,
+            "table_id": table_id,
+            "table_name": table_name,
+            "user_id_type": user_id_type,
+        }
+        payload = {
+            "records": convert_update_records(records),
+        }
+        res = self._send_request(url, params=params, payload=payload)
+        return res.get("data")
+
+    def delete_records(
+            self,
+            app_token: str,
+            table_id: str,
+            table_name: str,
+            record_ids: str,
+    ) -> dict:
+        # 删除多条记录
+        url = f"{self.API_BASE_URL}/base/delete_records"
+        params = {
+            "app_token": app_token,
+            "table_id": table_id,
+            "table_name": table_name,
+        }
+        if not record_ids:
+            record_id_list = []
+        else:
+            try:
+                record_id_list = json.loads(record_ids)
+            except json.JSONDecodeError:
+                raise ValueError("The input string is not valid JSON")
+        payload = {
+            "records": record_id_list,
+        }
+        res = self._send_request(url, params=params, payload=payload)
+        return res.get("data")
+
+    def search_record(
+            self,
+            app_token: str,
+            table_id: str,
+            table_name: str,
+            view_id: str,
+            field_names: str,
+            sort: str,
+            filters: str,
+            page_token: str,
+            automatic_fields: bool = False,
+            user_id_type: str = "open_id",
+            page_size: int = 20,
+    ) -> dict:
+        # 查询记录，单次最多查询 500 行记录。
+        url = f"{self.API_BASE_URL}/base/search_record"
+        params = {
+            "app_token": app_token,
+            "table_id": table_id,
+            "table_name": table_name,
+            "user_id_type": user_id_type,
+            "page_token": page_token,
+            "page_size": page_size,
+        }
+
+        if not field_names:
+            field_name_list = []
+        else:
+            try:
+                field_name_list = json.loads(field_names)
+            except json.JSONDecodeError:
+                raise ValueError("The input string is not valid JSON")
+
+        if not sort:
+            sort_list = []
+        else:
+            try:
+                sort_list = json.loads(sort)
+            except json.JSONDecodeError:
+                raise ValueError("The input string is not valid JSON")
+
+        if not filters:
+            filter_dict = {}
+        else:
+            try:
+                filter_dict = json.loads(filters)
+            except json.JSONDecodeError:
+                raise ValueError("The input string is not valid JSON")
+
+        payload = {
+        }
+
+        if view_id:
+            payload["view_id"] = view_id
+        if field_names:
+            payload["field_names"] = field_name_list
+        if sort:
+            payload["sort"] = sort_list
+        if filters:
+            payload["filter"] = filter_dict
+        if automatic_fields:
+            payload["automatic_fields"] = automatic_fields
+        res = self._send_request(url, params=params, payload=payload)
+        return res.get("data")
+
+    def get_base_info(
+            self,
+            app_token: str,
+    ) -> dict:
+        # 获取多维表格元数据
+        url = f"{self.API_BASE_URL}/base/get_base_info"
+        params = {
+            "app_token": app_token,
+        }
+        res = self._send_request(url, method="GET", params=params)
+        return res.get("data")
+
+    def create_table(
+            self,
+            app_token: str,
+            table_name: str,
+            default_view_name: str,
+            fields: str,
+    ) -> dict:
+        # 新增一个数据表
+        url = f"{self.API_BASE_URL}/base/create_table"
+        params = {
+            "app_token": app_token,
+        }
+        if not fields:
+            fields_list = []
+        else:
+            try:
+                fields_list = json.loads(fields)
+            except json.JSONDecodeError:
+                raise ValueError("The input string is not valid JSON")
+        payload = {
+            "name": table_name,
+            "fields": fields_list,
+        }
+        if default_view_name:
+            payload["default_view_name"] = default_view_name
+        res = self._send_request(url, params=params, payload=payload)
+        return res.get("data")
+
+    def delete_tables(
+            self,
+            app_token: str,
+            table_ids: str,
+            table_names: str,
+    ) -> dict:
+        # 删除多个数据表
+        url = f"{self.API_BASE_URL}/base/delete_tables"
+        params = {
+            "app_token": app_token,
+        }
+        if not table_ids:
+            table_id_list = []
+        else:
+            try:
+                table_id_list = json.loads(table_ids)
+            except json.JSONDecodeError:
+                raise ValueError("The input string is not valid JSON")
+
+        if not table_names:
+            table_name_list = []
+        else:
+            try:
+                table_name_list = json.loads(table_names)
+            except json.JSONDecodeError:
+                raise ValueError("The input string is not valid JSON")
+
+        payload = {
+            "table_ids": table_id_list,
+            "table_names": table_name_list,
+        }
+        res = self._send_request(url, params=params, payload=payload)
+        return res.get("data")
+
+    def list_tables(
+            self,
+            app_token: str,
+            page_token: str,
+            page_size: int = 20,
+    ) -> dict:
+        # 列出多维表格下的全部数据表
+        url = f"{self.API_BASE_URL}/base/list_tables"
+        params = {
+            "app_token": app_token,
+            "page_token": page_token,
+            "page_size": page_size,
+        }
+        res = self._send_request(url, method="GET", params=params)
+        return res.get("data")
+
+    def read_records(
+            self,
+            app_token: str,
+            table_id: str,
+            table_name: str,
+            record_ids: str,
+            user_id_type: str = "open_id",
+    ) -> dict:
+        url = f"{self.API_BASE_URL}/base/read_records"
+        params = {
+            "app_token": app_token,
+            "table_id": table_id,
+            "table_name": table_name,
+        }
+        if not record_ids:
+            record_id_list = []
+        else:
+            try:
+                record_id_list = json.loads(record_ids)
+            except json.JSONDecodeError:
+                raise ValueError("The input string is not valid JSON")
+        payload = {
+            "record_ids": record_id_list,
+            "user_id_type": user_id_type,
+        }
+        res = self._send_request(url, method="GET", params=params, payload=payload)
         return res.get("data")
