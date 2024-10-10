ce165fbe5780e72abb5a07f303774bc5588fd2fd
HowardChan <lonely829@gmail.com>
2024-10-10 11:32:53 +0800

    mock pinecone

--- api/tests/integration_tests/vdb/pinecone/test_pinecone.py
+++ api/tests/integration_tests/vdb/pinecone/test_pinecone.py
@@ -1,4 +1,5 @@
-import time
+import pytest
+from unittest.mock import MagicMock
 
 from core.rag.datasource.vdb.pinecone.pinecone_vector import PineconeConfig, PineconeVector
 from tests.integration_tests.vdb.test_vector_store import (
@@ -22,14 +23,31 @@ class PineconeVectorTest(AbstractVectorTest):
         assert len(hits_by_full_text) == 0
 
 
-def test_pinecone_vector(setup_mock_redis):
-    test_instance = PineconeVectorTest()
+@pytest.fixture
+def test_instance():
+    return PineconeVectorTest()
+
+
+def test_pinecone_vector(test_instance, setup_mock_redis, pinecone_mock):
+    test_instance.vector._client = pinecone_mock
     test_instance.create_vector()
     # It seems that pinecone needs sometime to persist the data.
-    time.sleep(10)
+    # time.sleep(10)
     test_instance.search_by_vector()
     test_instance.text_exists()
     test_instance.get_ids_by_metadata_field()
     added_doc_ids = test_instance.add_texts()
     test_instance.delete_by_ids(added_doc_ids)
     test_instance.delete_vector()
+
+
+@pytest.fixture
+def pinecone_mock(test_instance):
+    mock = MagicMock()
+    mock.has_index.return_value = True
+    mock.Index.return_value.query.return_value = {
+        "matches": [
+            {"id": "1", "metadata": {"page_content": "content1", 'doc_id': test_instance.example_doc_id}, "score": 0.9},
+        ]
+    }
+    return mock
