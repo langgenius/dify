c2328cb6769066d9816c137b39d3e2758d3c0fb4
-LAN- <laipz8200@outlook.com>
2024-10-11 00:56:37 +0800

    chore: update image versions to 0.9.1-fix1

--- api/configs/packaging/__init__.py
+++ api/configs/packaging/__init__.py
@@ -9,7 +9,7 @@ class PackagingInfo(BaseSettings):
 
     CURRENT_VERSION: str = Field(
         description="Dify version",
-        default="0.9.2",
+        default="0.9.1-fix1",
     )
 
     COMMIT_SHA: str = Field(
