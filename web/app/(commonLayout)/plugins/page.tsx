466f61d04493c0cb87aeb50a553027e06ad936a2
StyleZhang <jasonapring2015@outlook.com>
2024-10-12 11:05:03 +0800

    relocate file

--- web/app/(commonLayout)/plugins/page.tsx
+++ web/app/(commonLayout)/plugins/page.tsx
@@ -1,5 +1,5 @@
-import PluginsPanel from './plugins-panel'
-import Container from './container'
+import PluginsPanel from '@/app/components/plugins/plugins-panel'
+import Container from '@/app/components/plugins/container'
 import Marketplace from '@/app/components/plugins/marketplace'
 
 const PluginList = async () => {
