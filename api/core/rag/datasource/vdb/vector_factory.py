d15ba3939d39c14fd52cd2566bc3bf04cdf5f1f2
ice yao <yao3690093@gmail.com>
2024-10-13 21:26:05 +0800

    Add Volcengine VikingDB as new vector provider (#9287)

--- api/core/rag/datasource/vdb/vector_factory.py
+++ api/core/rag/datasource/vdb/vector_factory.py
@@ -107,6 +107,10 @@ class Vector:
                 from core.rag.datasource.vdb.baidu.baidu_vector import BaiduVectorFactory
 
                 return BaiduVectorFactory
+            case VectorType.VIKINGDB:
+                from core.rag.datasource.vdb.vikingdb.vikingdb_vector import VikingDBVectorFactory
+
+                return VikingDBVectorFactory
             case _:
                 raise ValueError(f"Vector store {vector_type} is not supported.")
 
