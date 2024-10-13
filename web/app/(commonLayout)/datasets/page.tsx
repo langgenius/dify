a25b65ddea4869b403826c0b7cfea68099628704
Julian Wong <e0725200@u.nus.edu>
2024-10-13 18:11:05 +0800

    initial name replacement to asa

--- web/app/(commonLayout)/datasets/page.tsx
+++ web/app/(commonLayout)/datasets/page.tsx
@@ -5,7 +5,7 @@ const AppList = async () => {
 }
 
 export const metadata = {
-  title: 'Datasets - Dify',
+  title: 'Datasets - Asa',
 }
 
 export default AppList
--- web/app/account/layout.tsx
+++ web/app/account/layout.tsx
@@ -34,7 +34,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
 }
 
 export const metadata = {
-  title: 'Dify',
+  title: 'Asa',
 }
 
 export default Layout
