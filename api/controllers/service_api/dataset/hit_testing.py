8501af298fbf89dc9146c3a65bb7f3d518dc786d
Summer-Gu <37869445+gubinjie@users.noreply.github.com>
2024-10-14 17:26:04 +0800

    feat: added dataset recall testing API (#9300)

--- api/controllers/service_api/dataset/hit_testing.py
+++ api/controllers/service_api/dataset/hit_testing.py
@@ -1,17 +1,10 @@
-from flask_restful import Resource
-
-from controllers.console import api
 from controllers.console.datasets.hit_testing_base import DatasetsHitTestingBase
-from controllers.console.setup import setup_required
-from controllers.console.wraps import account_initialization_required
-from libs.login import login_required
+from controllers.service_api import api
+from controllers.service_api.wraps import DatasetApiResource
 
 
-class HitTestingApi(Resource, DatasetsHitTestingBase):
-    @setup_required
-    @login_required
-    @account_initialization_required
-    def post(self, dataset_id):
+class HitTestingApi(DatasetApiResource, DatasetsHitTestingBase):
+    def post(self, tenant_id, dataset_id):
         dataset_id_str = str(dataset_id)
 
         dataset = self.get_and_validate_dataset(dataset_id_str)
