5f6099fc20ddc905b6300717af9d432295898678
Summer-Gu <37869445+gubinjie@users.noreply.github.com>
2024-10-14 17:26:04 +0800

    feat: added dataset recall testing API (#9300)

--- api/controllers/service_api/__init__.py
+++ api/controllers/service_api/__init__.py
@@ -5,7 +5,6 @@ from libs.external_api import ExternalApi
 bp = Blueprint("service_api", __name__, url_prefix="/v1")
 api = ExternalApi(bp)
 
-
 from . import index
 from .app import app, audio, completion, conversation, file, message, workflow
-from .dataset import dataset, document, segment
+from .dataset import dataset, document, hit_testing, segment
