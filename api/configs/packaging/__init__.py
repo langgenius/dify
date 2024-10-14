3707c67373a6142b41d440c66c6e5a8fb63066ec
-LAN- <laipz8200@outlook.com>
2024-10-14 17:21:58 +0800

    chore(release): bump version to 0.9.2 (#9314)

--- api/configs/packaging/__init__.py
+++ api/configs/packaging/__init__.py
@@ -9,7 +9,7 @@ class PackagingInfo(BaseSettings):
 
     CURRENT_VERSION: str = Field(
         description="Dify version",
-        default="0.9.1",
+        default="0.9.2",
     )
 
     COMMIT_SHA: str = Field(
