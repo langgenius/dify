89b2d948a598b30bf60cb3ddad7e30094e9a706c
Bowen Liang <liangbowen@gf.com.cn>
2024-10-14 23:47:58 +0800

    chore: clean-up unnecessary annotation on configs with non-null default value (#9323)

--- api/configs/middleware/vdb/vikingdb_config.py
+++ api/configs/middleware/vdb/vikingdb_config.py
@@ -11,27 +11,39 @@ class VikingDBConfig(BaseModel):
     """
 
     VIKINGDB_ACCESS_KEY: Optional[str] = Field(
-        default=None, description="The Access Key provided by Volcengine VikingDB for API authentication."
+        description="The Access Key provided by Volcengine VikingDB for API authentication."
+        "Refer to the following documentation for details on obtaining credentials:"
+        "https://www.volcengine.com/docs/6291/65568",
+        default=None,
     )
+
     VIKINGDB_SECRET_KEY: Optional[str] = Field(
-        default=None, description="The Secret Key provided by Volcengine VikingDB for API authentication."
+        description="The Secret Key provided by Volcengine VikingDB for API authentication.",
+        default=None,
     )
-    VIKINGDB_REGION: Optional[str] = Field(
-        default="cn-shanghai",
+
+    VIKINGDB_REGION: str = Field(
         description="The region of the Volcengine VikingDB service.(e.g., 'cn-shanghai', 'cn-beijing').",
+        default="cn-shanghai",
     )
-    VIKINGDB_HOST: Optional[str] = Field(
-        default="api-vikingdb.mlp.cn-shanghai.volces.com",
+
+    VIKINGDB_HOST: str = Field(
         description="The host of the Volcengine VikingDB service.(e.g., 'api-vikingdb.volces.com', \
             'api-vikingdb.mlp.cn-shanghai.volces.com')",
+        default="api-vikingdb.mlp.cn-shanghai.volces.com",
     )
-    VIKINGDB_SCHEME: Optional[str] = Field(
-        default="http",
+
+    VIKINGDB_SCHEME: str = Field(
         description="The scheme of the Volcengine VikingDB service.(e.g., 'http', 'https').",
+        default="http",
     )
-    VIKINGDB_CONNECTION_TIMEOUT: Optional[int] = Field(
-        default=30, description="The connection timeout of the Volcengine VikingDB service."
+
+    VIKINGDB_CONNECTION_TIMEOUT: int = Field(
+        description="The connection timeout of the Volcengine VikingDB service.",
+        default=30,
     )
-    VIKINGDB_SOCKET_TIMEOUT: Optional[int] = Field(
-        default=30, description="The socket timeout of the Volcengine VikingDB service."
+
+    VIKINGDB_SOCKET_TIMEOUT: int = Field(
+        description="The socket timeout of the Volcengine VikingDB service.",
+        default=30,
     )
