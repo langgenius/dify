86427468fd1fefe1465f6a4d449065e4ffc8ee9d
-LAN- <laipz8200@outlook.com>
2024-10-15 14:23:02 +0800

    Update version to 0.10.0-beta3 in packaging, docker, and web

--- api/configs/packaging/__init__.py
+++ api/configs/packaging/__init__.py
@@ -9,7 +9,7 @@ class PackagingInfo(BaseSettings):
 
     CURRENT_VERSION: str = Field(
         description="Dify version",
-        default="0.10.0-beta2",
+        default="0.10.0-beta3",
     )
 
     COMMIT_SHA: str = Field(
