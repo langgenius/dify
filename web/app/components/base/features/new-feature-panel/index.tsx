49d1176fbb517f230dc7b784b0504daefe3ab5db
JzoNg <jzongcode@gmail.com>
2024-10-18 17:03:55 +0800

    add link for legacy feature

--- web/app/components/base/features/new-feature-panel/index.tsx
+++ web/app/components/base/features/new-feature-panel/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { RiCloseLine } from '@remixicon/react'
+import { useContext } from 'use-context-selector'
+import { RiCloseLine, RiInformation2Fill } from '@remixicon/react'
 import DialogWrapper from '@/app/components/base/features/new-feature-panel/dialog-wrapper'
 import { useDefaultModel } from '@/app/components/header/account-setting/model-provider-page/hooks'
 import { ModelTypeEnum } from '@/app/components/header/account-setting/model-provider-page/declarations'
@@ -18,6 +19,8 @@ import Moderation from '@/app/components/base/features/new-feature-panel/moderat
 import AnnotationReply from '@/app/components/base/features/new-feature-panel/annotation-reply'
 import type { PromptVariable } from '@/models/debug'
 import type { InputVar } from '@/app/components/workflow/types'
+import I18n from '@/context/i18n'
+import { LanguagesSupported } from '@/i18n/language'
 
 type Props = {
   show: boolean
@@ -45,6 +48,7 @@ const NewFeaturePanel = ({
   onAutoAddPromptVariable,
 }: Props) => {
   const { t } = useTranslation()
+  const { locale } = useContext(I18n)
   const { data: speech2textDefaultModel } = useDefaultModel(ModelTypeEnum.speech2text)
   const { data: text2speechDefaultModel } = useDefaultModel(ModelTypeEnum.tts)
 
@@ -65,6 +69,24 @@ const NewFeaturePanel = ({
         </div>
         {/* list */}
         <div className='grow basis-0 overflow-y-auto px-4 pb-4'>
+          {showFileUpload && (
+            <div className='relative mb-1 p-2 rounded-xl border border-components-panel-border shadow-xs'>
+              <div className='absolute top-0 left-0 w-full h-full rounded-xl opacity-40' style={{ background: 'linear-gradient(92deg, rgba(11, 165, 236, 0.25) 18.12%, rgba(255, 255, 255, 0.00) 167.31%)' }}></div>
+              <div className='relative flex items-start w-full h-full'>
+                <div className='shrink-0 mr-0.5 p-0.5'>
+                  <RiInformation2Fill className='w-5 h-5 text-text-accent' />
+                </div>
+                <div className='p-1 text-text-primary system-xs-medium'>
+                  <span>{isChatMode ? t('workflow.common.fileUploadTip') : t('workflow.common.ImageUploadLegacyTip')}</span>
+                  <a
+                    className='text-text-accent'
+                    href={`https://docs.dify.ai/${locale === LanguagesSupported[1] && 'v/zh-hans/'}'guides/workflow/bulletin`}
+                    target='_blank' rel='noopener noreferrer'
+                  >{t('workflow.common.featuresDocLink')}</a>
+                </div>
+              </div>
+            </div>
+          )}
           {!isChatMode && !inWorkflow && (
             <MoreLikeThis disabled={disabled} onChange={onChange} />
           )}
