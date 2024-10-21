2094c5495187e4df28433174c341ff8b03b4f27c
AkaraChen <akarachen@outlook.com>
2024-10-21 16:21:12 +0800

    build: update eslint config antfu

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,3 +1,4 @@
+import { stylistic, typescript, combine, javascript } from '@antfu/eslint-config'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
@@ -11,45 +12,91 @@ const compat = new FlatCompat({
   allConfig: js.configs.all,
 })
 
-const ignores = [
-  '**/node_modules/*',
-  '**/node_modules/',
-  '**/dist/',
-  '**/build/',
-  '**/out/',
-  '**/.next/',
-  // TODO: remove this
-  '**/*.json',
-  '**/*.md',
-]
-
-const backup = {
-  rules: {
-    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-    '@typescript-eslint/no-var-requires': 'off',
-    'no-console': 'off',
-    'indent': 'off',
-
-    '@typescript-eslint/indent': ['error', 2, {
-      SwitchCase: 1,
-      flatTernaryExpressions: false,
-
-      ignoredNodes: [
-        'PropertyDefinition[decorators]',
-        'TSUnionType',
-        'FunctionExpression[params]:has(Identifier[decorators])',
-      ],
-    }],
-
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/display-name': 'warn',
+// storybook plugin not support v9, so add its recommended rules here
+const storybook = [
+  {
+    plugins: ['storybook'],
+    files: ['*.stories.@(ts|tsx|js|jsx|mjs|cjs)', '*.story.@(ts|tsx|js|jsx|mjs|cjs)'],
+      rules: {
+        'react-hooks/rules-of-hooks': 'off',
+        'import/no-anonymous-default-export': 'off',
+        'storybook/await-interactions': 'error',
+        'storybook/context-in-play-function': 'error',
+        'storybook/default-exports': 'error',
+        'storybook/hierarchy-separator': 'warn',
+        'storybook/no-redundant-story-name': 'warn',
+        'storybook/prefer-pascal-case': 'warn',
+        'storybook/story-exports': 'error',
+        'storybook/use-storybook-expect': 'error',
+        'storybook/use-storybook-testing-library': 'error',
+      }
   },
-}
-
-const config = [
-  { ignores },
-  ...compat.extends('next', '@antfu', 'plugin:storybook/recommended'),
-  backup,
+  {
+    plugins: ['storybook'],
+    files: ['*.stories.@(ts|tsx|js|jsx|mjs|cjs)', '*.story.@(ts|tsx|js|jsx|mjs|cjs)'],
+      rules: {
+        'storybook/no-uninstalled-addons': 'error',
+      }
+  }
 ]
 
-export default config
+export default combine(
+  stylistic({
+    lessOpinionated: true,
+    // original @antfu/eslint-config does not support jsx
+    jsx: false,
+    overrides: {
+      "style/indent": "off",
+
+      // these options does not exist in old version
+      "style/indent-binary-ops": "off",
+      "style/multiline-ternary": "off",
+
+      // big change
+      "style/quote-props": "off",
+      "style/member-delimiter-style": "off",
+      "style/quotes": "off",
+      "style/comma-dangle": "off",
+    }
+  }),
+  typescript({
+    overrides: {
+      // useful, but big change
+      "ts/no-empty-object-type": "off",
+    }
+  }),
+  // javascript(),
+  // TODO: remove this when upgrade to nextjs 15
+  compat.extends('next'),
+  {
+    ignores: [
+      '**/node_modules/*',
+      '**/node_modules/',
+      '**/dist/',
+      '**/build/',
+      '**/out/',
+      '**/.next/',
+      '**/public/*',
+      '**/*.json',
+    ]
+  },
+  {
+    // orignal config
+    rules: {
+      'ts/consistent-type-definitions': ['error', 'type'],
+      'ts/no-require-imports': 'off',
+      "no-console": 'off',
+      "react-hooks/exhaustive-deps": "warn",
+      "react/display-name": "off",
+      "curly": "off",
+    }
+  },
+  storybook,
+  // need futher research
+  {
+    rules: {
+      // not exist in old version
+      "antfu/consistent-list-newline": "off"
+    }
+  }
+)
