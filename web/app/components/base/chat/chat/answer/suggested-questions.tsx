d066c3e102054a9e66fc8ee529f24f7e54fca08b
Tao Wang <twang293@gmail.com>
2024-10-21 20:31:13 -0700

    Align suggested questions

--- web/app/components/base/chat/chat/answer/suggested-questions.tsx
+++ web/app/components/base/chat/chat/answer/suggested-questions.tsx
@@ -23,7 +23,7 @@ const SuggestedQuestions: FC<SuggestedQuestionsProps> = ({
       {suggestedQuestions.filter(q => !!q && q.trim()).map((question, index) => (
         <div
           key={index}
-          className='mt-1 mr-1 max-w-full last:mr-0 shrink-0 py-[5px] leading-[18px] items-center px-4 rounded-lg border border-gray-200 shadow-xs bg-white text-xs font-medium text-primary-600 cursor-pointer'
+          className='mt-1 mr-1 min-w-[50%] max-w-full last:mr-0 shrink-0 py-[5px] leading-[18px] items-center px-4 rounded-lg border border-gray-200 shadow-xs bg-white text-xs font-medium text-primary-600 cursor-pointer'
           onClick={() => onSend?.(question)}
         >
           {question}
