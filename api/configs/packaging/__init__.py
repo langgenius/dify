abdbaaa2afac1e685e8e6011a6da6c03c101c050
-LAN- <laipz8200@outlook.com>
2024-10-23 17:49:51 +0800

    chore: update version to 0.10.1 (#9689)

--- api/configs/packaging/__init__.py
+++ api/configs/packaging/__init__.py
@@ -9,7 +9,7 @@ class PackagingInfo(BaseSettings):
 
     CURRENT_VERSION: str = Field(
         description="Dify version",
-        default="0.10.0",
+        default="0.10.1",
     )
 
     COMMIT_SHA: str = Field(
