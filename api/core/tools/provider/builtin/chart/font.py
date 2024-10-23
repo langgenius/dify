da331c379f8050f464df4c3d85e4cf1e797f8200
hejl <hjlarry@163.com>
2024-10-23 15:49:34 +0800

    fix CI, remove the old init font

--- api/core/tools/provider/builtin/chart/font.py
+++ api/core/tools/provider/builtin/chart/font.py
@@ -2,12 +2,20 @@ from matplotlib.font_manager import FontProperties
 
 
 def set_chinese_font():
-    font_list = ['PingFang SC', 'SimHei', 'Microsoft YaHei', 'STSong', 'SimSun', 'Arial Unicode MS', 
-                 'Noto Sans CJK SC', 'Noto Sans CJK JP']
-    
+    font_list = [
+        "PingFang SC",
+        "SimHei",
+        "Microsoft YaHei",
+        "STSong",
+        "SimSun",
+        "Arial Unicode MS",
+        "Noto Sans CJK SC",
+        "Noto Sans CJK JP",
+    ]
+
     for font in font_list:
         chinese_font = FontProperties(font)
-        if chinese_font.get_name() == font: 
+        if chinese_font.get_name() == font:
             return chinese_font
 
     return FontProperties()
