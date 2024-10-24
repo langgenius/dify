8685c0d48b1382d03aa173294e40c8133d590dde
takatost <takatost@gmail.com>
2024-10-24 00:06:49 -0700

    test replace gevent socket patch in redis

--- api/app.py
+++ api/app.py
@@ -3,6 +3,15 @@ import os
 from configs import dify_config
 
 if os.environ.get("DEBUG", "false").lower() != "true":
+    if os.environ.get("REDIS_GEVENT_SUPPORT", "false").lower() == "true":
+        import socket
+
+        original_socket_socket = socket.socket
+
+        from extensions.ext_redis import GeventSafeConnection
+
+        GeventSafeConnection.socket_socket_class = original_socket_socket
+
     from gevent import monkey
 
     monkey.patch_all()
