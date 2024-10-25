ff689b026089736282f5114785bc6c51a618f44c
Scorpion <guo2gbq@gmail.com>
2024-10-25 16:18:59 +0800

    fix: Python执行

--- api/core/helper/code_executor/code_executor.py
+++ api/core/helper/code_executor/code_executor.py
@@ -117,3 +117,13 @@ class CodeExecutor:
             raise e
 
         return template_transformer.transform_response(response)
+
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio    # free up some memory
+        sys.stdout = self._stdout
