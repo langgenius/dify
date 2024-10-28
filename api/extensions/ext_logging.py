3412b44e80f34aa42c1eb403cc7a3ab28c021e03
root <root@localhost.localdomain>
2024-10-28 10:50:24 +0800

    add PATHON_2
    Signed-off-by: root <root@localhost.localdomain>

--- api/extensions/ext_logging.py
+++ api/extensions/ext_logging.py
@@ -10,8 +10,8 @@ import re
 
 def replace_env_variables(text):
     def replace(match):
-        var_name=math.group(1)
-        var_value=os.getenv(var_name)
+        var_name = match.group(1)
+        var_value = os.getenv(var_name)
         return '' if var_value is None else var_value
     pattern = r'\$\{([^}]+)\}'
     return re.sub(pattern, replace, text)
@@ -24,17 +24,17 @@ def init_app(app: Flask):
         os.makedirs(log_dir, exist_ok=True)
         log_handlers = [
             RotatingFileHandler(
-                filename=log_file,
-                maxBytes=replace_env_variables(dify_config.LOG_FILE_MAX_SIZE) * 1024 * 1024,
-                backupCount=replace_env_variables(dify_config.LOG_FILE_BACKUP_COUNT),
+                filename = log_file,
+                maxBytes = replace_env_variables(dify_config.LOG_FILE_MAX_SIZE) * 1024 * 1024,
+                backupCount = replace_env_variables(dify_config.LOG_FILE_BACKUP_COUNT),
             ),
             logging.StreamHandler(sys.stdout),
         ] 
     logging.basicConfig(
-        level=replace_env_variables(dify_config.LOG_LEVEL),
-        format=replace_env_variables(dify_config.LOG_FORMAT),
-        datefmt=replace_env_variables(dify_config.LOG_DATEFORMAT),
-        handlers=log_handlers,
+        level = replace_env_variables(dify_config.LOG_LEVEL),
+        format = replace_env_variables(dify_config.LOG_FORMAT),
+        datefmt = replace_env_variables(dify_config.LOG_DATEFORMAT),
+        handlers = log_handlers,
         force=True,
     )
     log_tz = replace_env_variables(dify_config.LOG_TZ)
