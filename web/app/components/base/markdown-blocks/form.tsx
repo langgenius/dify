c395fe4adb29cd829857684b69b4fcfa349d1c10
crazywoola <427733928@qq.com>
2024-10-29 13:00:56 +0800

    feat: disable auto complete

--- web/app/components/base/markdown-blocks/form.tsx
+++ web/app/components/base/markdown-blocks/form.tsx
@@ -58,6 +58,7 @@ const MarkdownForm = ({ node }: any) => {
   }
   return (
     <form
+      autoComplete="off"
       className='flex flex-col self-stretch'
       onSubmit={(e: any) => {
         e.preventDefault()
