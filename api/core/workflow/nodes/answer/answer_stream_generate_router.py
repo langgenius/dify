052379ce00e145e70907baea70de73a1b8002639
非法操作 <hjlarry@163.com>
2024-10-30 11:10:31 +0800

    fix: conversation variable may not change in the answer node (#10034)

--- api/core/workflow/nodes/answer/answer_stream_generate_router.py
+++ api/core/workflow/nodes/answer/answer_stream_generate_router.py
@@ -153,6 +153,7 @@ class AnswerStreamGeneratorRouter:
                 NodeType.IF_ELSE,
                 NodeType.QUESTION_CLASSIFIER,
                 NodeType.ITERATION,
+                NodeType.CONVERSATION_VARIABLE_ASSIGNER,
             }:
                 answer_dependencies[answer_node_id].append(source_node_id)
             else:
