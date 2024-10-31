d4608f057105f05b37b0b93b319ffbbc3322d1a9
非法操作 <hjlarry@163.com>
2024-10-31 10:35:45 +0800

    chore: remove an unnecessary link (#10088)

--- web/app/(commonLayout)/datasets/DatasetFooter.tsx
+++ web/app/(commonLayout)/datasets/DatasetFooter.tsx
@@ -9,8 +9,8 @@ const DatasetFooter = () => {
     <footer className='px-12 py-6 grow-0 shrink-0'>
       <h3 className='text-xl font-semibold leading-tight text-gradient'>{t('dataset.didYouKnow')}</h3>
       <p className='mt-1 text-sm font-normal leading-tight text-gray-700'>
-        {t('dataset.intro1')}<a className='inline-flex items-center gap-1 link' target='_blank' rel='noopener noreferrer' href='/'>{t('dataset.intro2')}</a>{t('dataset.intro3')}<br />
-        {t('dataset.intro4')}<a className='inline-flex items-center gap-1 link' target='_blank' rel='noopener noreferrer' href='/'>{t('dataset.intro5')}</a>{t('dataset.intro6')}
+        {t('dataset.intro1')}<span className='inline-flex items-center gap-1 text-blue-600'>{t('dataset.intro2')}</span>{t('dataset.intro3')}<br />
+        {t('dataset.intro4')}<span className='inline-flex items-center gap-1 text-blue-600'>{t('dataset.intro5')}</span>{t('dataset.intro6')}
       </p>
     </footer>
   )
