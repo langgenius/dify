629aba6d897eaebb0938211922c8ae006244ac08
Benjamin <benjaminx@gmail.com>
2024-11-07 18:07:05 +0800

    fix(conversation-service): return success response after conversation… (#10416)

--- api/controllers/service_api/app/conversation.py
+++ api/controllers/service_api/app/conversation.py
@@ -58,10 +58,9 @@ class ConversationDetailApi(Resource):
         conversation_id = str(c_id)
 
         try:
-            ConversationService.delete(app_model, conversation_id, end_user)
+            return ConversationService.delete(app_model, conversation_id, end_user)
         except services.errors.conversation.ConversationNotExistsError:
             raise NotFound("Conversation Not Exists.")
-        return {"result": "success"}, 200
 
 
 class ConversationRenameApi(Resource):
