67dd893a80492303391685903e38ca254fd2a2b7
leo.wang <wlrnet@163.com>
2024-11-07 17:56:26 +0800

    fix lint issue

--- api/core/rag/extractor/word_extractor.py
+++ api/core/rag/extractor/word_extractor.py
@@ -9,7 +9,6 @@ import tempfile
 import uuid
 from urllib.parse import urlparse
 from xml.etree import ElementTree
-import tempfile
 
 import requests
 from docx import Document as DocxDocument
@@ -28,7 +27,6 @@ logger = logging.getLogger(__name__)
 
 class WordExtractor(BaseExtractor):
     """Load docx files.
-
 
     Args:
         file_path: Path to the file to load.
