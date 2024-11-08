a714a8193253a90d03a956157344113a56cf3743
jyong <718720800@qq.com>
2024-11-08 11:46:36 +0800

    fix segment enable service api

--- api/services/dataset_service.py
+++ api/services/dataset_service.py
@@ -14,8 +14,6 @@ from configs import dify_config
 from core.errors.error import LLMBadRequestError, ProviderTokenNotInitError
 from core.model_manager import ModelManager
 from core.model_runtime.entities.model_entities import ModelType
-from core.rag.datasource.keyword.keyword_factory import Keyword
-from core.rag.models.document import Document as RAGDocument
 from core.rag.retrieval.retrieval_methods import RetrievalMethod
 from events.dataset_event import dataset_was_deleted
 from events.document_event import document_was_deleted
--- api/services/entities/knowledge_entities/knowledge_entities.py
+++ api/services/entities/knowledge_entities/knowledge_entities.py
@@ -1,4 +1,4 @@
-from typing import Literal, Optional, Union
+from typing import Optional
 
 from pydantic import BaseModel
 
