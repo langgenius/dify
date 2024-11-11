a8e8e3675606c762b88dc58bcfe769176c669cc7
Yi <yxiaoisme@gmail.com>
2024-11-11 18:00:44 +0800

    chore: update the upgrade button

--- web/app/components/header/index.tsx
+++ web/app/components/header/index.tsx
@@ -94,7 +94,11 @@ const Header = () => {
             <div className='select-none'>
               <PremiumBadge color='blue' allowHover={true} onClick={handlePlanClick}>
                 <SparklesSoft className='flex items-center py-[1px] pl-[3px] w-3.5 h-3.5 text-components-premium-badge-indigo-text-stop-0' />
-                <div className='system-xs-medium !px-1'>Upgrade</div>
+                <div className='system-xs-medium'>
+                  <span className='p-1'>
+                    Upgrade
+                  </span>
+                </div>
               </PremiumBadge>
             </div>
           )}
