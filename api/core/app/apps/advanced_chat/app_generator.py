5f3234657a3593a5ca7e78f030e4cfaf90071485
root <root@localhost.localdomain>
2024-11-13 16:44:28 +0800

    fix issues #10055
    Signed-off-by: root <root@localhost.localdomain>

--- api/core/app/apps/advanced_chat/app_generator.py
+++ api/core/app/apps/advanced_chat/app_generator.py
@@ -125,9 +125,7 @@ class AdvancedChatAppGenerator(MessageBasedAppGenerator):
             app_config=app_config,
             file_upload_config=file_extra_config,
             conversation_id=conversation.id if conversation else None,
-            inputs=conversation.inputs
-            if conversation
-            else self._prepare_user_inputs(user_inputs=inputs, app_config=app_config),
+            inputs=inputs if conversation else self._prepare_user_inputs(user_inputs=inputs, app_config=app_config),
             query=query,
             files=file_objs,
             parent_message_id=args.get("parent_message_id") if invoke_from != InvokeFrom.SERVICE_API else UUID_NIL,
