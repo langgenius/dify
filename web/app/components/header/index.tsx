a7a5c6f4cb984b7e835342435c9d128d29213f1e
Yi <yxiaoisme@gmail.com>
2024-11-15 16:09:04 +0800

    fix: update the upgrade button text content in the header

--- web/app/components/header/index.tsx
+++ web/app/components/header/index.tsx
@@ -76,7 +76,7 @@ const Header = () => {
                   <SparklesSoft className='flex items-center py-[1px] pl-[3px] w-3.5 h-3.5 text-components-premium-badge-indigo-text-stop-0' />
                   <div className='system-xs-medium'>
                     <span className='p-1'>
-                      {t('billing.upgradeBtn.encourage')}
+                      {t('billing.upgradeBtn.encourageShort')}
                     </span>
                   </div>
                 </PremiumBadge>
@@ -98,7 +98,7 @@ const Header = () => {
                 <SparklesSoft className='flex items-center py-[1px] pl-[3px] w-3.5 h-3.5 text-components-premium-badge-indigo-text-stop-0' />
                 <div className='system-xs-medium'>
                   <span className='p-1'>
-                    {t('billing.upgradeBtn.encourage')}
+                    {t('billing.upgradeBtn.encourageShort')}
                   </span>
                 </div>
               </PremiumBadge>
@@ -115,7 +115,7 @@ const Header = () => {
           {!isCurrentWorkspaceDatasetOperator && <ToolsNav className={navClassName} />}
         </div>
       )}
-      <div className='flex items-center flex-shrink-0'>
+      <div className='flex items-center shrink-0'>
         <EnvNav />
         <div className='mr-3'>
           <PluginsNav />
