146c027cf13b780f8433a8596f8f3dd0582d00ed
Tao Wang <twang293@gmail.com>
2024-11-16 15:49:15 -0800

    Remove 'iframe' from disallowedElements in markdown.tsx

--- web/app/components/base/markdown.tsx
+++ web/app/components/base/markdown.tsx
@@ -263,7 +263,7 @@ export function Markdown(props: { content: string; className?: string }) {
             }
           },
         ]}
-        disallowedElements={['script', 'iframe', 'head', 'html', 'meta', 'link', 'style', 'body']}
+        disallowedElements={['script', 'head', 'html', 'meta', 'link', 'style', 'body']}
         components={{
           code: CodeBlock,
           img: Img,
