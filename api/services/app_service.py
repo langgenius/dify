82bae1d746594ad79656b3fc62d96d5678a67a08
-LAN- <laipz8200@outlook.com>
2024-11-20 19:48:59 +0800

    chore(api/services/app_service.py): Remove the wrong type hint

--- api/services/app_service.py
+++ api/services/app_service.py
@@ -155,7 +155,7 @@ class AppService:
         """
         # get original app model config
         if app.mode == AppMode.AGENT_CHAT.value or app.is_agent:
-            model_config: AppModelConfig = app.app_model_config
+            model_config = app.app_model_config
             agent_mode = model_config.agent_mode_dict
             # decrypt agent tool parameters if it's secret-input
             for tool in agent_mode.get("tools") or []:
