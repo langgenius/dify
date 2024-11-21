3788fd840880406cb5728faa055517f00e1aea06
crazywoola <427733928@qq.com>
2024-11-21 14:09:20 +0800

    fix: lint

--- api/core/tools/provider/builtin/json_process/tools/parse.py
+++ api/core/tools/provider/builtin/json_process/tools/parse.py
@@ -39,10 +39,10 @@ class JSONParseTool(BuiltinTool):
             input_data = json.loads(content)
             expr = parse(json_filter)
             result = [match.value for match in expr.find(input_data)]
-            
+
             if not result:
                 return ""
-                        
+
             if len(result) == 1:
                 result = result[0]
 
