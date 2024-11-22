1a5d60b105e4f2ebb6c86e54f9d069b880c8442d
Benjamin <benjaminx@gmail.com>
2024-11-22 22:32:34 +0800

    refactor(json): remove JSON extractor tool and update parse tool parameters

--- api/core/tools/provider/builtin/json_process/tools/parse.py
+++ api/core/tools/provider/builtin/json_process/tools/parse.py
@@ -16,20 +16,34 @@ class JSONParseTool(BuiltinTool):
         """
         invoke tools
         """
-        # get content
+        # get tool parameters
         content = tool_parameters.get("content", "")
+        json_filter = tool_parameters.get("json_filter", "")
+        ensure_ascii = tool_parameters.get("ensure_ascii", True)
+        output_full_parsed_json = tool_parameters.get("output_full_parsed_json", True)
+        
         if not content:
             return self.create_text_message("Invalid parameter content")
 
-        # get json filter
-        json_filter = tool_parameters.get("json_filter", "")
-        if not json_filter:
-            return self.create_text_message("Invalid parameter json_filter")
-
-        ensure_ascii = tool_parameters.get("ensure_ascii", True)
         try:
-            result = self._extract(content, json_filter, ensure_ascii)
-            return self.create_text_message(str(result))
+            final_result = []
+            if output_full_parsed_json:
+                # parse full json
+                json_content = json.loads(content)
+
+                # append json_messages to final_result
+                if isinstance(json_content, list):
+                    for item in json_content:
+                        final_result.append(self.create_json_message(item))
+                else:
+                    final_result.append(self.create_json_message(json_content))
+
+            if json_filter:
+                filtered_result = self._extract(content, json_filter, ensure_ascii)
+                final_result.append(self.create_text_message(str(filtered_result)))
+
+            return final_result
+
         except Exception:
             return self.create_text_message("Failed to extract JSON content")
 
