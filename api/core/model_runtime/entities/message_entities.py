ed52139997b98a48de50ef4b327fb0f0c0f3f94b
-LAN- <laipz8200@outlook.com>
2024-11-22 19:20:37 +0800

    feat(llm_node): raise an error if model not support the file.

--- api/core/model_runtime/entities/message_entities.py
+++ api/core/model_runtime/entities/message_entities.py
@@ -1,6 +1,6 @@
 from abc import ABC
 from collections.abc import Sequence
-from enum import Enum
+from enum import Enum, StrEnum
 from typing import Literal, Optional
 
 from pydantic import BaseModel, Field, field_validator
@@ -49,7 +49,7 @@ class PromptMessageFunction(BaseModel):
     function: PromptMessageTool
 
 
-class PromptMessageContentType(Enum):
+class PromptMessageContentType(StrEnum):
     """
     Enum class for prompt message content type.
     """
