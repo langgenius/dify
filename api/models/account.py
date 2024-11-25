accbbae7558231f9005a49be0997f9c2812d4ad7
Yeuoly <admin@srmxy.cn>
2024-11-25 16:47:49 +0800

    cleanup: remove get_interates

--- api/models/account.py
+++ api/models/account.py
@@ -102,10 +102,6 @@ class Account(UserMixin, Base):
             return db.session.query(Account).filter(Account.id == account_integrate.account_id).one_or_none()
         return None
 
-    def get_integrates(self) -> list[db.Model]:
-        ai = db.Model
-        return db.session.query(ai).filter(ai.account_id == self.id).all()
-
     # check current_user.current_tenant.current_role in ['admin', 'owner']
     @property
     def is_admin_or_owner(self):
