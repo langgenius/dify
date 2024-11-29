2feb44e2c5cdff5794e2d5a7c4d03b54159c3471
Bowen Liang <liangbowen@gf.com.cn>
2024-11-29 09:28:53 +0800

    chore(dep): bump flask from 3.0.1 to 3.1.0 and flask-compress to 1.17 (#11195)

--- api/extensions/ext_compress.py
+++ api/extensions/ext_compress.py
@@ -7,11 +7,5 @@ def init_app(app: Flask):
     if dify_config.API_COMPRESSION_ENABLED:
         from flask_compress import Compress
 
-        app.config["COMPRESS_MIMETYPES"] = [
-            "application/json",
-            "image/svg+xml",
-            "text/html",
-        ]
-
         compress = Compress()
         compress.init_app(app)
