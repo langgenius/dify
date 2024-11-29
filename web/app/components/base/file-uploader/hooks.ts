f13f4330f59b08a1ea695fbb07e531a51a8f9b9f
非法操作 <hjlarry@163.com>
2024-11-29 09:19:27 +0800

    fix handleLoadFileFromLink's transfer method

--- web/app/components/base/file-uploader/hooks.ts
+++ web/app/components/base/file-uploader/hooks.ts
@@ -211,7 +211,7 @@ export const useFile = (fileConfig: FileUpload) => {
       type: '',
       size: 0,
       progress: 0,
-      transferMethod: TransferMethod.local_file,
+      transferMethod: TransferMethod.remote_url,
       supportFileType: '',
       url,
       isRemote: true,
