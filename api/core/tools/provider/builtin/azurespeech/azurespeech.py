aeb4457f936c3ebb9d9b4f659311f3d0e2b711cb
ymshenyu <ymshenyu@gmail.com>
2024-12-05 13:12:22 +0800

    feat: add credentials validator

--- api/core/tools/provider/builtin/azurespeech/azurespeech.py
+++ api/core/tools/provider/builtin/azurespeech/azurespeech.py
@@ -1,8 +1,24 @@
+from calendar import c
 from typing import Any
 
+from core.tools.errors import ToolProviderCredentialValidationError
+from core.tools.provider.builtin.azurespeech.tools.tts import AzureTTSTool
 from core.tools.provider.builtin_tool_provider import BuiltinToolProviderController
 
 
 class AzureSpeechProvider(BuiltinToolProviderController):
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
-        pass
+        try:
+            AzureTTSTool().fork_tool_runtime(
+                runtime={
+                    "credentials": credentials,
+                }
+            ).invoke(
+                user_id="",
+                tool_parameters={
+                    "text": "This is a test text",
+                    "speech_synthesis_voice_name": "en-US-AvaMultilingualNeural",
+                },
+            )
+        except Exception as e:
+            raise ToolProviderCredentialValidationError(str(e))
