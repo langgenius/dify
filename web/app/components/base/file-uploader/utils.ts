ffd1343a09c87f9605d866b2eff644acfe4de1b5
KVOJJJin <jzongcode@gmail.com>
2024-12-06 17:35:35 +0800

    Fix: compatible with outputs data structure (#11432)

--- web/app/components/base/file-uploader/utils.ts
+++ web/app/components/base/file-uploader/utils.ts
@@ -158,13 +158,13 @@ export const isAllowedFileExtension = (fileName: string, fileMimetype: string, a
 
 export const getFilesInLogs = (rawData: any) => {
   const result = Object.keys(rawData || {}).map((key) => {
-    if (typeof rawData[key] === 'object' && rawData[key].dify_model_identity === '__dify__file__') {
+    if (typeof rawData[key] === 'object' && rawData[key]?.dify_model_identity === '__dify__file__') {
       return {
         varName: key,
         list: getProcessedFilesFromResponse([rawData[key]]),
       }
     }
-    if (Array.isArray(rawData[key]) && rawData[key].some(item => item.dify_model_identity === '__dify__file__')) {
+    if (Array.isArray(rawData[key]) && rawData[key].some(item => item?.dify_model_identity === '__dify__file__')) {
       return {
         varName: key,
         list: getProcessedFilesFromResponse(rawData[key]),
