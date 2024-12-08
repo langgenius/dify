c03ae266ffac090f7fc03c4534360af775bdb13b
罗威 <luowei@cvte.com>
2024-12-09 01:19:59 +0800

    Refactor: Simplify PDF loading UI in pdf-preview

--- web/app/components/base/file-uploader/pdf-preview.tsx
+++ web/app/components/base/file-uploader/pdf-preview.tsx
@@ -45,14 +45,10 @@ const PdfPreview: FC<PdfPreviewProps> = ({
       tabIndex={-1}
     >
       <div className='h-[95vh] w-[100vw] max-w-full max-h-full overflow-y-auto scrollbar-hide'>
-        <PdfLoader url={url} beforeLoad={<div className='flex justify-center items-center h-64'>
-          <svg className='animate-spin -ml-1 mr-3 h-5 w-5 text-primary-600'
-            xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'>
-            <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'/>
-            <path className='opacity-75' fill='currentColor' d='M4 12a8 8 0 018-8V2.5'/>
-          </svg>
-          <span><Loading type='app' /></span>
-        </div>}>
+        <PdfLoader
+          url={url}
+          beforeLoad={<div className='flex justify-center items-center h-64'><Loading type='app' /></div>}
+        >
           {(pdfDocument) => {
             return (
               <PdfHighlighter
