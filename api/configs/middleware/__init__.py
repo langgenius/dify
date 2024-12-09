967b7d89e3d89c07e795251ce95c2c08482d2a5a
huanshare <huanshare@live.com>
2024-12-10 02:51:20 +0800

    feat:add apollo configuration to load env file (#11210)
    Signed-off-by: -LAN- <laipz8200@outlook.com>
    Co-authored-by: huanshare <liuhuan101@longfor.com>
    Co-authored-by: -LAN- <laipz8200@outlook.com>

--- api/configs/middleware/__init__.py
+++ api/configs/middleware/__init__.py
@@ -73,7 +73,7 @@ class KeywordStoreConfig(BaseSettings):
     )
 
 
-class DatabaseConfig:
+class DatabaseConfig(BaseSettings):
     DB_HOST: str = Field(
         description="Hostname or IP address of the database server.",
         default="localhost",
