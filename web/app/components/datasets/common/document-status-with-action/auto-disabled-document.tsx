d1743ee3e5163a30fdccd77e79b61fdcb72360e7
Joel <iamjoel007@gmail.com>
2024-12-10 14:11:53 +0800

    feat: auto disabled api

--- web/app/components/datasets/common/document-status-with-action/auto-disabled-document.tsx
+++ web/app/components/datasets/common/document-status-with-action/auto-disabled-document.tsx
@@ -1,9 +1,10 @@
 'use client'
 import type { FC } from 'react'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
 import StatusWithAction from './status-with-action'
-
+import { useAutoDisabledDocuments, useDocumentEnable, useInvalidDisabledDocument } from '@/service/knowledge/use-document'
+import Toast from '@/app/components/base/toast'
 type Props = {
   datasetId: string
 }
@@ -12,17 +13,25 @@ const AutoDisabledDocument: FC<Props> = ({
   datasetId,
 }) => {
   const { t } = useTranslation()
-  const data = ['', '']
-  const hasDisabledDocument = data.length > 0
-  if (!hasDisabledDocument)
+  const { data, isLoading } = useAutoDisabledDocuments(datasetId)
+  const invalidDisabledDocument = useInvalidDisabledDocument()
+  const documentIds = data?.document_ids
+  const hasDisabledDocument = documentIds && documentIds.length > 0
+  const { mutateAsync: enableDocument } = useDocumentEnable()
+  const handleEnableDocuments = useCallback(async () => {
+    await enableDocument({ datasetId, documentIds })
+    invalidDisabledDocument()
+    Toast.notify({ type: 'success', message: t('common.actionMsg.modifiedSuccessfully') })
+  }, [])
+  if (!hasDisabledDocument || isLoading)
     return null
 
   return (
     <StatusWithAction
       type='info'
-      description={t('dataset.documentsDisabled', { num: data.length })}
+      description={t('dataset.documentsDisabled', { num: documentIds?.length })}
       actionText={t('dataset.enable')}
-      onAction={() => { }}
+      onAction={handleEnableDocuments}
     />
   )
 }
