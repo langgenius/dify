6e154cd46a7be4b5b7a45a2a1b9628cb88cb490a
NFish <douxc512@gmail.com>
2024-12-11 17:34:08 +0800

    fix: compress screen shot; update AppIcon Emoji size

--- web/app/components/base/app-icon/index.tsx
+++ web/app/components/base/app-icon/index.tsx
@@ -22,17 +22,17 @@ export type AppIconProps = {
   onClick?: () => void
 }
 const appIconVariants = cva(
-  'flex items-center justify-center relative text-lg rounded-lg grow-0 shrink-0 overflow-hidden',
+  'flex items-center justify-center relative text-lg rounded-lg grow-0 shrink-0 overflow-hidden leading-none',
   {
     variants: {
       size: {
-        xs: 'w-3 h-3 text-base',
+        xs: 'w-4 h-4 text-xs',
         tiny: 'w-6 h-6 text-base',
-        small: 'w-8 h-8',
-        medium: 'w-9 h-9',
-        large: 'w-10 h-10',
-        xl: 'w-12 h-12',
-        xxl: 'w-14 h-14',
+        small: 'w-8 h-8 text-xl',
+        medium: 'w-9 h-9 text-[22px]',
+        large: 'w-10 h-10 text-[24px]',
+        xl: 'w-12 h-12 text-[28px]',
+        xxl: 'w-14 h-14 text-[32px]',
       },
       rounded: {
         true: 'rounded-full',
