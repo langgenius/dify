8de12ef1d73b874afc54b4c71194af8a03224205
nite-knite <nkCoding@gmail.com>
2024-12-13 14:37:22 +0800

    feat: update dataset creation UI

--- web/app/components/datasets/create/step-three/index.tsx
+++ web/app/components/datasets/create/step-three/index.tsx
@@ -24,29 +24,29 @@ const StepThree = ({ datasetId, datasetName, indexingType, creationCache, retrie
   const isMobile = media === MediaType.mobile
 
   return (
-    <div className='flex w-full max-h-full h-full overflow-y-auto'>
-      <div className={'h-full w-full overflow-y-scroll px-6 sm:px-16'}>
-        <div className='max-w-[636px]'>
+    <div className="flex justify-center w-full max-h-full h-full overflow-y-auto">
+      <div className="grow shrink-0 h-full max-w-[960px] overflow-y-scroll px-14 sm:px-16">
+        <div className="mx-auto max-w-[640px]">
           {!datasetId && (
             <>
-              <div className={s.creationInfo}>
-                <div className={s.title}>{t('datasetCreation.stepThree.creationTitle')}</div>
-                <div className={s.content}>{t('datasetCreation.stepThree.creationContent')}</div>
-                <div className='flex gap-4'>
-                  <AppIcon {...creationCache?.dataset} className='size-14' />
-                  <div className='w-full'>
-                    <div className={s.label}>{t('datasetCreation.stepThree.label')}</div>
-                    <div className={s.datasetName}>{datasetName || creationCache?.dataset?.name}</div>
+              <div className="pt-10">
+                <div className="mb-1 text-xl leading-[22px] font-semibold text-text-primary">{t('datasetCreation.stepThree.creationTitle')}</div>
+                <div className="mb-7 text-[13px] leading-4 text-text-tertiary">{t('datasetCreation.stepThree.creationContent')}</div>
+                <div className="flex gap-4">
+                  <AppIcon {...creationCache?.dataset} className="size-14 text-2xl self-center" />
+                  <div className="grow flex flex-col gap-1">
+                    <div className="text-[13px] leading-6 font-semibold">{t('datasetCreation.stepThree.label')}</div>
+                    <div className="w-full px-3 py-2 text-[13px] leading-4 bg-components-input-bg-normal rounded-lg truncate">{datasetName || creationCache?.dataset?.name}</div>
                   </div>
                 </div>
               </div>
-              <div className={s.dividerLine} />
+              <hr className="my-6 h-[1px] bg-divider-subtle border-0" />
             </>
           )}
           {datasetId && (
-            <div className={s.creationInfo}>
-              <div className={s.title}>{t('datasetCreation.stepThree.additionTitle')}</div>
-              <div className={s.content}>{`${t('datasetCreation.stepThree.additionP1')} ${datasetName || creationCache?.dataset?.name} ${t('datasetCreation.stepThree.additionP2')}`}</div>
+            <div className="pt-10">
+              <div className="mb-1 text-xl leading-[22px] font-semibold text-text-primary">{t('datasetCreation.stepThree.additionTitle')}</div>
+              <div className="mb-7 text-[13px] leading-4 text-text-tertiary">{`${t('datasetCreation.stepThree.additionP1')} ${datasetName || creationCache?.dataset?.name} ${t('datasetCreation.stepThree.additionP2')}`}</div>
             </div>
           )}
           <EmbeddingProcess
