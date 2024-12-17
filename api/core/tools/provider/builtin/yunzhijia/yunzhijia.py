cb0aeb7f8581ad4c3facbec84afb3cf73b12f2bc
LuZhuJun <zhujun_lu@yunzhijia.com>
2024-12-17 09:42:57 +0800

    feat: add a new built-in tool of Yunzhijia Group Robot

---
+++ api/core/tools/provider/builtin/yunzhijia/yunzhijia.py
@@ -0,0 +1,6 @@
+from core.tools.provider.builtin_tool_provider import BuiltinToolProviderController
+
+
+class YunzhijiaProvider(BuiltinToolProviderController):
+    def _validate_credentials(self, credentials: dict) -> None:
+        pass
