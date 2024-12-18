d5c708c62b50c93494efd5eab7a7e9b74bdf75f0
takatost <takatost@gmail.com>
2024-12-19 00:50:46 +0800

    feat: add plugin_model_providers context

--- api/contexts/__init__.py
+++ api/contexts/__init__.py
@@ -4,7 +4,9 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from core.tools.plugin_tool.provider import PluginToolProviderController
+    from core.plugin.entities.plugin_daemon import PluginModelProviderEntity
     from core.workflow.entities.variable_pool import VariablePool
+
 
 tenant_id: ContextVar[str] = ContextVar("tenant_id")
 
@@ -12,3 +14,6 @@ workflow_variable_pool: ContextVar["VariablePool"] = ContextVar("workflow_variab
 
 plugin_tool_providers: ContextVar[dict[str, "PluginToolProviderController"]] = ContextVar("plugin_tool_providers")
 plugin_tool_providers_lock: ContextVar[Lock] = ContextVar("plugin_tool_providers_lock")
+
+plugin_model_providers: ContextVar[list["PluginModelProviderEntity"]] = ContextVar("plugin_model_providers")
+plugin_model_providers_lock: ContextVar[Lock] = ContextVar("plugin_model_providers_lock")
