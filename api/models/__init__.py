a691884979548aa627b9a211007643e0b6ce933b
-LAN- <laipz8200@outlook.com>
2024-12-19 17:49:45 +0800

    chore(db): update tables
    1. Remove unused `tool_providers`
    2. Set `workflow_runs.output` NOT NULL
    
    Signed-off-by: -LAN- <laipz8200@outlook.com>

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,5 +1,7 @@
 from .account import Account, AccountIntegrate, InvitationCode, Tenant
+from .api_based_extension import APIBasedExtension, APIBasedExtensionPoint
 from .dataset import Dataset, DatasetProcessRule, Document, DocumentSegment
+from .enums import CreatedByRole, UserFrom, WorkflowRunTriggeredFrom
 from .model import (
     ApiToken,
     App,
@@ -14,8 +16,19 @@ from .model import (
     Site,
     UploadFile,
 )
-from .source import DataSourceOauthBinding
+from .provider import (
+    LoadBalancingModelConfig,
+    Provider,
+    ProviderModel,
+    ProviderModelSetting,
+    ProviderOrder,
+    TenantDefaultModel,
+    TenantPreferredModelProvider,
+)
+from .source import DataSourceApiKeyAuthBinding, DataSourceOauthBinding
+from .task import CeleryTask, CeleryTaskSet
 from .tools import ToolFile
+from .web import PinnedConversation, SavedMessage
 from .workflow import (
     ConversationVariable,
     Workflow,
@@ -24,13 +37,19 @@ from .workflow import (
 )
 
 __all__ = [
+    "APIBasedExtension",
+    "APIBasedExtensionPoint",
     "Account",
     "AccountIntegrate",
     "ApiToken",
     "App",
     "AppMode",
+    "CeleryTask",
+    "CeleryTaskSet",
     "Conversation",
     "ConversationVariable",
+    "CreatedByRole",
+    "DataSourceApiKeyAuthBinding",
     "DataSourceOauthBinding",
     "Dataset",
     "DatasetProcessRule",
@@ -39,15 +58,26 @@ __all__ = [
     "EndUser",
     "InstalledApp",
     "InvitationCode",
+    "LoadBalancingModelConfig",
     "Message",
     "MessageAnnotation",
     "MessageFile",
+    "PinnedConversation",
+    "Provider",
+    "ProviderModel",
+    "ProviderModelSetting",
+    "ProviderOrder",
     "RecommendedApp",
+    "SavedMessage",
     "Site",
     "Tenant",
+    "TenantDefaultModel",
+    "TenantPreferredModelProvider",
     "ToolFile",
     "UploadFile",
+    "UserFrom",
     "Workflow",
     "WorkflowAppLog",
     "WorkflowRun",
+    "WorkflowRunTriggeredFrom",
 ]
