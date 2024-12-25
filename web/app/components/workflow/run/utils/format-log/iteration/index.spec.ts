880496db0b01a76e6cb84274a7b16159d7dc8147
Joel <iamjoel007@gmail.com>
2024-12-25 14:12:38 +0800

    chore: add more tests

--- web/app/components/workflow/run/utils/format-log/iteration/index.spec.ts
+++ web/app/components/workflow/run/utils/format-log/iteration/index.spec.ts
@@ -2,8 +2,9 @@ import format from '.'
 import { simpleIterationData } from './data'
 
 describe('format api data to tracing panel data', () => {
-  // test('result should have no nodes in iteration node', () => {
-  // }
+  test('result should have no nodes in iteration node', () => {
+    expect(format(simpleIterationData.in as any).find(item => !!(item as any).execution_metadata?.iteration_id)).toBeUndefined()
+  })
   test('iteration should put nodes in details', () => {
     expect(format(simpleIterationData.in as any)).toEqual(simpleIterationData.output)
   })
