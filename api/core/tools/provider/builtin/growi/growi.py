a16aabe9f4b318fe8b61e51a71d09c90f335a35a
Shun Miyazawa <m0zurillex@gmail.com>
2024-12-26 15:55:41 +0000

    fix lint

--- api/core/tools/provider/builtin/growi/growi.py
+++ api/core/tools/provider/builtin/growi/growi.py
@@ -1,7 +1,11 @@
 from typing import Any
 
-from core.tools.provider.builtin_tool_provider import BuiltinToolProviderController
+
+from core.tools.provider.builtin_tool_provider import (
+    BuiltinToolProviderController,
+)
+
 
 class GrowiProvider(BuiltinToolProviderController):
-    def _validate_credentials(self, credentials: dict[str, Any]) -> None:
+    def validate_credentials(self, credentials: dict[str, Any]) -> None:
         return None
