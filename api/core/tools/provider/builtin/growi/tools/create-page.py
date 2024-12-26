c1882b52c54efc5491ffa19a5d95686942533634
Shun Miyazawa <m0zurillex@gmail.com>
2024-12-26 14:28:46 +0000

    Imprv methods

--- api/core/tools/provider/builtin/growi/tools/create-page.py
+++ api/core/tools/provider/builtin/growi/tools/create-page.py
@@ -1,26 +1,42 @@
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Union, Optional
 from core.tools.tool.builtin_tool import BuiltinTool
 from core.tools.entities.tool_entities import ToolInvokeMessage
-
 import requests
 
 
 class CreatePageTool(BuiltinTool):
     def _invoke(self, user_id: str, tool_parameters: Dict[str, Any]) -> Union[ToolInvokeMessage, List[ToolInvokeMessage]]:
-        path = tool_parameters["path"]
-        body = tool_parameters["body"]
+        # Validate credentials
+        growi_url = self.runtime.credentials.get("growi_url")
+        access_token = self.runtime.credentials.get("access_token")
 
-        growi_url = self.runtime.credentials["growi_url"]
-        access_token = self.runtime.credentials["access_token"]
-        
-        endpoint = f"{growi_url}/_api/v3/page"
+        if growi_url is None or access_token is None:
+            return self.create_text_message("Growi URL and Access Token is required.")
+
+        # Prepate request data
+        parent_path = tool_parameters.get("parent_path")
+        path = tool_parameters.get("path")
+        body = tool_parameters.get("body", "")
 
         data = {
-            "access_token": access_token,
-            "path": path,
-            "body": body
+            "access_token": access_token, 
+            "body": body,
         }
 
-        res = requests.post(endpoint, data=data)
-    
-        return self.create_json_message(res.json())
+        if not(path or parent_path):
+            data['parentPath'] = '/'
+        else:
+            if path:
+                data['path'] = path
+            if parent_path:
+                data['parentPath'] = parent_path
+
+        ## Send request
+        try:
+            endpoint = f"{growi_url}/_api/v3/page"
+            res = requests.post(endpoint, data=data)
+            res.raise_for_status()
+            return self.create_json_message(res.json())
+        except requests.RequestException as e:
+            print(f"Failed to create page on GROWI: {e}")
+            return self.create_text_message(str(e))
