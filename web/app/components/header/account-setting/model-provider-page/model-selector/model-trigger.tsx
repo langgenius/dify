be36aedac16705f15ad166bf16aa03675dee29b7
Yi <yxiaoisme@gmail.com>
2024-12-31 15:59:43 +0800

    agent node: fix installed model ui

--- web/app/components/header/account-setting/model-provider-page/model-selector/model-trigger.tsx
+++ web/app/components/header/account-setting/model-provider-page/model-selector/model-trigger.tsx
@@ -48,7 +48,7 @@ const ModelTrigger: FC<ModelTriggerProps> = ({
           modelName={model.model}
         />
       </div>
-      <div className='flex px-1 py-[3px] items-center gap-1 grow'>
+      <div className='flex px-1 py-[3px] items-center gap-1 truncate'>
         <ModelName
           className='grow'
           modelItem={model}
