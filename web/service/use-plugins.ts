a514bde4285cbcc8ecb2e56a3e979df76326a47a
zxhlyh <jasonapring2015@outlook.com>
2025-01-02 15:50:12 +0800

    fix: marketplace page size

--- web/service/use-plugins.ts
+++ web/service/use-plugins.ts
@@ -300,7 +300,7 @@ export const useMutationPluginsFromMarketplace = () => {
         exclude,
         type,
         page = 1,
-        pageSize = 20,
+        pageSize = 40,
       } = pluginsSearchParams
       return postMarketplace<{ data: PluginsFromMarketplaceResponse }>('/plugins/search/basic', {
         body: {
