8d39ec1da527ecd970213198612af6f964fe9e7f
JzoNg <jzongcode@gmail.com>
2025-01-03 15:07:42 +0800

    chart drak mode

--- web/context/app-context.tsx
+++ web/context/app-context.tsx
@@ -127,7 +127,7 @@ export const AppContextProvider: FC<AppContextProviderProps> = ({ children }) =>
       setCurrentWorkspace(currentWorkspaceResponse)
   }, [currentWorkspaceResponse])
 
-  const [theme, setTheme] = useState<Theme>(Theme.light)
+  const [theme, setTheme] = useState<Theme>(Theme.dark)
   const handleSetTheme = useCallback((theme: Theme) => {
     setTheme(theme)
     globalThis.document.documentElement.setAttribute('data-theme', theme)
