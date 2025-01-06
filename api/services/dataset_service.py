2675445a1db0852b71a4e4d2ca3880bda60283fb
jyong <718720800@qq.com>
2025-01-06 15:51:55 +0800

    sandbox doesn't provide auto disable log

--- api/services/dataset_service.py
+++ api/services/dataset_service.py
@@ -434,6 +434,12 @@ class DatasetService:
 
     @staticmethod
     def get_dataset_auto_disable_logs(dataset_id: str) -> dict:
+        features = FeatureService.get_features(current_user.current_tenant_id)
+        if not features.billing.enabled or features.billing.subscription.plan == "sandbox":
+            return {
+                "document_ids": [],
+                "count": 0,
+            }
         # get recent 30 days auto disable logs
         start_date = datetime.datetime.now() - datetime.timedelta(days=30)
         dataset_auto_disable_logs = DatasetAutoDisableLog.query.filter(
