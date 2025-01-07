477a378ed9ee62c4e98b9b320465cd7520e7a147
-LAN- <laipz8200@outlook.com>
2025-01-07 11:47:37 +0800

    refactor: replace GPT2Tokenizer implementation with tiktoken for improved performance
    Signed-off-by: -LAN- <laipz8200@outlook.com>

--- api/core/model_runtime/model_providers/__base/tokenizers/gpt2_tokenzier.py
+++ api/core/model_runtime/model_providers/__base/tokenizers/gpt2_tokenzier.py
@@ -1,13 +1,10 @@
-from concurrent.futures import ProcessPoolExecutor
-from os.path import abspath, dirname, join
 from threading import Lock
-from typing import Any, cast
+from typing import Any
 
-from transformers import GPT2Tokenizer as TransformerGPT2Tokenizer  # type: ignore
+import tiktoken
 
 _tokenizer: Any = None
 _lock = Lock()
-_executor = ProcessPoolExecutor(max_workers=1)
 
 
 class GPT2Tokenizer:
@@ -22,17 +19,23 @@ class GPT2Tokenizer:
 
     @staticmethod
     def get_num_tokens(text: str) -> int:
-        future = _executor.submit(GPT2Tokenizer._get_num_tokens_by_gpt2, text)
-        result = future.result()
-        return cast(int, result)
+        # Because this process needs more cpu resource, we turn this back before we find a better way to handle it.
+        #
+        # future = _executor.submit(GPT2Tokenizer._get_num_tokens_by_gpt2, text)
+        # result = future.result()
+        # return cast(int, result)
+        return GPT2Tokenizer._get_num_tokens_by_gpt2(text)
 
     @staticmethod
     def get_encoder() -> Any:
         global _tokenizer, _lock
         with _lock:
             if _tokenizer is None:
-                base_path = abspath(__file__)
-                gpt2_tokenizer_path = join(dirname(base_path), "gpt2")
-                _tokenizer = TransformerGPT2Tokenizer.from_pretrained(gpt2_tokenizer_path)
+                # Try to use tiktoken to get the tokenizer because it is faster
+                #
+                _tokenizer = tiktoken.get_encoding("gpt2")
+                # base_path = abspath(__file__)
+                # gpt2_tokenizer_path = join(dirname(base_path), "gpt2")
+                # _tokenizer = TransformerGPT2Tokenizer.from_pretrained(gpt2_tokenizer_path)
 
             return _tokenizer
