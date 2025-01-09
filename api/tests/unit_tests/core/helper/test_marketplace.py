e4c4490175ee9f83a75963a0679918b547e9622c
Yeuoly <admin@srmxy.cn>
2025-01-09 17:27:05 +0800

    refactor

--- api/tests/unit_tests/core/helper/test_marketplace.py
+++ api/tests/unit_tests/core/helper/test_marketplace.py
@@ -2,6 +2,6 @@ from core.helper.marketplace import download_plugin_pkg
 
 
 def test_download_plugin_pkg():
-    pkg = download_plugin_pkg("yeuoly/google:0.0.1@4ff79ee644987e5b744d9c5b7a735d459fe66f26b28724326a7834d7e459e708")
+    pkg = download_plugin_pkg("langgenius/bing:0.0.1@e58735424d2104f208c2bd683c5142e0332045b425927067acf432b26f3d970b")
     assert pkg is not None
     assert len(pkg) > 0
