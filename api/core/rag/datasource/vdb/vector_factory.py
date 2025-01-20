42189fb2e4d4f43141f0eb83652ac36cd3ce70db
rayshaw001 <396301947@163.com>
2025-01-20 20:47:48 +0800

    fix typo

--- api/core/rag/datasource/vdb/vector_factory.py
+++ api/core/rag/datasource/vdb/vector_factory.py
@@ -154,8 +154,8 @@ class Vector:
     max_batch_documents = 1000
     def create(self, texts: Optional[list] = None, **kwargs):
         if texts:
-            for i in range(0, len(texts), self.max_documents):
-                batch_documents = texts[i:i + self.max_documents]
+            for i in range(0, len(texts), self.max_batch_documents):
+                batch_documents = texts[i:i + self.max_batch_documents]
                 batch_embeddings = self._embeddings.embed_documents([document.page_content for document in batch_documents])
                 self._vector_processor.create(texts=batch_documents, embeddings=batch_embeddings, **kwargs)
 
