19401934502996e9ea66d15227cd8b2c7f8cf6eb
chenjh3 <chenjh3@wangsu.com>
2025-01-22 17:21:11 +0800

    feat: add giteeAI risk control identification(fix hint)

--- api/core/tools/provider/builtin/gitee_ai/tools/risk-control.py
+++ api/core/tools/provider/builtin/gitee_ai/tools/risk-control.py
@@ -18,7 +18,7 @@ class GiteeAIToolRiskControl(BuiltinTool):
         inputs = [{"type": "text", "text": tool_parameters.get("input-text")}]
         model = tool_parameters.get("model", "Security-semantic-filtering")
         payload = {"model": model, "input": inputs}
-        url = f"https://ai.gitee.com/v1/moderations"
+        url = "https://ai.gitee.com/v1/moderations"
         response = requests.post(url, json=payload, headers=headers)
         if response.status_code != 200:
             return self.create_text_message(f"Got Error Response:{response.text}")
