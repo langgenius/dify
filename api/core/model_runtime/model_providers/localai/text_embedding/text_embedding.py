45bdabea94c479a4d1e74f5c70cd7f54c587c706
Yingchun Lai <laiyingchun@apache.org>
2025-01-25 23:47:27 +0800

    fix: fix a typo of get_customizable_model_schema method name

--- api/core/model_runtime/model_providers/localai/text_embedding/text_embedding.py
+++ api/core/model_runtime/model_providers/localai/text_embedding/text_embedding.py
@@ -115,7 +115,7 @@ class LocalAITextEmbeddingModel(TextEmbeddingModel):
             num_tokens += self._get_num_tokens_by_gpt2(text)
         return num_tokens
 
-    def _get_customizable_model_schema(self, model: str, credentials: dict) -> Optional[AIModelEntity]:
+    def get_customizable_model_schema(self, model: str, credentials: dict) -> Optional[AIModelEntity]:
         """
         Get customizable model schema
 
