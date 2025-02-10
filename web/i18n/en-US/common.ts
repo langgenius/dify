007840b125932a98696e1452ad5f1cee2699f871
NFish <douxc512@gmail.com>
2025-02-10 10:26:20 +0800

    fix: update tip message

--- web/i18n/en-US/common.ts
+++ web/i18n/en-US/common.ts
@@ -290,7 +290,7 @@ const translation = {
       usedUp: 'Trial quota used up. Add own Model Provider.',
       useYourModel: 'Currently using own Model Provider.',
       close: 'Close',
-      trialQuotaTip: 'Your Anthropic trial quota will expire on 2025/03/10.',
+      trialQuotaTip: 'Your Anthropic trial quota will expire on 2025/03/10 and will no longer be available thereafter. Please make use of it in time.',
     },
     anthropic: {
       using: 'The embedding capability is using',
