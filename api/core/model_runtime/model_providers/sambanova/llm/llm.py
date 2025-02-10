b8a406cfe42fc2ea0a450d00af443e78fae25e14
Chiung-Yi <chiungyits@gmail.com>
2025-02-10 00:04:43 -0800

    Skeleton for onboarding SamvaNova AI Cloud to Dify. Onboarded Meta-Llama-3.1-8B-Instruct.  Still working on test

---
+++ api/core/model_runtime/model_providers/sambanova/llm/llm.py
@@ -0,0 +1,32 @@
+from collections.abc import Generator
+from typing import Optional, Union
+
+from core.model_runtime.entities.llm_entities import LLMResult
+from core.model_runtime.entities.message_entities import PromptMessage, PromptMessageTool
+from core.model_runtime.model_providers.openai_api_compatible.llm.llm import OAIAPICompatLargeLanguageModel
+
+
+class SambaNovaLargeLanguageModel(OAIAPICompatLargeLanguageModel):
+    def _invoke(
+        self,
+        model: str,
+        credentials: dict,
+        prompt_messages: list[PromptMessage],
+        model_parameters: dict,
+        tools: Optional[list[PromptMessageTool]] = None,
+        stop: Optional[list[str]] = None,
+        stream: bool = True,
+        user: Optional[str] = None,
+    ) -> Union[LLMResult, Generator]:
+        self._add_custom_parameters(credentials)
+        return super()._invoke(model, credentials, prompt_messages, model_parameters, tools, stop, stream)
+
+    def validate_credentials(self, model: str, credentials: dict) -> None:
+        self._add_custom_parameters(credentials)
+        super().validate_credentials(model, credentials)
+
+    @staticmethod
+    # TODO: SambaNova's LLM models also provide translation mode. It does not fit into the below structure
+    def _add_custom_parameters(credentials: dict) -> None:
+        credentials["mode"] = "chat"
+        credentials["endpoint_url"] = "https://api.sambanova.ai/v1/chat/completions"
