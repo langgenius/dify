c440431a2c2ec4a3a3d3cc475166cc27198b32c6
Li Alan <alan.li@shopee.com>
2025-02-10 10:32:41 +0800

    pdf2img增加参数

--- api/core/tools/provider/builtin/shopee/tools/pdf2img.py
+++ api/core/tools/provider/builtin/shopee/tools/pdf2img.py
@@ -16,6 +16,29 @@ from typing import Any, Dict, List, Union
 logger = logging.getLogger(__name__)
 
 
+def get_average_font_size(page) -> float:
+    font_sizes = []
+    text = page.get_text("dict")
+    for block in text["blocks"]:
+        # 确保块中有行
+        if "lines" in block:
+            for line in block["lines"]:
+                # 遍历行中的 span
+                for span in line["spans"]:
+                    font_sizes.append(span["size"])
+    if len(font_sizes) == 0:
+        return 0
+
+    return sum(font_sizes) / len(font_sizes)
+
+
+def check_for_vector_graphics(page):
+    operators = page.get_drawings()
+    if operators:
+        return True
+    return False
+
+
 class Pdf2ImgTool(BuiltinTool):
     def _invoke(self,
                 user_id: str,
@@ -26,7 +49,8 @@ class Pdf2ImgTool(BuiltinTool):
         """
         logger.info("run shopee pdf2img")
         file_variable = tool_parameters.get("file")
-        max_size = tool_parameters.get("max_size")
+        vector_max_size = tool_parameters.get("vector_max_size")
+        bit_max_size = tool_parameters.get("bit_max_size")
         quality = tool_parameters.get("quality")
         dpi = tool_parameters.get("dpi")
 
@@ -40,13 +64,13 @@ class Pdf2ImgTool(BuiltinTool):
         if not image_binary:
             return self.create_text_message("Image not found, please request user to generate image firstly.")
 
-        res = self.handle(image_binary, max_size, quality, dpi)
+        res = self.handle(image_binary, vector_max_size, bit_max_size, quality, dpi)
         if not res:
             return self.create_text_message("Pdf2Img error, maybe pdf is encrypted")
 
         return self.create_blob_message(blob=res, meta={"mime_type": "image/jpeg"})
 
-    def handle(self, pdf_bytes, max_size, quality, dpi):
+    def handle(self, pdf_bytes, vector_max_size, bit_max_size, quality, dpi=350):
         pdf_stream = io.BytesIO(pdf_bytes)
         doc = fitz.open(stream=pdf_stream, filetype="pdf")
         if 'encryption' in doc.metadata and doc.metadata['encryption'] is not None:
@@ -60,7 +84,14 @@ class Pdf2ImgTool(BuiltinTool):
             pix = page.get_pixmap(matrix=matrix)
             image = Image.frombytes("RGB", (pix.width, pix.height), pix.samples)
             original_width, original_length = image.size
-            ratio = min(max_size / original_width, max_size / original_length)
+            font_size = get_average_font_size(page)
+            # 矢量图
+            if check_for_vector_graphics(page) and font_size > 0:
+                ratio = min(vector_max_size / original_width, vector_max_size / original_length)
+                if font_size < 9.0:
+                    ratio = ratio / (font_size / 9.0)
+            else:
+                ratio = min(bit_max_size / original_width, bit_max_size / original_length)
 
             # 计算新的尺寸
             new_size = (int(original_width * ratio), int(original_length * ratio))
