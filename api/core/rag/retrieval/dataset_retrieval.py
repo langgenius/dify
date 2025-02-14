fbe94b0f0c0d2c08df41355cfc2dcd3a5c40add1
cpwan <chingpui.wan@lpstech.com>
2025-02-14 08:59:34 +0000

    feat: Add page_number attribute to document segments and update related retrieval logic

--- api/core/rag/retrieval/dataset_retrieval.py
+++ api/core/rag/retrieval/dataset_retrieval.py
@@ -265,6 +265,7 @@ class DatasetRetrieval:
                                 source["word_count"] = segment.word_count
                                 source["segment_position"] = segment.position
                                 source["index_node_hash"] = segment.index_node_hash
+                                source["page_number"] = segment.page_number
                             if segment.answer:
                                 source["content"] = f"question:{segment.content} \nanswer:{segment.answer}"
                             else:
