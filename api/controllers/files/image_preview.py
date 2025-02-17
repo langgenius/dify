2c26066f4e9208293dbde5f7b73e03e9617e4df1
kenwoodjw <blackxin55+@gmail.com>
2025-02-17 17:12:51 +0800

    fix #13573
    Signed-off-by: kenwoodjw <blackxin55+@gmail.com>

--- api/controllers/files/image_preview.py
+++ api/controllers/files/image_preview.py
@@ -1,6 +1,7 @@
 from flask import Response, request
 from flask_restful import Resource, reqparse  # type: ignore
 from werkzeug.exceptions import NotFound
+from urllib.parse import quote
 
 import services
 from controllers.files import api
@@ -71,7 +72,8 @@ class FilePreviewApi(Resource):
         if upload_file.size > 0:
             response.headers["Content-Length"] = str(upload_file.size)
         if args["as_attachment"]:
-            response.headers["Content-Disposition"] = f"attachment; filename={upload_file.name}"
+            encoded_filename = quote(upload_file.name)
+            response.headers["Content-Disposition"] = f"attachment; filename*=UTF-8''{encoded_filename}"
 
         return response
 
