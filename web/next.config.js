a90fbac143695c966013c2ed2f68960b24d1ec82
keting lu <luketing1999@gmail.com>
2025-03-07 18:13:19 +0800

    Add next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -14,6 +14,7 @@ const withMDX = require('@next/mdx')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/dify',
   webpack: (config, { dev, isServer }) => {
     config.plugins.push(codeInspectorPlugin({ bundler: 'webpack' }))
     return config
