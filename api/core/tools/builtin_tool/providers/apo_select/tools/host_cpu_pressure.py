ff2c59a88195e77f6102ab60a07f5fdc45d71477
MisluNotFound <108245592+MisluNotFound@users.noreply.github.com>
2025-03-07 13:37:07 +0800

    feat: add some host metrics query tools (#2)
    * feat: add some host metrics query tools
    
    * fix: wrong params

---
+++ api/core/tools/builtin_tool/providers/apo_select/tools/host_cpu_pressure.py
@@ -0,0 +1,46 @@
+import json
+from collections.abc import Generator
+from typing import Any, Optional
+
+import requests
+
+from configs import dify_config
+from core.tools.builtin_tool.tool import BuiltinTool
+from core.tools.entities.tool_entities import ToolInvokeMessage
+from libs.apo_utils import APOUtils
+
+
+class HostCpuPressureTool(BuiltinTool):
+    def _invoke(
+        self,
+        user_id: str,
+        tool_parameters: dict[str, Any],
+        conversation_id: Optional[str] = None,
+        app_id: Optional[str] = None,
+        message_id: Optional[str] = None,
+    ) -> Generator[ToolInvokeMessage, None, None]:
+        node = tool_parameters.get("node")
+        job = tool_parameters.get("job")
+        start_time = tool_parameters.get("startTime")
+        end_time = tool_parameters.get("endTime")
+        params = {
+          'metricName': "宿主机监控指标 - Quick CPU / Mem / Disk - Pressure - CPU",
+          'params': {
+            'node': node,
+            **({'job': job} if job else {})
+          },
+          'startTime': start_time,
+          'endTime': end_time,
+          'step': APOUtils.get_step(start_time, end_time),
+          }
+        resp = requests.post(dify_config.APO_BACKEND_URL + '/api/metric/query', json=params)
+        list = resp.json()['result']
+        list = json.dumps({
+            'type': 'metric',
+            'display': True,
+            'unit': list['unit'],
+            'data': {
+                'timeseries': list['timeseries']
+            }
+        })
+        yield self.create_text_message(list)
