67bd04c50f7d91dd2c883717a93dd634c903ba36
Mars <524574386@qq.com>
2025-03-07 17:56:28 +0800

    fix message sort
     fix message sort

--- api/controllers/service_api/app/message.py
+++ api/controllers/service_api/app/message.py
@@ -70,7 +70,7 @@ class MessageListApi(Resource):
 
         try:
             return MessageService.pagination_by_first_id(
-                app_model, end_user, args["conversation_id"], args["first_id"], args["limit"], "desc"
+                app_model, end_user, args["conversation_id"], args["first_id"], args["limit"]
             )
         except services.errors.conversation.ConversationNotExistsError:
             raise NotFound("Conversation Not Exists.")
