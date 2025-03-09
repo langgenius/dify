681f4ac7a8c8c936718bba06a56bee03bbdbfc81
ytqh <yutianqiuhao@gmail.com>
2025-03-09 11:00:53 +0800

    support muplti appid memory gen

--- api/configs/school/__init__.py
+++ api/configs/school/__init__.py
@@ -9,15 +9,20 @@ class SchoolConfig(BaseSettings):
 
     DEFAULT_APP_ID: str = Field(
         description="Default app id for school-level features.",
-        default="b278ba96-fa8e-48a8-b3e9-debe34468be0",
+        default="",
+    )
+
+    NEED_MEMORY_GENERATION_APP_IDS: str = Field(
+        description="Development app ids for school-level features.",
+        default="",
     )
 
     DEFAULT_TENANT_ID: str = Field(
         description="Default tenant id for school-level features.",
-        default="5cd3029e-7f92-428a-a5c8-14a790c70233",
+        default="",
     )
 
     MEMORY_GENERATION_APP_ID: str = Field(
         description="App id for memory generation.",
-        default="b278ba96-fa8e-48a8-b3e9-debe34468be0",
+        default="",
     )
