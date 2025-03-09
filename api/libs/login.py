c3e327bcab32e3b2effd106963015c12b7dcafda
albcunha <albcunha@users.noreply.github.com>
2025-03-09 17:53:25 -0300

    Update login.py
    If the user wanted to user ADMIN_API_KEY env variable, he would get this error:
    ```
     File "/app/api/libs/login.py", line 80, in decorated_view
        account = Account.query.filter_by(id=ta.account_id).first()
                  ^^^^^^^^^^^^^
    AttributeError: type object 'Account' has no attribute 'query'
    ```
    This is probably due changes is more recent sqlalchemy. The proposed change fix this.

--- api/libs/login.py
+++ api/libs/login.py
@@ -77,7 +77,7 @@ def login_required(func):
                             )
                             if tenant_account_join:
                                 tenant, ta = tenant_account_join
-                                account = Account.query.filter_by(id=ta.account_id).first()
+                                account = db.session.query(Account).filter_by(id=ta.account_id).first()
                                 # Login admin
                                 if account:
                                     account.current_tenant = tenant
