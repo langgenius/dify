2cf0cb471ff74a2a7e9983f0107d676aa8a09920
Wu Tianwei <30284043+WTW0313@users.noreply.github.com>
2025-03-10 15:34:40 +0800

    fix: fix document list overlap and optimize document list fetching (#15377)

--- web/service/datasets.ts
+++ web/service/datasets.ts
@@ -5,7 +5,6 @@ import type {
   CreateDocumentReq,
   DataSet,
   DataSetListResponse,
-  DocumentListResponse,
   ErrorDocsResponse,
   ExternalAPIDeleteResponse,
   ExternalAPIItem,
@@ -120,10 +119,6 @@ export const fetchDefaultProcessRule: Fetcher<ProcessRuleResponse, { url: string
 }
 export const fetchProcessRule: Fetcher<ProcessRuleResponse, { params: { documentId: string } }> = ({ params: { documentId } }) => {
   return get<ProcessRuleResponse>('/datasets/process-rule', { params: { document_id: documentId } })
-}
-
-export const fetchDocuments: Fetcher<DocumentListResponse, { datasetId: string; params: { keyword: string; page: number; limit: number; sort?: SortType } }> = ({ datasetId, params }) => {
-  return get<DocumentListResponse>(`/datasets/${datasetId}/documents`, { params })
 }
 
 export const createFirstDocument: Fetcher<createDocumentResponse, { body: CreateDocumentReq }> = ({ body }) => {
