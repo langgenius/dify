eb9b256ee812b31d2534fda9965ce9c115c330d6
NFish <douxc512@gmail.com>
2025-03-12 09:49:15 +0800

    fix: remove size prop in PlanBadge component because UpgradeBtn size … (#15544)

--- web/app/components/header/account-dropdown/workplace-selector/index.tsx
+++ web/app/components/header/account-dropdown/workplace-selector/index.tsx
@@ -71,7 +71,7 @@ const WorkplaceSelector = () => {
                       <div className='flex py-1 pl-3 pr-2 items-center gap-2 self-stretch hover:bg-state-base-hover rounded-lg' key={workspace.id} onClick={() => handleSwitchWorkspace(workspace.id)}>
                         <div className='flex items-center justify-center w-6 h-6 bg-[#EFF4FF] rounded-md text-xs font-medium text-primary-600'>{workspace.name[0].toLocaleUpperCase()}</div>
                         <div className='line-clamp-1 grow overflow-hidden text-text-secondary text-ellipsis system-md-regular cursor-pointer'>{workspace.name}</div>
-                        <PlanBadge size='s' plan={workspace.plan as Plan} />
+                        <PlanBadge plan={workspace.plan as Plan} />
                       </div>
                     ))
                   }
