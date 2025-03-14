4b055595b3360c1b7887baf191cd1f508d558df9
GareArc <chen4851@purdue.edu>
2025-03-14 04:25:31 -0400

    fix: seperate education fields from billing

--- api/services/feature_service.py
+++ api/services/feature_service.py
@@ -18,6 +18,11 @@ class BillingModel(BaseModel):
     subscription: SubscriptionModel = SubscriptionModel()
 
 
+class EducationModel(BaseModel):
+    enabled: bool = False
+    is_activated: bool = False
+
+
 class LimitationModel(BaseModel):
     size: int = 0
     limit: int = 0
@@ -39,6 +44,7 @@ class LicenseModel(BaseModel):
 
 class FeatureModel(BaseModel):
     billing: BillingModel = BillingModel()
+    education: EducationModel = EducationModel()
     members: LimitationModel = LimitationModel(size=0, limit=1)
     apps: LimitationModel = LimitationModel(size=0, limit=10)
     vector_space: LimitationModel = LimitationModel(size=0, limit=5)
@@ -112,6 +118,7 @@ class FeatureService:
         features.can_replace_logo = dify_config.CAN_REPLACE_LOGO
         features.model_load_balancing_enabled = dify_config.MODEL_LB_ENABLED
         features.dataset_operator_enabled = dify_config.DATASET_OPERATOR_ENABLED
+        features.education.enabled = dify_config.EDUCATION_ENABLED
 
     @classmethod
     def _fulfill_params_from_billing_api(cls, features: FeatureModel, tenant_id: str):
@@ -120,7 +127,7 @@ class FeatureService:
         features.billing.enabled = billing_info["enabled"]
         features.billing.subscription.plan = billing_info["subscription"]["plan"]
         features.billing.subscription.interval = billing_info["subscription"]["interval"]
-        features.billing.subscription.education = billing_info["subscription"].get("education", False)
+        features.education.is_activated = billing_info["subscription"].get("education", False)
 
         if "members" in billing_info:
             features.members.size = billing_info["members"]["size"]
