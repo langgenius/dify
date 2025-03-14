766ee485318bb82d7bcdd3d2e12e2c95837a75c2
NFish <douxc512@gmail.com>
2025-03-15 01:25:04 +0800

    fix: update document title and favicon in client side

--- web/hooks/use-document-title.ts
+++ web/hooks/use-document-title.ts
@@ -5,9 +5,14 @@ import { useGlobalPublicStore } from '@/context/global-public-context'
 export default function useDocumentTitle(title: string) {
   const { systemFeatures } = useGlobalPublicStore()
   useLayoutEffect(() => {
-    if (systemFeatures.branding.enabled)
-      document.title = `${title} - ${systemFeatures.branding.application_title}`
-    else
-      document.title = `${title} - Dify`
+    const prefix = title ? `${title} - ` : ''
+    if (systemFeatures.branding.enabled) {
+      document.title = `${prefix}${systemFeatures.branding.application_title}`
+      const faviconEle = document.querySelector('link[rel*=\'icon\']') as HTMLLinkElement
+      faviconEle.href = systemFeatures.branding.favicon
+    }
+    else {
+      document.title = `${prefix}Dify`
+    }
   }, [systemFeatures, title])
 }
