624fe323348944bb1a17e1031927ea7905dd35e0
ytqh <yutianqiuhao@gmail.com>
2025-03-16 16:50:35 +0800

    fix path

--- api/controllers/admin/__init__.py
+++ api/controllers/admin/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from libs.external_api import ExternalApi
 
-bp = Blueprint("admin_api", __name__, url_prefix="/admin")
+bp = Blueprint("admin_api", __name__, url_prefix="/admin/api")
 api = ExternalApi(bp)
 
 from .auth import login
