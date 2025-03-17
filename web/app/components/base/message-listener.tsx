d7acd1355388c4b21f3fc4952130949f29facaff
keting lu <luketing1999@gmail.com>
2025-03-17 17:36:12 +0800

    chore: reload after get auto login message

--- web/app/components/base/message-listener.tsx
+++ web/app/components/base/message-listener.tsx
@@ -11,7 +11,13 @@ const MessageListener = () => {
         localStorage.setItem('console_token', event.data.data.token)
         localStorage.setItem('refresh_token', event.data.data.refreshToken)
         sessionStorage.setItem('disable_log_out', true)
+        window.location.reload()
         router.replace(event.data.src)
+      }
+      else if(event.data.action === 'clear-token') {
+        event.source.postMessage('got', event.origin)
+        localStorage.removeItem('console_token')
+        localStorage.removeItem('refresh_token')
       }
     }
 
