f1efaabf97ed8beeb8af58dac98d02e25cb23d78
zxhlyh <jasonapring2015@outlook.com>
2025-03-17 13:16:06 +0800

    feat: add education apply

--- web/app/education-apply/components/constants.ts
+++ web/app/education-apply/components/constants.ts
@@ -1,2 +1,2 @@
-export const EDUCATION_VERIFY_URL_SEARCHPARAMS_ACTION = 'getEducationVerifyA'
+export const EDUCATION_VERIFY_URL_SEARCHPARAMS_ACTION = 'getEducationVerify'
 export const EDUCATION_VERIFYING_LOCALSTORAGE_ITEM = 'educationVerifying'
