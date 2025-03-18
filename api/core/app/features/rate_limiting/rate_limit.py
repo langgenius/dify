cd280db4f7f975bd3d4b96839912b287e086356a
crazywoola <100913391+crazywoola@users.noreply.github.com>
2025-03-18 17:58:32 +0800

    Update api/core/app/features/rate_limiting/rate_limit.py

--- api/core/app/features/rate_limiting/rate_limit.py
+++ api/core/app/features/rate_limiting/rate_limit.py
@@ -45,7 +45,6 @@ class RateLimit:
             self.max_active_requests = int(redis_client.get(self.max_active_requests_key).decode("utf-8"))
             redis_client.expire(self.max_active_requests_key, timedelta(days=1))
 
-            with redis_client.pipeline() as pipe:
                 self.max_active_requests = int(redis_client.get(self.max_active_requests_key).decode("utf-8"))
                 redis_client.expire(self.max_active_requests_key, timedelta(days=1))
 
