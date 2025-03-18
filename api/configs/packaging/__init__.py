f829d9cd2f3a96ed8ef255b93558ab9ca3134a95
-LAN- <laipz8200@outlook.com>
2025-03-18 20:01:24 +0800

    chore: bump version to 1.1.0 (#16128)
    Signed-off-by: -LAN- <laipz8200@outlook.com>

--- api/configs/packaging/__init__.py
+++ api/configs/packaging/__init__.py
@@ -9,7 +9,7 @@ class PackagingInfo(BaseSettings):
 
     CURRENT_VERSION: str = Field(
         description="Dify version",
-        default="1.0.1",
+        default="1.1.0",
     )
 
     COMMIT_SHA: str = Field(
