c3c957bb80fc81d762644fd7ae04d25e3a5a4336
Jyong <76649700+JohnJyong@users.noreply.github.com>
2025-03-19 17:13:08 +0800

    change recreate_collection function to create_collection (#16212)

--- api/core/rag/datasource/vdb/qdrant/qdrant_vector.py
+++ api/core/rag/datasource/vdb/qdrant/qdrant_vector.py
@@ -119,7 +119,7 @@ class QdrantVector(BaseVector):
                     max_indexing_threads=0,
                     on_disk=False,
                 )
-                self._client.recreate_collection(
+                self._client.create_collection(
                     collection_name=collection_name,
                     vectors_config=vectors_config,
                     hnsw_config=hnsw_config,
