ae5d2ecf48f2d6274de5acd12ebdedfbf18b5c12
LiuBodong <liubodong2010@126.com>
2025-03-22 20:21:57 +0800

    fix:weight_type missing when create document in dataset (#16503)

--- web/app/components/workflow/nodes/knowledge-retrieval/types.ts
+++ web/app/components/workflow/nodes/knowledge-retrieval/types.ts
@@ -10,6 +10,7 @@ import type {
   DataSet,
   MetadataInDoc,
   RerankingModeEnum,
+  WeightedScoreEnum,
 } from '@/models/datasets'
 
 export type MultipleRetrievalConfig = {
@@ -21,6 +22,7 @@ export type MultipleRetrievalConfig = {
   }
   reranking_mode?: RerankingModeEnum
   weights?: {
+    weight_type: WeightedScoreEnum
     vector_setting: {
       vector_weight: number
       embedding_provider_name: string
