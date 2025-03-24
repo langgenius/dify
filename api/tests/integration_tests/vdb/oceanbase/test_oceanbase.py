cc0e6a4e9741a1d81d35aa472b192f2ccfc43f99
JF.Hsiong <peak1203@126.com>
2025-03-24 19:50:15 +0800

    Feat: update oceanbase test

--- api/tests/integration_tests/vdb/oceanbase/test_oceanbase.py
+++ api/tests/integration_tests/vdb/oceanbase/test_oceanbase.py
@@ -12,9 +12,8 @@ from tests.integration_tests.vdb.test_vector_store import (
     get_example_text,
     setup_mock_redis,
 )
+from core.rag.models.document import Document
 
-
-@pytest.fixture
 def oceanbase_vector():
     return OceanBaseVector(
         "dify_test_collection",
@@ -22,24 +21,43 @@ def oceanbase_vector():
             host="127.0.0.1",
             port="2881",
             user="root@test",
-            database="test",
-            password="test",
+            database="difyai",
+            password="difyai123456",
+            enable_hybrid_search=True
         ),
     )
+
+def get_example_document(doc_id: str) -> Document:
+    doc = Document(
+        page_content=get_example_text(),
+        metadata={
+            "doc_id": doc_id,
+            "doc_hash": doc_id,
+            "document_id": doc_id,
+            "dataset_id": doc_id,
+        },
+    )
+    return doc
 
 
 class OceanBaseVectorTest(AbstractVectorTest):
     def __init__(self, vector: OceanBaseVector):
         super().__init__()
         self.vector = vector
+        
+    def create_vector(self):
+        self.vector.create(
+            texts=[get_example_document(doc_id=self.example_doc_id)],
+            embeddings=[self.example_embedding],
+        )
 
     def search_by_vector(self):
         hits_by_vector = self.vector.search_by_vector(query_vector=self.example_embedding)
-        assert len(hits_by_vector) == 0
+        assert len(hits_by_vector) > 0
 
     def search_by_full_text(self):
         hits_by_full_text = self.vector.search_by_full_text(query=get_example_text())
-        assert len(hits_by_full_text) == 0
+        assert len(hits_by_full_text) > 0
 
     def text_exists(self):
         exist = self.vector.text_exists(self.example_doc_id)
@@ -47,25 +65,8 @@ class OceanBaseVectorTest(AbstractVectorTest):
 
     def get_ids_by_metadata_field(self):
         ids = self.vector.get_ids_by_metadata_field(key="document_id", value=self.example_doc_id)
-        assert len(ids) == 0
+        assert len(ids) > 0
 
 
-@pytest.fixture
-def setup_mock_oceanbase_client():
-    with patch("core.rag.datasource.vdb.oceanbase.oceanbase_vector.ObVecClient", new_callable=MagicMock) as mock_client:
-        yield mock_client
-
-
-@pytest.fixture
-def setup_mock_oceanbase_vector(oceanbase_vector):
-    with patch.object(oceanbase_vector, "_client"):
-        yield oceanbase_vector
-
-
-def test_oceanbase_vector(
-    setup_mock_redis,
-    setup_mock_oceanbase_client,
-    setup_mock_oceanbase_vector,
-    oceanbase_vector,
-):
-    OceanBaseVectorTest(oceanbase_vector).run_all_tests()
+def test_oceanbase_vector(setup_mock_redis):
+    OceanBaseVectorTest(oceanbase_vector()).run_all_tests()
