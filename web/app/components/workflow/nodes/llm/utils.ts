11994eac1ac7bb048105c171d444c530ac1725f5
twwu <twwu@dify.ai>
2025-03-25 16:18:51 +0800

    Revert "feat: implement JSON schema validation and error handling in configuration modal"
    This reverts commit 9cac14b04148ea399297f353a14004f4f95fac3c.

--- web/app/components/workflow/nodes/llm/utils.ts
+++ web/app/components/workflow/nodes/llm/utils.ts
@@ -1,7 +1,8 @@
 import { ArrayType, Type } from './types'
 import type { ArrayItems, Field, LLMNodeType } from './types'
-import Ajv, { type ErrorObject } from 'ajv'
-import produce from 'immer'
+// import Ajv, { type ErrorObject } from 'ajv'
+// import draft7MetaSchema from 'ajv/dist/refs/json-schema-draft-07.json'
+// import produce from 'immer'
 
 export const checkNodeValid = (payload: LLMNodeType) => {
   return true
@@ -82,281 +83,29 @@ export const findPropertyWithPath = (target: any, path: string[]) => {
   return current
 }
 
-const draft7MetaSchema = {
-  $schema: 'http://json-schema.org/draft-07/schema#',
-  $id: 'http://json-schema.org/draft-07/schema#',
-  title: 'Core schema meta-schema',
-  definitions: {
-    schemaArray: {
-      type: 'array',
-      minItems: 1,
-      items: {
-        $ref: '#',
-      },
-    },
-    nonNegativeInteger: {
-      type: 'integer',
-      minimum: 0,
-    },
-    nonNegativeIntegerDefault0: {
-      allOf: [
-        {
-          $ref: '#/definitions/nonNegativeInteger',
-        },
-        {
-          default: 0,
-        },
-      ],
-    },
-    simpleTypes: {
-      enum: [
-        'array',
-        'boolean',
-        'integer',
-        'null',
-        'number',
-        'object',
-        'string',
-      ],
-    },
-    stringArray: {
-      type: 'array',
-      items: {
-        type: 'string',
-      },
-      uniqueItems: true,
-      default: [],
-    },
-  },
-  type: [
-    'object',
-    'boolean',
-  ],
-  properties: {
-    $id: {
-      type: 'string',
-      format: 'uri-reference',
-    },
-    $schema: {
-      type: 'string',
-      format: 'uri',
-    },
-    $ref: {
-      type: 'string',
-      format: 'uri-reference',
-    },
-    $comment: {
-      type: 'string',
-    },
-    title: {
-      type: 'string',
-    },
-    description: {
-      type: 'string',
-    },
-    default: true,
-    readOnly: {
-      type: 'boolean',
-      default: false,
-    },
-    writeOnly: {
-      type: 'boolean',
-      default: false,
-    },
-    examples: {
-      type: 'array',
-      items: true,
-    },
-    multipleOf: {
-      type: 'number',
-      exclusiveMinimum: 0,
-    },
-    maximum: {
-      type: 'number',
-    },
-    exclusiveMaximum: {
-      type: 'number',
-    },
-    minimum: {
-      type: 'number',
-    },
-    exclusiveMinimum: {
-      type: 'number',
-    },
-    maxLength: {
-      $ref: '#/definitions/nonNegativeInteger',
-    },
-    minLength: {
-      $ref: '#/definitions/nonNegativeIntegerDefault0',
-    },
-    pattern: {
-      type: 'string',
-      format: 'regex',
-    },
-    additionalItems: {
-      $ref: '#',
-    },
-    items: {
-      anyOf: [
-        {
-          $ref: '#',
-        },
-        {
-          $ref: '#/definitions/schemaArray',
-        },
-      ],
-      default: true,
-    },
-    maxItems: {
-      $ref: '#/definitions/nonNegativeInteger',
-    },
-    minItems: {
-      $ref: '#/definitions/nonNegativeIntegerDefault0',
-    },
-    uniqueItems: {
-      type: 'boolean',
-      default: false,
-    },
-    contains: {
-      $ref: '#',
-    },
-    maxProperties: {
-      $ref: '#/definitions/nonNegativeInteger',
-    },
-    minProperties: {
-      $ref: '#/definitions/nonNegativeIntegerDefault0',
-    },
-    required: {
-      $ref: '#/definitions/stringArray',
-    },
-    additionalProperties: {
-      $ref: '#',
-    },
-    definitions: {
-      type: 'object',
-      additionalProperties: {
-        $ref: '#',
-      },
-      default: {
+// const ajv = new Ajv({
+//   allErrors: true,
+//   verbose: true,
+//   validateSchema: true,
+//   meta: false,
+// })
+// ajv.addMetaSchema(draft7MetaSchema)
 
-      },
-    },
-    properties: {
-      type: 'object',
-      additionalProperties: {
-        $ref: '#',
-      },
-      default: {
+// export const validateSchemaAgainstDraft7 = (schemaToValidate: any) => {
+//   const schema = produce(schemaToValidate, (draft: any) => {
+//   // Make sure the schema has the $schema property for draft-07
+//     if (!draft.$schema)
+//       draft.$schema = 'http://json-schema.org/draft-07/schema#'
+//   })
 
-      },
-    },
-    patternProperties: {
-      type: 'object',
-      additionalProperties: {
-        $ref: '#',
-      },
-      propertyNames: {
-        format: 'regex',
-      },
-      default: {
+//   const valid = ajv.validateSchema(schema)
 
-      },
-    },
-    dependencies: {
-      type: 'object',
-      additionalProperties: {
-        anyOf: [
-          {
-            $ref: '#',
-          },
-          {
-            $ref: '#/definitions/stringArray',
-          },
-        ],
-      },
-    },
-    propertyNames: {
-      $ref: '#',
-    },
-    const: true,
-    enum: {
-      type: 'array',
-      items: true,
-      minItems: 1,
-      uniqueItems: true,
-    },
-    type: {
-      anyOf: [
-        {
-          $ref: '#/definitions/simpleTypes',
-        },
-        {
-          type: 'array',
-          items: {
-            $ref: '#/definitions/simpleTypes',
-          },
-          minItems: 1,
-          uniqueItems: true,
-        },
-      ],
-    },
-    format: {
-      type: 'string',
-    },
-    contentMediaType: {
-      type: 'string',
-    },
-    contentEncoding: {
-      type: 'string',
-    },
-    if: {
-      $ref: '#',
-    },
-    then: {
-      $ref: '#',
-    },
-    else: {
-      $ref: '#',
-    },
-    allOf: {
-      $ref: '#/definitions/schemaArray',
-    },
-    anyOf: {
-      $ref: '#/definitions/schemaArray',
-    },
-    oneOf: {
-      $ref: '#/definitions/schemaArray',
-    },
-    not: {
-      $ref: '#',
-    },
-  },
-  default: true,
-}
+//   return valid ? [] : ajv.errors || []
+// }
 
-const ajv = new Ajv({
-  allErrors: true,
-  verbose: true,
-  validateSchema: true,
-  meta: false,
-})
-ajv.addMetaSchema(draft7MetaSchema)
-
-export const validateSchemaAgainstDraft7 = (schemaToValidate: any) => {
-  const schema = produce(schemaToValidate, (draft: any) => {
-  // Make sure the schema has the $schema property for draft-07
-    if (!draft.$schema)
-      draft.$schema = 'http://json-schema.org/draft-07/schema#'
-  })
-
-  const valid = ajv.validateSchema(schema)
-
-  return valid ? [] : ajv.errors || []
-}
-
-export const getValidationErrorMessage = (errors: ErrorObject[]) => {
-  const message = errors.map((error) => {
-    return `Error: ${error.instancePath} ${error.message} Details: ${JSON.stringify(error.params)}`
-  }).join('; ')
-  return message
-}
+// export const getValidationErrorMessage = (errors: ErrorObject[]) => {
+//   const message = errors.map((error) => {
+//     return `Error: ${error.instancePath} ${error.message} Details: ${JSON.stringify(error.params)}`
+//   }).join('; ')
+//   return message
+// }
