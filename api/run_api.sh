c6892c2c9d98166864673454c0711bf399d8fbc8
fuwx <f2928560492@163.com>
2025-03-25 11:04:59 +0800

    add gen tools

--- api/run_api.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d --name dify-api --network host --pid host -v /root/workspace/dify/api/.env:/app/api/.env -v /root/workspace/dify/docker/volumes/app/storage:/app/api/storage -e MIGRATION_ENABLED="true" -e DEBUG="true"  -e MODE="api" registry.cn-hangzhou.aliyuncs.com/kindlingx/dify-api:apo-1.0.0-9d88021
