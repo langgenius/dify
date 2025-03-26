3187c887b82e45ddd7fec2da5fa5bc498fd719f0
fuwx <f2928560492@163.com>
2025-03-26 16:38:57 +0800

    add query thread polaris tools

---
+++ api/core/tools/builtin_tool/providers/apo_select/tools/thread_polaris_file_time.py
@@ -0,0 +1,44 @@
+import json
+from collections.abc import Generator
+from typing import Any, Optional
+
+import requests
+
+from configs import dify_config
+from core.tools.builtin_tool.tool import BuiltinTool
+from core.tools.entities.tool_entities import ToolInvokeMessage
+from libs.apo_utils import APOUtils
+
+
+class ThreadPolarisFileTimeTool(BuiltinTool):
+    def _invoke(
+        self,
+        user_id: str,
+        tool_parameters: dict[str, Any],
+        conversation_id: Optional[str] = None,
+        app_id: Optional[str] = None,
+        message_id: Optional[str] = None,
+    ) -> Generator[ToolInvokeMessage, None, None]:
+        pod = tool_parameters.get('pod', '.*')
+        start_time = tool_parameters.get("startTime")
+        end_time = tool_parameters.get("endTime")
+        params = {
+            'metricName': 'Thread Polaris Metrics - 北极星指标（线程） - 各类型耗时折线图 - File',
+            'params': {
+                'pod': pod
+            },
+            'startTime': start_time,
+            'endTime': end_time,
+            'step': APOUtils.get_step(start_time, end_time),
+        }
+        resp = requests.post(dify_config.APO_BACKEND_URL + '/api/metric/query', json=params)
+        list = resp.json()['result']
+        list = json.dumps({
+            'type': 'metric',
+            'display': True,
+            'unit': list['unit'],
+            'data': {
+                'timeseries': list['timeseries']
+            }
+        })
+        yield self.create_text_message(list)
