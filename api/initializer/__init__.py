d8a21a8ac3896763d4032f73922f56089fed642d
fuwx <f2928560492@163.com>
2025-03-28 18:05:13 +0800

    fix init

--- api/initializer/__init__.py
+++ api/initializer/__init__.py
@@ -5,8 +5,12 @@ from . import (
     plugin
 )
 from .decorator import _initializers
+import os
+
 
 def run_initializers(app: Flask):
+    if os.environ.get("MODE") != "api":
+        return
     with app.app_context():
         for func, _ in sorted(_initializers, key=lambda x: x[1]):
             func()
