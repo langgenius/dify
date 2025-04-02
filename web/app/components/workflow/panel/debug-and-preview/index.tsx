98893030bf6e54c456b4e4cdbadc25e0e72c134c
jZonG <jzongcode@gmail.com>
2025-04-02 15:46:53 +0800

    workflow graph header

--- web/app/components/workflow/panel/debug-and-preview/index.tsx
+++ web/app/components/workflow/panel/debug-and-preview/index.tsx
@@ -125,12 +125,12 @@ const DebugAndPreview = () => {
               {expanded && <div className='absolute bottom-[-17px] right-[5px] z-10 h-3 w-3 rotate-45 border-l-[0.5px] border-t-[0.5px] border-components-panel-border-subtle bg-components-panel-on-panel-item-bg'/>}
             </div>
           )}
-          <div className='mx-3 h-3.5 w-[1px] bg-gray-200'></div>
+          <div className='mx-3 h-3.5 w-[1px] bg-divider-regular'></div>
           <div
             className='flex h-6 w-6 cursor-pointer items-center justify-center'
             onClick={handleCancelDebugAndPreviewPanel}
           >
-            <RiCloseLine className='h-4 w-4 text-gray-500' />
+            <RiCloseLine className='h-4 w-4 text-text-tertiary' />
           </div>
         </div>
       </div>
