ad9cbb3f2f337a2923364bda2037f269b1de10e1
TedaLIEz <aliezted@gmail.com>
2025-04-02 20:06:17 +0800

    feat: add basic resource attributes

--- api/extensions/ext_otel.py
+++ api/extensions/ext_otel.py
@@ -1,13 +1,16 @@
 import logging
+import os
 
 from flask_login import current_user
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
     BatchSpanProcessor,
     ConsoleSpanExporter,
 )
+from opentelemetry.semconv.resource import ResourceAttributes
 from opentelemetry.trace import Span, get_tracer_provider, set_tracer_provider
 
 from configs import dify_config
@@ -18,11 +21,17 @@ logger = logging.getLogger(__name__)
 def init_app(app: DifyApp):
     if dify_config.ENABLE_OTEL:
         # Initialize OpenTelemetry
-        set_tracer_provider(TracerProvider())
+        # Follow Semantic Convertions 1.32.0 to define resource attributes
+        resource = Resource(attributes={
+            ResourceAttributes.SERVICE_NAME: dify_config.APPLICATION_NAME,
+            ResourceAttributes.SERVICE_VERSION: f"dify-{dify_config.CURRENT_VERSION}-{dify_config.COMMIT_SHA}",
+            ResourceAttributes.PROCESS_PID: os.getpid(),
+            ResourceAttributes.DEPLOYMENT_ENVIRONMENT: f"{dify_config.DEPLOY_ENV}-{dify_config.EDITION}",
+        })
+        set_tracer_provider(TracerProvider(resource=resource))
         if dify_config.OTEL_EXPORTER_TYPE == "console":
             exporter = ConsoleSpanExporter()
         else:
-            # TODO: deploy with otel collector
             exporter = OTLPSpanExporter(
                 endpoint=dify_config.OTLP_BASE_ENDPOINT + "/v1/traces",
                 headers={"Authorization": f"Bearer {dify_config.OTLP_API_KEY}"},
