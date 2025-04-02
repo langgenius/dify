ad11c86096a10616dd5fcfb7e0a23d8fca40485a
Kindy Lin <kindy.lin@gmail.com>
2025-04-02 10:08:37 +0800

    i18n: use table for checker

--- web/i18n/i18next-config.ts
+++ web/i18n/i18next-config.ts
@@ -36,7 +36,7 @@ const loadLangResources = (lang: string) => ({
 
 type Resource = Record<string, ReturnType<typeof loadLangResources>>
 // Automatically generate the resources object
-export const resources = LanguagesSupported.reduce<Resource>((acc: any, lang: string) => {
+export const resources = LanguagesSupported.reduce<Resource>((acc, lang) => {
   acc[lang] = loadLangResources(lang)
   return acc
 }, {})
