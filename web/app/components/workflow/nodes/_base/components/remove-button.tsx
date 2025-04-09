4e0ec9a1fde468f30d648a38a3dd76c2b4d05717
jZonG <jzongcode@gmail.com>
2025-04-09 16:45:45 +0800

    parameters extractor node dark mode

--- web/app/components/workflow/nodes/_base/components/remove-button.tsx
+++ web/app/components/workflow/nodes/_base/components/remove-button.tsx
@@ -13,8 +13,8 @@ const Remove: FC<Props> = ({
   onClick,
 }) => {
   return (
-    <ActionButton size='l' onClick={onClick}>
-      <RiDeleteBinLine className='h-4 w-4' />
+    <ActionButton size='l' className='group shrink-0 hover:!bg-state-destructive-hover' onClick={onClick}>
+      <RiDeleteBinLine className='h-4 w-4 text-text-tertiary group-hover:text-text-destructive' />
     </ActionButton>
   )
 }
