94923c0c89f6fcb5fb47918c67b1d57b827f153d
twwu <twwu@dify.ai>
2025-04-11 14:36:01 +0800

    test: add unit tests for file-uploader utils and update jest configuration

--- web/jest.config.ts
+++ web/jest.config.ts
@@ -147,7 +147,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
