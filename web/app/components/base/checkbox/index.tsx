d841581679d1f7cef8855b6068dfb629a107e10a
twwu <twwu@dify.ai>
2025-04-15 17:30:18 +0800

    feat: add IndeterminateIcon component and update Checkbox to support indeterminate state refactor: remove mixed state handling and update related styles fix: update useCallback dependencies for better performance

--- web/app/components/base/checkbox/index.tsx
+++ web/app/components/base/checkbox/index.tsx
@@ -1,23 +1,25 @@
 import { RiCheckLine } from '@remixicon/react'
-import s from './index.module.css'
 import cn from '@/utils/classnames'
+import IndeterminateIcon from './assets/indeterminate-icon'
 
 type CheckboxProps = {
+  id?: string
   checked?: boolean
   onCheck?: () => void
   className?: string
   disabled?: boolean
-  mixed?: boolean
+  indeterminate?: boolean
 }
 
-const Checkbox = ({ checked, onCheck, className, disabled, mixed }: CheckboxProps) => {
+const Checkbox = ({ id, checked, onCheck, className, disabled, indeterminate }: CheckboxProps) => {
   if (!checked) {
     return (
       <div
+        id={id}
         className={cn(
-          'h-4 w-4 cursor-pointer rounded-[4px] border border-components-checkbox-border bg-components-checkbox-bg-unchecked shadow-xs hover:border-components-checkbox-border-hover',
-          mixed ? s.mixed : 'hover:bg-components-checkbox-bg-unchecked-hover',
-          disabled && 'cursor-not-allowed border-components-checkbox-border-disabled bg-components-checkbox-bg-disabled hover:border-components-checkbox-border-disabled hover:bg-components-checkbox-bg-disabled',
+          'flex h-4 w-4 cursor-pointer items-center justify-center rounded-[4px] border border-components-checkbox-border bg-components-checkbox-bg-unchecked shadow-xs',
+          indeterminate ? 'border-none bg-components-checkbox-bg text-components-checkbox-icon' : 'hover:bg-components-checkbox-bg-unchecked-hover',
+          disabled && 'cursor-not-allowed border-components-checkbox-border-disabled bg-components-checkbox-bg-disabled text-components-checkbox-icon-disabled hover:border-components-checkbox-border-disabled hover:bg-components-checkbox-bg-disabled',
           className,
         )}
         onClick={() => {
@@ -25,11 +27,16 @@ const Checkbox = ({ checked, onCheck, className, disabled, mixed }: CheckboxProp
             return
           onCheck?.()
         }}
-      ></div>
+      >
+        {indeterminate && (
+          <IndeterminateIcon />
+        )}
+      </div>
     )
   }
   return (
     <div
+      id={id}
       className={cn(
         'flex h-4 w-4 cursor-pointer items-center justify-center rounded-[4px] bg-components-checkbox-bg text-components-checkbox-icon shadow-xs hover:bg-components-checkbox-bg-hover',
         disabled && 'cursor-not-allowed bg-components-checkbox-bg-disabled-checked text-components-checkbox-icon-disabled hover:bg-components-checkbox-bg-disabled-checked',
@@ -42,7 +49,7 @@ const Checkbox = ({ checked, onCheck, className, disabled, mixed }: CheckboxProp
         onCheck?.()
       }}
     >
-      <RiCheckLine className={cn('h-3 w-3')} />
+      <RiCheckLine className='h-3 w-3' />
     </div>
   )
 }
