3e74dcba2c7bfd8687a5aef5a4dcbc8767be7b14
AlexChimTrm <132866042+AlexChim1231@users.noreply.github.com>
2025-04-23 16:51:39 +0800

    Update conversation.py

--- api/controllers/service_api/app/conversation.py
+++ api/controllers/service_api/app/conversation.py
@@ -1,6 +1,5 @@
 from flask_restful import Resource, marshal_with, reqparse  # type: ignore
 from flask_restful.inputs import int_range  # type: ignore
-from sqlalchemy import select
 from sqlalchemy.orm import Session
 from werkzeug.exceptions import NotFound
 
