c07fdd669f006617a632a115102384a65ba6dcf9
lizb <lizb@sugon.com>
2025-04-24 11:42:23 +0800

    fix agentflow error if first variable is num

--- api/core/app/apps/message_based_app_generator.py
+++ api/core/app/apps/message_based_app_generator.py
@@ -153,6 +153,8 @@ class MessageBasedAppGenerator(BaseAppGenerator):
             query = application_generate_entity.query or "New conversation"
         else:
             query = next(iter(application_generate_entity.inputs.values()), "New conversation")
+            if isinstance(query, int):
+                query = str(query)
         query = query or "New conversation"
         conversation_name = (query[:20] + "…") if len(query) > 20 else query
 
