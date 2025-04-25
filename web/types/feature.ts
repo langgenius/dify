170139bb0faec97e2828e231d5f1c627bcf37a4e
NFish <douxc512@gmail.com>
2025-04-25 16:20:20 +0800

    fix: update sso protocol default value to '' (#18773)

--- web/types/feature.ts
+++ web/types/feature.ts
@@ -39,7 +39,7 @@ export type SystemFeatures = {
     enabled: boolean
     allow_sso: boolean
     sso_config: {
-      protocol: SSOProtocol
+      protocol: SSOProtocol | ''
     }
     allow_email_code_login: boolean
     allow_email_password_login: boolean
@@ -70,7 +70,7 @@ export const defaultSystemFeatures: SystemFeatures = {
     enabled: false,
     allow_sso: false,
     sso_config: {
-      protocol: SSOProtocol.SAML,
+      protocol: '',
     },
     allow_email_code_login: false,
     allow_email_password_login: false,
