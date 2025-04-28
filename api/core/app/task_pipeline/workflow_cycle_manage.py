3141bd1781ef9e444609200b5f4610d2c5ad9222
kevin.zhang <51311316+Kevin9703@users.noreply.github.com>
2025-04-28 17:22:45 +0800

    chore: ruff

--- api/core/app/task_pipeline/workflow_cycle_manage.py
+++ api/core/app/task_pipeline/workflow_cycle_manage.py
@@ -380,7 +380,7 @@ class WorkflowCycleManage:
         workflow_node_execution.finished_at = finished_at
         workflow_node_execution.elapsed_time = elapsed_time
         workflow_node_execution.execution_metadata = execution_metadata
-        
+
         self._workflow_node_execution_repository.update(workflow_node_execution)
 
         return workflow_node_execution
