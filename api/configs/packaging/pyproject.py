5c1826ccd126e5114b1ada585b4d1310e4d8969e
Bowen Liang <liangbowen@gf.com.cn>
2025-04-30 21:01:13 +0800

    nit

--- api/configs/packaging/pyproject.py
+++ api/configs/packaging/pyproject.py
@@ -14,14 +14,3 @@ class PyProjectTomlConfig(BaseSettings):
     """
     # [project] section
     project: PyProjectConfig
-
-    @classmethod
-    def settings_customise_sources(
-        cls,
-        settings_cls: type[BaseSettings],
-        init_settings: PydanticBaseSettingsSource,
-        env_settings: PydanticBaseSettingsSource,
-        dotenv_settings: PydanticBaseSettingsSource,
-        file_secret_settings: PydanticBaseSettingsSource,
-    ) -> tuple[PydanticBaseSettingsSource, ...]:
-        return (TomlConfigSettingsSource(settings_cls, toml_file="pyproject.toml"),)
