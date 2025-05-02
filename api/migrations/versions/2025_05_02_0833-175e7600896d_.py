fc1a45c41e68f382c82283d901433b39579cc444
ytqh <yutianqiuhao@gmail.com>
2025-05-02 17:28:54 +0800

    feat: add initial migration script for revision 175e7600896d

---
+++ api/migrations/versions/2025_05_02_0833-175e7600896d_.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: 175e7600896d
+Revises: 08ec4f75af5e, ad332d76d058
+Create Date: 2025-05-02 08:33:08.990003
+
+"""
+from alembic import op
+import models as models
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '175e7600896d'
+down_revision = ('08ec4f75af5e', 'ad332d76d058')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
