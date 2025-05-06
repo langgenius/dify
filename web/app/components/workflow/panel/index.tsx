30f580c42e394f51a4ff0eb2788874712e056c4e
JzoNg <jzongcode@gmail.com>
2025-05-06 15:04:36 +0800

    reset z-index

--- web/app/components/workflow/panel/index.tsx
+++ web/app/components/workflow/panel/index.tsx
@@ -62,7 +62,7 @@ const Panel: FC<PanelProps> = ({
     <div
       ref={rightPanelRef}
       tabIndex={-1}
-      className={cn('absolute bottom-1 right-0 top-14 z-[11] flex outline-none')}
+      className={cn('absolute bottom-1 right-0 top-14 z-10 flex outline-none')}
       key={`${isRestoring}`}
     >
       {
