11299a8e8faf8eb6c8d948a2b0d1a57de27c3026
Joel <iamjoel007@gmail.com>
2025-05-06 14:57:41 +0800

    chore: start sys params

--- web/app/components/workflow/nodes/start/use-single-run-form-params.ts
+++ web/app/components/workflow/nodes/start/use-single-run-form-params.ts
@@ -1,8 +1,9 @@
 import type { MutableRefObject } from 'react'
 import { useTranslation } from 'react-i18next'
 import type { Props as FormProps } from '@/app/components/workflow/nodes/_base/components/before-run-form/form'
-import type { InputVar, Variable } from '@/app/components/workflow/types'
+import { type InputVar, InputVarType, type Variable } from '@/app/components/workflow/types'
 import type { StartNodeType } from './types'
+import { useIsChatMode } from '../../hooks'
 
 type Params = {
   id: string,
@@ -19,18 +20,36 @@ const useSingleRunFormParams = ({
   setRunInputData,
 }: Params) => {
   const { t } = useTranslation()
+  const isChatMode = useIsChatMode()
 
   const forms = (() => {
     const forms: FormProps[] = []
+    const inputs = payload.variables.map((item: InputVar) => ({
+      label: item.variable,
+      variable: item.variable,
+      type: item.type,
+      required: item.required,
+    }))
+
+    if (isChatMode) {
+      inputs.push({
+        label: 'sys.query',
+        variable: '#sys.query#',
+        type: InputVarType.textInput,
+        required: true,
+      })
+    }
+
+    inputs.push({
+      label: 'sys.files',
+      variable: '#sys.files#',
+      type: InputVarType.multiFiles,
+      required: false,
+    })
     forms.push(
       {
         label: t('workflow.nodes.llm.singleRun.variable')!,
-        inputs: payload.variables.map((item: InputVar) => ({
-          label: item.variable,
-          variable: item.variable,
-          type: item.type,
-          required: item.required,
-        })),
+        inputs,
         values: runInputData,
         onChange: setRunInputData,
       },
