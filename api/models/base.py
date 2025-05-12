35a3a285574528674f645af09044fcf3182a6622
IthacaDream <vvfriday@gmail.com>
2025-05-12 12:32:11 +0800

    specifying the query_cls when calling query_property

--- api/models/base.py
+++ api/models/base.py
@@ -1,6 +1,7 @@
+from flask_sqlalchemy.query import Query
 from sqlalchemy.orm import declarative_base
 
 from models.engine import db, metadata
 
 Base = declarative_base(metadata=metadata)
-Base.query = db.session.query_property()
+Base.query = db.session.query_property(query_cls=Query)
