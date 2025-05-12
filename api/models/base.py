c7aff739edc02c7ffd6fd307f297bc725b0fc268
IthacaDream <vvfriday@gmail.com>
2025-05-12 11:15:15 +0800

    temporary compatibility with old query syntax

--- api/models/base.py
+++ api/models/base.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import declarative_base
 
-from models.engine import metadata
+from models.engine import db, metadata
 
 Base = declarative_base(metadata=metadata)
+Base.query = db.session.query_property()
