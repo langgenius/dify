dc75a1098921b4daaca3a23c520d5e29bfe772e5
Nite Knite <nkCoding@gmail.com>
2025-05-15 12:38:20 +0800

    feat: update branding (#19719)
    Co-authored-by: twwu <twwu@dify.ai>

--- web/app/account/header.tsx
+++ web/app/account/header.tsx
@@ -4,23 +4,25 @@ import { RiArrowRightUpLine, RiRobot2Line } from '@remixicon/react'
 import { useRouter } from 'next/navigation'
 import Button from '../components/base/button'
 import Avatar from './avatar'
-import LogoSite from '@/app/components/base/logo/logo-site'
+import DifyLogo from '@/app/components/base/logo/dify-logo'
+import { useCallback } from 'react'
 
 const Header = () => {
   const { t } = useTranslation()
   const router = useRouter()
 
-  const back = () => {
+  const back = useCallback(() => {
     router.back()
-  }
+  }, [router])
+
   return (
     <div className='flex flex-1 items-center justify-between px-4'>
       <div className='flex items-center gap-3'>
         <div className='flex cursor-pointer items-center' onClick={back}>
-          <LogoSite className='object-contain' />
+          <DifyLogo />
         </div>
-        <div className='h-4 w-[1px] bg-divider-regular' />
-        <p className='title-3xl-semi-bold text-text-primary'>{t('common.account.account')}</p>
+        <div className='h-4 w-[1px] origin-center rotate-[11.31deg] bg-divider-regular' />
+        <p className='title-3xl-semi-bold relative mt-[-2px] text-text-primary'>{t('common.account.account')}</p>
       </div>
       <div className='flex shrink-0 items-center gap-3'>
         <Button className='system-sm-medium gap-2 px-3 py-2' onClick={back}>
