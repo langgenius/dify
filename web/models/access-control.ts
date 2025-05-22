cd7f61a0981b201149f81bf657542b0b221ad719
NFish <douxc512@gmail.com>
2025-05-22 16:39:37 +0800

    fix: update web app access control mode enum

--- web/models/access-control.ts
+++ web/models/access-control.ts
@@ -7,7 +7,7 @@ export enum AccessMode {
   PUBLIC = 'public',
   SPECIFIC_GROUPS_MEMBERS = 'private',
   ORGANIZATION = 'private_all',
-  EXTERNAL_MEMBERS = 'verified_external',
+  EXTERNAL_MEMBERS = 'sso_verified',
 }
 
 export type AccessControlGroup = {
