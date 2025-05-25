fce629c201b002eb77e2bcafd4a22de6dd393e3c
ytqh <yutianqiuhao@gmail.com>
2025-05-25 17:42:58 +0800

    update nginx config

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -19,8 +19,6 @@ if [ "${NGINX_HTTPS_ENABLED}" = "true" ]; then
     # set the HTTPS_CONFIG environment variable to the content of the https.conf.template
     HTTPS_CONFIG=$(envsubst < /etc/nginx/https.conf.template)
     export HTTPS_CONFIG
-    # Substitute the HTTPS_CONFIG in the default.conf.template with content from https.conf.template
-    envsubst '${HTTPS_CONFIG}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 fi
 export HTTPS_CONFIG
 
@@ -36,7 +34,13 @@ env_vars=$(printenv | cut -d= -f1 | sed 's/^/$/g' | paste -sd, -)
 envsubst "$env_vars" < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 envsubst "$env_vars" < /etc/nginx/proxy.conf.template > /etc/nginx/proxy.conf
 
-envsubst "$env_vars" < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+# Process all template files in conf.d directory
+for template in /etc/nginx/conf.d/*.template; do
+    if [ -f "$template" ]; then
+        output_file="${template%.template}"
+        envsubst "$env_vars" < "$template" > "$output_file"
+    fi
+done
 
 # Start Nginx using the default entrypoint
 exec nginx -g 'daemon off;'
