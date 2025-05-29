71ce505631cd5dfa17202b739c09e94ebb99adde
zxhlyh <jasonapring2015@outlook.com>
2025-05-29 11:03:22 +0800

    data source panel

--- web/app/components/workflow/nodes/data-source/panel.tsx
+++ web/app/components/workflow/nodes/data-source/panel.tsx
@@ -1,79 +1,29 @@
 import type { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { memo } from 'react'
-import { RiAddLine } from '@remixicon/react'
 import type { DataSourceNodeType } from './types'
 import type { NodePanelProps } from '@/app/components/workflow/types'
 import { BoxGroupField } from '@/app/components/workflow/nodes/_base/components/layout'
 import OutputVars, { VarItem } from '@/app/components/workflow/nodes/_base/components/output-vars'
 import TagInput from '@/app/components/base/tag-input'
-import FieldList from '@/app/components/rag-pipeline/components/input-field/field-list/field-list-container'
-import { useFieldList } from '@/app/components/rag-pipeline/components/input-field/field-list/hooks'
-import InputFieldEditor from '@/app/components/rag-pipeline/components/input-field/editor'
 import { useNodesReadOnly } from '@/app/components/workflow/hooks'
 import { useConfig } from './hooks/use-config'
 import { OUTPUT_VARIABLES_MAP } from './constants'
-import ActionButton from '@/app/components/base/action-button'
 
 const Panel: FC<NodePanelProps<DataSourceNodeType>> = ({ id, data }) => {
   const { t } = useTranslation()
   const { nodesReadOnly } = useNodesReadOnly()
   const {
-    variables,
     provider_type,
     fileExtensions = [],
   } = data
   const {
-    handleInputFieldVariablesChange,
     handleFileExtensionsChange,
   } = useConfig(id)
   const isLocalFile = provider_type === 'local_file'
-  const {
-    inputFields,
-    handleListSortChange,
-    handleRemoveField,
-    handleOpenInputFieldEditor,
-    showInputFieldEditor,
-    editingField,
-    handleSubmitField,
-    handleCancelInputFieldEditor,
-  } = useFieldList(variables, handleInputFieldVariablesChange, id)
 
   return (
     <div >
-      {
-        !isLocalFile && (
-          <BoxGroupField
-            boxGroupProps={{
-              boxProps: { withBorderBottom: true },
-            }}
-            fieldProps={{
-              fieldTitleProps: {
-                title: t('workflow.nodes.common.inputVars'),
-                operation: (
-                  <ActionButton
-                    onClick={(e) => {
-                      e.stopPropagation()
-                      handleOpenInputFieldEditor()
-                    }}
-                  >
-                    <RiAddLine className='h-4 w-4' />
-                  </ActionButton>
-                ),
-              },
-              supportCollapse: true,
-            }}
-          >
-            <FieldList
-              inputFields={inputFields}
-              readonly={nodesReadOnly}
-              onListSortChange={handleListSortChange}
-              onRemoveField={handleRemoveField}
-              onEditField={handleOpenInputFieldEditor}
-            />
-          </BoxGroupField>
-        )
-      }
       {
         isLocalFile && (
           <BoxGroupField
@@ -92,6 +42,8 @@ const Panel: FC<NodePanelProps<DataSourceNodeType>> = ({ id, data }) => {
                 onChange={handleFileExtensionsChange}
                 placeholder={t('workflow.nodes.dataSource.supportedFileFormatsPlaceholder')}
                 inputClassName='bg-transparent'
+                disableAdd={nodesReadOnly}
+                disableRemove={nodesReadOnly}
               />
             </div>
           </BoxGroupField>
@@ -118,14 +70,6 @@ const Panel: FC<NodePanelProps<DataSourceNodeType>> = ({ id, data }) => {
           )
         }
       </OutputVars>
-      {showInputFieldEditor && (
-        <InputFieldEditor
-          show={showInputFieldEditor}
-          initialData={editingField}
-          onSubmit={handleSubmitField}
-          onClose={handleCancelInputFieldEditor}
-        />
-      )}
     </div>
   )
 }
