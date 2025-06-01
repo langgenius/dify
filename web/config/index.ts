35450d6cf9588f5ab9cf200751497a81d49195e7
songyuan <yuan.song@aminer.cn>
2025-06-01 12:03:20 +0800

    upd

--- web/config/index.ts
+++ web/config/index.ts
@@ -24,9 +24,9 @@ else {
   // const domainParts = globalThis.location?.host?.split('.');
   // in production env, the host is dify.app . In other env, the host is [dev].dify.app
   // const env = domainParts.length === 2 ? 'ai' : domainParts?.[0];
-  apiPrefix = 'http://localhost:5001/console/api'
-  publicApiPrefix = 'http://localhost:5001/api' // avoid browser private mode api cross origin
-  marketplaceApiPrefix = 'http://localhost:5002/api'
+  apiPrefix = 'http://127.0.0.1:5001/console/api'
+  publicApiPrefix = 'http://127.0.0.1:5001/api' // avoid browser private mode api cross origin
+  marketplaceApiPrefix = 'http://127.0.0.1:5002/api'
 }
 
 if (process.env.NEXT_PUBLIC_MARKETPLACE_API_PREFIX && process.env.NEXT_PUBLIC_MARKETPLACE_URL_PREFIX) {
