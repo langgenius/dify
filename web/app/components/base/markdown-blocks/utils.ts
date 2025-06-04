341645c5ac95a118129843f58ae3bac23e3fa2a5
Joel <iamjoel007@gmail.com>
2025-06-04 15:51:54 +0800

    chore: handle link url

--- web/app/components/base/markdown-blocks/utils.ts
+++ web/app/components/base/markdown-blocks/utils.ts
@@ -1,9 +1,3 @@
 export const isValidUrl = (url: string): boolean => {
-  try {
-    const parsed_url = new URL(url)
-    return ['http:', 'https:'].includes(parsed_url.protocol)
-  }
-  catch {
-    return false
-  }
+  return ['http:', 'https:', '//', 'mailto:'].some(prefix => url.startsWith(prefix))
 }
