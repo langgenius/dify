4271602cfca984aa999f505cf2e30d43c61ad280
jefferyvvv <33647240+jefferyvvv@users.noreply.github.com>
2025-06-06 09:10:01 +0800

    fix: opensearch metadata filtering returns empty (#20701)
    Co-authored-by: wenjun.gu <wenjun.gu@envision-energy.com>
    Co-authored-by: crazywoola <427733928@qq.com>

--- api/core/rag/datasource/vdb/opensearch/opensearch_vector.py
+++ api/core/rag/datasource/vdb/opensearch/opensearch_vector.py
@@ -255,7 +255,8 @@ class OpenSearchVector(BaseVector):
                             Field.METADATA_KEY.value: {
                                 "type": "object",
                                 "properties": {
-                                    "doc_id": {"type": "keyword"}  # Map doc_id to keyword type
+                                    "doc_id": {"type": "keyword"},  # Map doc_id to keyword type
+                                    "document_id": {"type": "keyword"},
                                 },
                             },
                         }
