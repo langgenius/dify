c7d40268004caccecbaa32fa96ea84daf48d3e9d
NFish <douxc512@gmail.com>
2025-06-06 15:53:40 +0800

    fix: remove all app token when logout

--- web/app/components/share/utils.ts
+++ web/app/components/share/utils.ts
@@ -37,20 +37,6 @@ export const setAccessToken = async (sharedToken: string, token: string) => {
 }
 
 export const removeAccessToken = () => {
-  const sharedToken = globalThis.location.pathname.split('/').slice(-1)[0]
-
-  const accessToken = localStorage.getItem('token') || JSON.stringify({ [sharedToken]: '' })
-  let accessTokenJson = { [sharedToken]: '' }
-  try {
-    accessTokenJson = JSON.parse(accessToken)
-  }
-  catch (e) {
-
-  }
-
-  localStorage.removeItem(CONVERSATION_ID_INFO)
+  localStorage.removeItem('token')
   localStorage.removeItem('webapp_access_token')
-
-  delete accessTokenJson[sharedToken]
-  localStorage.setItem('token', JSON.stringify(accessTokenJson))
 }
