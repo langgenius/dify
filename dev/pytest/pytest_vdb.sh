bf8ac526c05a8ab4b2c5f707d3b0993a39d560c1
Bowen Liang <liangbowen@gf.com.cn>
2025-06-12 17:13:14 +0800

    update

--- dev/pytest/pytest_vdb.sh
+++ dev/pytest/pytest_vdb.sh
@@ -15,7 +15,6 @@ pytest api/tests/integration_tests/vdb/chroma \
   api/tests/integration_tests/vdb/baidu \
   api/tests/integration_tests/vdb/tcvectordb \
   api/tests/integration_tests/vdb/upstash \
-  api/tests/integration_tests/vdb/couchbase \
   api/tests/integration_tests/vdb/oceanbase \
   api/tests/integration_tests/vdb/tidb_vector \
   api/tests/integration_tests/vdb/huawei \
