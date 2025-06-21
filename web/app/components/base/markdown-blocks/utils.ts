8ecbc0ffb162bc3f5a2dcb7af5f92890c6792fac
kurokobo <2920259+kurokobo@users.noreply.github.com>
2025-06-21 16:03:17 +0000

    fix: correct lint errors

--- web/app/components/base/markdown-blocks/utils.ts
+++ web/app/components/base/markdown-blocks/utils.ts
@@ -2,8 +2,6 @@ import { ALLOW_UNSAFE_DATA_SCHEME } from '@/config'
 
 export const isValidUrl = (url: string): boolean => {
   const validPrefixes = ['http:', 'https:', '//', 'mailto:']
-  if (ALLOW_UNSAFE_DATA_SCHEME) {
-    validPrefixes.push('data:')
-  }
+  if (ALLOW_UNSAFE_DATA_SCHEME) validPrefixes.push('data:')
   return validPrefixes.some(prefix => url.startsWith(prefix))
 }
