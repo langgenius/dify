f44015c512af1ec6e603d83cb08498133f119479
Bowen Liang <liangbowen@gf.com.cn>
2025-06-24 22:26:54 +0800

    change os.path to pathlib.Path

--- api/configs/app_config.py
+++ api/configs/app_config.py
@@ -1,5 +1,4 @@
 import logging
-import os
 from pathlib import Path
 from typing import Any
 
--- api/libs/file_utils.py
+++ api/libs/file_utils.py
@@ -1,6 +1,4 @@
-import os
 from pathlib import Path
-from typing import Optional
 
 
 def search_file_upwards(
