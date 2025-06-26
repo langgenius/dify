976a72c65d2d5375d37f7cccd9578351a30d948b
Joel <iamjoel007@gmail.com>
2025-06-26 15:31:50 +0800

    feat: can select plugins

--- web/app/components/plugins/reference-setting-modal/auto-update-setting/config.ts
+++ web/app/components/plugins/reference-setting-modal/auto-update-setting/config.ts
@@ -4,6 +4,6 @@ export const defaultValue: AutoUpdateConfig = {
   strategy_setting: AUTO_UPDATE_STRATEGY.fixOnly, // For test
   upgrade_time_of_day: 0,
   upgrade_mode: AUTO_UPDATE_MODE.exclude, // For test
-  exclude_plugins: ['a', 'c'],
-  include_plugins: ['b'],
+  exclude_plugins: ['langgenius/openai_api_compatible', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai', 'langgenius/openai'],
+  include_plugins: ['langgenius/openai'],
 }
--- web/app/components/plugins/reference-setting-modal/auto-update-setting/plugins-selected.tsx
+++ web/app/components/plugins/reference-setting-modal/auto-update-setting/plugins-selected.tsx
@@ -2,6 +2,8 @@
 import type { FC } from 'react'
 import React from 'react'
 import cn from '@/utils/classnames'
+import { MARKETPLACE_API_PREFIX } from '@/config'
+import Icon from '@/app/components/plugins/card/base/card-icon'
 
 const MAX_DISPLAY_COUNT = 14
 type Props = {
@@ -14,17 +16,13 @@ const PluginsSelected: FC<Props> = ({
   plugins,
 }) => {
   const isShowAll = plugins.length < MAX_DISPLAY_COUNT
-  const displayPlugins = isShowAll ? plugins.slice(0, MAX_DISPLAY_COUNT) : plugins
+  const displayPlugins = plugins.slice(0, MAX_DISPLAY_COUNT)
   return (
-    <div className={cn('flex space-x-1', className)}>
-      {displayPlugins.map((plugin, index) => (
-        <div
-          key={index}
-          className='size-6 rounded-lg border-[0.5px] border-[components-panel-border-subtle] bg-background-default-dodge text-center text-xs leading-6 text-text-secondary'
-        >
-        </div>
+    <div className={cn('flex items-center space-x-1', className)}>
+      {displayPlugins.map(plugin => (
+        <Icon key={plugin} size='tiny' src={`${MARKETPLACE_API_PREFIX}/plugins/${plugin}/icon`} />
       ))}
-      {!isShowAll && <div>+{plugins.length - MAX_DISPLAY_COUNT}</div>}
+      {!isShowAll && <div className='system-xs-medium text-text-tertiary'>+{plugins.length - MAX_DISPLAY_COUNT}</div>}
     </div>
   )
 }
