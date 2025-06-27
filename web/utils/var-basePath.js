22cedf066d1dd9ed72510d5c1521857c859f6aa3
morrain <626100676@qq.com>
2025-06-27 11:04:27 +0800

    fix: 修改静态资源路径，适配supos

--- web/utils/var-basePath.js
+++ web/utils/var-basePath.js
@@ -2,5 +2,5 @@
 // same as the one exported from var.ts
 module.exports = {
   basePath: '',
-  assetPrefix: process.env.NEXT_PUBLIC_DEPLOY_ENV === 'PRODUCTION' ? '/os/dify/ai-dify' : '',
+  assetPrefix: process.env.DEPLOY_ENV === 'PRODUCTION' ? '/os/dify/ai-dify' : '',
 }
