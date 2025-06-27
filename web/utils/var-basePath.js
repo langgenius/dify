5b9f7912334102b4ab689224b07c10ef6323cded
morrain <626100676@qq.com>
2025-06-27 12:21:47 +0800

    Revert "fix: 修改静态资源路径，适配supos"
    This reverts commit 60960d1fafa9ee8821d52f66abf0ba057e504d73.

--- web/utils/var-basePath.js
+++ web/utils/var-basePath.js
@@ -1,6 +1,6 @@
 // export basePath to next.config.js
 // same as the one exported from var.ts
 module.exports = {
-  basePath: '',
+  basePath: process.env.NEXT_PUBLIC_DEPLOY_ENV === 'PRODUCTION' ? '/os/dify/ai-dify' : '',
   assetPrefix: process.env.NEXT_PUBLIC_DEPLOY_ENV === 'PRODUCTION' ? '/os/dify/ai-dify' : '',
 }
