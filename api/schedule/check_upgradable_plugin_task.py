bcfbeee333467f074b03f69c78e660cd4649ca42
Junyan Qin <rockchinq@gmail.com>
2025-07-01 17:51:49 +0800

    perf: split tasks to multi worker

--- api/schedule/check_upgradable_plugin_task.py
+++ api/schedule/check_upgradable_plugin_task.py
@@ -1,18 +1,13 @@
 import time
-import traceback
 
 import click
 
 import app
-from core.helper import marketplace
-from core.plugin.entities.plugin import PluginInstallationSource
-from core.plugin.impl.plugin import PluginInstaller
 from extensions.ext_database import db
 from models.account import TenantPluginAutoUpgradeStrategy
+from tasks.process_tenant_plugin_autoupgrade_check_task import process_tenant_plugin_autoupgrade_check_task
 
 AUTO_UPGRADE_MINIMAL_CHECKING_INTERVAL = 15 * 60  # 15 minutes
-
-RETRY_TIMES_OF_ONE_PLUGIN_IN_ONE_TENANT = 3
 
 
 @app.celery.task(queue="plugin")
@@ -23,7 +18,7 @@ def check_upgradable_plugin_task():
     now_seconds_of_day = time.time() % 86400  # we assume the tz is UTC
     click.echo(click.style("Now seconds of day: {}".format(now_seconds_of_day), fg="green"))
 
-    # get strategies that set to be performed in the next AUTO_UPGRADE_MINIMAL_CHECKING_INTERVAL
+    # 获取需要在下一个AUTO_UPGRADE_MINIMAL_CHECKING_INTERVAL内执行的策略
     strategies = (
         db.session.query(TenantPluginAutoUpgradeStrategy)
         .filter(
@@ -36,113 +31,15 @@ def check_upgradable_plugin_task():
         .all()
     )
 
-    manager = PluginInstaller()
-
     for strategy in strategies:
-        try:
-            tenant_id = strategy.tenant_id
-            strategy_setting = strategy.strategy_setting
-            upgrade_mode = strategy.upgrade_mode
-            exclude_plugins = strategy.exclude_plugins
-            include_plugins = strategy.include_plugins
-
-            if strategy_setting == TenantPluginAutoUpgradeStrategy.StrategySetting.DISABLED:
-                continue
-
-            # get plugins that need to be checked
-            plugin_ids: list[tuple[str, str, str]] = []  # plugin_id, version, unique_identifier
-
-            if upgrade_mode == TenantPluginAutoUpgradeStrategy.UpgradeMode.PARTIAL and include_plugins:
-                all_plugins = manager.list_plugins(tenant_id)
-
-                for plugin in all_plugins:
-                    if plugin.source == PluginInstallationSource.Marketplace and plugin.plugin_id in include_plugins:
-                        plugin_ids.append((plugin.plugin_id, plugin.version, plugin.plugin_unique_identifier))
-
-            elif upgrade_mode == TenantPluginAutoUpgradeStrategy.UpgradeMode.EXCLUDE:
-                # get all plugins and remove the exclude plugins
-                all_plugins = manager.list_plugins(tenant_id)
-                plugin_ids = [
-                    (plugin.plugin_id, plugin.version, plugin.plugin_unique_identifier)
-                    for plugin in all_plugins
-                    if plugin.source == PluginInstallationSource.Marketplace and plugin.plugin_id not in exclude_plugins
-                ]
-            elif upgrade_mode == TenantPluginAutoUpgradeStrategy.UpgradeMode.ALL:
-                all_plugins = manager.list_plugins(tenant_id)
-                plugin_ids = [
-                    (plugin.plugin_id, plugin.version, plugin.plugin_unique_identifier)
-                    for plugin in all_plugins
-                    if plugin.source == PluginInstallationSource.Marketplace
-                ]
-
-            if not plugin_ids:
-                continue
-
-            plugin_ids_plain_list = [plugin_id for plugin_id, _, _ in plugin_ids]
-
-            click.echo(click.style("Fetching manifests for plugins: {}".format(plugin_ids_plain_list), fg="green"))
-
-            # fetch latest versions from marketplace
-            manifests = marketplace.batch_fetch_plugin_manifests(plugin_ids_plain_list)
-
-            for manifest in manifests:
-                for plugin_id, version, original_unique_identifier in plugin_ids:
-                    if manifest.plugin_id != plugin_id:
-                        continue
-
-                    try:
-                        current_version = version
-                        latest_version = manifest.latest_version
-
-                        # @yeuoly review here
-                        def fix_only_checker(latest_version, current_version):
-                            latest_version_tuple = tuple(int(val) for val in latest_version.split("."))
-                            current_version_tuple = tuple(int(val) for val in current_version.split("."))
-
-                            if (
-                                latest_version_tuple[0] == current_version_tuple[0]
-                                and latest_version_tuple[1] == current_version_tuple[1]
-                            ):
-                                return latest_version_tuple[2] != current_version_tuple[2]
-                            return False
-
-                        version_checker = {
-                            TenantPluginAutoUpgradeStrategy.StrategySetting.LATEST: lambda latest_version,
-                            current_version: latest_version != current_version,
-                            TenantPluginAutoUpgradeStrategy.StrategySetting.FIX_ONLY: fix_only_checker,
-                        }
-
-                        if version_checker[strategy_setting](latest_version, current_version):
-                            # execute upgrade
-                            new_unique_identifier = manifest.latest_package_identifier
-
-                            marketplace.record_install_plugin_event(new_unique_identifier)
-                            click.echo(
-                                click.style(
-                                    "Upgrade plugin: {} -> {}".format(
-                                        original_unique_identifier, new_unique_identifier
-                                    ),
-                                    fg="green",
-                                )
-                            )
-                            task_start_resp = manager.upgrade_plugin(
-                                tenant_id,
-                                original_unique_identifier,
-                                new_unique_identifier,
-                                PluginInstallationSource.Marketplace,
-                                {
-                                    "plugin_unique_identifier": new_unique_identifier,
-                                },
-                            )
-                    except Exception as e:
-                        click.echo(click.style("Error when upgrading plugin: {}".format(e), fg="red"))
-                        traceback.print_exc()
-                    break
-
-        except Exception as e:
-            click.echo(click.style("Error when checking upgradable plugin: {}".format(e), fg="red"))
-            traceback.print_exc()
-            continue
+        process_tenant_plugin_autoupgrade_check_task.delay(
+            strategy.tenant_id,
+            strategy.strategy_setting,
+            strategy.upgrade_time_of_day,
+            strategy.upgrade_mode,
+            strategy.exclude_plugins,
+            strategy.include_plugins,
+        )
 
     end_at = time.perf_counter()
     click.echo(
