d60d67bf23eda9af2551c701efe01d01fd94074d
zxhlyh <jasonapring2015@outlook.com>
2025-07-02 11:38:11 +0800

    fix

--- web/app/components/workflow/hooks/use-set-workflow-vars-with-value.ts
+++ web/app/components/workflow/hooks/use-set-workflow-vars-with-value.ts
@@ -1,7 +1,7 @@
 import { useHooksStore } from '@/app/components/workflow/hooks-store'
 
 export const useSetWorkflowVarsWithValue = () => {
-  const fetchInspectVars = useHooksStore(s => s.doSyncWorkflowDraft)
+  const fetchInspectVars = useHooksStore(s => s.fetchInspectVars)
 
   return {
     fetchInspectVars,
