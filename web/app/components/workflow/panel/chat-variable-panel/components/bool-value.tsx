e1a7b59160c9efb1de3a8037557db241ab59ed79
Joel <iamjoel007@gmail.com>
2025-07-04 16:48:56 +0800

    chore: missing files

---
+++ web/app/components/workflow/panel/chat-variable-panel/components/bool-value.tsx
@@ -0,0 +1,35 @@
+'use client'
+import type { FC } from 'react'
+import React, { useCallback } from 'react'
+import OptionCard from '../../../nodes/_base/components/option-card'
+
+type Props = {
+  value: boolean
+  onChange: (value: boolean) => void
+}
+
+const BoolValue: FC<Props> = ({
+  value,
+  onChange,
+}) => {
+  const handleChange = useCallback((newValue: boolean) => {
+    return () => {
+      onChange(newValue)
+    }
+  }, [onChange])
+  return (
+    <div className='flex w-full space-x-1'>
+      <OptionCard className='grow'
+        selected={value}
+        title='True'
+        onSelect={handleChange(true)}
+      />
+      <OptionCard className='grow'
+        selected={!value}
+        title='False'
+        onSelect={handleChange(false)}
+      />
+    </div>
+  )
+}
+export default React.memo(BoolValue)
