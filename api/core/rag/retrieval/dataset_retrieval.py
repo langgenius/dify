f54cb8fa832061c501827702401c5c231929cba2
profilechenyang <profilechenyang@didiglobal.com>
2025-07-16 16:49:31 +0800

    feat: retrieve external_documents add doc_metadata

--- api/core/rag/retrieval/dataset_retrieval.py
+++ api/core/rag/retrieval/dataset_retrieval.py
@@ -214,6 +214,7 @@ class DatasetRetrieval:
                 retriever_from=invoke_from.to_source(),
                 score=item.metadata.get("score"),
                 content=item.page_content,
+                doc_metadata=item.metadata,
             )
             retrieval_resource_list.append(source)
         # deal with dify documents
