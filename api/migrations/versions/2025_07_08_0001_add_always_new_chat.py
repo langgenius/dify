6878aa5c1c2128b0f6a345a49d6a8b981192040e
Kalo Chin <frog.beepers.0n@icloud.com>
2025-07-17 05:52:44 +0900

    Update 2025_07_08_0001_add_always_new_chat.py

--- api/migrations/versions/2025_07_08_0001_add_always_new_chat.py
+++ api/migrations/versions/2025_07_08_0001_add_always_new_chat.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'd57ba9ebb254'
-down_revision = '58eb7bdb93fe'
+down_revision = '1c9ba48be8e4'
 branch_labels = None
 depends_on = None
 
