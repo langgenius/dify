6d826d4dc6032dd243a0f4801cdab32706a82312
Joel <iamjoel007@gmail.com>
2025-07-17 10:35:05 +0800

    fix: not from markpalceplace show auto update

--- web/app/components/plugins/plugin-detail-panel/detail-header.tsx
+++ web/app/components/plugins/plugin-detail-panel/detail-header.tsx
@@ -107,7 +107,7 @@ const DetailHeader = ({
   const { referenceSetting } = useReferenceSetting()
   const { auto_upgrade: autoUpgradeInfo } = referenceSetting || {}
   const isAutoUpgradeEnabled = useMemo(() => {
-    if (!autoUpgradeInfo)
+    if (!autoUpgradeInfo || !isFromMarketplace)
       return false
     if(autoUpgradeInfo.upgrade_mode === AUTO_UPDATE_MODE.update_all)
       return true
@@ -116,7 +116,7 @@ const DetailHeader = ({
     if(autoUpgradeInfo.upgrade_mode === AUTO_UPDATE_MODE.exclude && !autoUpgradeInfo.exclude_plugins.includes(plugin_id))
       return true
     return false
-  }, [autoUpgradeInfo, plugin_id])
+  }, [autoUpgradeInfo, plugin_id, isFromMarketplace])
 
   const [isDowngrade, setIsDowngrade] = useState(false)
   const handleUpdate = async (isDowngrade?: boolean) => {
