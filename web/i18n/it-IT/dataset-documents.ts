965e952336efc8abc096bf5218de00de6bf62b3f
NeatGuyCoding <15627489+NeatGuyCoding@users.noreply.github.com>
2025-07-17 16:05:33 +0800

    minor translation fix: fix translation duplicate and typo, fix date format (#22548)

--- web/i18n/it-IT/dataset-documents.ts
+++ web/i18n/it-IT/dataset-documents.ts
@@ -380,7 +380,7 @@ const translation = {
     regenerationConfirmTitle: 'Si desidera rigenerare i blocchi figlio?',
     chunks_other: 'BLOCCHI',
     editedAt: 'A cura di',
-    dateTimeFormat: 'MM/DD/YYYY h:mm',
+    dateTimeFormat: 'DD/MM/YYYY HH:mm',
     collapseChunks: 'Comprimi blocchi',
     clearFilter: 'Cancella filtro',
     chunks_one: 'PEZZO',
