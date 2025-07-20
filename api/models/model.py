e2f23042c0691cb70f2415b05510c9bcc68fa5f8
Asuka Minato <i@asukaminato.eu.org>
2025-07-20 15:22:28 +0900

    typing

--- api/models/model.py
+++ api/models/model.py
@@ -1326,7 +1326,7 @@ class MessageAnnotation(Base):
     app_id: Mapped[str] = mapped_column(StringUUID)
     conversation_id: Mapped[Optional[str]] = mapped_column(StringUUID, db.ForeignKey("conversations.id"))
     message_id: Mapped[Optional[str]] = mapped_column(StringUUID)
-    question: Mapped[Optional[str]] = mapped_column(db.Text, nullable=True)
+    question: Mapped[str] = mapped_column(db.Text)
     content: Mapped[str]
     hit_count = mapped_column(db.Integer, nullable=False, server_default=db.text("0"))
     account_id = mapped_column(StringUUID, nullable=False)
