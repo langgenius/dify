ee86ceb906f9b41fce5b8df732bda016914b942e
hjlarry <hjlarry@163.com>
2025-07-21 16:09:51 +0800

    fix gunicorn gvent

--- api/docker/entrypoint.sh
+++ api/docker/entrypoint.sh
@@ -34,7 +34,7 @@ else
     exec gunicorn \
       --bind "${DIFY_BIND_ADDRESS:-0.0.0.0}:${DIFY_PORT:-5001}" \
       --workers ${SERVER_WORKER_AMOUNT:-1} \
-      --worker-class ${SERVER_WORKER_CLASS:-geventwebsocket.gunicorn.workers.GeventWebSocketWorker} \
+      --worker-class ${SERVER_WORKER_CLASS:-gevent} \
       --worker-connections ${SERVER_WORKER_CONNECTIONS:-10} \
       --timeout ${GUNICORN_TIMEOUT:-200} \
       app:app
