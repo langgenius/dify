0d3fc89ed37e163f29a90ad160fdb053f17efc90
Yongtao Huang <yongtaoh2022@gmail.com>
2025-07-22 18:55:05 +0800

    Ruff linter

--- api/tests/unit_tests/core/workflow/graph_engine/test_graph.py
+++ api/tests/unit_tests/core/workflow/graph_engine/test_graph.py
@@ -1,6 +1,4 @@
 from core.workflow.graph_engine.entities.graph import Graph
-from core.workflow.graph_engine.entities.run_condition import RunCondition
-from core.workflow.utils.condition.entities import Condition
 
 
 def test_init():
