de611ab344e9fc6eb28699d7011906cfbef0adf9
KVOJJJin <jzongcode@gmail.com>
2025-07-24 14:16:39 +0800

    Feat: add notification for change email completed (#22812)
    Co-authored-by: Yansong Zhang <916125788@qq.com>

--- api/tasks/mail_change_mail_task.py
+++ api/tasks/mail_change_mail_task.py
@@ -5,7 +5,7 @@ import click
 from celery import shared_task  # type: ignore
 
 from extensions.ext_mail import mail
-from libs.email_i18n import get_email_i18n_service
+from libs.email_i18n import EmailType, get_email_i18n_service
 
 
 @shared_task(queue="mail")
@@ -40,3 +40,41 @@ def send_change_mail_task(language: str, to: str, code: str, phase: str) -> None
         )
     except Exception:
         logging.exception("Send change email mail to {} failed".format(to))
+
+
+@shared_task(queue="mail")
+def send_change_mail_completed_notification_task(language: str, to: str) -> None:
+    """
+    Send change email completed notification with internationalization support.
+
+    Args:
+        language: Language code for email localization
+        to: Recipient email address
+    """
+    if not mail.is_inited():
+        return
+
+    logging.info(click.style("Start change email completed notify mail to {}".format(to), fg="green"))
+    start_at = time.perf_counter()
+
+    try:
+        email_service = get_email_i18n_service()
+        email_service.send_email(
+            email_type=EmailType.CHANGE_EMAIL_COMPLETED,
+            language_code=language,
+            to=to,
+            template_context={
+                "to": to,
+                "email": to,
+            },
+        )
+
+        end_at = time.perf_counter()
+        logging.info(
+            click.style(
+                "Send change email completed mail to {} succeeded: latency: {}".format(to, end_at - start_at),
+                fg="green",
+            )
+        )
+    except Exception:
+        logging.exception("Send change email completed mail to {} failed".format(to))
