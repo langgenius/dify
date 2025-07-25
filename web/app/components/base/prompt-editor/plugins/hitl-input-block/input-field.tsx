ab373197f9cfc9e1eec8eade17d672525689c33c
Joel <iamjoel007@gmail.com>
2025-07-25 17:53:48 +0800

    feat: i18n

--- web/app/components/base/prompt-editor/plugins/hitl-input-block/input-field.tsx
+++ web/app/components/base/prompt-editor/plugins/hitl-input-block/input-field.tsx
@@ -6,36 +6,37 @@ import Button from '../../../button'
 import { useTranslation } from 'react-i18next'
 import { getKeyboardKeyNameBySystem } from '@/app/components/workflow/utils'
 
+const i18nPrefix = 'workflow.nodes.humanInput.insertInputField'
 const InputField: React.FC = () => {
   const { t } = useTranslation()
   return (
     <div className="w-[372px] rounded-xl border-[0.5px] border-components-panel-border bg-components-panel-bg-blur p-3 shadow-lg backdrop-blur-[5px]">
-      <div className='system-md-semibold text-text-primary'>Insert input field</div>
+      <div className='system-md-semibold text-text-primary'>{t(`${i18nPrefix}.title`)}</div>
       <div className="mt-3">
         <div className='system-xs-medium text-text-secondary'>
-          Save response as<span className='system-xs-regular relative text-text-destructive-secondary'>*</span>
+          {t(`${i18nPrefix}.saveResponseAs`)}<span className='system-xs-regular relative text-text-destructive-secondary'>*</span>
         </div>
         <Input
           className="mt-1.5"
-          placeholder="Name this variable for later reference"
+          placeholder={t(`${i18nPrefix}.saveResponseAsPlaceholder`)}
         />
       </div>
       <div className='mt-4'>
         <div className='system-xs-medium text-text-secondary'>
-          Pre-populate field
+          {t(`${i18nPrefix}.prePopulateField`)}
         </div>
         <PromptEditor
           className='mt-1.5 h-[72px] rounded-lg bg-components-input-bg-normal px-3 py-1'
           placeholder={
           <div className='system-sm-regular mt-1 px-3 text-text-tertiary'>
             <div className="flex h-5 items-center space-x-1">
-              <span>Add</span>
-              <TagLabel type='edit' text='static content' />
-              <span>or</span>
-              <TagLabel type='variable' text='variable' />
-              <span>users</span>
+              <span>{t(`${i18nPrefix}.add`)}</span>
+              <TagLabel type='edit' text={t(`${i18nPrefix}.staticContent`)} />
+              <span>{t(`${i18nPrefix}.or`)}</span>
+              <TagLabel type='variable' text={t(`${i18nPrefix}.variable`)} />
+              <span>{t(`${i18nPrefix}.users`)}</span>
             </div>
-            <div className="flex h-5 items-center">will see initially, or leave empty.</div>
+            <div className="flex h-5 items-center">{t(`${i18nPrefix}.prePopulateFieldPlaceholderEnd`)}</div>
           </div>}
         />
       </div>
@@ -45,9 +46,9 @@ const InputField: React.FC = () => {
           className='flex'
           variant='primary'
         >
-          <span className='mr-1'>Insert</span>
+          <span className='mr-1'>{t(`${i18nPrefix}.insert`)}</span>
           <span className='system-kbd mr-0.5 flex h-4 items-center rounded-[4px] bg-components-kbd-bg-white px-1'>{getKeyboardKeyNameBySystem('ctrl')}</span>
-          <span className=' system-kbd flex items-center rounded-[4px] bg-components-kbd-bg-white px-1'>↩︎</span>
+          <span className=' system-kbd flex h-4 items-center rounded-[4px] bg-components-kbd-bg-white px-1'>↩︎</span>
         </Button>
       </div>
     </div>
