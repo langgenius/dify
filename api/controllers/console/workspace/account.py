78f554c687f0602cfa2d5eb77c25c5aff0a40bd9
GareArc <chen4851@purdue.edu>
2025-07-30 11:30:51 +0800

    chore: format

--- api/controllers/console/workspace/account.py
+++ api/controllers/console/workspace/account.py
@@ -1,29 +1,40 @@
 from datetime import datetime
 
 import pytz
-from configs import dify_config
-from constants.languages import supported_language
-from controllers.console import api
-from controllers.console.auth.error import (EmailAlreadyInUseError,
-                                            EmailChangeLimitError,
-                                            EmailCodeError, InvalidEmailError,
-                                            InvalidTokenError)
-from controllers.console.error import (AccountInFreezeError, AccountNotFound,
-                                       EmailSendIpLimitError)
-from controllers.console.workspace.error import (
-    AccountAlreadyInitedError, CurrentPasswordIncorrectError,
-    InvalidAccountDeletionCodeError, InvalidInvitationCodeError,
-    RepeatPasswordNotMatchError)
-from controllers.console.wraps import (account_initialization_required,
-                                       cloud_edition_billing_enabled,
-                                       enable_change_email,
-                                       enterprise_license_required,
-                                       only_edition_cloud, setup_required)
-from extensions.ext_database import db
-from fields.member_fields import account_fields
 from flask import request
 from flask_login import current_user
 from flask_restful import Resource, fields, marshal_with, reqparse
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from configs import dify_config
+from constants.languages import supported_language
+from controllers.console import api
+from controllers.console.auth.error import (
+    EmailAlreadyInUseError,
+    EmailChangeLimitError,
+    EmailCodeError,
+    InvalidEmailError,
+    InvalidTokenError,
+)
+from controllers.console.error import AccountInFreezeError, AccountNotFound, EmailSendIpLimitError
+from controllers.console.workspace.error import (
+    AccountAlreadyInitedError,
+    CurrentPasswordIncorrectError,
+    InvalidAccountDeletionCodeError,
+    InvalidInvitationCodeError,
+    RepeatPasswordNotMatchError,
+)
+from controllers.console.wraps import (
+    account_initialization_required,
+    cloud_edition_billing_enabled,
+    enable_change_email,
+    enterprise_license_required,
+    only_edition_cloud,
+    setup_required,
+)
+from extensions.ext_database import db
+from fields.member_fields import account_fields
 from libs.datetime_utils import naive_utc_now
 from libs.helper import TimestampField, email, extract_remote_ip, timezone
 from libs.login import login_required
@@ -31,10 +42,7 @@ from models import AccountIntegrate, InvitationCode
 from models.account import Account
 from services.account_service import AccountService
 from services.billing_service import BillingService
-from services.errors.account import \
-    CurrentPasswordIncorrectError as ServiceCurrentPasswordIncorrectError
-from sqlalchemy import select
-from sqlalchemy.orm import Session
+from services.errors.account import CurrentPasswordIncorrectError as ServiceCurrentPasswordIncorrectError
 
 
 class AccountInitApi(Resource):
