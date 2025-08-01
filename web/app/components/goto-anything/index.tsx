36abe4a9342b98ab37f13c40cd3ddecf6e4f8efe
crazywoola <427733928@qq.com>
2025-08-01 14:11:37 +0800

    feat: apps kbs tools selector

--- web/app/components/goto-anything/index.tsx
+++ web/app/components/goto-anything/index.tsx
@@ -3,33 +3,71 @@
 import type { FC } from 'react'
 import { useState } from 'react'
 import Modal from '@/app/components/base/modal'
+import Input from '@/app/components/base/input'
 import { useKeyPress } from 'ahooks'
+
 type Props = {
   onHide?: () => void
 }
-
+const Actions = {
+  app: {
+    key: '@app',
+    shortcurt: '@app',
+    action: () => console.log('App'),
+  },
+  knowledge: {
+    key: '@knowledge',
+    shortcurt: '@kb',
+    action: () => console.log('Knowledge'),
+  },
+  tools: {
+    key: '@tools',
+    shortcurt: '@tools',
+    action: () => console.log('Tools'),
+  },
+}
 const GotoAnything: FC<Props> = ({
   onHide,
 }) => {
   const [show, setShow] = useState(false)
+  const [text, setText] = useState('')
   // Handle key press for opening the modal
-  useKeyPress(['cmd', 'g'], (e) => {
+
+  useKeyPress(['esc'], (e) => {
+    e.preventDefault()
+    setShow(false)
+  })
+  useKeyPress(['meta.g'], (e) => {
     e.preventDefault()
     setShow(!show)
   })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+    Object.values(Actions).forEach(({ key, action }) => {
+      if (e.target.value.startsWith(key))
+        action()
+    })
+  }
+
   return (
     <Modal
       isShow={show}
       onClose={onHide}
-      closable
+      closable={false}
       className='w-[480px] !p-0'
     >
       <div className='shadows-shadow-xl flex w-[480px] flex-col items-start rounded-2xl border border-components-panel-border bg-components-panel-bg'>
-        <div className='flex items-start gap-2 self-stretch pb-3 pl-6 pr-14 pt-6'>
-          <span className='title-2xl-semi-bold self-stretch text-text-primary'>Goto Anything</span>
-        </div>
-        <div className='flex flex-col items-start justify-center gap-4 self-stretch px-6 py-3'>
-          {/* Content goes here */}
+
+        <div className='flex flex-col items-start justify-center gap-4 self-stretch px-2 py-2'>
+          <Input
+            value={text}
+            placeholder='Type to search...'
+            onChange={handleChange}
+          />
+          <div>
+
+          </div>
         </div>
       </div>
     </Modal>
