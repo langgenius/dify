f4cba2abaddc464ccaa27b9b9e98b1a487bc828c
znn <jubinkumarsoni@gmail.com>
2025-08-03 20:09:59 +0530

    Update base.py3

--- api/core/plugin/impl/base.py
+++ api/core/plugin/impl/base.py
@@ -208,7 +208,7 @@ class BasePluginClient:
                     except Exception:
                         raise PluginDaemonInnerError(code=rep.code, message=rep.message)
 
-                    logger.error(f"Error in stream reponse for plugin {rep.__dict__}")
+                    logger.exception(f"Error in stream reponse for plugin {rep.__dict__}")
                     self._handle_plugin_daemon_error(error.error_type, error.message)
                 raise ValueError(f"plugin daemon: {rep.message}, code: {rep.code}")
             if rep.data is None:
