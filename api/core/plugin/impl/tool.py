d90e30d4b6e78cb246f98560dca1332f37c7fc7d
Bowen Liang <liangbowen@gf.com.cn>
2025-08-10 14:05:52 +0800

    update

--- api/core/plugin/impl/tool.py
+++ api/core/plugin/impl/tool.py
@@ -160,11 +160,12 @@ class PluginToolManager(BasePluginClient):
                         # Delete the file if it's too large
                         del files[chunk_id]
                         # Skip yielding this message
-                        raise ValueError("File is too large which reached the limit of 30MB")
+                        raise ValueError(
+                            f"File is too large which reached the limit of {dify_config.TOOL_FILE_MAX_SIZE} bytes")
 
                     # Append the blob data to the buffer
                     files[chunk_id].data[
-                        files[chunk_id].bytes_written : files[chunk_id].bytes_written + len(blob_data)
+                        files[chunk_id].bytes_written: files[chunk_id].bytes_written + len(blob_data)
                     ] = blob_data
                     files[chunk_id].bytes_written += len(blob_data)
             else:
