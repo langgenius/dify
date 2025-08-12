63ae65fafd524c2896b3d76fbac28a1442c52c19
Yongtao Huang <yongtaoh2022@gmail.com>
2025-08-12 22:09:14 +0800

    Fix missing import in app.ts

--- web/models/app.ts
+++ web/models/app.ts
@@ -1,4 +1,4 @@
-import type { AliyunConfig, LangFuseConfig, LangSmithConfig, OpikConfig, PhoenixConfig, TracingProvider, WeaveConfig } from '@/app/(commonLayout)/app/(appDetailLayout)/[appId]/overview/tracing/type'
+import type { AliyunConfig, ArizeConfig, LangFuseConfig, LangSmithConfig, OpikConfig, PhoenixConfig, TracingProvider, WeaveConfig } from '@/app/(commonLayout)/app/(appDetailLayout)/[appId]/overview/tracing/type'
 import type { App, AppTemplate, SiteConfig } from '@/types/app'
 import type { Dependency } from '@/app/components/plugins/types'
 
