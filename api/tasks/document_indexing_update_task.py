f2859279058c206e7fa152f58b3af8aa41103c65
huangzhuo <huangzhuo1@xiaomi.com>
2025-08-14 10:43:59 +0800

    feat: document avaliable after update failed

--- api/tasks/document_indexing_update_task.py
+++ api/tasks/document_indexing_update_task.py
@@ -11,7 +11,7 @@ from extensions.ext_database import db
 from models.dataset import Dataset, Document, DocumentSegment
 
 
-@shared_task(queue="dataset")
+@shared_task(queue="dataset_hz")
 def document_indexing_update_task(dataset_id: str, document_id: str):
     """
     Async update document
@@ -40,16 +40,9 @@ def document_indexing_update_task(dataset_id: str, document_id: str):
         if not dataset:
             raise Exception("Dataset not found")
 
-        index_type = document.doc_form
-        index_processor = IndexProcessorFactory(index_type).init_index_processor()
-
+        # only delay delete segment because need retrive when update document
         segments = db.session.query(DocumentSegment).where(DocumentSegment.document_id == document_id).all()
         if segments:
-            index_node_ids = [segment.index_node_id for segment in segments]
-
-            # delete from vector index
-            index_processor.clean(dataset, index_node_ids, with_keywords=True, delete_child_chunks=True)
-
             for segment in segments:
                 db.session.delete(segment)
             db.session.commit()
@@ -70,9 +63,43 @@ def document_indexing_update_task(dataset_id: str, document_id: str):
         indexing_runner.run([document])
         end_at = time.perf_counter()
         logging.info(click.style(f"update document: {document.id} latency: {end_at - start_at}", fg="green"))
+        document_indexing_update_task_sync(dataset, document, segments, True)
     except DocumentIsPausedError as ex:
         logging.info(click.style(str(ex), fg="yellow"))
+        document_indexing_update_task_sync(dataset, document, segments, False)
     except Exception:
         logging.exception("document_indexing_update_task failed, document_id: %s", document_id)
+        document_indexing_update_task_sync(dataset, document, segments, False)
     finally:
         db.session.close()
+
+def document_indexing_update_task_sync(dataset: Dataset, document: Document, old_segments: list[DocumentSegment], update_success: bool):
+    if not old_segments:
+        return
+    try:
+        index_type = document.doc_form
+        index_processor = IndexProcessorFactory(index_type).init_index_processor()
+        if update_success:
+            index_node_ids = [segment.index_node_id for segment in old_segments]
+
+            # delete from vector index
+            index_processor.clean(dataset, index_node_ids, with_keywords=True, delete_child_chunks=True)
+
+        else:
+            # delete all new document segment and index
+            segments = db.session.query(DocumentSegment).where(DocumentSegment.document_id == document.id).all()
+            if segments:
+                index_node_ids = [segment.index_node_id for segment in segments]
+
+                # delete from vector index
+                index_processor.clean(dataset, index_node_ids, with_keywords=True, delete_child_chunks=True)
+
+                for segment in segments:
+                    db.session.delete(segment)
+                db.session.commit()
+            # revert segment
+            for segment in old_segments:
+                db.session.add(segment)
+            db.session.commit()
+    except Exception:
+        logging.exception("document_indexing_update_task_sync failed, document_id: %s", document.id)
