db8a875a312c835c224f6ba14d6ae39000ab8422
lyzno1 <yuanyouhuilyz@gmail.com>
2025-08-18 16:46:04 +0800

    feat: make trigger status label bolder with font-semibold

--- web/app/components/workflow/nodes/_base/components/trigger-container.tsx
+++ web/app/components/workflow/nodes/_base/components/trigger-container.tsx
@@ -31,7 +31,7 @@ const TriggerContainer: FC<TriggerContainerProps> = ({
     <div className="w-[242px] rounded-2xl bg-workflow-block-wrapper-bg-1 px-0 pb-0 pt-0.5">
       <div className="mb-0.5 flex items-center px-1.5 pt-0.5">
         <div className={cn('ml-0.5 mr-0.5 h-1.5 w-1.5 rounded-sm border border-black/15', statusConfig.dotColor)} />
-        <span className="system-2xs-medium-uppercase text-text-tertiary">
+        <span className="text-2xs font-semibold uppercase text-text-tertiary">
           {statusConfig.label}
         </span>
       </div>
