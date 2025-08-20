c0069b7edde922524c5179708e43148725d32fd4
lyzno1 <yuanyouhuilyz@gmail.com>
2025-08-20 17:03:57 +0800

    docs: add TODO comments for placeholder trigger plugin APIs

--- web/service/use-triggers.ts
+++ web/service/use-triggers.ts
@@ -5,6 +5,7 @@ import type { ToolWithProvider } from '@/app/components/workflow/types'
 const NAME_SPACE = 'triggers'
 
 // Get all plugins that support trigger functionality
+// TODO: Backend API not implemented yet - replace with actual triggers endpoint
 export const useAllTriggerPlugins = (enabled = true) => {
   return useQuery<ToolWithProvider[]>({
     queryKey: [NAME_SPACE, 'all'],
@@ -14,6 +15,7 @@ export const useAllTriggerPlugins = (enabled = true) => {
 }
 
 // Get trigger-capable plugins by type (schedule, webhook, etc.)
+// TODO: Backend API not implemented yet - replace with actual triggers endpoint
 export const useTriggerPluginsByType = (triggerType: string, enabled = true) => {
   return useQuery<ToolWithProvider[]>({
     queryKey: [NAME_SPACE, 'byType', triggerType],
