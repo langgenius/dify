bd63fb0945c34b6155b0a45258c7e665324399db
lyzno1 <yuanyouhuilyz@gmail.com>
2025-08-20 15:21:17 +0800

    fix: use proper translation key and remove hardcoded text

--- web/app/components/workflow/block-selector/all-start-blocks.tsx
+++ web/app/components/workflow/block-selector/all-start-blocks.tsx
@@ -1,15 +1,11 @@
 'use client'
-import {
-  useMemo,
-  useRef,
-} from 'react'
+import { useRef } from 'react'
+import { useTranslation } from 'react-i18next'
 import type { BlockEnum } from '../types'
 import type { ToolDefaultValue } from './types'
-import { useAllBuiltInTools } from '@/service/use-tools'
 import StartBlocks from './start-blocks'
 import TriggerPluginSelector from './trigger-plugin-selector'
 import cn from '@/utils/classnames'
-import { useGetLanguage } from '@/context/i18n'
 import Link from 'next/link'
 import { RiArrowRightUpLine } from '@remixicon/react'
 import { getMarketplaceUrl } from '@/utils/var'
@@ -27,26 +23,8 @@ const AllStartBlocks = ({
   onSelect,
   availableBlocksTypes,
 }: AllStartBlocksProps) => {
-  const language = useGetLanguage()
-  const { data: buildInTools = [] } = useAllBuiltInTools()
+  const { t } = useTranslation()
   const wrapElemRef = useRef<HTMLDivElement>(null)
-
-  // Filter trigger plugins based on search text
-  const triggerPlugins = useMemo(() => {
-    return buildInTools.filter((toolWithProvider) => {
-      // For now, assume all plugins can be triggers
-      // This will be refined when backend provides trigger capability info
-      return toolWithProvider.tools.length > 0
-    }).filter((toolWithProvider) => {
-      if (!searchText) return true
-      return toolWithProvider.name.toLowerCase().includes(searchText.toLowerCase())
-        || toolWithProvider.tools.some(tool =>
-          tool.label[language].toLowerCase().includes(searchText.toLowerCase()),
-        )
-    })
-  }, [buildInTools, searchText, language])
-
-  const hasTriggerPlugins = triggerPlugins.length > 0
 
   return (
     <div className={cn('min-w-[400px] max-w-[500px]', className)}>
@@ -60,21 +38,19 @@ const AllStartBlocks = ({
           availableBlocksTypes={availableBlocksTypes}
         />
 
-        {hasTriggerPlugins && (
-          <TriggerPluginSelector
-            onSelect={onSelect}
-            searchText={searchText}
-          />
-        )}
+        <TriggerPluginSelector
+          onSelect={onSelect}
+          searchText={searchText}
+        />
       </div>
 
-      {/* Footer - Find more triggers in marketplace */}
+      {/* Footer - Same as Tools tab marketplace footer */}
       <Link
         className='system-sm-medium sticky bottom-0 z-10 flex h-8 cursor-pointer items-center rounded-b-lg border-[0.5px] border-t border-components-panel-border bg-components-panel-bg-blur px-4 py-1 text-text-accent-light-mode-only shadow-lg'
-        href={getMarketplaceUrl('', { category: 'trigger' })}
+        href={getMarketplaceUrl('')}
         target='_blank'
       >
-        <span>Find more triggers in marketplace</span>
+        <span>{t('plugin.findMoreInMarketplace')}</span>
         <RiArrowRightUpLine className='ml-0.5 h-3 w-3' />
       </Link>
     </div>
