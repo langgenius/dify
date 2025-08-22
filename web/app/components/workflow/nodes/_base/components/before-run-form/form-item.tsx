a1f278bdc8f3f5982d9e6ed33d11b92879a2a840
Joel <iamjoel007@gmail.com>
2025-08-22 14:43:23 +0800

    fix: one step run form

--- web/app/components/workflow/nodes/_base/components/before-run-form/form-item.tsx
+++ web/app/components/workflow/nodes/_base/components/before-run-form/form-item.tsx
@@ -187,6 +187,18 @@ const FormItem: FC<Props> = ({
             />
           )
         }
+        { type === InputVarType.jsonObject && (
+          <CodeEditor
+            value={value}
+            language={CodeLanguage.json}
+            onChange={onChange}
+            noWrapper
+              className='bg h-[80px] overflow-y-auto rounded-[10px] bg-components-input-bg-normal p-1'
+              placeholder={
+                <div className='whitespace-pre'>{payload.json_schema}</div>
+              }
+          />
+        )}
         {(type === InputVarType.singleFile) && (
           <FileUploaderInAttachmentWrapper
             value={singleFileValue}
