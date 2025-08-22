c6dff07f89cf6e1955796aab84ff2d96502788ed
jubinsoni <jubinkumarsoni@gmail.com>
2025-08-23 00:19:35 +0530

    allow editing of hidden inputs in preview

--- web/app/components/workflow/panel/debug-and-preview/user-input.tsx
+++ web/app/components/workflow/panel/debug-and-preview/user-input.tsx
@@ -10,14 +10,18 @@ import {
 } from '../../store'
 import type { StartNodeType } from '../../nodes/start/types'
 import cn from '@/utils/classnames'
+import useTheme from '@/hooks/use-theme'
+import { Theme } from '@/types/app'
 
 const UserInput = () => {
+  const { theme } = useTheme()
   const workflowStore = useWorkflowStore()
   const inputs = useStore(s => s.inputs)
+  const showDebugAndPreviewPanel = useStore(s => s.showDebugAndPreviewPanel)
   const nodes = useNodes<StartNodeType>()
   const startNode = nodes.find(node => node.data.type === BlockEnum.Start)
   const variables = startNode?.data.variables || []
-  const visibleVariables = variables.filter(v => v.hide !== true)
+  const visibleVariables = showDebugAndPreviewPanel ? variables : variables.filter(v => v.hide !== true)
 
   const handleValueChange = (variable: string, v: string) => {
     const {
@@ -41,6 +45,12 @@ const UserInput = () => {
             key={variable.variable}
             className='mb-4 last-of-type:mb-0'
           >
+            {showDebugAndPreviewPanel && variable.hide && (
+              <div className={cn('mb-2 flex items-center text-xs', theme === Theme.light ? 'text-black' : 'text-white')}>
+                <div className={cn('h-1 w-1 rounded-full mr-1.5', theme === Theme.light ? 'bg-black' : 'bg-white')}> </div>
+                <span className={cn(theme === Theme.light ? 'text-black' : 'text-white')}>Hidden input (visible only in preview mode)</span>
+              </div>
+            )}
             <FormItem
               autoFocus={index === 0}
               payload={variable}
