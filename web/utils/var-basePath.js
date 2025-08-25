68df42aa5f9340a333ab88751e07b4d4165ee304
17hz <0x149527@gmail.com>
2025-08-25 11:25:42 +0800

    refactor: Promote basepath to environment variable

--- web/utils/var-basePath.js
+++ web/utils/var-basePath.js
@@ -1,6 +1,6 @@
 // export basePath to next.config.js
 // same as the one exported from var.ts
 module.exports = {
-  basePath: '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: '',
 }
