cc0e23b3b99f346499f7913ab17df05e9c921eb4
Yongtao Huang <yongtaoh2022@gmail.com>
2025-08-26 12:16:57 +0800

    Post fix

--- api/tests/test_containers_integration_tests/conftest.py
+++ api/tests/test_containers_integration_tests/conftest.py
@@ -26,6 +26,7 @@ from models import db
 
 # Configure logging for test containers
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
 
 
 class DifyTestContainers:
