38a5d9028812b6ad51f2377fcf2617bc9560f8b6
kurokobo <2920259+kurokobo@users.noreply.github.com>
2025-08-28 08:05:55 +0000

    fix(datasource): load default fileExtensions for file node earlier

--- web/app/components/workflow/nodes/data-source/constants.ts
+++ web/app/components/workflow/nodes/data-source/constants.ts
@@ -1,27 +1,5 @@
 import { VarType } from '@/app/components/workflow/types'
 
-export const DEFAULT_FILE_EXTENSIONS_IN_LOCAL_FILE_DATA_SOURCE = [
-  'txt',
-  'markdown',
-  'mdx',
-  'pdf',
-  'html',
-  'xlsx',
-  'xls',
-  'vtt',
-  'properties',
-  'doc',
-  'docx',
-  'csv',
-  'eml',
-  'msg',
-  'pptx',
-  'xml',
-  'epub',
-  'ppt',
-  'md',
-]
-
 export const COMMON_OUTPUT = [
   {
     name: 'datasource_type',
