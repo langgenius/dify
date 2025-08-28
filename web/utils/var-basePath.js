bda55f691eb2cccf9857bc6de2ab4d797951e34a
17hz <0x149527@gmail.com>
2025-08-28 18:00:43 +0800

    nake basePath Null safety
    Co-authored-by: crazywoola <100913391+crazywoola@users.noreply.github.com>

--- web/utils/var-basePath.js
+++ web/utils/var-basePath.js
@@ -1,6 +1,6 @@
 // export basePath to next.config.js
 // same as the one exported from var.ts
 module.exports = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: '',
 }
