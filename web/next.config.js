eec7615e38d58991990d6fc0d183d5bab14a0d8e
17hz <0x149527@gmail.com>
2025-08-28 09:40:00 +0800

    feat: create patchedCodeInspectorPlugin

--- web/next.config.js
+++ web/next.config.js
@@ -16,6 +16,17 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+/**
+ * Creates a patched code inspector plugin configuration
+ * @param {import('code-inspector-plugin').CodeInspectorPluginOptions} [options] - Configuration options
+ */
+const patchedCodeInspectorPlugin = (options) => {
+  return {
+    '**/*.{jsx,tsx,js,mjs,mts}': Object.values(codeInspectorPlugin(options))[0],
+  }
+}
+
+
 // the default url to prevent parse url error when running jest
 const hasSetWebPrefix = process.env.NEXT_PUBLIC_WEB_PREFIX
 const port = process.env.PORT || 3000
@@ -27,12 +38,9 @@ const nextConfig = {
   basePath,
   assetPrefix,
   turbopack: {
-    rules: {
-      "**/*.{jsx,tsx,js,mjs,mts}": Object.values(codeInspectorPlugin({
-        bundler: 'turbopack',
-        exclude: ['.mdx.tsx']
-      }))[0]
-    }
+    rules: patchedCodeInspectorPlugin({
+      bundler: 'turbopack'
+    })
   },
   productionBrowserSourceMaps: false, // enable browser source map generation during the production build
   // Configure pageExtensions to include md and mdx
