574abdd60409808f5d118c72edc8632fabbf947a
jubinsoni <jubinkumarsoni@gmail.com>
2025-09-01 23:31:21 +0530

    downvote with reason

--- web/app/components/base/chat/embedded-chatbot/hooks.tsx
+++ web/app/components/base/chat/embedded-chatbot/hooks.tsx
@@ -390,7 +390,7 @@ export const useEmbeddedChatbot = () => {
   }, [mutateAppConversationData, handleConversationIdInfoChange])
 
   const handleFeedback = useCallback(async (messageId: string, feedback: Feedback) => {
-    await updateFeedback({ url: `/messages/${messageId}/feedbacks`, body: { rating: feedback.rating } }, isInstalledApp, appId)
+    await updateFeedback({ url: `/messages/${messageId}/feedbacks`, body: { rating: feedback.rating, content: feedback.content } }, isInstalledApp, appId)
     notify({ type: 'success', message: t('common.api.success') })
   }, [isInstalledApp, appId, t, notify])
 
