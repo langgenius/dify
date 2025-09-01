b811f1a5df6f74b6eacec674585539df1fe9c411
WTW0313 <twwu@dify.ai>
2025-09-01 15:20:08 +0800

    chore: update Jest configuration to ignore e2e test paths

--- web/jest.config.ts
+++ web/jest.config.ts
@@ -172,9 +172,10 @@ const config: Config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/e2e/',
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
