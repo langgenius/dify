9e125e2029184c4642f13a914961d35860aa998e
zxhlyh <jasonapring2015@outlook.com>
2025-09-03 13:36:59 +0800

    Refactor/model credential (#24994)

--- web/app/components/header/account-setting/model-provider-page/model-auth/hooks/use-model-form-schemas.ts
+++ web/app/components/header/account-setting/model-provider-page/model-auth/hooks/use-model-form-schemas.ts
@@ -3,7 +3,6 @@ import { useTranslation } from 'react-i18next'
 import type {
   Credential,
   CustomModelCredential,
-  ModelLoadBalancingConfig,
   ModelProvider,
 } from '../../declarations'
 import {
@@ -18,7 +17,6 @@ export const useModelFormSchemas = (
   credentials?: Record<string, any>,
   credential?: Credential,
   model?: CustomModelCredential,
-  draftConfig?: ModelLoadBalancingConfig,
 ) => {
   const { t } = useTranslation()
   const {
@@ -27,26 +25,15 @@ export const useModelFormSchemas = (
     model_credential_schema,
   } = provider
   const formSchemas = useMemo(() => {
-    const modelTypeSchema = genModelTypeFormSchema(supported_model_types)
-    const modelNameSchema = genModelNameFormSchema(model_credential_schema?.model)
-    if (!!model) {
-      modelTypeSchema.disabled = true
-      modelNameSchema.disabled = true
-    }
     return providerFormSchemaPredefined
       ? provider_credential_schema.credential_form_schemas
-      : [
-        modelTypeSchema,
-        modelNameSchema,
-        ...(draftConfig?.enabled ? [] : model_credential_schema.credential_form_schemas),
-      ]
+      : model_credential_schema.credential_form_schemas
   }, [
     providerFormSchemaPredefined,
     provider_credential_schema?.credential_form_schemas,
     supported_model_types,
     model_credential_schema?.credential_form_schemas,
     model_credential_schema?.model,
-    draftConfig?.enabled,
     model,
   ])
 
@@ -55,7 +42,7 @@ export const useModelFormSchemas = (
       type: FormTypeEnum.textInput,
       variable: '__authorization_name__',
       label: t('plugin.auth.authorizationName'),
-      required: true,
+      required: false,
     }
 
     return [
@@ -79,8 +66,33 @@ export const useModelFormSchemas = (
     return result
   }, [credentials, credential, model, formSchemas])
 
+  const modelNameAndTypeFormSchemas = useMemo(() => {
+    if (providerFormSchemaPredefined)
+      return []
+
+    const modelNameSchema = genModelNameFormSchema(model_credential_schema?.model)
+    const modelTypeSchema = genModelTypeFormSchema(supported_model_types)
+    return [
+      modelNameSchema,
+      modelTypeSchema,
+    ]
+  }, [supported_model_types, model_credential_schema?.model, providerFormSchemaPredefined])
+
+  const modelNameAndTypeFormValues = useMemo(() => {
+    let result = {}
+    if (providerFormSchemaPredefined)
+      return result
+
+    if (model)
+      result = { ...result, __model_name: model?.model, __model_type: model?.model_type }
+
+    return result
+  }, [model, providerFormSchemaPredefined])
+
   return {
     formSchemas: formSchemasWithAuthorizationName,
     formValues,
+    modelNameAndTypeFormSchemas,
+    modelNameAndTypeFormValues,
   }
 }
