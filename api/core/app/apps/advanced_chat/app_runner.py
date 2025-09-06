81fdc7c54b082df22cfd8dcbdf0253fafe257b2d
-LAN- <laipz8200@outlook.com>
2025-09-06 16:09:59 +0800

    fix: type errors
    Signed-off-by: -LAN- <laipz8200@outlook.com>

--- api/core/app/apps/advanced_chat/app_runner.py
+++ api/core/app/apps/advanced_chat/app_runner.py
@@ -184,6 +184,7 @@ class AdvancedChatAppRunner(WorkflowBasedAppRunner):
             ),
             invoke_from=self.application_generate_entity.invoke_from,
             call_depth=self.application_generate_entity.call_depth,
+            variable_pool=variable_pool,
             graph_runtime_state=graph_runtime_state,
             command_channel=command_channel,
         )
