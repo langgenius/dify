d8693084880d822eb5999dfd18d4c30b9b1af589
QuantumGhost <obelisk.reg+git@gmail.com>
2025-09-09 17:16:22 +0800

    fix(api): privilege escalation in chat-messages API
    The `/chat-messages` API used by Agent and Chatbot applications did not
    properly enforce permission checks, allowing users with 'normal'
    permissions debug applications they shouldn't have access to.

--- api/controllers/console/app/completion.py
+++ api/controllers/console/app/completion.py
@@ -2,7 +2,7 @@ import logging
 
 from flask import request
 from flask_restx import Resource, reqparse
-from werkzeug.exceptions import InternalServerError, NotFound
+from werkzeug.exceptions import Forbidden, InternalServerError, NotFound
 
 import services
 from controllers.console import api
@@ -105,6 +105,9 @@ class ChatMessageApi(Resource):
     @account_initialization_required
     @get_app_model(mode=[AppMode.CHAT, AppMode.AGENT_CHAT])
     def post(self, app_model):
+        if not current_user.is_editor:
+            raise Forbidden()
+
         parser = reqparse.RequestParser()
         parser.add_argument("inputs", type=dict, required=True, location="json")
         parser.add_argument("query", type=str, required=True, location="json")
