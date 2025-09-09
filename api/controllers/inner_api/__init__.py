fc7c7ebeb2cf6dc9083b0ca4fd7cb806e68e9864
-LAN- <laipz8200@outlook.com>
2025-09-09 19:25:05 +0800

    fix: type errors in controllers/inner_api

--- api/controllers/inner_api/__init__.py
+++ api/controllers/inner_api/__init__.py
@@ -16,8 +16,8 @@ api = ExternalApi(
 # Create namespace
 inner_api_ns = Namespace("inner_api", description="Internal API operations", path="/")
 
-from . import mail
-from .plugin import plugin
-from .workspace import workspace
+from . import mail as _mail  # pyright: ignore[reportUnusedImport]  # noqa: F401
+from .plugin import plugin as _plugin  # pyright: ignore[reportUnusedImport]  # noqa: F401
+from .workspace import workspace as _workspace  # pyright: ignore[reportUnusedImport]  # noqa: F401
 
 api.add_namespace(inner_api_ns)
