8ff062ec8bda505b51377896ce5f80a702ab29d8
hjlarry <hjlarry@163.com>
2025-09-09 10:20:02 +0800

    change user default color

--- web/app/components/workflow/collaboration/utils/user-color.ts
+++ web/app/components/workflow/collaboration/utils/user-color.ts
@@ -3,7 +3,7 @@
  * Used for cursor colors and avatar backgrounds
  */
 export const getUserColor = (id: string): string => {
-  const colors = ['#3B82F6', '#EF4444', '#10B981', '#F59E0B', '#8B5CF6', '#EC4899', '#06B6D4', '#84CC16']
+  const colors = ['#155AEF', '#0BA5EC', '#444CE7', '#7839EE', '#4CA30D', '#0E9384', '#DD2590', '#FF4405', '#D92D20', '#F79009', '#828DAD']
   const hash = id.split('').reduce((a, b) => {
     a = ((a << 5) - a) + b.charCodeAt(0)
     return a & a
