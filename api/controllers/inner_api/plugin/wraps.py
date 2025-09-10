7c40245da8c3c7b7c674ab62691a806ce4eff8ff
Will <vvfriday@gmail.com>
2025-09-10 11:00:03 +0800

    fix: inner_api get_user_tenant

--- api/controllers/inner_api/plugin/wraps.py
+++ api/controllers/inner_api/plugin/wraps.py
@@ -75,8 +75,8 @@ def get_user_tenant(view: Optional[Callable[P, R]] = None):
             if not user_id:
                 user_id = DEFAULT_SERVICE_API_USER_ID
 
-            del kwargs["tenant_id"]
-            del kwargs["user_id"]
+            # del kwargs["tenant_id"]
+            # del kwargs["user_id"]
 
             try:
                 tenant_model = (
