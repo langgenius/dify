040125ef7eaab9f6da377d1b60364250e3ef9b00
nite-knite <nkCoding@gmail.com>
2025-09-11 13:09:46 +0800

    chore: handle data absence

--- web/app/components/base/zendesk/index.tsx
+++ web/app/components/base/zendesk/index.tsx
@@ -7,11 +7,11 @@ const Zendesk = () => {
   if (IS_CE_EDITION || !ZENDESK_WIDGET_KEY)
     return null
 
-  const nonce = process.env.NODE_ENV === 'production' ? (headers() as unknown as UnsafeUnwrappedHeaders).get('x-nonce') : ''
+  const nonce = process.env.NODE_ENV === 'production' ? (headers() as unknown as UnsafeUnwrappedHeaders).get('x-nonce') ?? '' : ''
 
   return (
     <Script
-      nonce={nonce!}
+      nonce={nonce ?? undefined}
       id="ze-snippet"
       src={`https://static.zdassets.com/ekr/snippet.js?key=${ZENDESK_WIDGET_KEY}`}
     />
