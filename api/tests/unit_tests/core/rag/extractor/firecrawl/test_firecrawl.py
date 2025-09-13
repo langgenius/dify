16e3cf8ae232ecee32bee36ee1c16c8032e12643
Asuka Minato <i@asukaminato.eu.org>
2025-09-14 03:59:15 +0900

    fix one

--- api/tests/unit_tests/core/rag/extractor/firecrawl/test_firecrawl.py
+++ api/tests/unit_tests/core/rag/extractor/firecrawl/test_firecrawl.py
@@ -18,7 +18,7 @@ def test_firecrawl_web_extractor_crawl_mode(mocker):
     mocked_firecrawl = {
         "id": "test",
     }
-    mocker.patch("requests.post", return_value=_mock_response(mocked_firecrawl))
+    mocker.patch("httpx.post", return_value=_mock_response(mocked_firecrawl))
     job_id = firecrawl_app.crawl_url(url, params)
 
     assert job_id is not None
