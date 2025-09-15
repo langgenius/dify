0ccd0d4296c2be86a9a494f57947478f6bd256fb
qiaofenglin <qiaofenglin@baidu.com>
2025-09-15 10:24:14 +0800

    fixed mypy check error

--- api/controllers/console/__init__.py
+++ api/controllers/console/__init__.py
@@ -1,7 +1,11 @@
 from flask import Blueprint
 from flask_restx import Namespace
+from typing import TYPE_CHECKING
 
 from libs.external_api import ExternalApi
+
+if TYPE_CHECKING:
+    from flask_restx import Api
 
 from .app.app_import import AppImportApi, AppImportCheckDependenciesApi, AppImportConfirmApi
 from .explore.audio import ChatAudioApi, ChatTextApi
@@ -28,7 +32,7 @@ from .remote_files import RemoteFileInfoApi, RemoteFileUploadApi
 
 bp = Blueprint("console", __name__, url_prefix="/console/api")
 
-api = ExternalApi(
+api: 'Api' = ExternalApi(
     bp,
     version="1.0",
     title="Console API",
