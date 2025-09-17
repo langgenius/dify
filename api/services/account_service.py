d5ec32e193186ba0c1da4c2ead0d5d54ec58c1da
Asuka Minato <i@asukaminato.eu.org>
2025-09-17 14:47:35 +0900

    one more

--- api/services/account_service.py
+++ api/services/account_service.py
@@ -136,12 +136,9 @@ class AccountService:
         if current_tenant:
             account.set_tenant_id(current_tenant.tenant_id)
         else:
-            available_ta = (
-                db.session.query(TenantAccountJoin)
-                .filter_by(account_id=account.id)
-                .order_by(TenantAccountJoin.id.asc())
-                .first()
-            )
+            available_ta = db.session.scalars(
+                select(TenantAccountJoin).filter_by(account_id=account.id).order_by(TenantAccountJoin.id.asc())
+            ).first()
             if not available_ta:
                 return None
 
