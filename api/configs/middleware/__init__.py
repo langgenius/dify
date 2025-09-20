c0d8e452bac8faff3bc0c8c29ce5b9e82ede2e3d
ganyang <yanpei.wyp@alibaba-inc.com>
2025-09-20 11:02:30 +0800

    feat(vdb): Support for Alibaba Cloud MySQL databases
    - Added the AliyunMySQLVector class to support Alibaba Cloud MySQL databases
    - Added the AliyunMySQLVectorFactory factory class for creating AliyunMySQLVector instances
    - Added the ALIYUN_MYSQL option to the VectorType enumeration
    - Updated the datasets controller to support Alibaba Cloud MySQL databases
    - Modified the middleware configuration to include Alibaba Cloud MySQL-related settings
    - Added the pymysql dependency to support MySQL database connections

--- api/configs/middleware/__init__.py
+++ api/configs/middleware/__init__.py
@@ -18,6 +18,7 @@ from .storage.opendal_storage_config import OpenDALStorageConfig
 from .storage.supabase_storage_config import SupabaseStorageConfig
 from .storage.tencent_cos_storage_config import TencentCloudCOSStorageConfig
 from .storage.volcengine_tos_storage_config import VolcengineTOSStorageConfig
+from .vdb.aliyun_mysql_config import AliyunMySQLConfig
 from .vdb.analyticdb_config import AnalyticdbConfig
 from .vdb.baidu_vector_config import BaiduVectorDBConfig
 from .vdb.chroma_config import ChromaConfig
@@ -330,6 +331,7 @@ class MiddlewareConfig(
     ClickzettaConfig,
     HuaweiCloudConfig,
     MilvusConfig,
+    AliyunMySQLConfig,
     MyScaleConfig,
     OpenSearchConfig,
     OracleConfig,
