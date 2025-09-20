7a3825cfced767eddfaddbe03eeaac918f3f10cd
hjlarry <hjlarry@163.com>
2025-09-20 11:15:13 +0800

    fix docker config

--- api/docker/entrypoint.sh
+++ api/docker/entrypoint.sh
@@ -44,9 +44,9 @@ else
     exec gunicorn \
       --bind "${DIFY_BIND_ADDRESS:-0.0.0.0}:${DIFY_PORT:-5001}" \
       --workers ${SERVER_WORKER_AMOUNT:-1} \
-      --worker-class ${SERVER_WORKER_CLASS:-gevent} \
+      --worker-class ${SERVER_WORKER_CLASS:-geventwebsocket.gunicorn.workers.GeventWebSocketWorker} \
       --worker-connections ${SERVER_WORKER_CONNECTIONS:-10} \
       --timeout ${GUNICORN_TIMEOUT:-200} \
-      app:app
+      app:socketio_app
   fi
 fi
--- api/extensions/ext_socketio.py
+++ api/extensions/ext_socketio.py
@@ -1,5 +1,3 @@
 import socketio
 
-from configs import dify_config
-
-sio = socketio.Server(async_mode="gevent", cors_allowed_origins=dify_config.CONSOLE_CORS_ALLOW_ORIGINS)
+sio = socketio.Server(async_mode="gevent", cors_allowed_origins="*")
