0f5ce5011eb5c3e16a2d40b3601261e97b0a58d3
Asuka Minato <i@asukaminato.eu.org>
2025-09-23 01:02:37 +0900

    sessionmaker

--- api/commands.py
+++ api/commands.py
@@ -62,7 +62,7 @@ def reset_password(email, new_password, password_confirm):
     if str(new_password).strip() != str(password_confirm).strip():
         click.echo(click.style("Passwords do not match.", fg="red"))
         return
-    with Session(db.engine, expire_on_commit=False) as session, session.begin():
+    with sessionmaker(db.engine, expire_on_commit=False).begin() as session:
         account = session.query(Account).where(Account.email == email).one_or_none()
 
         if not account:
