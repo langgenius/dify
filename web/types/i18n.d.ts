0b3b075937efad4e97dc2664ea3a6058d272337d
k-brahma <k.brahma.nirvana@gmail.com>
2025-09-23 18:30:58 +0900

    fix: correct ESLint rule name for consistent-type-definitions

--- web/types/i18n.d.ts
+++ web/types/i18n.d.ts
@@ -83,7 +83,7 @@ export type ValidTranslationKeys = FlattenKeys<Messages>
 
 // Extend react-i18next with Dify's type structure
 declare module 'react-i18next' {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  // eslint-disable-next-line ts/consistent-type-definitions
   interface CustomTypeOptions {
     defaultNS: 'translation';
     resources: {
@@ -94,7 +94,7 @@ declare module 'react-i18next' {
 
 // Extend i18next for complete type safety
 declare module 'i18next' {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  // eslint-disable-next-line ts/consistent-type-definitions
   interface CustomTypeOptions {
     defaultNS: 'translation';
     resources: {
