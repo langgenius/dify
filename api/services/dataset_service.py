52fa6b490395064d1bf33aa5b967bc2fb3dddcc4
Asuka Minato <i@asukaminato.eu.org>
2025-09-25 13:33:58 +0900

    2.0 style

--- api/services/dataset_service.py
+++ api/services/dataset_service.py
@@ -3395,6 +3395,8 @@ class DatasetPermissionService:
     def clear_partial_member_list(cls, dataset_id):
         try:
             with sessionmaker(db.engine, expire_on_commit=False).begin() as session:
-                session.query(DatasetPermission).where(DatasetPermission.dataset_id == dataset_id).delete()
+                session.execute(
+                    sa.delete(DatasetPermission).where(DatasetPermission.dataset_id == dataset_id)
+                )
         except Exception as e:
             raise e
