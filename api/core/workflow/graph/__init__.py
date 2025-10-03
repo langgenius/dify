a92f3c5446fe276bde32eaae8619c323d734f00f
-LAN- <laipz8200@outlook.com>
2025-10-04 01:38:31 +0800

    feat(graph): add a GraphBuilder for Graph

--- api/core/workflow/graph/__init__.py
+++ api/core/workflow/graph/__init__.py
@@ -1,13 +1,14 @@
 from core.workflow.runtime import ReadOnlyGraphRuntimeState, ReadOnlyVariablePool
 
 from .edge import Edge
-from .graph import Graph, NodeFactory
+from .graph import Graph, GraphBuilder, NodeFactory
 from .graph_template import GraphTemplate
 from .read_only_state_wrapper import ReadOnlyGraphRuntimeStateWrapper, ReadOnlyVariablePoolWrapper
 
 __all__ = [
     "Edge",
     "Graph",
+    "GraphBuilder",
     "GraphTemplate",
     "NodeFactory",
     "ReadOnlyGraphRuntimeState",
