56b5a2e517cf0303c28e97b73805676a7e5e9170
bowenliang123 <liang.bowen.123@qq.com>
2025-10-05 22:54:48 +0800

    update

--- api/libs/collection_utils.py
+++ api/libs/collection_utils.py
@@ -1,5 +1,6 @@
 def convert_to_lower_and_upper_set(inputs: list[str] | set[str]) -> set[str]:
-    """Convert a list or set of strings to a set containing both lower and upper case versions of each string.
+    """
+    Convert a list or set of strings to a set containing both lower and upper case versions of each string.
 
     Args:
         inputs (list[str] | set[str]): A list or set of strings to be converted.
