61afb7ce9c21ea5b9511caf6d3a878fefde40d3c
jubinsoni <jubinkumarsoni@gmail.com>
2025-10-07 08:37:23 +0530

    fix chunks 2

--- api/core/rag/splitter/fixed_text_splitter.py
+++ api/core/rag/splitter/fixed_text_splitter.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from typing import Any
 
+import re
 from core.model_manager import ModelInstance
 from core.model_runtime.model_providers.__base.tokenizers.gpt2_tokenizer import GPT2Tokenizer
 from core.rag.splitter.text_splitter import (
@@ -52,7 +53,7 @@ class FixedRecursiveCharacterTextSplitter(EnhanceRecursiveCharacterTextSplitter)
         """Create a new TextSplitter."""
         super().__init__(**kwargs)
         self._fixed_separator = fixed_separator
-        self._separators = separators or ["\n\n", "\n", " ", ""]
+        self._separators = separators or ["\n\n", "\n", "。", ". ", " ", ""]
 
     def split_text(self, text: str) -> list[str]:
         """Split incoming text and return chunks."""
@@ -84,22 +85,25 @@ class FixedRecursiveCharacterTextSplitter(EnhanceRecursiveCharacterTextSplitter)
                 break
             if _s in text:
                 separator = _s
-                new_separators = self._separators[i + 1 :]
+                new_separators = self._separators[i + 1:]
                 break
 
         # Now that we have the separator, split the text
         if separator:
             if separator == " ":
-                splits = text.split()
+                splits = re.split(r' +', text)
             else:
                 splits = text.split(separator)
                 splits = [item + separator if i < len(splits) else item for i, item in enumerate(splits)]
         else:
             splits = list(text)
-        splits = [s for s in splits if (s not in {"", "\n"})]
+        if separator == "\n":
+            splits = [s for s in splits if s != ""]
+        else:
+            splits = [s for s in splits if (s not in {"", "\n"})]
         _good_splits = []
         _good_splits_lengths = []  # cache the lengths of the splits
-        _separator = "" if self._keep_separator else separator
+        _separator = separator if self._keep_separator else ""
         s_lens = self._length_function(splits)
         if separator != "":
             for s, s_len in zip(splits, s_lens):
