648979c5feb39ec90c330e746743725c7696cda5
耐小心 <i@nxx.email>
2025-10-09 00:34:34 +0800

    merge

--- api/extensions/ext_otel.py
+++ api/extensions/ext_otel.py
@@ -135,6 +135,7 @@ def init_app(app: DifyApp):
     from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter as HTTPMetricExporter
     from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter as HTTPSpanExporter
     from opentelemetry.instrumentation.celery import CeleryInstrumentor
+    from opentelemetry.instrumentation.flask import FlaskInstrumentor
     from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
     from opentelemetry.instrumentation.redis import RedisInstrumentor
     from opentelemetry.instrumentation.requests import RequestsInstrumentor
