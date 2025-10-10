de6684241a7c5e38d11f44079c74a56caf5e0af5
liugddx <liugddx@gmail.com>
2025-10-10 22:08:32 +0800

    feat: add tracking for updated_by and updated_at fields in app models

--- api/controllers/console/app/workflow.py
+++ api/controllers/console/app/workflow.py
@@ -25,6 +25,7 @@ from factories import file_factory, variable_factory
 from fields.workflow_fields import workflow_fields, workflow_pagination_fields
 from fields.workflow_run_fields import workflow_run_node_execution_fields
 from libs import helper
+from libs.datetime_utils import naive_utc_now
 from libs.helper import TimestampField, uuid_value
 from libs.login import current_user, login_required
 from models import App
@@ -675,6 +676,8 @@ class PublishedWorkflowApi(Resource):
             )
 
             app_model.workflow_id = workflow.id
+            app_model.updated_by = current_user.id
+            app_model.updated_at = naive_utc_now()
             db.session.commit()  # NOTE: this is necessary for update app_model.workflow_id
 
             workflow_created_at = TimestampField().format(workflow.created_at)
