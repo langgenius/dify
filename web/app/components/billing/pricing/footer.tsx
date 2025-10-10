b6114266af6ef56c0463b1f97d8f1d6cfbae1315
CodingOnStar <hanxujiang@dify.ai>
2025-10-10 14:03:02 +0800

    refactor(billing): enhance pricing footer layout with conditional classnames for better responsiveness

--- web/app/components/billing/pricing/footer.tsx
+++ web/app/components/billing/pricing/footer.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { useTranslation } from 'react-i18next'
 import { RiArrowRightUpLine } from '@remixicon/react'
 import { type Category, CategoryEnum } from '.'
+import cn from '@/utils/classnames'
 
 type FooterProps = {
   pricingPageURL: string
@@ -17,14 +18,14 @@ const Footer = ({
 
   return (
     <div className='flex min-h-16 w-full justify-center border-t border-divider-accent px-10'>
-      <div className='flex max-w-[1680px] grow justify-between border-x border-divider-accent p-6'>
+      <div className={cn('flex max-w-[1680px] grow border-x border-divider-accent p-6', currentCategory === CategoryEnum.CLOUD ? 'justify-between' : 'justify-end') }>
         {currentCategory === CategoryEnum.CLOUD && (
           <div className='flex flex-col text-text-tertiary'>
             <span className='system-xs-regular'>{t('billing.plansCommon.taxTip')}</span>
             <span className='system-xs-regular'>{t('billing.plansCommon.taxTipSecond')}</span>
           </div>
         )}
-        <span className='ml-auto flex h-fit items-center gap-x-1 text-saas-dify-blue-accessible'>
+        <span className='flex h-fit items-center gap-x-1 text-saas-dify-blue-accessible'>
           <Link
             href={pricingPageURL}
             className='system-md-regular'
