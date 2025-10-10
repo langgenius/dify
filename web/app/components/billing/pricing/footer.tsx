8d2b5c5464761ee26dbd0e234d877ac354dbd084
CodingOnStar <hanxujiang@dify.ai>
2025-10-10 10:00:29 +0800

    feat(billing): refactor pricing footer to conditionally display tax information based on category

--- web/app/components/billing/pricing/footer.tsx
+++ web/app/components/billing/pricing/footer.tsx
@@ -2,24 +2,29 @@ import React from 'react'
 import Link from 'next/link'
 import { useTranslation } from 'react-i18next'
 import { RiArrowRightUpLine } from '@remixicon/react'
+import { type Category, CategoryEnum } from '.'
 
 type FooterProps = {
   pricingPageURL: string
+  currentCategory: Category
 }
 
 const Footer = ({
   pricingPageURL,
+  currentCategory,
 }: FooterProps) => {
   const { t } = useTranslation()
 
   return (
     <div className='flex min-h-16 w-full justify-center border-t border-divider-accent px-10'>
       <div className='flex max-w-[1680px] grow justify-between border-x border-divider-accent p-6'>
-        <div className='flex flex-col text-text-tertiary'>
-          <span className='system-xs-regular'>{t('billing.plansCommon.taxTip')}</span>
-          <span className='system-xs-regular'>{t('billing.plansCommon.taxTipSecond')}</span>
-        </div>
-        <span className='flex h-fit items-center gap-x-1 text-saas-dify-blue-accessible'>
+        {currentCategory === CategoryEnum.CLOUD && (
+          <div className='flex flex-col text-text-tertiary'>
+            <span className='system-xs-regular'>{t('billing.plansCommon.taxTip')}</span>
+            <span className='system-xs-regular'>{t('billing.plansCommon.taxTipSecond')}</span>
+          </div>
+        )}
+        <span className='ml-auto flex h-fit items-center gap-x-1 text-saas-dify-blue-accessible'>
           <Link
             href={pricingPageURL}
             className='system-md-regular'
