29d0b8a32323333aea32b839664261b0ef0b26eb
yanghuixiang <yanghuixiang>
2025-10-15 08:45:01 +0800

    Add support for real_file_id in datasource node and improve file existence check

--- api/core/workflow/nodes/datasource/datasource_node.py
+++ api/core/workflow/nodes/datasource/datasource_node.py
@@ -166,9 +166,12 @@ class DatasourceNode(Node):
                     )
                 case DatasourceProviderType.LOCAL_FILE:
                     related_id = datasource_info.get("related_id")
-                    if not related_id:
+                    real_file_id = datasource_info.get("real_file_id")
+                    if not related_id and not real_file_id:
                         raise DatasourceNodeError("File is not exist")
-                    upload_file = db.session.query(UploadFile).where(UploadFile.id == related_id).first()
+                    upload_file = (
+                        db.session.query(UploadFile).filter(UploadFile.id.in_([related_id, real_file_id])).first()
+                    )
                     if not upload_file:
                         raise ValueError("Invalid upload file Info")
 
