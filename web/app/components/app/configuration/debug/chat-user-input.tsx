18f2d24f8e9fdb115df98c7537859c5c4221787e
Joel <iamjoel007@gmail.com>
2025-10-16 10:42:47 +0800

    chore: preview input field readonly

--- web/app/components/app/configuration/debug/chat-user-input.tsx
+++ web/app/components/app/configuration/debug/chat-user-input.tsx
@@ -18,7 +18,7 @@ const ChatUserInput = ({
   inputs,
 }: Props) => {
   const { t } = useTranslation()
-  const { modelConfig, setInputs } = useContext(ConfigContext)
+  const { modelConfig, setInputs, readonly } = useContext(ConfigContext)
 
   const promptVariables = modelConfig.configs.prompt_variables.filter(({ key, name }) => {
     return key && key?.trim() && name && name?.trim()
@@ -70,6 +70,7 @@ const ChatUserInput = ({
                     placeholder={name}
                     autoFocus={index === 0}
                     maxLength={max_length || DEFAULT_VALUE_MAX_LEN}
+                    readOnly={readonly}
                   />
                 )}
                 {type === 'paragraph' && (
@@ -78,6 +79,7 @@ const ChatUserInput = ({
                     placeholder={name}
                     value={inputs[key] ? `${inputs[key]}` : ''}
                     onChange={(e) => { handleInputValueChange(key, e.target.value) }}
+                    readOnly={readonly}
                   />
                 )}
                 {type === 'select' && (
@@ -87,6 +89,7 @@ const ChatUserInput = ({
                     onSelect={(i) => { handleInputValueChange(key, i.value as string) }}
                     items={(options || []).map(i => ({ name: i, value: i }))}
                     allowSearch={false}
+                    disabled={readonly}
                   />
                 )}
                 {type === 'number' && (
@@ -97,6 +100,7 @@ const ChatUserInput = ({
                     placeholder={name}
                     autoFocus={index === 0}
                     maxLength={max_length || DEFAULT_VALUE_MAX_LEN}
+                    readOnly={readonly}
                   />
                 )}
                 {type === 'checkbox' && (
@@ -105,6 +109,7 @@ const ChatUserInput = ({
                     value={!!inputs[key]}
                     required={required}
                     onChange={(value) => { handleInputValueChange(key, value) }}
+                    readonly={readonly}
                   />
                 )}
               </div>
