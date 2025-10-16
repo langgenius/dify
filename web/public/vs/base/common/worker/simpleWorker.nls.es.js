1ea09d5c9bf7091006697cc5eada340d0aef9224
Eric-Guo <eric.guocz@gmail.com>
2025-10-16 21:17:06 +0800

    ✨It is known that upgrade either @monaco-editor/loader from 1.5.0 to 1.6.1 or @remixicon/react from 4.6.0 to 4.7.0 will break web/app/ components/workflow/nodes/_base/components/editor/code-editor/index.tsx, please check the root cause and propose the fix if available, maybe you need check web first to see the changelog for those two components.

--- web/public/vs/base/common/worker/simpleWorker.nls.es.js
+++
@@ -1,8 +0,0 @@
-/*!-----------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.46.0(21007360cad28648bdf46282a2592cb47c3a7a6f)
- * Released under the MIT license
- * https://github.com/microsoft/vscode/blob/main/LICENSE.txt
- *-----------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls.es",{"vs/base/common/platform":["_"],"vs/editor/common/languages":["matriz","booleano","clase","constante","constructor","enumeraci\xF3n","miembro de la enumeraci\xF3n","evento","campo","archivo","funci\xF3n","interfaz","clave","m\xE9todo","m\xF3dulo","espacio de nombres","NULL","n\xFAmero","objeto","operador","paquete","propiedad","cadena","estructura","par\xE1metro de tipo","variable","{0} ({1})"]});
-
-//# sourceMappingURL=../../../../../min-maps/vs/base/common/worker/simpleWorker.nls.es.js.map
