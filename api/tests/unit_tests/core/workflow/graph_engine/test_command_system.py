756572c0ede9758754c6704d1b70e02d3e61ee00
liugddx <liugddx@gmail.com>
2025-10-16 13:54:43 +0800

    fix: ensure custom header is set only when api_key is provided

--- api/tests/unit_tests/core/workflow/graph_engine/test_command_system.py
+++ api/tests/unit_tests/core/workflow/graph_engine/test_command_system.py
@@ -11,7 +11,12 @@ from core.workflow.graph import Graph
 from core.workflow.graph_engine import GraphEngine
 from core.workflow.graph_engine.command_channels import InMemoryChannel
 from core.workflow.graph_engine.entities.commands import AbortCommand
-from core.workflow.graph_events import GraphRunAbortedEvent, GraphRunStartedEvent, NodeRunStartedEvent, NodeRunSucceededEvent
+from core.workflow.graph_events import (
+    GraphRunAbortedEvent,
+    GraphRunStartedEvent,
+    NodeRunStartedEvent,
+    NodeRunSucceededEvent,
+)
 from core.workflow.node_events import NodeRunResult
 
 
