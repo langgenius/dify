ab6e00a6c1b592b559887c3ca2d3d6eefcc72c63
CodingOnStar <hanxujiang@dify.ai>
2025-10-17 17:07:03 +0800

    refactor: remove console logs from Google Analytics component

--- web/app/components/base/ga/index.tsx
+++ web/app/components/base/ga/index.tsx
@@ -21,8 +21,6 @@ export type IGAProps = {
 const GA: FC<IGAProps> = ({
   gaType,
 }) => {
-  console.log('IS_CE_EDITION', IS_CE_EDITION)
-  console.log('process.env.NODE_ENV', process.env.NODE_ENV)
   if (IS_CE_EDITION)
     return null
 
