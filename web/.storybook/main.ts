dcd62f07c73b650df124c46e6837435aadc4bb1b
hjlarry <hjlarry@163.com>
2025-10-20 09:37:18 +0800

    improve storybook

--- web/.storybook/main.ts
+++ web/.storybook/main.ts
@@ -1,19 +1,29 @@
 import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
-  // stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   stories: ['../app/components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    '@storybook/addon-docs',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/nextjs',
-    options: {},
+    options: {
+      builder: {
+        useSWC: true,
+        lazyCompilation: false,
+      },
+      nextConfigPath: undefined,
+    },
   },
   staticDirs: ['../public'],
+  core: {
+    disableWhatsNewNotifications: true,
+  },
+  docs: {
+    defaultName: 'Documentation',
+  },
 }
 export default config
--- web/app/components/base/button/index.stories.tsx
+++ web/app/components/base/button/index.stories.tsx
@@ -1,5 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react'
-import { fn } from '@storybook/test'
+import type { Meta, StoryObj } from '@storybook/nextjs'
 
 import { RocketLaunchIcon } from '@heroicons/react/20/solid'
 import { Button } from '.'
@@ -20,7 +19,6 @@ const meta = {
   },
   args: {
     variant: 'ghost',
-    onClick: fn(),
     children: 'adsf',
   },
 } satisfies Meta<typeof Button>
@@ -33,6 +31,9 @@ export const Default: Story = {
     variant: 'primary',
     loading: false,
     children: 'Primary Button',
+    styleCss: {},
+    spinnerClassName: '',
+    destructive: false,
   },
 }
 
