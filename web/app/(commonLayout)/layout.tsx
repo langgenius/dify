a293f5ab3dfdf4861a8fc8d87f3e85df3fb6d4ab
nite-knite <nkCoding@gmail.com>
2025-10-22 10:54:10 +0800

    chore: adjust the route scope for loading Zendesk scripts

--- web/app/(commonLayout)/layout.tsx
+++ web/app/(commonLayout)/layout.tsx
@@ -9,6 +9,7 @@ import { EventEmitterContextProvider } from '@/context/event-emitter'
 import { ProviderContextProvider } from '@/context/provider-context'
 import { ModalContextProvider } from '@/context/modal-context'
 import GotoAnything from '@/app/components/goto-anything'
+import Zendesk from '@/app/components/base/zendesk'
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
@@ -29,6 +30,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
           </EventEmitterContextProvider>
         </AppContextProvider>
       </SwrInitializer>
+      <Zendesk />
     </>
   )
 }
