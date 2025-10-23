a5eeb721a7cd11afc88198bdaabd7229b4ab39a5
GuanMu <ballmanjq@gmail.com>
2025-10-23 12:37:11 +0000

    fix: update object assignment in local file uploader to prevent mutation of original file object

--- web/app/components/datasets/documents/create-from-pipeline/data-source/local-file/index.tsx
+++ web/app/components/datasets/documents/create-from-pipeline/data-source/local-file/index.tsx
@@ -139,7 +139,7 @@ const LocalFile = ({
       onprogress: onProgress,
     }, false, undefined, '?source=datasets')
       .then((res: UploadResult) => {
-        const updatedFile = Object.assign(fileItem.file, {
+        const updatedFile = Object.assign({}, fileItem.file, {
           id: res.id,
           ...(res as Partial<File>),
         }) as File
