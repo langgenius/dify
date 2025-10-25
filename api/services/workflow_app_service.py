cc219cc81c86d67f4faea042c060d5023f8a99a8
lyzno1 <yuanyouhuilyz@gmail.com>
2025-10-25 14:21:52 +0800

    fix: workflow_trigger

--- api/services/workflow_app_service.py
+++ api/services/workflow_app_service.py
@@ -194,7 +194,7 @@ class WorkflowAppService:
                 api_entity = controller.to_api_entity()
                 metadata = {
                     "provider_name": api_entity.name,
-                    "provider_label": api_entity.label,
+                    "provider_label": api_entity.label.to_dict(),
                     "icon": api_entity.icon or "",
                     "plugin_id": controller.plugin_id,
                     "plugin_unique_identifier": controller.plugin_unique_identifier,
