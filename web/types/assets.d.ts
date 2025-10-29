17d922452e0fa5a22ab0d2c2ffeff3655d8d5628
twwu <twwu@dify.ai>
2025-10-29 17:40:16 +0800

    chore: add TypeScript declaration for image assets and remove SVG module declaration

---
+++ web/types/assets.d.ts
@@ -0,0 +1,4 @@
+declare module '*.{png,jpg,jpeg,gif,svg}' {
+  const value: any
+  export default value
+}
--- web/types/svg.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.svg' {
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>
-  export default content
-}
