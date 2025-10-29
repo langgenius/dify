cb2a9e6bc90c684ae9ae422ac95a590ada006e55
CrabSAMA <71915282@qq.com>
2025-10-29 19:53:11 +0800

    chore: remove `test_model.py` unittest, will modify at another branch and issue

--- api/tests/unit_tests/core/file/test_models.py
+++ api/tests/unit_tests/core/file/test_models.py
@@ -23,37 +23,3 @@ def test_file():
     assert file.extension == ".png"
     assert file.mime_type == "image/png"
     assert file.size == 67
-
-
-def test_file_model_validate_with_legacy_fields():
-    """Test `File` model can handle data containing compatibility fields."""
-    data = {
-        "id": "test-file",
-        "tenant_id": "test-tenant-id",
-        "type": "image",
-        "transfer_method": "tool_file",
-        "related_id": "test-related-id",
-        "filename": "image.png",
-        "extension": ".png",
-        "mime_type": "image/png",
-        "size": 67,
-        "storage_key": "test-storage-key",
-        "url": "https://example.com/image.png",
-        # Extra legacy fields
-        "tool_file_id": "tool-file-123",
-        "upload_file_id": "upload-file-456",
-        "datasource_file_id": "datasource-file-789",
-    }
-
-    # Should be able to create `File` object without raising an exception
-    file = File.model_validate(data)
-
-    assert file.id == "test-file"
-    assert file.tenant_id == "test-tenant-id"
-    assert file.type == FileType.IMAGE
-    assert file.transfer_method == FileTransferMethod.TOOL_FILE
-    assert file.related_id == "test-related-id"
-    assert file.filename == "image.png"
-    assert file.extension == ".png"
-    assert file.mime_type == "image/png"
-    assert file.size == 67
