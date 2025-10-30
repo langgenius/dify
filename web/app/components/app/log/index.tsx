660c0d7b171851a52588c4a0d0412f844c0cc02d
yangzheli <1936558266@qq.com>
2025-10-30 23:00:51 +0800

    fix(web): fix issues with links, Chinese translations, and styling on the logs page

--- web/app/components/app/log/index.tsx
+++ web/app/components/app/log/index.tsx
@@ -29,7 +29,7 @@ export type QueryParam = {
 
 const ThreeDotsIcon = ({ className }: SVGProps<SVGElement>) => {
   return <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className={className ?? ''}>
-    <path d="M5 6.5V5M8.93934 7.56066L10 6.5M10.0103 11.5H11.5103" stroke="#374151" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M5 6.5V5M8.93934 7.56066L10 6.5M10.0103 11.5H11.5103" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 }
 
@@ -40,11 +40,11 @@ const EmptyElement: FC<{ appUrl: string }> = ({ appUrl }) => {
   pathSegments.pop()
   return <div className='flex h-full items-center justify-center'>
     <div className='box-border h-fit w-[560px] rounded-2xl bg-background-section-burn px-5 py-4'>
-      <span className='system-md-semibold text-text-secondary'>{t('appLog.table.empty.element.title')}<ThreeDotsIcon className='relative -left-1.5 -top-3 inline' /></span>
+      <span className='system-md-semibold text-text-secondary'>{t('appLog.table.empty.element.title')}<ThreeDotsIcon className='relative -left-1.5 -top-3 inline text-text-secondary' /></span>
       <div className='system-sm-regular mt-2 text-text-tertiary'>
         <Trans
           i18nKey="appLog.table.empty.element.content"
-          components={{ shareLink: <Link href={`${pathSegments.join('/')}/overview`} className='text-util-colors-blue-blue-600' />, testLink: <Link href={appUrl} className='text-util-colors-blue-blue-600' target='_blank' rel='noopener noreferrer' /> }}
+          components={{ shareLink: <Link href={appUrl} className='text-util-colors-blue-blue-600' target='_blank' rel='noopener noreferrer' />, testLink: <Link href={`${pathSegments.join('/')}/overview`} className='text-util-colors-blue-blue-600' /> }}
         />
       </div>
     </div>
