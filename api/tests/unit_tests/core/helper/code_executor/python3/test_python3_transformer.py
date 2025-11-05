e948c14e60ee506c9d8b3efd074af489ef072b8d
wangjifeng <wangjifeng@kkworld.com>
2025-11-05 14:12:27 +0800

    add unit tests to verify script starts with user code for Python and JavaScript transformers

---
+++ api/tests/unit_tests/core/helper/code_executor/python3/test_python3_transformer.py
@@ -0,0 +1,11 @@
+from core.helper.code_executor.python3.python3_transformer import Python3TemplateTransformer
+from core.helper.code_executor.python3.python3_code_provider import Python3CodeProvider
+
+def test_get_runner_script():
+    code = Python3CodeProvider.get_default_code()
+    inputs = {"arg1": "hello, ", "arg2": "world!"}
+    script = Python3TemplateTransformer.assemble_runner_script(code, inputs)
+    script_lines = script.splitlines()
+    code_lines = code.splitlines()
+    # Check that the first lines of script are exactly the same as code
+    assert script_lines[:len(code_lines)] == code_lines
