9a64edd4ca774c8385bd45284b31d0b0f3b2d9d2
Joel <iamjoel007@gmail.com>
2025-11-05 14:13:38 +0800

    fix: hanle select date ui

--- web/app/(commonLayout)/app/(appDetailLayout)/[appId]/overview/time-range-picker.tsx
+++ web/app/(commonLayout)/app/(appDetailLayout)/[appId]/overview/time-range-picker.tsx
@@ -9,6 +9,8 @@ import { HourglassShape } from '@/app/components/base/icons/src/vender/other'
 import TimePicker from '@/app/components/base/date-and-time-picker/time-picker'
 import { noop } from 'lodash-es'
 import { useTranslation } from 'react-i18next'
+import cn from '@/utils/classnames'
+import { RiArrowDownSLine, RiCheckLine } from '@remixicon/react'
 
 const today = dayjs()
 
@@ -24,6 +26,35 @@ const TimeRangePicker: FC<Props> = ({
   queryDateFormat,
 }) => {
   const { t } = useTranslation()
+
+  const isCustom = false
+
+  const renderRangeTrigger = useCallback((item: Item | null, isOpen: boolean) => {
+    // todo: custom i18n and last 30days i18n
+    return (
+      <div className={cn('flex h-8 cursor-pointer items-center space-x-1.5 rounded-lg bg-components-input-bg-normal pl-3 pr-2', isOpen && 'bg-state-base-hover-alt')}>
+        <div className='system-sm-regular text-components-input-text-filled'>{isCustom ? 'Custom' : item?.name}</div>
+        <RiArrowDownSLine className={cn('size-4 text-text-quaternary', isOpen && 'text-text-secondary')} />
+      </div>
+    )
+  }, [isCustom])
+
+  const renderOption = useCallback(({ item, selected }: { item: Item; selected: boolean }) => {
+    return (
+      <>
+        {selected && (
+          <span
+            className={cn(
+              'absolute left-2 top-[9px] flex items-center  text-text-accent',
+            )}
+          >
+            <RiCheckLine className="h-4 w-4" aria-hidden="true" />
+          </span>
+        )}
+        <span className={cn('system-md-regular block truncate')}>{item.name}</span>
+      </>
+    )
+  }, [])
 
   const handleSelectRange = useCallback((item: Item) => {
     const { name, value } = item
@@ -46,6 +77,11 @@ const TimeRangePicker: FC<Props> = ({
         notClearable={true}
         onSelect={handleSelectRange}
         defaultValue={0}
+        wrapperClassName='h-8'
+        optionWrapClassName='w-[200px] translate-x-[-24px]'
+        renderTrigger={renderRangeTrigger}
+        optionClassName='flex items-center py-0 pl-7 pr-2 h-8'
+        renderOption={renderOption}
       />
       <HourglassShape className='h-3.5 w-2 text-components-input-bg-normal' />
       <TimePicker
