eee5edd1a392bd0edb347ffbdc71478ff8ed8017
Asuka Minato <i@asukaminato.eu.org>
2025-11-17 15:17:57 +0900

    fix

--- api/cnt_base.sh
+++ api/cnt_base.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-printf "This file is gen by cnt_base.sh\n" > ./stat.txt
 for pattern in "Base" "TypeBase"; do
-    printf "%s " "$pattern" >> ./stat.txt
-    grep "($pattern):" -r --exclude-dir=".venv" --exclude-dir="tests" . | wc -l >> ./stat.txt
+    printf "%s " "$pattern"
+    grep "($pattern):" -r --exclude-dir=".venv" --exclude-dir="tests" . | wc -l
 done
