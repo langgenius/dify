7079e96aadda44ded0ad8f3881d016aedffe0229
crazywoola <100913391+crazywoola@users.noreply.github.com>
2025-11-21 14:27:21 +0000

    Update i18n files and type definitions based on en-US changes

--- web/i18n/ro-RO/share.ts
+++ web/i18n/ro-RO/share.ts
@@ -76,6 +76,7 @@ const translation = {
     },
     execution: 'EXECUȚIE',
     executions: '{{num}} EXECUȚII',
+    stopRun: 'Oprește alergarea',
   },
   login: {
     backToHome: 'Înapoi la Acasă',
