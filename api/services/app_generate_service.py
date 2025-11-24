7fd7769c04ca5597f408573b0e25a5bd02b3e459
hieheihei <270985384@qq.com>
2025-11-24 14:52:49 +0800

    auto fix

--- api/services/app_generate_service.py
+++ api/services/app_generate_service.py
@@ -10,7 +10,7 @@ from core.app.apps.completion.app_generator import CompletionAppGenerator
 from core.app.apps.workflow.app_generator import WorkflowAppGenerator
 from core.app.entities.app_invoke_entities import InvokeFrom
 from core.app.features.rate_limiting import RateLimit
-from core.observability.otel import trace_span, AppGenerateHandler
+from core.observability.otel import AppGenerateHandler, trace_span
 from enums.quota_type import QuotaType, unlimited
 from models.model import Account, App, AppMode, EndUser
 from models.workflow import Workflow
