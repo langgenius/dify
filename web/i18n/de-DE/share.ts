6bd114285c048a342f03403a6d34e9b986ad2988
NeatGuyCoding <15627489+NeatGuyCoding@users.noreply.github.com>
2025-11-25 09:39:17 +0800

    fix: i18n: exexutions translation (#28610)
    Signed-off-by: NeatGuyCoding <15627489+NeatGuyCoding@users.noreply.github.com>

--- web/i18n/de-DE/share.ts
+++ web/i18n/de-DE/share.ts
@@ -74,8 +74,8 @@ const translation = {
       moreThanMaxLengthLine: 'Zeile {{rowIndex}}: {{varName}} Wert darf nicht mehr als {{maxLength}} Zeichen sein',
       atLeastOne: 'Bitte geben Sie mindestens eine Zeile in die hochgeladene Datei ein.',
     },
-    executions: '{{num}} HINRICHTUNGEN',
-    execution: 'AUSFÜHRUNG',
+    execution: 'Ausführen',
+    executions: '{{num}} Ausführungen',
     stopRun: 'Ausführung stoppen',
   },
   login: {
