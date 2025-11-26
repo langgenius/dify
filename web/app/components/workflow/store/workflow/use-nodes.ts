f8284826d85fcd2559c5981bdf5a572990f2d961
Joel <iamjoel007@gmail.com>
2025-11-26 14:47:27 +0800

    chore: remove useless code

--- web/app/components/workflow/store/workflow/use-nodes.ts
+++ web/app/components/workflow/store/workflow/use-nodes.ts
@@ -1,8 +1,6 @@
 import {
   useStore,
 } from '@/app/components/workflow/store'
-// import { useNodes as useFlowNodes } from 'reactflow'
-// const nodes = useFlowNodes()
 
 const useNodes = () => {
   const nodes = useStore(s => s.nodes)
