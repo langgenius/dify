db98e216f657d0ead009c6d3dcf95fe497ee4eb8
Joel <iamjoel007@gmail.com>
2025-12-05 18:01:05 +0800

    chore: enchance

--- web/context/provider-context-mock.spec.tsx
+++ web/context/provider-context-mock.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { Plan } from '@/app/components/billing/type'
 import ProviderContextMock from './provider-context-mock'
 import { createMockPlan } from '@/__mocks__/provider-context'
@@ -21,11 +21,11 @@ describe('ProviderContextMock', () => {
     jest.clearAllMocks()
   })
   it('mock sandbox plan', async () => {
-    renderWithPlan(Plan.sandbox)
-    await waitFor(() => expect(screen.getByTestId('plan-type').textContent).toBe(Plan.sandbox))
+    const { getByTestId } = renderWithPlan(Plan.sandbox)
+    expect(getByTestId('plan-type').textContent).toBe(Plan.sandbox)
   })
   it('mock pro plan', () => {
-    const { getByTestId } = renderWithPlan(Plan.professional)
-    expect(getByTestId('plan-type').textContent).toBe(Plan.professional)
+    const { getByTestId } = renderWithPlan(Plan.team)
+    expect(getByTestId('plan-type').textContent).toBe(Plan.team)
   })
 })
