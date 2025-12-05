44607b7aac2971692c6ab3174cdf58bd392ce3be
Joel <iamjoel007@gmail.com>
2025-12-05 18:02:05 +0800

    chore: enchance

--- web/context/provider-context-mock.spec.tsx
+++ web/context/provider-context-mock.spec.tsx
@@ -20,11 +20,11 @@ describe('ProviderContextMock', () => {
     mockPlan = Plan.sandbox
     jest.clearAllMocks()
   })
-  it('mock sandbox plan', async () => {
+  it('should display sandbox plan type when mocked with sandbox plan', async () => {
     const { getByTestId } = renderWithPlan(Plan.sandbox)
     expect(getByTestId('plan-type').textContent).toBe(Plan.sandbox)
   })
-  it('mock pro plan', () => {
+  it('should display team plan type when mocked with team plan', () => {
     const { getByTestId } = renderWithPlan(Plan.team)
     expect(getByTestId('plan-type').textContent).toBe(Plan.team)
   })
