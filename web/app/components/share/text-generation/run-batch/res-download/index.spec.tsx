75356682d53bcd89d789c034dcde581f69f84048
Joel <iamjoel007@gmail.com>
2025-12-12 14:43:50 +0800

    chore: ts problem

--- web/app/components/share/text-generation/run-batch/res-download/index.spec.tsx
+++ web/app/components/share/text-generation/run-batch/res-download/index.spec.tsx
@@ -15,7 +15,7 @@ jest.mock('react-papaparse', () => ({
   useCSVDownloader: () => {
     const CSVDownloader = ({ children, ...props }: React.PropsWithChildren<Record<string, unknown>>) => {
       capturedProps = props
-      return <div data-testid="csv-downloader" className={props.className}>{children}</div>
+      return <div data-testid="csv-downloader" className={props.className as string}>{children}</div>
     }
     return {
       CSVDownloader,
