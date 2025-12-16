0926463142af1f1904e2cbb947cd30cc61265acf
Joel <iamjoel007@gmail.com>
2025-12-16 14:06:09 +0800

    chore: remove useless

--- web/app/components/billing/pricing/plans/cloud-plan-item/list/item/tooltip.spec.tsx
+++ web/app/components/billing/pricing/plans/cloud-plan-item/list/item/tooltip.spec.tsx
@@ -3,10 +3,8 @@ import { render, screen } from '@testing-library/react'
 import Tooltip from './tooltip'
 
 describe('CloudPlanItem/List/Item/Tooltip', () => {
-  test('should render tooltip content and icon container', () => {
-    const { container } = render(<Tooltip content="Tooltip details" />)
-
+  test('should render tooltip content', () => {
+    render(<Tooltip content="Tooltip details" />)
     expect(screen.getByText('Tooltip details')).toBeInTheDocument()
-    expect(container.querySelector('svg')).not.toBeNull()
   })
 })
