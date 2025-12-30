fce700d55634b10090df85ce35278f26cf5980b5
aqiu <819110812@qq.com>
2025-12-30 19:47:49 +0800

    fix: prevent crash when loop start node is missing

--- web/app/components/workflow/utils/workflow-init.ts
+++ web/app/components/workflow/utils/workflow-init.ts
@@ -113,8 +113,8 @@ export const preprocessNodesAndEdges = (nodes: Node[], edges: Edge[]) => {
     const currentNode = nodes[i] as Node<IterationNodeType | LoopNodeType>
 
     if (currentNode.data.type === BlockEnum.Iteration) {
-      if (currentNode.data.start_node_id) {
-        if (nodesMap[currentNode.data.start_node_id]?.type !== CUSTOM_ITERATION_START_NODE)
+      if (currentNode.data.start_node_id && nodesMap[currentNode.data.start_node_id]) {
+        if (nodesMap[currentNode.data.start_node_id].type !== CUSTOM_ITERATION_START_NODE)
           iterationNodesWithStartNode.push(currentNode)
       }
       else {
@@ -123,8 +123,8 @@ export const preprocessNodesAndEdges = (nodes: Node[], edges: Edge[]) => {
     }
 
     if (currentNode.data.type === BlockEnum.Loop) {
-      if (currentNode.data.start_node_id) {
-        if (nodesMap[currentNode.data.start_node_id]?.type !== CUSTOM_LOOP_START_NODE)
+      if (currentNode.data.start_node_id && nodesMap[currentNode.data.start_node_id]) {
+        if (nodesMap[currentNode.data.start_node_id].type !== CUSTOM_LOOP_START_NODE)
           loopNodesWithStartNode.push(currentNode)
       }
       else {
