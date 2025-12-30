e153be61c5413012659814dcb7f069a4aa1f150c
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-12-30 16:55:44 +0800

    remove eslint config

--- web/next.config.js
+++ web/next.config.js
@@ -124,13 +124,6 @@ const nextConfig = {
       '@heroicons/react',
     ],
   },
-  // fix all before production. Now it slow the develop speed.
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-    dirs: ['app', 'bin', 'config', 'context', 'hooks', 'i18n', 'models', 'service', 'test', 'types', 'utils'],
-  },
   typescript: {
     // https://nextjs.org/docs/api-reference/next.config.js/ignoring-typescript-errors
     ignoreBuildErrors: true,
