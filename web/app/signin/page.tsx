22bf027a0d7681528f2d00e9d7d3d3adcd7ecc79
Germey <cqc@cuiqingcai.com>
2026-01-02 19:29:22 +0800

    fix: fix login loop (#27)

--- web/app/signin/page.tsx
+++ web/app/signin/page.tsx
@@ -1,35 +1,6 @@
-import type { SearchParams } from '@/utils/search-params'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
-import { firstSearchParam, searchParamsToString } from '@/utils/search-params'
 import SignInClient from './signin-client'
 
-type CookieStore = Awaited<ReturnType<typeof cookies>>
-
-const hasConsoleAccessTokenCookie = (cookieStore: CookieStore) =>
-  Boolean(cookieStore.get('access_token')?.value || cookieStore.get('__Host-access_token')?.value)
-
-const shouldAutoRedirectToAceDataCloudOAuth = (cookieStore: CookieStore, searchParams: SearchParams) => {
-  if (hasConsoleAccessTokenCookie(cookieStore))
-    return false
-  if (cookieStore.get('no_acedatacloud_oauth')?.value === '1')
-    return false
-  if (firstSearchParam(searchParams.message))
-    return false
-  if (firstSearchParam(searchParams.step) === 'next')
-    return false
-  if (firstSearchParam(searchParams.no_acedatacloud_oauth) === '1')
-    return false
-  return true
-}
-
-const SignIn = async ({ searchParams }: { searchParams?: SearchParams }) => {
-  const safeSearchParams = searchParams || {}
-  const cookieStore = await cookies()
-  if (shouldAutoRedirectToAceDataCloudOAuth(cookieStore, safeSearchParams)) {
-    const query = searchParamsToString(safeSearchParams)
-    redirect(`/console/api/oauth/login/acedatacloud${query}`)
-  }
+const SignIn = () => {
   return <SignInClient />
 }
 
--- web/app/signup/page.tsx
+++ web/app/signup/page.tsx
@@ -1,31 +1,6 @@
-import type { SearchParams } from '@/utils/search-params'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
-import { firstSearchParam, searchParamsToString } from '@/utils/search-params'
 import SignupClient from './signup-client'
 
-type CookieStore = Awaited<ReturnType<typeof cookies>>
-
-const hasConsoleAccessTokenCookie = (cookieStore: CookieStore) =>
-  Boolean(cookieStore.get('access_token')?.value || cookieStore.get('__Host-access_token')?.value)
-
-const shouldAutoRedirectToAceDataCloudOAuth = (cookieStore: CookieStore, searchParams: SearchParams) => {
-  if (hasConsoleAccessTokenCookie(cookieStore))
-    return false
-  if (cookieStore.get('no_acedatacloud_oauth')?.value === '1')
-    return false
-  if (firstSearchParam(searchParams.no_acedatacloud_oauth) === '1')
-    return false
-  return true
-}
-
-const Signup = async ({ searchParams }: { searchParams?: SearchParams }) => {
-  const safeSearchParams = searchParams || {}
-  const cookieStore = await cookies()
-  if (shouldAutoRedirectToAceDataCloudOAuth(cookieStore, safeSearchParams)) {
-    const query = searchParamsToString(safeSearchParams)
-    redirect(`/console/api/oauth/login/acedatacloud${query}`)
-  }
+const Signup = () => {
   return <SignupClient />
 }
 
