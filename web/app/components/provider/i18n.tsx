374562bd814b25d6fb9192bb8e65eceda6cdd790
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-01-06 20:49:05 +0800

    chore: update

--- web/app/components/provider/i18n.tsx
+++ web/app/components/provider/i18n.tsx
@@ -1,11 +1,9 @@
 'use client'
 
-import type { i18n, Resource } from 'i18next'
+import type { Resource } from 'i18next'
 import type { Locale } from '@/i18n-config'
 import { I18nextProvider } from 'react-i18next'
 import { createI18nextInstance } from '@/i18n-config/i18next-config'
-
-let i18nextInstance: i18n | null = null
 
 export function I18nClientProvider({
   locale,
@@ -16,11 +14,10 @@ export function I18nClientProvider({
   resource: Resource
   children: React.ReactNode
 }) {
-  if (!i18nextInstance || i18nextInstance.language !== locale) {
-    i18nextInstance = createI18nextInstance(locale, resource)
-  }
+  const i18n = createI18nextInstance(locale, resource)
+
   return (
-    <I18nextProvider i18n={i18nextInstance}>
+    <I18nextProvider i18n={i18n}>
       {children}
     </I18nextProvider>
   )
