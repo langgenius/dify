810e40aaf5bf80c4b0a537086f9f1eb62ecc7571
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-01-06 18:53:37 +0800

    load resource client

--- web/i18n-config/i18next-config.ts
+++ web/i18n-config/i18next-config.ts
@@ -2,44 +2,26 @@
 import type { Resource } from 'i18next'
 import type { Locale } from '.'
 import type { NamespaceCamelCase, NamespaceKebabCase } from './resources'
-import { camelCase } from 'es-toolkit/string'
+import { kebabCase } from 'es-toolkit/string'
 import { createInstance } from 'i18next'
 import { getI18n, initReactI18next } from 'react-i18next'
-import { NAMESPACES } from './resources'
 
-const requireSilent = async (lang: Locale, namespace: NamespaceKebabCase) => {
-  let res
-  try {
-    res = (await import(`../i18n/${lang}/${namespace}.json`)).default
+function getBackend() {
+  return {
+    type: 'backend' as const,
+    init() {},
+    read(language: string, namespace: NamespaceKebabCase | NamespaceCamelCase, callback: (err: unknown, data?: unknown) => void) {
+      const ns = kebabCase(namespace) as NamespaceKebabCase
+      import(`../i18n/${language}/${ns}.json`)
+        .then(data => callback(null, data.default ?? data))
+        .catch(callback)
+    },
   }
-  catch {
-    res = (await import(`../i18n/en-US/${namespace}.json`)).default
-  }
-
-  return res
-}
-
-// Load a single namespace for a language
-export const loadNamespace = async (lang: Locale, ns: NamespaceKebabCase) => {
-  const i18n = getI18n()
-  const camelNs = camelCase(ns) as NamespaceCamelCase
-  if (i18n.hasResourceBundle(lang, camelNs))
-    return
-
-  const resource = await requireSilent(lang, ns)
-  i18n.addResourceBundle(lang, camelNs, resource, true, true)
-}
-
-// Load all namespaces for a language (used when switching language)
-export const loadLangResources = async (lang: Locale) => {
-  await Promise.all(
-    NAMESPACES.map(ns => loadNamespace(lang, ns)),
-  )
 }
 
 export function createI18nextInstance(lng: Locale, resources: Resource) {
   const instance = createInstance()
-  instance.use(initReactI18next).init({
+  instance.use(initReactI18next).use(getBackend()).init({
     lng,
     fallbackLng: 'en-US',
     resources,
@@ -53,7 +35,6 @@ export function createI18nextInstance(lng: Locale, resources: Resource) {
 export const changeLanguage = async (lng?: Locale) => {
   if (!lng)
     return
-  await loadLangResources(lng)
   const i18n = getI18n()
   await i18n.changeLanguage(lng)
 }
