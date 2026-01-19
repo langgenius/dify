2d4289a92588390f92f192bb130bfb23c79392eb
zxhlyh <jasonapring2015@outlook.com>
2026-01-19 16:15:51 +0800

    chore: relocate datasets api form (#31224)

--- web/app/components/datasets/extra-info/api-access/index.tsx
+++ web/app/components/datasets/extra-info/api-access/index.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react'
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { ApiAggregate } from '@/app/components/base/icons/src/vender/knowledge'
 import { PortalToFollowElem, PortalToFollowElemContent, PortalToFollowElemTrigger } from '@/app/components/base/portal-to-follow-elem'
 import Indicator from '@/app/components/header/indicator'
 import { cn } from '@/utils/classnames'
 import Card from './card'
 
-type ServiceApiProps = {
-  apiBaseUrl: string
+type ApiAccessProps = {
+  expand: boolean
+  apiEnabled: boolean
 }
 
-const ServiceApi = ({
-  apiBaseUrl,
-}: ServiceApiProps) => {
+const ApiAccess = ({
+  expand,
+  apiEnabled,
+}: ApiAccessProps) => {
   const { t } = useTranslation()
   const [open, setOpen] = useState(false)
 
@@ -21,7 +24,7 @@ const ServiceApi = ({
   }
 
   return (
-    <div>
+    <div className="p-3 pt-2">
       <PortalToFollowElem
         open={open}
         onOpenChange={setOpen}
@@ -36,22 +39,22 @@ const ServiceApi = ({
           onClick={handleToggle}
         >
           <div className={cn(
-            'relative flex h-8 cursor-pointer items-center gap-2 rounded-lg border-[0.5px] border-components-button-secondary-border-hover bg-components-button-secondary-bg px-3',
-            open ? 'bg-components-button-secondary-bg-hover' : 'hover:bg-components-button-secondary-bg-hover',
+            'relative flex h-8 cursor-pointer items-center gap-2 rounded-lg border border-components-panel-border px-3',
+            !expand && 'w-8 justify-center',
+            open ? 'bg-state-base-hover' : 'hover:bg-state-base-hover',
           )}
           >
+            <ApiAggregate className="size-4 shrink-0 text-text-secondary" />
+            {expand && <div className="system-sm-medium grow text-text-secondary">{t('appMenus.apiAccess', { ns: 'common' })}</div>}
             <Indicator
-              className={cn('shrink-0')}
-              color={
-                apiBaseUrl ? 'green' : 'yellow'
-              }
+              className={cn('shrink-0', !expand && 'absolute -right-px -top-px')}
+              color={apiEnabled ? 'green' : 'yellow'}
             />
-            <div className="system-sm-medium grow text-text-secondary">{t('serviceApi.title', { ns: 'dataset' })}</div>
           </div>
         </PortalToFollowElemTrigger>
         <PortalToFollowElemContent className="z-[10]">
           <Card
-            apiBaseUrl={apiBaseUrl}
+            apiEnabled={apiEnabled}
           />
         </PortalToFollowElemContent>
       </PortalToFollowElem>
@@ -59,4 +62,4 @@ const ServiceApi = ({
   )
 }
 
-export default React.memo(ServiceApi)
+export default React.memo(ApiAccess)
