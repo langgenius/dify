67efac59941b141a6ffdb865574ad0c5a5e3e199
Yansong Zhang <916125788@qq.com>
2026-01-19 16:06:18 +0800

    fix migration

--- api/migrations/versions/2026_01_17_1110-f9f6d18a37f9_add_table_explore_banner_and_trial.py
+++ api/migrations/versions/2026_01_17_1110-f9f6d18a37f9_add_table_explore_banner_and_trial.py
@@ -1,8 +1,8 @@
 """add table explore banner and trial
 
 Revision ID: f9f6d18a37f9
-Revises: 7df29de0f6be
-Create Date: 2026-01-09 11:10:18.079355
+Revises: 288345cd01d1
+Create Date: 2026-01-017 11:10:18.079355
 
 """
 from alembic import op
@@ -12,7 +12,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = 'f9f6d18a37f9'
-down_revision = '7df29de0f6be'
+down_revision = '288345cd01d1'
 branch_labels = None
 depends_on = None
 
