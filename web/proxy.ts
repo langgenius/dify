3ce371a63f110a0d9e4e7d859830a1b84042d3ed
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-01-20 15:38:36 +0800

    litn

--- web/proxy.ts
+++ web/proxy.ts
@@ -1,4 +1,5 @@
 import type { NextRequest } from 'next/server'
+import { Buffer } from 'node:buffer'
 import { NextResponse } from 'next/server'
 
 const NECESSARY_DOMAIN = '*.sentry.io http://localhost:* http://127.0.0.1:* https://analytics.google.com googletagmanager.com *.googletagmanager.com https://www.google-analytics.com https://api.github.com https://api2.amplitude.com *.amplitude.com'
