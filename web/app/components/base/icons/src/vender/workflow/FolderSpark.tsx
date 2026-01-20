8154d0af53ee531dfa0a75e7345ce520662ab370
yyh <yuanyouhuilyz@gmail.com>
2026-01-20 13:51:49 +0800

    feat: add FolderSpark icon for workflow

--- web/app/components/base/icons/src/vender/workflow/FolderSpark.tsx
+++ web/app/components/base/icons/src/vender/workflow/FolderSpark.tsx
@@ -4,7 +4,7 @@
 import type { IconData } from '@/app/components/base/icons/IconBase'
 import * as React from 'react'
 import IconBase from '@/app/components/base/icons/IconBase'
-import data from './AssembleVariablesAlt.json'
+import data from './FolderSpark.json'
 
 const Icon = (
   {
@@ -15,6 +15,6 @@ const Icon = (
   },
 ) => <IconBase {...props} ref={ref} data={data as IconData} />
 
-Icon.displayName = 'AssembleVariablesAlt'
+Icon.displayName = 'FolderSpark'
 
 export default Icon
