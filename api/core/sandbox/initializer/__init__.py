c3b4029d0b4456f2e8946b30395e005cf9e81f47
Yeuoly <admin@srmxy.cn>
2026-01-22 19:18:46 +0800

    feat: DraftAppAssetsInitializer

--- api/core/sandbox/initializer/__init__.py
+++ api/core/sandbox/initializer/__init__.py
@@ -1,11 +1,11 @@
 from .app_assets_initializer import AppAssetsInitializer
-from .draft_app_assets_initializer import DraftAppAssetsInitializer
 from .base import SandboxInitializer
 from .dify_cli_initializer import DifyCliInitializer
+from .draft_app_assets_initializer import DraftAppAssetsInitializer
 
 __all__ = [
     "AppAssetsInitializer",
-    "DraftAppAssetsInitializer",
     "DifyCliInitializer",
+    "DraftAppAssetsInitializer",
     "SandboxInitializer",
 ]
