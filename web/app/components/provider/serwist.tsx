baac9678a22292b2a8c2f12edeb8371e37db9d40
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-01-23 11:29:35 +0800

    remove service worker in dev

--- web/app/components/provider/serwist.tsx
+++ web/app/components/provider/serwist.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import { SerwistProvider } from '@serwist/turbopack/react'
+import { useEffect } from 'react'
 import { IS_DEV } from '@/config'
+import { isClient } from '@/utils/client'
 
 export function PWAProvider({ children }: { children: React.ReactNode }) {
   if (IS_DEV) {
-    return <>{children}</>
+    return <DisabledPWAProvider>{children}</DisabledPWAProvider>
   }
 
   const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
@@ -16,4 +18,18 @@ export function PWAProvider({ children }: { children: React.ReactNode }) {
       {children}
     </SerwistProvider>
   )
+}
+
+function DisabledPWAProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    if (isClient && 'serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then((registrations) => {
+        registrations.forEach((registration) => {
+          registration.unregister()
+        })
+      })
+    }
+  }, [])
+
+  return <>{children}</>
 }
