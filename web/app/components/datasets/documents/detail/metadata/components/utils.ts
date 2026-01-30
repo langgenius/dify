a90c056121409d053fa865c867ee701f7f87beb9
CodingOnStar <hanxujiang@dify.com>
2026-01-30 11:28:44 +0800

    Revert "refactor(web): restructure metadata components and add tests"
    This reverts commit a25e4c1b3abd77c33c4cbc4fbc2ad9f90b9825aa.

--- web/app/components/datasets/documents/detail/metadata/components/utils.ts
+++
@@ -1,3 +0,0 @@
-export const map2Options = (map: { [key: string]: string }) => {
-  return Object.keys(map).map(key => ({ value: key, name: map[key] }))
-}
--- web/app/components/datasets/documents/detail/metadata/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export { useMetadataEditor } from './use-metadata-editor'
-export type { MetadataState } from './use-metadata-editor'
-export { useMetadataSave } from './use-metadata-save'
