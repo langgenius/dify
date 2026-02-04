49baf07f1fda276cda91c51144526a8334cb4b27
yessenia <yessenia.contact@gmail.com>
2026-02-04 17:53:36 +0800

    feat: layout opt

--- web/app/components/plugins/marketplace/marketplace-header.tsx
+++ web/app/components/plugins/marketplace/marketplace-header.tsx
@@ -6,15 +6,16 @@ import SearchResultsHeader from './search-results-header'
 
 type MarketplaceHeaderProps = {
   descriptionClassName?: string
+  marketplaceNav?: React.ReactNode
 }
 
-const MarketplaceHeader = ({ descriptionClassName }: MarketplaceHeaderProps) => {
+const MarketplaceHeader = ({ descriptionClassName, marketplaceNav }: MarketplaceHeaderProps) => {
   const isSearchMode = useMarketplaceSearchMode()
 
   if (isSearchMode)
     return <SearchResultsHeader />
 
-  return <Description className={descriptionClassName} />
+  return <Description className={descriptionClassName} marketplaceNav={marketplaceNav} />
 }
 
 export default MarketplaceHeader
