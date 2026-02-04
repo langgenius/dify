1ed50cc7aebd2dab47f99afc5a13993d33ab1da2
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-02-04 11:18:32 +0800

    fix: base url in client

--- web/service/client.ts
+++ web/service/client.ts
@@ -13,11 +13,36 @@ import {
   consoleRouterContract,
   marketplaceRouterContract,
 } from '@/contract/router'
+import { isClient } from '@/utils/client'
 import { request } from './base'
 
 const getMarketplaceHeaders = () => new Headers({
   'X-Dify-Version': !IS_MARKETPLACE ? APP_VERSION : '999.0.0',
 })
+
+function isURL(path: string) {
+  try {
+    // eslint-disable-next-line no-new
+    new URL(path)
+    return true
+  }
+  catch {
+    return false
+  }
+}
+
+function getBaseURL(path: string) {
+  if (isURL(path)) {
+    return new URL(path)
+  }
+
+  if (!isClient) {
+    // TODO: adjust the default base URL for server side requests if needed
+    throw new Error('Base URL is required for server side requests')
+  }
+
+  return new URL(path, window.location.origin)
+}
 
 const marketplaceLink = new OpenAPILink(marketplaceRouterContract, {
   url: MARKETPLACE_API_PREFIX,
@@ -39,7 +64,7 @@ export const marketplaceClient: JsonifiedClient<ContractRouterClient<typeof mark
 export const marketplaceQuery = createTanstackQueryUtils(marketplaceClient, { path: ['marketplace'] })
 
 const consoleLink = new OpenAPILink(consoleRouterContract, {
-  url: API_PREFIX,
+  url: getBaseURL(API_PREFIX),
   fetch: (input, init) => {
     return request(
       input.url,
