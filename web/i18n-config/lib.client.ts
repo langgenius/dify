06175af5805b9045b03b29122ce4486f297b1e02
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-02-09 16:07:38 +0800

    update

--- web/i18n-config/lib.client.ts
+++ web/i18n-config/lib.client.ts
@@ -3,9 +3,7 @@
 import type { NamespaceCamelCase } from './resources'
 import { useTranslation as useTranslationOriginal } from 'react-i18next'
 
-export function useTranslation(): ReturnType<typeof useTranslationOriginal>
-export function useTranslation<T extends NamespaceCamelCase>(ns: T): ReturnType<typeof useTranslationOriginal<T>>
-export function useTranslation(ns?: NamespaceCamelCase) {
+export function useTranslation<T extends NamespaceCamelCase | undefined = undefined>(ns?: T) {
   return useTranslationOriginal(ns)
 }
 
--- web/i18n-config/lib.server.ts
+++ web/i18n-config/lib.server.ts
@@ -2,12 +2,12 @@ import type { NamespaceCamelCase } from './resources'
 import { use } from 'react'
 import { getLocaleOnServer, getTranslation } from './server'
 
-async function getI18nConfig<T extends NamespaceCamelCase>(ns?: T) {
+async function getI18nConfig<T extends NamespaceCamelCase | undefined = undefined>(ns?: T) {
   const lang = await getLocaleOnServer()
   return getTranslation(lang, ns)
 }
 
-export function useTranslation<T extends NamespaceCamelCase>(ns?: T) {
+export function useTranslation<T extends NamespaceCamelCase | undefined = undefined>(ns?: T) {
   return use(getI18nConfig(ns))
 }
 
