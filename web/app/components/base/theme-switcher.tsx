3099b3d80fabfd17589bbbddcc40a4641b84bb70
Saumya Talwani <saumya@infocusp.com>
2026-02-16 11:54:47 +0530

    test: add tests for theme-switcher

--- web/app/components/base/theme-switcher.tsx
+++ web/app/components/base/theme-switcher.tsx
@@ -1,9 +1,4 @@
 'use client'
-import {
-  RiComputerLine,
-  RiMoonLine,
-  RiSunLine,
-} from '@remixicon/react'
 import { useTheme } from 'next-themes'
 import { cn } from '@/utils/classnames'
 
@@ -24,33 +19,36 @@ export default function ThemeSwitcher() {
           theme === 'system' && 'bg-components-segmented-control-item-active-bg text-text-accent-light-mode-only shadow-sm hover:bg-components-segmented-control-item-active-bg hover:text-text-accent-light-mode-only',
         )}
         onClick={() => handleThemeChange('system')}
+        data-testid="system-theme-container"
       >
         <div className="p-0.5">
-          <RiComputerLine className="h-4 w-4" />
+          <span className="i-ri-computer-line h-4 w-4" />
         </div>
       </div>
-      <div className={cn('h-[14px] w-px bg-transparent', theme === 'dark' && 'bg-divider-regular')}></div>
+      <div className={cn('h-[14px] w-px bg-transparent', theme === 'dark' && 'bg-divider-regular')} data-testid="divider"></div>
       <div
         className={cn(
           'rounded-lg px-2 py-1 text-text-tertiary hover:bg-state-base-hover hover:text-text-secondary',
           theme === 'light' && 'bg-components-segmented-control-item-active-bg text-text-accent-light-mode-only shadow-sm hover:bg-components-segmented-control-item-active-bg hover:text-text-accent-light-mode-only',
         )}
         onClick={() => handleThemeChange('light')}
+        data-testid="light-theme-container"
       >
         <div className="p-0.5">
-          <RiSunLine className="h-4 w-4" />
+          <span className="i-ri-sun-line h-4 w-4" />
         </div>
       </div>
-      <div className={cn('h-[14px] w-px bg-transparent', theme === 'system' && 'bg-divider-regular')}></div>
+      <div className={cn('h-[14px] w-px bg-transparent', theme === 'system' && 'bg-divider-regular')} data-testid="divider"></div>
       <div
         className={cn(
           'rounded-lg px-2 py-1 text-text-tertiary hover:bg-state-base-hover hover:text-text-secondary',
           theme === 'dark' && 'bg-components-segmented-control-item-active-bg text-text-accent-light-mode-only shadow-sm hover:bg-components-segmented-control-item-active-bg hover:text-text-accent-light-mode-only',
         )}
         onClick={() => handleThemeChange('dark')}
+        data-testid="dark-theme-container"
       >
         <div className="p-0.5">
-          <RiMoonLine className="h-4 w-4" />
+          <span className="i-ri-moon-line h-4 w-4" />
         </div>
       </div>
     </div>
