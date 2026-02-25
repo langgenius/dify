138855546fe9c8a351ce164928e4813673841899
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-02-26 01:57:59 +0800

    fix: mdx import

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,7 @@ import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react'
 import vinext from 'vinext'
 import { defineConfig, loadEnv } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 const isCI = !!process.env.CI
@@ -27,6 +28,7 @@ export default defineConfig(({ mode }) => {
         : [
             mdx(),
             vinext(),
+            mkcert(),
           ]),
       tsconfigPaths(),
     ],
