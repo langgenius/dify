db8d61eefb9284d2053056849e060762c6fcc42e
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-02-26 02:26:37 +0800

    update

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,7 +5,6 @@ import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react'
 import vinext from 'vinext'
 import { defineConfig, loadEnv } from 'vite'
-import mkcert from 'vite-plugin-mkcert'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
@@ -31,7 +30,6 @@ export default defineConfig(({ mode }) => {
         : [
             mdx(),
             vinext(),
-            mkcert(),
           ]),
       tsconfigPaths(),
     ],
