dfe712548b094a19c3550a6b79683a833593fe28
QuantumGhost <obelisk.reg+git@gmail.com>
2026-03-04 17:03:04 +0800

    chore(api): add two other indexes to the HumanInputForm model

--- api/migrations/versions/2026_03_02_1805-0ec65df55790_add_indexes_for_human_input_forms.py
+++ api/migrations/versions/2026_03_02_1805-0ec65df55790_add_indexes_for_human_input_forms.py
@@ -5,6 +5,7 @@ Revises: fce013ca180e
 Create Date: 2026-03-02 18:05:00.000000
 
 """
+
 from alembic import op
 
 
@@ -22,8 +23,20 @@ def upgrade():
             ["workflow_run_id", "node_id"],
             unique=False,
         )
+        batch_op.create_index(
+            "human_input_forms_status_created_at_idx",
+            ["status", "created_at"],
+            unique=False,
+        )
+        batch_op.create_index(
+            "human_input_forms_status_expiration_time_idx",
+            ["status", "expiration_time"],
+            unique=False,
+        )
 
 
 def downgrade():
     with op.batch_alter_table("human_input_forms", schema=None) as batch_op:
         batch_op.drop_index("human_input_forms_workflow_run_id_node_id_idx")
+        batch_op.drop_index("human_input_forms_status_expiration_time_idx")
+        batch_op.drop_index("human_input_forms_status_created_at_idx")
