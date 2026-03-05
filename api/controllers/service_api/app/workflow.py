47f2749fe3e0794fa12873a4c55937cb1e4664d0
fatelei <fatelei@gmail.com>
2026-03-05 14:19:09 +0800

    fix: fix check workflow_run

--- api/controllers/service_api/app/workflow.py
+++ api/controllers/service_api/app/workflow.py
@@ -132,6 +132,8 @@ class WorkflowRunDetailApi(Resource):
             app_id=app_model.id,
             run_id=workflow_run_id,
         )
+        if not workflow_run:
+            raise NotFound(f"{workflow_run_id} not found")
         return workflow_run
 
 
