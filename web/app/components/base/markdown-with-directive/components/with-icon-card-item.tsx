10bb7863417e50ed630d141165a11beb71061b1f
Joel <iamjoel007@gmail.com>
2026-03-05 17:16:22 +0800

    chore: fix text too long

--- web/app/components/base/markdown-with-directive/components/with-icon-card-item.tsx
+++ web/app/components/base/markdown-with-directive/components/with-icon-card-item.tsx
@@ -10,7 +10,7 @@ function WithIconCardItem({ icon, children }: WithIconItemProps) {
   return (
     <div className="flex h-11 items-center space-x-3 rounded-lg bg-background-section px-2">
       <Image src={icon} className="!border-none object-contain" alt="icon" width={40} height={40} />
-      <div className="w-0 grow text-text-secondary system-sm-medium [&_p]:!mb-0 [&_p]:flex [&_p]:h-11 [&_p]:w-full [&_p]:items-center [&_p]:truncate">
+      <div className="min-w-0 grow overflow-hidden text-text-secondary system-sm-medium [&_p]:!m-0 [&_p]:block [&_p]:w-full [&_p]:overflow-hidden [&_p]:text-ellipsis [&_p]:whitespace-nowrap">
         {children}
       </div>
     </div>
