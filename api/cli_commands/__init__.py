fc4e0f10d4f52d4d0d5fa3402765864efb7d42c7
hjlarry <hjlarry@163.com>
2026-03-10 09:53:05 +0800

    migrate system

--- api/cli_commands/__init__.py
+++ api/cli_commands/__init__.py
@@ -15,6 +15,7 @@ from cli_commands.plugin import (
     transform_datasource_credentials,
 )
 from cli_commands.storage import clear_orphaned_file_records, file_usage, migrate_oss, remove_orphaned_files_on_storage
+from cli_commands.system import convert_to_agent_apps, fix_app_site_missing, reset_encrypt_key_pair, upgrade_db
 from cli_commands.vector import (
     add_qdrant_index,
     migrate_annotation_vector_database,
@@ -26,10 +27,12 @@ from cli_commands.vector import (
 __all__ = [
     "add_qdrant_index",
     "clear_orphaned_file_records",
+    "convert_to_agent_apps",
     "create_tenant",
     "extract_plugins",
     "extract_unique_plugins",
     "file_usage",
+    "fix_app_site_missing",
     "install_plugins",
     "install_rag_pipeline_plugins",
     "migrate_annotation_vector_database",
@@ -39,10 +42,12 @@ __all__ = [
     "old_metadata_migration",
     "remove_orphaned_files_on_storage",
     "reset_email",
+    "reset_encrypt_key_pair",
     "reset_password",
     "setup_datasource_oauth_client",
     "setup_system_tool_oauth_client",
     "setup_system_trigger_oauth_client",
     "transform_datasource_credentials",
+    "upgrade_db",
     "vdb_migrate",
 ]
