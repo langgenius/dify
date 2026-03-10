d737b6e3fefa617df5f12f84ed98ec01464785b0
hjlarry <hjlarry@163.com>
2026-03-10 09:15:51 +0800

    migrate vector

--- api/cli_commands/__init__.py
+++ api/cli_commands/__init__.py
@@ -14,19 +14,31 @@ from cli_commands.plugin import (
     transform_datasource_credentials,
 )
 from cli_commands.storage import clear_orphaned_file_records, file_usage, migrate_oss, remove_orphaned_files_on_storage
+from cli_commands.vector import (
+    add_qdrant_index,
+    migrate_annotation_vector_database,
+    migrate_knowledge_vector_database,
+    old_metadata_migration,
+    vdb_migrate,
+)
 
 __all__ = [
+    "add_qdrant_index",
     "clear_orphaned_file_records",
     "extract_plugins",
     "extract_unique_plugins",
     "file_usage",
     "install_plugins",
     "install_rag_pipeline_plugins",
+    "migrate_annotation_vector_database",
     "migrate_data_for_plugin",
+    "migrate_knowledge_vector_database",
     "migrate_oss",
+    "old_metadata_migration",
     "remove_orphaned_files_on_storage",
     "setup_datasource_oauth_client",
     "setup_system_tool_oauth_client",
     "setup_system_trigger_oauth_client",
     "transform_datasource_credentials",
+    "vdb_migrate",
 ]
