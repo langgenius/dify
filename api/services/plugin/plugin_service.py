7770e8d44d78578970e58eefdab78038aea646d9
yunlu.wen <yunlu.wen@dify.ai>
2026-03-12 12:50:18 +0800

    simplify test

--- api/services/plugin/plugin_service.py
+++ api/services/plugin/plugin_service.py
@@ -6,7 +6,6 @@ from pydantic import BaseModel
 from sqlalchemy import select
 from yarl import URL
 
-from services.enterprise.plugin_manager_service import PluginManagerService, PreUninstallPluginRequest
 from configs import dify_config
 from core.helper import marketplace
 from core.helper.download import download_with_size_limit
@@ -31,6 +30,7 @@ from extensions.ext_database import db
 from extensions.ext_redis import redis_client
 from models.provider import ProviderCredential
 from models.provider_ids import GenericProviderID
+from services.enterprise.plugin_manager_service import PluginManagerService, PreUninstallPluginRequest
 from services.errors.plugin import PluginInstallationForbiddenError
 from services.feature_service import FeatureService, PluginInstallationScope
 
