5dda0eff0ea45c816d3466dd5e7dd521510113cd
WH-2099 <wh2099@pm.me>
2026-03-14 02:44:08 +0800

    refactor(workflow): remove unused workflow package re-export

--- api/core/workflow/__init__.py
+++ api/core/workflow/__init__.py
@@ -1,9 +1,4 @@
-from . import node_resolution
 from .node_factory import DifyNodeFactory
 from .workflow_entry import WorkflowEntry
-
-ensure_workflow_nodes_registered = node_resolution.ensure_workflow_nodes_registered
-
-__all__ = ["DifyNodeFactory", "WorkflowEntry", "ensure_workflow_nodes_registered"]
 
 __all__ = ["DifyNodeFactory", "WorkflowEntry"]
