76accc99ef1f1aee68242be12de0972007857f6f
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-03-16 21:54:19 +0800

    csp

--- web/app/components/base/modern-monaco/init.ts
+++ web/app/components/base/modern-monaco/init.ts
@@ -4,7 +4,6 @@ import {
   HOIST_BASE_PATH,
   HOIST_LANGUAGE_IDS,
   HOIST_THEME_IDS,
-  MODERN_MONACO_IMPORT_MAP,
   TM_GRAMMARS_VERSION,
   TM_THEMES_VERSION,
 } from './hoisted-config'
@@ -12,12 +11,9 @@ import {
 export const LIGHT_THEME_ID = 'light-plus'
 export const DARK_THEME_ID = 'dark-plus'
 
-const MODERN_MONACO_IMPORTMAP_ATTR = 'data-modern-monaco-importmap'
-
 const assetPath = (pathname: string) => `${basePath}${HOIST_BASE_PATH}${pathname}`
 const themeAssetPath = (themeId: string) => assetPath(`/tm-themes@${TM_THEMES_VERSION}/themes/${themeId}.json`)
 const grammarAssetPath = (languageId: string) => assetPath(`/tm-grammars@${TM_GRAMMARS_VERSION}/grammars/${languageId}.json`)
-const monacoModuleAssetPath = (pathname: string) => new URL(assetPath(pathname), location.origin).toString()
 
 const DEFAULT_INIT_OPTIONS: InitOptions = {
   defaultTheme: themeAssetPath(DARK_THEME_ID),
@@ -27,28 +23,9 @@ const DEFAULT_INIT_OPTIONS: InitOptions = {
 
 let monacoInitPromise: Promise<typeof import('modern-monaco/editor-core') | null> | null = null
 
-function ensureMonacoImportMap() {
-  const importMap = JSON.stringify({
-    imports: Object.fromEntries(
-      Object.entries(MODERN_MONACO_IMPORT_MAP).map(([specifier, pathname]) => [specifier, monacoModuleAssetPath(pathname)]),
-    ),
-  })
-
-  let script = document.querySelector<HTMLScriptElement>(`script[type="importmap"][${MODERN_MONACO_IMPORTMAP_ATTR}]`)
-  if (!script) {
-    script = document.createElement('script')
-    script.type = 'importmap'
-    script.setAttribute(MODERN_MONACO_IMPORTMAP_ATTR, '')
-  }
-
-  script.textContent = importMap
-  document.head.prepend(script)
-}
-
 export const initMonaco = async () => {
   if (!monacoInitPromise) {
     monacoInitPromise = (async () => {
-      ensureMonacoImportMap()
       const { init } = await import('modern-monaco')
       return init(DEFAULT_INIT_OPTIONS)
     })()
