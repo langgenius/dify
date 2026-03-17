e76d697bcdecf89653bd607ed0f64af1a8eaa7ad
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-03-17 12:50:06 +0800

    Revert "refactor: use hoisted modern monaco (#33540)"
    This reverts commit 569748189e0bf2e838dbbf8b5c8dcd3bd833b231.

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -8,7 +8,6 @@ import GlobalPublicStoreProvider from '@/context/global-public-context'
 import { TanstackQueryInitializer } from '@/context/query-client'
 import { getDatasetMap } from '@/env'
 import { getLocaleOnServer } from '@/i18n-config/server'
-import MonacoImportMap from './components/base/modern-monaco/import-map'
 import { ToastProvider } from './components/base/toast'
 import { TooltipProvider } from './components/base/ui/tooltip'
 import BrowserInitializer from './components/browser-initializer'
@@ -38,7 +37,6 @@ const LocaleLayout = async ({
   return (
     <html lang={locale ?? 'en'} className="h-full" suppressHydrationWarning>
       <head>
-        <MonacoImportMap />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#1C64F2" />
         <meta name="mobile-web-app-capable" content="yes" />
