54d29cdf70f94e3071c039863855a9abf8eec3a9
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-03-18 19:26:34 +0800

    no redirects

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,16 +1,5 @@
-import Link from '@/next/link'
+import { redirect } from '@/next/navigation'
 
-const Home = async () => {
-  return (
-    <div className="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8">
-
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="mt-10 text-center">
-          <Link href="/apps">🚀</Link>
-        </div>
-      </div>
-    </div>
-  )
+export default async function Home() {
+  redirect('/apps')
 }
-
-export default Home
