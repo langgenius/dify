479b8135310543de929e1d0b4dd0d35a7936d430
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-03-20 11:22:15 +0800

    tweaks

--- web/app/(commonLayout)/datasets/layout.tsx
+++ web/app/(commonLayout)/datasets/layout.tsx
@@ -1,24 +1,17 @@
 'use client'
 
-import { useEffect } from 'react'
 import { useAppContext } from '@/context/app-context'
 import { ExternalApiPanelProvider } from '@/context/external-api-panel-context'
 import { ExternalKnowledgeApiProvider } from '@/context/external-knowledge-api-context'
-import { useRouter } from '@/next/navigation'
+import { redirect } from '@/next/navigation'
 
 export default function DatasetsLayout({ children }: { children: React.ReactNode }) {
   const { isCurrentWorkspaceEditor, isCurrentWorkspaceDatasetOperator, currentWorkspace, isLoadingCurrentWorkspace } = useAppContext()
-  const router = useRouter()
   const isWorkspaceReady = !isLoadingCurrentWorkspace && !!currentWorkspace.id
   const shouldRedirect = isWorkspaceReady && !(isCurrentWorkspaceEditor || isCurrentWorkspaceDatasetOperator)
 
-  useEffect(() => {
-    if (shouldRedirect)
-      router.replace('/apps')
-  }, [shouldRedirect, router])
-
   if (shouldRedirect) {
-    return null
+    return redirect('/apps')
   }
 
   return (
