0bf95fda095eea0f09ed415cd0b549c17ff8a6c1
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-03-24 15:49:09 +0800

    tweaks

--- web/instrumentation-client.ts
+++ web/instrumentation-client.ts
@@ -32,48 +32,50 @@ async function main() {
     }
   }
 
-  class StorageMock {
-    data: Record<string, string>
+  if (!('localStorage' in globalThis) || !('sessionStorage' in globalThis)) {
+    class StorageMock {
+      data: Record<string, string>
 
-    constructor() {
-      this.data = {} as Record<string, string>
+      constructor() {
+        this.data = {} as Record<string, string>
+      }
+
+      setItem(name: string, value: string) {
+        this.data[name] = value
+      }
+
+      getItem(name: string) {
+        return this.data[name] || null
+      }
+
+      removeItem(name: string) {
+        delete this.data[name]
+      }
+
+      clear() {
+        this.data = {}
+      }
     }
 
-    setItem(name: string, value: string) {
-      this.data[name] = value
+    let localStorage, sessionStorage
+
+    try {
+      localStorage = globalThis.localStorage
+      sessionStorage = globalThis.sessionStorage
+    }
+    catch {
+      localStorage = new StorageMock()
+      sessionStorage = new StorageMock()
     }
 
-    getItem(name: string) {
-      return this.data[name] || null
-    }
+    Object.defineProperty(globalThis, 'localStorage', {
+      value: localStorage,
+    })
 
-    removeItem(name: string) {
-      delete this.data[name]
-    }
-
-    clear() {
-      this.data = {}
-    }
+    Object.defineProperty(globalThis, 'sessionStorage', {
+      value: sessionStorage,
+    })
   }
-
-  let localStorage, sessionStorage
-
-  try {
-    localStorage = globalThis.localStorage
-    sessionStorage = globalThis.sessionStorage
-  }
-  catch {
-    localStorage = new StorageMock()
-    sessionStorage = new StorageMock()
-  }
-
-  Object.defineProperty(globalThis, 'localStorage', {
-    value: localStorage,
-  })
-
-  Object.defineProperty(globalThis, 'sessionStorage', {
-    value: sessionStorage,
-  })
 }
 
 main()
