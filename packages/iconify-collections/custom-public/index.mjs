c1777746d0ad0f44787c984a8ceb866ce51fab20
Stephen Zhou <hi@hyoban.cc>
2026-04-02 15:58:15 +0800

    refactor: split icon collections (#34453)

---
+++ packages/iconify-collections/custom-public/index.mjs
@@ -0,0 +1,7 @@
+import icons from './icons.json' with { type: 'json' }
+import info from './info.json' with { type: 'json' }
+import metadata from './metadata.json' with { type: 'json' }
+import chars from './chars.json' with { type: 'json' }
+
+export { icons, info, metadata, chars }
+
---
+++ packages/iconify-collections/custom-vender/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const icons = require('./icons.json')
+const info = require('./info.json')
+const metadata = require('./metadata.json')
+const chars = require('./chars.json')
+
+module.exports = { icons, info, metadata, chars }
+
