799e754b10626cd789b5c4002ec8892a3b52f770
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-04-02 16:57:04 +0800

    tweaks

--- web/app/(shareLayout)/webapp-signin/page.tsx
+++ web/app/(shareLayout)/webapp-signin/page.tsx
@@ -45,13 +45,10 @@ const WebSSOForm: FC = () => {
   if (!systemFeatures.webapp_auth.enabled) {
     return (
       <div className="flex h-full items-center justify-center">
-        <p className="text-text-tertiary system-xs-regular">{t('webapp.disabled', { ns: 'login' })}</p>
+        <p className="system-xs-regular text-text-tertiary">{t('webapp.disabled', { ns: 'login' })}</p>
       </div>
     )
   }
-  if (webAppAccessMode === null)
-    return <div className="w-full max-w-[400px]" />
-
   if (webAppAccessMode && (webAppAccessMode === AccessMode.ORGANIZATION || webAppAccessMode === AccessMode.SPECIFIC_GROUPS_MEMBERS)) {
     return (
       <div className="w-full max-w-[400px]">
@@ -66,7 +63,7 @@ const WebSSOForm: FC = () => {
   return (
     <div className="flex h-full flex-col items-center justify-center gap-y-4">
       <AppUnavailable className="h-auto w-auto" isUnknownReason={true} />
-      <span className="cursor-pointer text-text-tertiary system-sm-regular" onClick={backToHome}>{t('login.backToHome', { ns: 'share' })}</span>
+      <span className="cursor-pointer system-sm-regular text-text-tertiary" onClick={backToHome}>{t('login.backToHome', { ns: 'share' })}</span>
     </div>
   )
 }
