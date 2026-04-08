5668afc1213fdeb41af82fda411aea3c1c7f38b1
wangxiaolei <fatelei@gmail.com>
2026-04-08 21:27:53 +0800

    fix: fix import error (#34728)

--- api/core/workflow/nodes/knowledge_index/entities.py
+++ api/core/workflow/nodes/knowledge_index/entities.py
@@ -4,7 +4,7 @@ from graphon.entities.base_node_data import BaseNodeData
 from graphon.enums import NodeType
 from pydantic import BaseModel
 
-from core.rag.entities import WeightedScoreConfig
+from core.rag.entities.retrieval_settings import WeightedScoreConfig
 from core.rag.index_processor.index_processor_base import SummaryIndexSettingDict
 from core.rag.retrieval.retrieval_methods import RetrievalMethod
 from core.workflow.nodes.knowledge_index import KNOWLEDGE_INDEX_NODE_TYPE
