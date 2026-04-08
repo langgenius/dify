297f65ba5cc16acabc363c0f38024bf42b7c2ad6
Asuka Minato <i@asukaminato.eu.org>
2026-04-08 11:52:19 +0900

    ignore

--- api/controllers/console/__init__.py
+++ api/controllers/console/__init__.py
@@ -3,7 +3,7 @@ from importlib import import_module
 from flask import Blueprint
 from flask_restx import Namespace
 
-from core.model_runtime import model_type_compat
+from core.model_runtime import model_type_compat  # type: ignore
 from libs.external_api import ExternalApi
 
 bp = Blueprint("console", __name__, url_prefix="/console/api")
