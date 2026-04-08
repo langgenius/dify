90186f4bd5f0830d4e8ce0598ee1fc10056e840c
corevibe555 <leaderbossprog2025@gmail.com>
2026-04-08 14:33:11 +0200

    fix: remove duplicate error imports in core app generators

--- api/core/app/apps/completion/app_generator.py
+++ api/core/app/apps/completion/app_generator.py
@@ -21,7 +21,6 @@ from core.app.apps.exc import GenerateTaskStoppedError
 from core.app.apps.message_based_app_generator import MessageBasedAppGenerator
 from core.app.apps.message_based_app_queue_manager import MessageBasedAppQueueManager
 from core.app.entities.app_invoke_entities import CompletionAppGenerateEntity, InvokeFrom
-from core.errors import MessageNotExistsError, MoreLikeThisDisabledError
 from core.errors.service_errors import MessageNotExistsError, MoreLikeThisDisabledError
 from core.ops.ops_trace_manager import TraceQueueManager
 from extensions.ext_database import db
