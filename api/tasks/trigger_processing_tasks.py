b60533dcc3ced7aabc2b5b3556ecd7d725f13a5b
FFXN <lizy@dify.ai>
2026-04-17 16:16:26 +0800

    fix import

--- api/tasks/trigger_processing_tasks.py
+++ api/tasks/trigger_processing_tasks.py
@@ -27,7 +27,8 @@ from core.trigger.entities.entities import TriggerProviderEntity
 from core.trigger.provider import PluginTriggerProviderController
 from core.trigger.trigger_manager import TriggerManager
 from core.workflow.nodes.trigger_plugin.entities import TriggerEventNodeData
-from enums.quota_type import QuotaType, unlimited
+from enums.quota_type import QuotaType
+from services.quota_service import unlimited
 from graphon.enums import WorkflowExecutionStatus
 from models.enums import (
     AppTriggerType,
