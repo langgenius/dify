548f87966d94b948e4ea958886072e9c1df75c7f
Hanqing Zhao <sherry9277@gmail.com>
2026-04-24 11:06:35 +0800

    revert the language button and add translation for Portuguese

--- web/app/components/plugins/marketplace/category-switch/template.tsx
+++ web/app/components/plugins/marketplace/category-switch/template.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import { Playground } from '@/app/components/base/icons/src/vender/plugin'
-import { useActiveTemplateCategory } from '../atoms'
+import { useActiveTemplateCategory, useFilterTemplateLanguages } from '../atoms'
 import { CATEGORY_ALL, TEMPLATE_CATEGORY_MAP } from '../constants'
 import { useTemplateCategoryText } from './category-text'
 import { CommonCategorySwitch } from './common'
+import HeroLanguagesFilter from './hero-languages-filter'
 
 type TemplateCategorySwitchProps = {
   className?: string
@@ -27,7 +28,7 @@ export const TemplateCategorySwitch = ({
   variant = 'default',
 }: TemplateCategorySwitchProps) => {
   const [activeTemplateCategory, handleActiveTemplateCategoryChange] = useActiveTemplateCategory()
-  // const [filterTemplateLanguages, setFilterTemplateLanguages] = useFilterTemplateLanguages()
+  const [filterTemplateLanguages, setFilterTemplateLanguages] = useFilterTemplateLanguages()
   const getTemplateCategoryText = useTemplateCategoryText()
 
   const isHeroVariant = variant === 'hero'
@@ -51,20 +52,17 @@ export const TemplateCategorySwitch = ({
   }
 
   return (
-    <div className="flex shrink-0 items-center gap-2">
-      {/* <HeroLanguagesFilter
-        languages={filterTemplateLanguages}
-        onLanguagesChange={languages => setFilterTemplateLanguages(languages.length ? languages : null)}
-      />
-      <div className="text-text-primary-on-surface">
-        ·
-      </div> */}
+    <div className="flex shrink-0 items-center gap-2 justify-between">
       <CommonCategorySwitch
         className={className}
         variant={variant}
         options={options}
         activeValue={activeTemplateCategory}
         onChange={handleActiveTemplateCategoryChange}
+      />
+      <HeroLanguagesFilter
+        languages={filterTemplateLanguages}
+        onLanguagesChange={languages => setFilterTemplateLanguages(languages.length ? languages : null)}
       />
     </div>
   )
