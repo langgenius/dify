4666f401407f7c2d783008f9461292eb8de90ef8
Joel <iamjoel007@gmail.com>
2026-04-28 16:06:12 +0800

    fix: to fill form page

--- web/app/(commonLayout)/education-apply/page.tsx
+++ web/app/(commonLayout)/education-apply/page.tsx
@@ -1,10 +1,8 @@
 'use client'
 
-import {
-  useEffect,
-  useMemo,
-} from 'react'
+import { useEffect } from 'react'
 import EducationApplyPage from '@/app/education-apply/education-apply-page'
+import RootLoading from '@/app/loading'
 import { useProviderContext } from '@/context/provider-context'
 import {
   useRouter,
@@ -13,17 +11,20 @@ import {
 
 export default function EducationApply() {
   const router = useRouter()
-  const { enableEducationPlan } = useProviderContext()
+  const { enableEducationPlan, isFetchedPlanInfo } = useProviderContext()
   const searchParams = useSearchParams()
   const token = searchParams.get('token')
-  const showEducationApplyPage = useMemo(() => {
-    return enableEducationPlan && token
-  }, [enableEducationPlan, token])
 
   useEffect(() => {
-    if (!showEducationApplyPage)
+    if (!isFetchedPlanInfo)
+      return
+
+    if (!enableEducationPlan || !token)
       router.replace('/')
-  }, [showEducationApplyPage, router])
+  }, [enableEducationPlan, isFetchedPlanInfo, router, token])
+
+  if (!isFetchedPlanInfo || !enableEducationPlan || !token)
+    return <RootLoading />
 
   return <EducationApplyPage />
 }
