f384c762fb842d4d85c71139d774e76021854355
yyh <yuanyouhuilyz@gmail.com>
2026-05-09 18:13:11 +0800

    add eslint rules

--- web/eslint.constants.mjs
+++ web/eslint.constants.mjs
@@ -39,9 +39,19 @@ const BASE_UI_RESTRICTED_IMPORT_PATTERNS = [
   },
 ]
 
+const FLOATING_UI_RESTRICTED_IMPORT_PATTERNS = [
+  {
+    group: [
+      '@floating-ui/*',
+    ],
+    message: 'Do not import Floating UI directly in web. Use @langgenius/dify-ui/* primitives instead.',
+  },
+]
+
 export const WEB_RESTRICTED_IMPORT_PATTERNS = [
   ...NEXT_PLATFORM_RESTRICTED_IMPORT_PATTERNS,
   ...BASE_UI_RESTRICTED_IMPORT_PATTERNS,
+  ...FLOATING_UI_RESTRICTED_IMPORT_PATTERNS,
 ]
 
 export const OVERLAY_RESTRICTED_IMPORT_PATTERNS = [
