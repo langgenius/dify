fd305e52ee86cc54d6f78d92599a2b443b29a9af
Asuka Minato <i@asukaminato.eu.org>
2026-05-12 21:51:52 +0900

    .

--- api/services/dataset_service.py
+++ api/services/dataset_service.py
@@ -2193,7 +2193,7 @@ class DocumentService:
         knowledge_config: KnowledgeConfig,
         account: Account | Any,
         dataset_process_rule: DatasetProcessRule | None = None,
-        created_from: str = DocumentCreatedFrom.WEB,
+        created_from: DocumentCreatedFrom = DocumentCreatedFrom.WEB,
         *,
         session: Session,
     ) -> tuple[list[Document], str]:
