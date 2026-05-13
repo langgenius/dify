fce6d017d6551a304211792ed46b8b739a7bc4a8
QuantumGhost <obelisk.reg+git@gmail.com>
2026-05-13 10:00:06 +0800

    test(api): renaming import according to graphon changes

--- api/tests/unit_tests/core/app/apps/test_workflow_app_runner_core.py
+++ api/tests/unit_tests/core/app/apps/test_workflow_app_runner_core.py
@@ -375,7 +375,11 @@ class TestWorkflowBasedAppRunner:
 
         runner = WorkflowBasedAppRunner(queue_manager=_QueueManager(), app_id="app")
         graph_runtime_state = GraphRuntimeState(
-            variable_pool=VariablePool(system_variables=default_system_variables()),
+            variable_pool=VariablePool.from_bootstrap(
+                system_variables=default_system_variables(),
+                user_inputs={},
+                environment_variables=[],
+            ),
             start_at=0.0,
         )
         workflow_entry = SimpleNamespace(graph_engine=SimpleNamespace(graph_runtime_state=graph_runtime_state))
