de81fc5e09d3ead4a40345cb8e321e112545a270
yunlu.wen <yunlu.wen@dify.ai>
2026-05-18 17:27:24 +0800

    remove unused property

--- cli/src/commands/_shared/dify-command.ts
+++ cli/src/commands/_shared/dify-command.ts
@@ -3,11 +3,7 @@ import { Command } from '../../framework/command.js'
 import { buildAuthedContext } from './authed-command.js'
 
 export abstract class DifyCommand extends Command {
-  protected outputFormat = ''
-
   protected async authedCtx(opts: AuthedContextOptions): Promise<AuthedContext> {
-    this.outputFormat = opts.format ?? ''
-
     return buildAuthedContext(this, opts)
   }
 }
