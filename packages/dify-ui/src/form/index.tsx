c1779112c6c89d4fe6c77723477327eb12f81a49
yyh <yuanyouhuilyz@gmail.com>
2026-05-18 16:53:40 +0800

    refactor: expose base ui form directly

--- packages/dify-ui/src/form/index.tsx
+++ packages/dify-ui/src/form/index.tsx
@@ -2,26 +2,10 @@
 
 import type { Form as BaseFormNS } from '@base-ui/react/form'
 import { Form as BaseForm } from '@base-ui/react/form'
-import { cn } from '../cn'
 
-export type FormProps<FormValues extends Record<string, unknown> = Record<string, unknown>>
-  = Omit<BaseFormNS.Props<FormValues>, 'className'>
-    & {
-      className?: string
-    }
+export const Form = BaseForm
 
+export type FormProps = BaseFormNS.Props
 export type FormActions = BaseFormNS.Actions
 export type FormValidationMode = BaseFormNS.ValidationMode
 export type FormSubmitEventDetails = BaseFormNS.SubmitEventDetails
-
-export function Form<FormValues extends Record<string, unknown> = Record<string, unknown>>({
-  className,
-  ...props
-}: FormProps<FormValues>) {
-  return (
-    <BaseForm
-      className={cn(className)}
-      {...props}
-    />
-  )
-}
