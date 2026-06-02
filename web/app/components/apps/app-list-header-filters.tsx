add7c75f18fb815e7fde7876312e783426bf33a0
Joel <iamjoel007@gmail.com>
2026-06-02 17:53:12 +0800

    chore: stadio no app tiny

--- web/app/components/apps/app-list-header-filters.tsx
+++ web/app/components/apps/app-list-header-filters.tsx
@@ -75,14 +75,15 @@ function AppListHeaderFilters({
           value={keywords}
           onChange={onKeywordsChange}
         />
-      </div>
-      <div className="flex items-center gap-2">
+        <div className="h-3.5 w-px bg-divider-regular" />
         <label className="flex h-8 items-center gap-2 rounded-lg bg-components-input-bg-normal px-2 text-text-secondary">
           <Checkbox checked={isCreatedByMe} onCheckedChange={onCreatedByMeChange} />
           <span className="system-sm-regular whitespace-nowrap">
             {t('showMyCreatedAppsOnly', { ns: 'app' })}
           </span>
         </label>
+      </div>
+      <div className="flex items-center gap-2">
         {showCreateButton && (
           <DropdownMenu modal={false}>
             <DropdownMenuTrigger
