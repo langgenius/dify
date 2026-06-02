f9efd26e0dd2f3a22f1635fcdef7573a16d21098
-LAN- <laipz8200@outlook.com>
2026-06-02 21:11:48 +0800

    fix(api): block legacy end user type

---
+++ api/migrations/versions/2026_06_02_2050-4f7b2c8d9a10_normalize_legacy_end_user_type.py
@@ -0,0 +1,24 @@
+"""normalize legacy end user type
+
+Revision ID: 4f7b2c8d9a10
+Revises: 121e7346074d
+Create Date: 2026-06-02 20:50:00.000000
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "4f7b2c8d9a10"
+down_revision = "121e7346074d"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute(sa.text("UPDATE end_users SET type = 'service-api' WHERE type = 'service_api'"))
+
+
+def downgrade() -> None:
+    op.execute(sa.text("UPDATE end_users SET type = 'service_api' WHERE type = 'service-api'"))
