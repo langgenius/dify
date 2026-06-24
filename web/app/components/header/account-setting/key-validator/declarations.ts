8e137c80dd224a56a67c295321e46f2695b65fbb
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-06-24 17:03:50 +0800

    chore(web): remove orphaned metadata code

--- web/app/components/header/account-setting/key-validator/declarations.ts
+++
@@ -1,7 +0,0 @@
-export const ValidatedStatus = {
-  Success: 'success',
-  Error: 'error',
-  Exceed: 'exceed',
-} as const
-
-export type ValidatedStatus = typeof ValidatedStatus[keyof typeof ValidatedStatus]
