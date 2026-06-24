597d17247f7ed370a78d839dee2996840ec7fc09
fatelei <fatelei@gmail.com>
2026-06-25 06:54:46 +0800

    feat: delete member delete rbac binding

--- api/services/account_service.py
+++ api/services/account_service.py
@@ -66,6 +66,7 @@ from services.errors.account import (
     RoleAlreadyAssignedError,
     TenantNotFoundError,
 )
+from services.enterprise.rbac_service import RBACService
 from services.errors.workspace import WorkSpaceNotAllowedCreateError, WorkspacesLimitExceededError
 from services.feature_service import FeatureService
 from services.plugin.plugin_auto_upgrade_service import PluginAutoUpgradeService
@@ -1787,6 +1788,9 @@ class TenantService:
                 tenant.id,
                 account_id,
             )
+
+        if dify_config.RBAC_ENABLED:
+            RBACService.MemberRoles.delete_rbac_bindings(tenant_id=tenant.id, account_id=account_id)
 
     @staticmethod
     def update_member_role(
