5f6219a16d03487ca39473fba04840375aef236c
OMengWei_ctyun <391278491@qq.com>
2026-06-27 11:39:22 +0800

    fix: merge enterprise and 1.15.0 migration heads

---
+++ api/migrations/versions/2026_06_27_1145-e2f0a9b7c6d5_merge_1_15_0_enterprise_heads.py
@@ -0,0 +1,20 @@
+"""merge 1.15.0 enterprise migration heads
+
+Revision ID: e2f0a9b7c6d5
+Revises: f1a14e1e9b41, d9e8f7a6b5c4
+Create Date: 2026-06-27 11:45:00.000000
+
+"""
+
+revision = "e2f0a9b7c6d5"
+down_revision = ("f1a14e1e9b41", "d9e8f7a6b5c4")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
