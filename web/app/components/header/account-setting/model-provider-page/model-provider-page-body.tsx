2467df55c7215a0187eaca2000232014616a8afc
非法操作 <hjlarry@163.com>
2026-07-07 21:03:32 +0800

    fix: can't debug model plugins (#38500)
    (cherry picked from commit 6edce14e887fbbc94ae838002abea519a24acfbd)

--- web/app/components/header/account-setting/model-provider-page/model-provider-page-body.tsx
+++ web/app/components/header/account-setting/model-provider-page/model-provider-page-body.tsx
@@ -3,6 +3,7 @@ import type { ModelProvider } from './declarations'
 import type { PluginDetail } from '@/app/components/plugins/types'
 import { Trans, useTranslation } from 'react-i18next'
 import { SkeletonContainer, SkeletonRectangle, SkeletonRow } from '@/app/components/base/skeleton'
+import { PluginSource } from '@/app/components/plugins/types'
 import { IS_CLOUD_EDITION } from '@/config'
 import InstallFromMarketplace from './install-from-marketplace'
 import ProviderAddedCard from './provider-added-card'
@@ -99,21 +100,40 @@ type ProviderCardListProps = {
   notConfigured?: boolean
 }
 
+function isDebuggingProvider(provider: ModelProvider, pluginDetailMap: Map<string, PluginDetail>) {
+  return pluginDetailMap.get(providerToPluginId(provider.provider))?.source === PluginSource.debugging
+}
+
 function ProviderCardList({
   providers,
   pluginDetailMap,
   notConfigured,
 }: ProviderCardListProps) {
+  const sortedProviders = [...providers]
+    .sort((a, b) => {
+      const aIsDebuggingPlugin = isDebuggingProvider(a, pluginDetailMap)
+      const bIsDebuggingPlugin = isDebuggingProvider(b, pluginDetailMap)
+
+      if (aIsDebuggingPlugin === bIsDebuggingPlugin)
+        return 0
+
+      return aIsDebuggingPlugin ? -1 : 1
+    })
+
   return (
     <div className="relative flex flex-col gap-2">
-      {providers.map(provider => (
-        <ProviderAddedCard
-          key={provider.provider}
-          notConfigured={notConfigured}
-          provider={provider}
-          pluginDetail={pluginDetailMap.get(providerToPluginId(provider.provider))}
-        />
-      ))}
+      {sortedProviders.map((provider) => {
+        const pluginDetail = pluginDetailMap.get(providerToPluginId(provider.provider))
+
+        return (
+          <ProviderAddedCard
+            key={provider.provider}
+            notConfigured={notConfigured}
+            provider={provider}
+            pluginDetail={pluginDetail}
+          />
+        )
+      })}
     </div>
   )
 }
@@ -157,8 +177,8 @@ const ModelProviderPageBody: FC<ModelProviderPageBodyProps> = ({
         <div className="flex flex-col gap-2 pt-2">
           <div className="flex h-5 items-center system-md-semibold text-text-primary">{t('modelProvider.toBeConfigured', { ns: 'common' })}</div>
           <ProviderCardList
-            notConfigured
             providers={filteredNotConfiguredProviders}
+            notConfigured
             pluginDetailMap={pluginDetailMap}
           />
         </div>
