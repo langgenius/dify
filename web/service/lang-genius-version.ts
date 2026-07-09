aba0f3d559f52a183b3c8acb94f06cf994bef958
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-07-09 11:04:21 +0800

    refactor(web): use generated version contract

--- web/service/lang-genius-version.ts
+++ web/service/lang-genius-version.ts
@@ -1,13 +1,12 @@
-import type { LangGeniusVersionResponse } from '@/models/common'
-import { queryOptions } from '@tanstack/react-query'
-// eslint-disable-next-line no-restricted-imports
-import { get } from './base'
-import { commonQueryKeys } from './use-common'
+import { consoleQuery } from './client'
 
 export const langGeniusVersionQueryOptions = (currentVersion?: string | null, enabled?: boolean) => {
-  return queryOptions<LangGeniusVersionResponse>({
-    queryKey: commonQueryKeys.langGeniusVersion(currentVersion || undefined),
-    queryFn: () => get<LangGeniusVersionResponse>('/version', { params: { current_version: currentVersion } }),
+  return consoleQuery.version.get.queryOptions({
+    input: {
+      query: {
+        current_version: currentVersion ?? '',
+      },
+    },
     enabled: !!currentVersion && (enabled ?? true),
   })
 }
