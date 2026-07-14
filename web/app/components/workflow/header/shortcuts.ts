decf99b8a1df1f860e94381152d0448d507eee05
yyh <yuanyouhuilyz@gmail.com>
2026-07-14 22:45:43 +0800

    refactor(web): type workflow header hotkeys

--- web/app/components/workflow/header/shortcuts.ts
+++ web/app/components/workflow/header/shortcuts.ts
@@ -1,1 +1,4 @@
-export const TEST_RUN_MENU_HOTKEY = 'Alt+R'
+import type { Hotkey } from '@tanstack/react-hotkeys'
+
+export const TEST_RUN_MENU_HOTKEY = 'Alt+R' satisfies Hotkey
+export const VERSION_HISTORY_HOTKEY = 'Mod+Shift+H' satisfies Hotkey
