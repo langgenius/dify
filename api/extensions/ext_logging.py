08f08c61c8a21a3f1b3f3a7de88e8ae23aa83c63
hjlarry <hjlarry@163.com>
2026-07-23 13:16:32 +0800

    test(api): re-enable identity context log filter

--- api/extensions/ext_logging.py
+++ api/extensions/ext_logging.py
@@ -32,10 +32,11 @@ def init_app(app: DifyApp):
     log_handlers.append(sh)
 
     # Apply filters to all handlers
-    from core.logging.filters import TraceContextFilter
+    from core.logging.filters import IdentityContextFilter, TraceContextFilter
 
     for handler in log_handlers:
         handler.addFilter(TraceContextFilter())
+        handler.addFilter(IdentityContextFilter())
 
     # Configure formatter based on format type
     formatter = _create_formatter()
