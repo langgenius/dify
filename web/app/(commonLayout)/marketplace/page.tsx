0aa20ae2d77348ea830c1b7e89bf316c20b13650
CodingOnStar <hanxujiangjhx@gmail.com>
2026-07-24 15:47:59 +0800

    fix: enable redesigned marketplace route

--- web/app/(commonLayout)/marketplace/page.tsx
+++ web/app/(commonLayout)/marketplace/page.tsx
@@ -1,4 +1,5 @@
 import type { SearchParams } from 'nuqs'
+import AccountDropdown from '@/app/components/header/account-dropdown'
 import Marketplace from '@/app/components/plugins/marketplace'
 import MarketplaceInstallPermissionProvider from '@/app/components/plugins/marketplace/marketplace-install-permission-provider'
 
@@ -10,10 +11,19 @@ const MarketplacePage = ({ searchParams }: MarketplacePageProps) => {
   return (
     <div
       id="marketplace-container"
-      className="flex h-full min-h-0 flex-col overflow-y-auto bg-background-default-subtle pr-1"
+      className="flex h-full min-h-0 flex-col overflow-y-auto bg-background-default"
     >
       <MarketplaceInstallPermissionProvider>
-        <Marketplace searchParams={searchParams} isMarketplacePlatform showInstallButton />
+        <Marketplace
+          searchParams={searchParams}
+          showInstallButton
+          variant="home"
+          homeHeaderActions={(
+            <div className="p-0.5">
+              <AccountDropdown />
+            </div>
+          )}
+        />
       </MarketplaceInstallPermissionProvider>
     </div>
   )
