565f63bf6e6d768a00e6eda5fc150b5228577076
yyh <yuanyouhuilyz@gmail.com>
2026-07-24 18:09:38 +0800

    fix(web): defer permission query until workspace is ready

--- web/context/permission-state.ts
+++ web/context/permission-state.ts
@@ -4,10 +4,15 @@ import { atom } from 'jotai'
 import { atomWithQuery } from 'jotai-tanstack-query'
 import { consoleQuery } from '@/service/client'
 import { emptyWorkspacePermissionKeys } from './app-context-normalizers'
+import { currentWorkspaceIdAtom } from './workspace-state'
 
-const workspacePermissionKeysQueryAtom = atomWithQuery(() =>
-  consoleQuery.workspaces.current.rbac.myPermissions.get.queryOptions(),
-)
+const workspacePermissionKeysQueryAtom = atomWithQuery((get) => {
+  const workspaceId = get(currentWorkspaceIdAtom)
+
+  return consoleQuery.workspaces.current.rbac.myPermissions.get.queryOptions({
+    enabled: workspaceId === undefined || Boolean(workspaceId),
+  })
+})
 
 export const workspacePermissionKeysAtom = atom((get) => {
   return (
