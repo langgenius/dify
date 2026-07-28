a8ffc6072db52ed2fd5a74c9c7f831f3d0c68265
CodingOnStar <hanxujiang@dify.com>
2026-07-28 19:16:17 +0800

    fix: add hybrid SSR for embedded marketplace

--- web/app/(commonLayout)/marketplace/page.tsx
+++ web/app/(commonLayout)/marketplace/page.tsx
@@ -1,8 +1,14 @@
 import AccountDropdown from '@/app/components/header/account-dropdown'
 import { EmbeddedMarketplace } from '@/app/components/plugins/marketplace/embedded'
+import { fetchPluginBanners } from '@/app/components/plugins/marketplace/home/banners'
 import MarketplaceInstallPermissionProvider from '@/app/components/plugins/marketplace/marketplace-install-permission-provider'
+import { getLocaleOnServer } from '@/i18n-config/server'
 
-const MarketplacePage = () => {
+const MarketplacePage = async () => {
+  const initialBanners = await getLocaleOnServer()
+    .then(fetchPluginBanners)
+    .catch(() => undefined)
+
   return (
     <div
       id="marketplace-container"
@@ -10,13 +16,14 @@ const MarketplacePage = () => {
     >
       <MarketplaceInstallPermissionProvider>
         <EmbeddedMarketplace
+          initialBanners={initialBanners}
           showInstallButton
           variant="home"
-          homeHeaderActions={(
+          homeHeaderActions={
             <div className="p-0.5">
               <AccountDropdown />
             </div>
-          )}
+          }
         />
       </MarketplaceInstallPermissionProvider>
     </div>
