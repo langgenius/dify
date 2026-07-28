dcb4b4f6afa87977c26c1918c20a6e6c0c3e75b8
CodingOnStar <hanxujiang@dify.com>
2026-07-28 19:09:07 +0800

    fix: isolate embedded marketplace client

--- web/app/components/plugins/marketplace/index.tsx
+++ web/app/components/plugins/marketplace/index.tsx
@@ -1,26 +1,13 @@
 import type { SearchParams } from 'nuqs'
 import type { PluginBanner } from './home/banners'
-import { PluginInstallPermissionProviderGuard } from '@/app/components/plugins/install-plugin/components/plugin-install-permission-provider'
-import { TanStackQueryProvider } from '@/app/query-provider'
+import type { MarketplaceViewProps } from './view'
 import { getLocaleOnServer } from '@/i18n-config/server'
-import Description from './description'
-import MarketplaceHome from './home'
 import { fetchPluginBanners } from './home/banners'
 import { HydrateQueryClient } from './hydration-server'
-import ListWrapper from './list/list-wrapper'
-import StickySearchAndSwitchWrapper from './sticky-search-and-switch-wrapper'
+import { MarketplaceView } from './view'
 
-type MarketplaceVariant = 'default' | 'home'
-
-type MarketplaceProps = {
-  showInstallButton?: boolean
-  linkToMarketplaceDetail?: boolean
-  pluginTypeSwitchClassName?: string
-  isMarketplacePlatform?: boolean
-  marketplaceNav?: React.ReactNode
-  variant?: MarketplaceVariant
+type MarketplaceProps = Omit<MarketplaceViewProps, 'banners'> & {
   language?: string
-  homeHeaderActions?: React.ReactNode
   /**
    * Pass the search params from the request to prefetch data on the server.
    */
@@ -28,15 +15,10 @@ type MarketplaceProps = {
 }
 
 const Marketplace = async ({
-  showInstallButton = false,
-  linkToMarketplaceDetail = false,
-  pluginTypeSwitchClassName,
-  isMarketplacePlatform = false,
-  marketplaceNav,
-  variant = 'default',
   language,
-  homeHeaderActions,
   searchParams,
+  variant = 'default',
+  ...viewProps
 }: MarketplaceProps) => {
   let trendingBanners: PluginBanner[] = []
 
@@ -51,37 +33,9 @@ const Marketplace = async ({
   }
 
   return (
-    <TanStackQueryProvider>
-      <HydrateQueryClient searchParams={searchParams}>
-        <PluginInstallPermissionProviderGuard canInstallPlugin={showInstallButton}>
-          {variant === 'home' ? (
-            <MarketplaceHome
-              actions={homeHeaderActions}
-              banners={trendingBanners}
-              isMarketplacePlatform={isMarketplacePlatform}
-              linkToMarketplaceDetail={linkToMarketplaceDetail}
-              showInstallButton={showInstallButton}
-            />
-          ) : (
-            <>
-              <Description
-                isMarketplacePlatform={isMarketplacePlatform}
-                marketplaceNav={marketplaceNav}
-              />
-              {!isMarketplacePlatform && (
-                <StickySearchAndSwitchWrapper
-                  pluginTypeSwitchClassName={pluginTypeSwitchClassName}
-                />
-              )}
-              <ListWrapper
-                showInstallButton={showInstallButton}
-                linkToMarketplaceDetail={linkToMarketplaceDetail}
-              />
-            </>
-          )}
-        </PluginInstallPermissionProviderGuard>
-      </HydrateQueryClient>
-    </TanStackQueryProvider>
+    <HydrateQueryClient searchParams={searchParams}>
+      <MarketplaceView {...viewProps} banners={trendingBanners} variant={variant} />
+    </HydrateQueryClient>
   )
 }
 
