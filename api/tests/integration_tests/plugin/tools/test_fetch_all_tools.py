ac4578d290c4fd863a4efdadbcd4e5a7d1522c75
Asuka Minato <i@asukaminato.eu.org>
2026-08-13 13:56:05 +0900

    test: remove duplicate integration-only coverage

--- api/tests/integration_tests/plugin/tools/test_fetch_all_tools.py
+++
@@ -1,9 +0,0 @@
-from core.plugin.impl.tool import PluginToolManager
-
-pytest_plugins = ("tests.integration_tests.plugin.__mock.http",)
-
-
-def test_fetch_all_plugin_tools(setup_http_mock):
-    manager = PluginToolManager()
-    tools = manager.fetch_tool_providers(tenant_id="test-tenant")
-    assert len(tools) >= 1
