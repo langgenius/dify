96bfcf20cda18bbb227bd877ee5c83d060eb51c9
WH-2099 <wh2099@pm.me>
2026-08-14 12:03:54 +0000

    fix(api): protect workspace credential mutations (#40790)

--- api/controllers/console/workspace/tool_providers.py
+++ api/controllers/console/workspace/tool_providers.py
@@ -580,6 +580,8 @@ class ToolBuiltinProviderAddApi(Resource):
     )
     @setup_required
     @login_required
+    @is_admin_or_owner_required
+    @rbac_permission_required(RBACResourceScope.WORKSPACE, RBACPermission.CREDENTIAL_CREATE, resource_required=False)
     @account_initialization_required
     @with_current_user
     @with_current_tenant_id
