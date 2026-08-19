2fde4611caff917b99a134c318df8da1c81909eb
fatelei <fatelei@gmail.com>
2026-08-19 16:10:41 +0800

    fix: merge latest migration heads

--- api/migrations/versions/2026_08_18_1200-b7c8d9e0f1a2_merge_skill_and_main_heads.py
+++ api/migrations/versions/2026_08_18_1200-b7c8d9e0f1a2_merge_skill_and_main_heads.py
@@ -1,14 +1,14 @@
 """merge skill and main migration heads
 
 Revision ID: b7c8d9e0f1a2
-Revises: a4f8d2c9e1b0, 56124e050600
+Revises: a4f8d2c9e1b0, 89919253ca7a
 Create Date: 2026-08-18 12:00:00.000000
 
 """
 
 # revision identifiers, used by Alembic.
 revision = "b7c8d9e0f1a2"
-down_revision = ("a4f8d2c9e1b0", "56124e050600")
+down_revision = ("a4f8d2c9e1b0", "89919253ca7a")
 branch_labels = None
 depends_on = None
 
