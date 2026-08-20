a51df05b7bf9cd6d3a1daeda57c204343e8e2877
CodingOnStar <hanxujiang@dify.com>
2026-08-20 15:44:50 +0800

    fix(api): merge remaining alembic heads on deploy/dev

---
+++ api/migrations/versions/2026_08_20_1545-c8e7f6a5b4d3_merge_skill_debug_and_agent_drive_heads.py
@@ -0,0 +1,21 @@
+"""merge skill debug and agent drive removal heads
+
+Revision ID: c8e7f6a5b4d3
+Revises: b7c8d9e0f1a2, e9f4a1b2c3d5
+Create Date: 2026-08-20 15:45:00.000000
+
+"""
+
+# revision identifiers, used by Alembic.
+revision: str = "c8e7f6a5b4d3"
+down_revision = ("b7c8d9e0f1a2", "e9f4a1b2c3d5")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
