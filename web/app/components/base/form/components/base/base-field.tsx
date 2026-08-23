c62947f1f1d20f96f76e54402c528e88b3610a4d
yyh <92089059+lyzno1@users.noreply.github.com>
2026-08-23 08:08:29 +0000

    refactor(dify-ui): rename radio family entrypoint (#41104)

--- web/app/components/base/form/components/base/base-field.tsx
+++ web/app/components/base/form/components/base/base-field.tsx
@@ -3,7 +3,7 @@ import type { FieldState, FormSchema, TypeWithI18N } from '@/app/components/base
 import { cn } from '@langgenius/dify-ui/cn'
 import { Field, FieldItem, FieldLabel } from '@langgenius/dify-ui/field'
 import { Fieldset, FieldsetLegend } from '@langgenius/dify-ui/fieldset'
-import { Radio, RadioGroup } from '@langgenius/dify-ui/radio'
+import { Radio, RadioGroup } from '@langgenius/dify-ui/radio-group'
 import {
   Select,
   SelectContent,
