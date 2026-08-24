6c7158d8e0932db78157b471ccef6c30166b66e0
Joel <iamjoel007@gmail.com>
2026-08-24 17:26:10 +0800

    chore: copywriting

--- web/features/agent-v2/agent-detail/configure/components/orchestrate/common/tip-content.tsx
+++ web/features/agent-v2/agent-detail/configure/components/orchestrate/common/tip-content.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Trans, useTranslation } from 'react-i18next'
+import { Trans } from 'react-i18next'
 import { useDocLink } from '@/context/i18n'
 import { DocsLink } from './docs-link'
 
@@ -9,7 +9,6 @@ type AgentConfigureTipContentProps = {
 }
 
 export function AgentConfigureTipContent({ type }: AgentConfigureTipContentProps) {
-  const { t } = useTranslation('agentV2')
   const docLink = useDocLink()
 
   if (type === 'prompt') {
@@ -80,10 +79,19 @@ export function AgentConfigureTipContent({ type }: AgentConfigureTipContentProps
     )
   }
 
-  if (type === 'files')
+  if (type === 'files') {
     return (
-      <span className="whitespace-pre-line">{t(($) => $['agentDetail.configure.files.tip'])}</span>
+      <span className="whitespace-pre-line">
+        <Trans
+          i18nKey={($) => $['agentDetail.configure.files.richTip']}
+          ns="agentV2"
+          components={{
+            docLink: <DocsLink href={docLink('/use-dify/build/new-agent/build#files')} />,
+          }}
+        />
+      </span>
     )
+  }
 
   return null
 }
