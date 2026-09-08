89d191ee9aecfe05ddd1a97d7257d9e27a8f4cf1
JackSmith <17537270608@163.com>
2026-09-08 15:54:55 +0800

    test(api): add return annotations to remote settings tests

--- api/tests/unit_tests/configs/test_remote_settings_sources.py
+++ api/tests/unit_tests/configs/test_remote_settings_sources.py
@@ -9,7 +9,7 @@ from configs.remote_settings_sources.nacos import NacosSettingsSource
 
 
 @pytest.mark.parametrize("source_cls", [NacosSettingsSource, ApolloSettingsSource])
-def test_get_field_value_prefers_field_name(source_cls: type[Any]):
+def test_get_field_value_prefers_field_name(source_cls: type[Any]) -> None:
     source = object.__new__(source_cls)
     source.remote_configs = {"SETTING": "direct", "ALIAS": "aliased"}
     field = FieldInfo(validation_alias="ALIAS")
@@ -18,7 +18,7 @@ def test_get_field_value_prefers_field_name(source_cls: type[Any]):
 
 
 @pytest.mark.parametrize("source_cls", [NacosSettingsSource, ApolloSettingsSource])
-def test_get_field_value_falls_back_to_single_validation_alias(source_cls: type[Any]):
+def test_get_field_value_falls_back_to_single_validation_alias(source_cls: type[Any]) -> None:
     source = object.__new__(source_cls)
     source.remote_configs = {"ALIAS": "aliased"}
     field = FieldInfo(validation_alias="ALIAS")
@@ -27,7 +27,7 @@ def test_get_field_value_falls_back_to_single_validation_alias(source_cls: type[
 
 
 @pytest.mark.parametrize("source_cls", [NacosSettingsSource, ApolloSettingsSource])
-def test_get_field_value_falls_back_to_alias_choices_in_order(source_cls: type[Any]):
+def test_get_field_value_falls_back_to_alias_choices_in_order(source_cls: type[Any]) -> None:
     source = object.__new__(source_cls)
     source.remote_configs = {"SECOND_ALIAS": "second"}
     field = FieldInfo(validation_alias=AliasChoices("FIRST_ALIAS", "SECOND_ALIAS"))
