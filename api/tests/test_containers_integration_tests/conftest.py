e8aaa5574a337856e7fc79813e10f751dbe0ba4b
Asuka Minato <i@asukaminato.eu.org>
2026-09-10 20:27:38 +0900

    tle

--- api/tests/test_containers_integration_tests/conftest.py
+++ api/tests/test_containers_integration_tests/conftest.py
@@ -173,6 +173,9 @@ class DifyTestContainers:
         self.dify_sandbox.waiting_for(_wait_for_log_message("config init success", 60))
         self.dify_sandbox.env = {
             "API_KEY": "test_api_key",
+            # Match Docker Compose: the image's 5-second limit can expire during
+            # Python/Jinja2 startup under parallel CI load.
+            "WORKER_TIMEOUT": "15",
         }
         self.dify_sandbox.start()
         sandbox_host = self.dify_sandbox.get_container_host_ip()
