ae933e3fcc340d527f2b76ec2327ad1768cbd7d6
fatelei <fatelei@gmail.com>
2026-09-22 17:13:23 +0800

    fix: fix /documents use rbac scene

--- api/controllers/console/datasets/datasets_document.py
+++ api/controllers/console/datasets/datasets_document.py
@@ -405,7 +405,7 @@ class DatasetDocumentListApi(Resource):
     @account_initialization_required
     @with_current_user
     @with_current_tenant_id
-    @rbac_permission_required(RBACCheck(RBACPermission.DATASET_CREATE_AND_MANAGEMENT, DatasetId()))
+    @rbac_permission_required(RBACCheck(RBACPermission.DATASET_READONLY, DatasetId()))
     @with_session(write=False)
     def get(self, session: Session, current_tenant_id: str, current_user: Account, dataset_id: UUID):
         dataset_id_str = str(dataset_id)
