ab55804c51866260c2027c8aee395896b466e316
Joel <iamjoel007@gmail.com>
2026-09-24 15:03:37 +0800

    fix(web): announce Swagger API test results

--- web/app/components/tools/edit-custom-collection-modal/test-api.tsx
+++ web/app/components/tools/edit-custom-collection-modal/test-api.tsx
@@ -195,7 +195,10 @@ const TestApi: FC<Props> = ({ positionCenter, customCollection, tool, onHide }) 
                       </div>
                       <div className="h-px w-0 grow"></div>
                     </div>
-                    <div className="mt-2 h-50 overflow-x-hidden overflow-y-auto rounded-lg bg-components-input-bg-normal px-3 py-2 system-xs-regular text-text-secondary">
+                    <div
+                      role="status"
+                      className="mt-2 h-50 overflow-x-hidden overflow-y-auto rounded-lg bg-components-input-bg-normal px-3 py-2 system-xs-regular text-text-secondary"
+                    >
                       {result || (
                         <span className="text-text-quaternary">
                           {t(($) => $['test.testResultPlaceholder'], { ns: 'tools' })}
